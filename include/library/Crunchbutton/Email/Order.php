fc1cc2268232e5d57dabf40d975707ebb7f0b5c6
arzynik <github@arzynik.com>
2012-07-19 23:18:07 -0700

    added wenzel to email titles. aded me to reply

--- include/library/Crunchbutton/Email/Order.php
+++ include/library/Crunchbutton/Email/Order.php
@@ -7,16 +7,16 @@ class Crunchbutton_Email_Order extends Crunchbutton_Email {
 	public function __construct($params) {
 		
 		$params['to'] 				= $params['email'];
-		$params['subject'] 				= 'Customer Order';		
-		$params['from'] 			= 'Crunchbutton <devin@_DOMAIN_>';
-		$params['reply']				= 'replytome@arzynik.com';
+		$params['subject'] 			= 'Customer Order';		
+		$params['from'] 			= ($params['order']->id_restaurant == 1 ? 'One Button Wenzel' : 'Crunchbutton').' <devin@_DOMAIN_>';
+		$params['reply']			= 'Tech Support <devin@_DOMAIN_>';
 
 		$this->buildView($params);
 		$this->view()->subject		= $params['subject'];
-		$this->view()->email	= $params['email'];
-		$this->view()->message	= $params['message'];
+		$this->view()->email		= $params['email'];
+		$this->view()->message		= $params['message'];
 		
-		$params['messageHtml']			= $this->view()->render('order/index',['display' => true, 'set' => ['order' => $params['order']]]);
+		$params['messageHtml']		= $this->view()->render('order/index',['display' => true, 'set' => ['order' => $params['order']]]);
 
 		parent::__construct($params);				
 	}
