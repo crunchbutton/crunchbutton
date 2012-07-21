2690d97d73cebd040f7b9a5fda5233b0c274e582
arzynik <github@arzynik.com>
2012-07-20 20:44:02 -0700

    tweaks

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -36,7 +36,6 @@ History.Adapter.bind(window,'statechange',function() {
 
 var App = {
 	slogans: ['streamlined food ordering','hungry ?','only the best'],
-	areacodes: [201, 202, 203, 205, 206, 207, 208, 209, 210, 212, 213, 214, 215, 216, 217, 218, 219, 224, 225, 228, 229, 231, 234, 236, 239, 240, 248, 251, 253, 254, 256, 260, 262, 267, 269, 270, 276, 278, 281, 283, 301, 302, 303, 304, 305, 307, 308, 309, 310, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 323, 325, 330, 331, 334, 336, 337, 339, 341, 347, 351, 352, 360, 361, 369, 380, 385, 386, 401, 402, 404, 405, 406, 407, 408, 409, 410, 412, 413, 414, 415, 417, 419, 423, 424, 425, 430, 432, 434, 435, 440, 442, 443, 464, 469, 470, 475, 478, 479, 480, 484, 501, 502, 503, 504, 505, 507, 508, 509, 510, 512, 513, 515, 516, 517, 518, 520, 530, 540, 541, 551, 557, 559, 561, 562, 563, 564, 567, 570, 571, 573, 574, 575, 580, 585, 586, 601, 602, 603, 605, 606, 607, 608, 609, 610, 612, 614, 615, 616, 617, 618, 619, 620, 623, 626, 627, 628, 630, 631, 636, 641, 646, 650, 651, 660, 661, 662, 669, 678, 679, 682, 689, 701, 702, 703, 704, 706, 707, 708, 712, 713, 714, 715, 716, 717, 718, 719, 720, 724, 727, 731, 732, 734, 737, 740, 747, 754, 757, 760, 762, 736, 764, 765, 769, 770, 772, 773, 774, 775, 779, 781, 785, 786, 801, 802, 803, 804, 805, 806, 808, 810, 812, 813, 841, 815, 816, 817, 818, 828, 830, 831, 832, 835, 845, 847, 848, 850, 856, 857, 858, 859, 860, 862, 863, 864, 865, 870, 872, 878, 901, 903, 904, 906, 907, 908, 909, 910, 912, 913, 914, 915, 916, 917, 918, 919, 920, 925, 927, 928, 931, 935, 936, 937, 940, 941, 947, 949, 951, 954, 956, 957, 959, 970, 971, 972, 973, 975, 978, 979, 980, 984, 985, 989],
 	service: '/api/',
 	cached: {},
 	cart: {},
@@ -131,6 +130,10 @@ App.loadRestaurant = function(id) {
 		
 		}
 	});
+};
+
+App.loadHome = function() {
+	History.pushState({}, 'Crunchbutton', '/');
 };
 
 App.loadPaymentinfo = function() {
@@ -279,7 +282,7 @@ App.page.restaurant = function(id) {
 		}
 
 		if (App.config.user.id_user) {
-			App.drawPay();
+			App.drawPay(this);
 			$('.payment-form').hide();
 
 			var dp = $('<div class="delivery-payment-info main-content-readable"></div>')
@@ -302,7 +305,7 @@ App.page.restaurant = function(id) {
 
 
 		} else {
-			App.drawPay();
+			App.drawPay(this);
 		}
 
 		var total = App.cart.updateTotal();
@@ -310,7 +313,7 @@ App.page.restaurant = function(id) {
 
 };
 
-App.drawPay = function() {
+App.drawPay = function(restaurant) {
 	var total = App.cart.total();
 
 	$('.main-content-item').append(
@@ -329,7 +332,7 @@ App.drawPay = function() {
 		
 			'<label class="pay-title-label">Delivery Info</label>' + 
 			'<div class="input-item toggle-wrapper clearfix">' +
-				'<a href="javascript:;" class="delivery-toggle-delivery toggle-item">delivery</a> <span class="toggle-spacer">or</span> <a href="javascript:;" class="delivery-toggle-takeout toggle-item">takeout</a>' + 
+				'<a href="javascript:;" class="delivery-toggle-delivery toggle-item delivery-only">delivery</a> <span class="toggle-spacer delivery-only">or</span> <a href="javascript:;" class="delivery-toggle-takeout toggle-item">takeout</a>' + 
 			'</div><div class="divider"></div>' + 
 
 			'<label>Name</label>' + 
@@ -411,6 +414,10 @@ App.drawPay = function() {
 	$('[name="pay-phone"]').val(App.phone.format(App.config.user.phone));
 	$('[name="pay-address"]').val(App.config.user.address);
 	$('[name="pay-card-number"]').val(App.config.user.card);
+	
+	if (!restaurant.delivery) {
+		$('.delivery-only').hide();
+	}
 
 };
 
@@ -1264,7 +1271,13 @@ $(function() {
 	});
 	
 	$('.nav-back').live('click',function() {
-		window.history.back();
+		History.back();
+	});
+	
+	$('.link-home').live('click',function() {
+		if (screen.width > 769) {
+			App.loadHome();
+		}
 	});
 	
 	$('.community-select').live('change',function() {
