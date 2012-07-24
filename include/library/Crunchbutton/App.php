b4ae31a3fb41b660d947a26990023d484b9bf9ae
arzynik <github@arzynik.com>
2012-07-24 11:25:09 -0700

    removed ssl

--- include/library/Crunchbutton/App.php
+++ include/library/Crunchbutton/App.php
@@ -60,8 +60,8 @@ class Crunchbutton_App extends Cana_App {
 			error_reporting(E_ERROR | E_PARSE);
 			
 			if ($_SERVER['HTTPS'] != 'on') {
-				header('Location: https://_DOMAIN_'.$_SERVER['REQUEST_URI']);
-				exit;
+				//header('Location: https://_DOMAIN_'.$_SERVER['REQUEST_URI']);
+				//exit;
 			}
 		}
 
