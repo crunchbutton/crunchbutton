0e34c4c24f8e477568d2db19995f85229bde10be
arzynik <github@arzynik.com>
2012-09-13 19:06:28 -0700

    unfinished shit for #67

--- include/library/Crunchbutton/Dish/Option.php
+++ include/library/Crunchbutton/Dish/Option.php
@@ -1,6 +1,23 @@
 <?php
 
 class Crunchbutton_Dish_Option extends Cana_Table {
+	public function exports() {
+		$out = $this->properties();
+
+		foreach ($this->prices() as $price) {
+			$out['prices'][$price->id_dish_option_price] = $price->exports();
+		}
+
+		return $out;
+	}
+	
+	public function prices() {
+		if (!isset($this->_prices)) {
+			$this->_prices = Dish_Option_Price::q('select * from dish_option_price where id_option="'.$this->id_option.'"');
+		}
+		return $this->_prices;
+	}
+
 	public function __construct($id = null) {
 		parent::__construct();
 		$this
