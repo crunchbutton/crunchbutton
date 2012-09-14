5d6b0f49ec51b5cbcc0a861cb2121395c771ae74
arzynik <github@arzynik.com>
2012-09-14 09:35:35 -0700

    fixed refunds

--- include/library/Crunchbutton/Balanced/Debit.php
+++ include/library/Crunchbutton/Balanced/Debit.php
@@ -2,7 +2,7 @@
 
 class Crunchbutton_Balanced_Debit extends Cana_Model {
 	public static function byId($id) {
-		$account = Balanced\Account::get(c::balanced()->debits->uri.'/'.$id);
+		$account = Balanced\Debit::get(c::balanced()->debits->uri.'/'.$id);
 		return $account;
 	}
 }
