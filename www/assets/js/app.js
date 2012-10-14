d3cfd28a0950b000cacbb4643ec4f831c5e4dfa6
arzynik <github@arzynik.com>
2012-10-13 21:57:06 -0700

    fixes #447

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -263,7 +263,7 @@ App.page.restaurant = function(id) {
 			categories = App.restaurant.categories(),
 			dishes, list;
 			
-		$('.restaurant-items').append('<div class="content-item-name content-item-main-name"><h1>The best food here</h1></div>')
+		$('.restaurant-items').append('<div class="content-item-name content-item-main-name"><h1>Add to your order</h1></div>')
 	
 		for (var x in categories) {
 			dishes = categories[x].dishes();
