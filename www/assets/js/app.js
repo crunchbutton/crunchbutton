1aedc5ac33ab47759ed931bfc70aa9e017d3cb67
arzynik <github@arzynik.com>
2012-10-13 22:00:40 -0700

    fixes #440

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -126,7 +126,7 @@ App.page.home = function() {
 		'<form class="button-letseat-formform" onsubmit="return false;">' +
 		'<table class="button-letseat-table" cellpadding="0" cellspacing="0">' +
 			'<tr>' +
-				'<td style="width: 100%;"><input type="text" class="location-address" placeholder="Enter your full address or zip code" '+ ($(window).width() >= 768 ? 'autofocus="autofocus"' : '') + '></td>' +
+				'<td style="width: 100%;"><input type="text" class="location-address" placeholder="Enter your zip code or full address" '+ ($(window).width() >= 768 ? 'autofocus="autofocus"' : '') + '></td>' +
 				/*
 				'<td>' +
 					'<div class="location-detect">' +
