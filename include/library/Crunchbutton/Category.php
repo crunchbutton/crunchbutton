46c1d5ef254cee1288ff45ac28595d02de31636e
arzynik <github@arzynik.com>
2012-10-16 17:05:13 -0700

    seo progress

--- include/library/Crunchbutton/Category.php
+++ include/library/Crunchbutton/Category.php
@@ -4,7 +4,11 @@ class Crunchbutton_Category extends Cana_Table {
 	public function restaurant() {
 		return Restaurant::o($this->id_restaurant);
 	}
-	
+
+	public function community() {
+		return $this->restaurant()->community();
+	}
+
 	public function dishes() {
 		if (!isset($this->_dishes)) {
 			$this->_dishes = Dish::q('select * from dish where id_category="'.$this->id_category.'" and dish.active=1');
@@ -18,6 +22,10 @@ class Crunchbutton_Category extends Cana_Table {
 			$out['_dishes'][$dish->id_dish] = $dish->exports();
 		}
 		return $out;
+	}
+	
+	public function name() {
+		return $this->name.($this->loc ? (' '.$this->community()->prep.' '.$this->community()->name) : '');
 	}
 
 	public function __construct($id = null) {
