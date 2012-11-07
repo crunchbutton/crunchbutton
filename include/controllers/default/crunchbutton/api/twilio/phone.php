47c3134a17fe3e535f77fcd4689de405810cb3dc
arzynik <github@arzynik.com>
2012-11-07 15:57:26 -0500

    fixes #533

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -5,7 +5,7 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 	    header('Content-type: text/xml');
 		echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
 			.'<Response>'
-			.'<Dial timeout="10" record="false">'.c::config()->phone->restaurant.'</Dial>'
+			.'<Dial timeout="10" record="false">'.c::config()->phone->support.'</Dial>'
 			.'</Response>';
 			
 		exit;
