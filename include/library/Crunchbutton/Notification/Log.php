b745fd8a6e8af688ca50ba51d723d396a090c236
arzynik <github@arzynik.com>
2012-11-27 17:42:26 -0500

    lots of twilio changes. fixes #419. fixes #397.

--- include/library/Crunchbutton/Notification/Log.php
+++ include/library/Crunchbutton/Notification/Log.php
@@ -14,11 +14,26 @@ class Crunchbutton_Notification_Log extends Cana_Table {
 	}
 	
 	public function callback() {
-		$nl = Notification_Log::q('select * from notification_log where id_order="'.$this->id_order.'" and status="callback"');
+		$nl = Notification_Log::q('select * from notification_log where id_order="'.$this->id_order.'" and status="callback" and `type`="twilio"');
 
 		if ($nl->count() >= c::config()->twilio->maxcallback) {
 			$this->status = 'maxcallbackexceeded';
 			$this->save();
+			
+			Log::critical([
+				'id_order' => $this->id_order, 
+				'id_notification_log' => $this->id_notification_log,
+				'id_notification' => $this->id_notification,
+				'id_restaurant' => $this->order()->restaurant()->id_restaurant,
+				'restaurant' => $this->order()->restaurant()->name,
+				'restaurant_phone' => $this->order()->restaurant()->phone,
+				'customer_phone' => $this->order()->phone,
+				'customer_name' => $this->order()->name,
+				'action' => '#'.$this->id_order.' MAX CB for '.$this->order()->restaurant()->name."\nR# ".$this->order()->restaurant()->phone()."\n C# ".$this->order()->phone(),
+				'host' => $_SERVER['__HTTP_HOST'],
+				'type' => 'notification'
+			]);
+
 		} else {
 			$this->queCallback();
 		}
@@ -26,11 +41,37 @@ class Crunchbutton_Notification_Log extends Cana_Table {
 	
 	public function queCallback() {
 		$log = $this;
+
 		c::timeout(function() use($log) {
 			$not = $log->notification();
 			$order = $log->order();
 			$not->send($order);
-		}, 2 * 60 * 1000);		
+		}, c::config()->twilio->callbackTime);		
+	}
+	
+	public function confirm() {
+		$nl = Notification_Log::q('select * from notification_log where id_order="'.$this->id_order.'" and status="callback" and `type`="confirm"');
+
+		if ($nl->count() >= c::config()->twilio->maxconfirmback) {
+			$this->status = 'maxconfirmbackexceeded';
+			$this->save();
+			
+			Log::critical([
+				'id_order' => $this->id_order, 
+				'id_notification_log' => $this->id_notification_log,
+				'id_notification' => $this->id_notification,
+				'id_restaurant' => $this->order()->restaurant()->id_restaurant,
+				'restaurant' => $this->order()->restaurant()->name,
+				'restaurant_phone' => $this->order()->restaurant()->phone,
+				'customer_phone' => $this->order()->phone,
+				'customer_name' => $this->order()->name,
+				'action' => '#'.$this->id_order.' MAX CONFIRM CB for '.$this->order()->restaurant()->name."\nR# ".$this->order()->restaurant()->phone()."\nC# ".$this->order()->phone(),
+				'host' => $_SERVER['__HTTP_HOST'],
+				'type' => 'notification'
+			]);
+		} else {
+			$this->order()->queConfirm();
+		}
 	}
 
 	public function __construct($id = null) {
