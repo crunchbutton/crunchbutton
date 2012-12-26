ba77c7e2ba99d5c46b70647c48324ab1ea72df46
Stuardo -StR- Rodríguez <str@maphpia.com>
2012-12-26 14:57:58 -0500

    fixed #605 allow restaurants to close between midnight and 1 am

--- www/assets/js/restaurant.js
+++ www/assets/js/restaurant.js
@@ -1,3 +1,8 @@
+/**
+ * Restaurant ORM
+ *
+ * @todo Use JS syntax to outline the structure
+ */
 var Restaurant = function(id) {
 	this.type = 'Restaurant';
 	this.id_var = 'id_restaurant';
@@ -5,9 +10,9 @@ var Restaurant = function(id) {
 	var
 		self = this,
 		complete;
-		
+
 	$.extend(self,Orm);
-	
+
 	if (arguments[1]) {
 		complete = arguments[1];
 	} else {
@@ -17,7 +22,7 @@ var Restaurant = function(id) {
 	self.categories = function() {
 		return self.loadType('Category','categories');
 	}
-	
+
 	self.top = function() {
 		var categories = self.categories();
 
@@ -32,19 +37,19 @@ var Restaurant = function(id) {
 			}
 		}
 	}
-	
+
 	self.deliveryDiff = function() {
 		var total = self.delivery_min_amt == 'subtotal' ? App.cart.subtotal() : App.cart.total();
 		var diff = parseFloat(App.restaurant.delivery_min - total).toFixed(2);
 		/* console.log(App.cart.subtotal(), App.cart.total(),self.delivery_min_amt, total, diff); */
 		return diff;
 	}
-	
+
 	self.meetDeliveryMin = function() {
 		var total = self.delivery_min_amt == 'subtotal' ? App.cart.subtotal() : App.cart.total();
 		return total < parseFloat(self.delivery_min) ? true : false;
 	}
-	
+
 	self.dateFromItem = function(item, offset) {
 		var
 			theTime = item.split(':'),
@@ -54,47 +59,46 @@ var Restaurant = function(id) {
 		theDate.setMinutes(theTime[1] + offset);
 		return theDate;
 	}
-	
+
+	/**
+	 * Checks if a restaurant is open now for delivery
+	 *
+	 * Checks if now() is between openTime and closeTime. The closing time could
+	 * be for tomorrow morning (1am) so we need to handle those cases too. See
+	 * issue #605.
+	 *
+	 * Do not use logic in PHP as the page could have been open for a while.
+	 *
+	 * @todo add offset validation
+	 *       offset = -(today.getTimezoneOffset()); // @todo: ensure this works on positive tz
+	 */
 	self.open = function() {
-		return this._open;
-		var
-			hours = self._hoursFormat,
-			today = new Date(),
-			offset = -(today.getTimezoneOffset()); // @todo: ensure this works on positive tz
-
-		for (x in hours) {
-			for (xx in hours[x]) {
-				console.log(hours[x]);
-				var
-					open = self.dateFromItem(hours[x][xx][0], offset),
-					close = self.dateFromItem(hours[x][xx][1], offset);
-				if (today >= open && today <= close) {
-					return true;
-				}
+		var isOpen =  false;
+		var today = Date.today().toString('ddd').toLowerCase();
+		if (this._hours == undefined ||  this._hours[today] == undefined) {
+			return false;
+		}
+		todayHours  = this._hours[today];
+		for (i in todayHours) {
+			var openTime  = Date.parse(todayHours[i][0]);
+			var closeTime = Date.parse(todayHours[i][1]);
+			// there is no real 24:00 hours, it's 00:00 for tomorrow
+			if (todayHours[i][1] == '24:00') {
+				closeTime = Date.parse('00:00');
+				closeTime.addDays(1);
+			}
+			// if closeTime before openTime, then closeTime should be for tomorrow
+			if (closeTime.compareTo(openTime) == -1) {
+				closeTime.addDays(1);
+			}
+			if (Date.now().between(openTime, closeTime)) {
+				isOpen = true;
+				break;
 			}
 		}
-
-		return false;
-		/*
-		var today = new DateTime('today', new DateTimeZone($this->timezone));
-		var totay = new Date(Date.UTC(year, month, day, hour, minute, second))
-		$day = strtolower($today->format('D'));
-
-		foreach ($hours as $hour) {
-			if ($hour->day != $day) {
-				continue;
-			}
-			$open = new DateTime('today '.$hour->time_open, new DateTimeZone($this->timezone));
-			$close = new DateTime('today '.$hour->time_close, new DateTimeZone($this->timezone));
-			if ($today->getTimestamp() >= $open->getTimestamp() && $today->getTimestamp() <= $close->getTimestamp()) {
-				return true;
-			}
-		}
-
-		return false;
-		*/
+		return isOpen;
 	}
-	
+
 	self.preset = function() {
 		return self['_preset'];
 	}
@@ -111,7 +115,7 @@ var Restaurant = function(id) {
 		}
 
 	}
-	
+
 	self.load(id);
 }
 
