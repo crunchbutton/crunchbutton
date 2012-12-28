29eedee9bfd92827aafe7f2b85efd4762c607118
arzynik <github@arzynik.com>
2012-12-28 15:44:36 -0500

    fixed some css bundle issues

--- include/controllers/default/crunchbutton/assets/css/bundle.css.php
+++ include/controllers/default/crunchbutton/assets/css/bundle.css.php
@@ -4,8 +4,9 @@ class Controller_assets_css_bundle_css extends Crunchbutton_Controller_AssetBund
 	public function init() {
 		$cacheid = 'crunchr-bundle-node-css'.$_REQUEST['v'];
 		
-		if (Cana::app()->cache()->cached($cacheid)) {
-			$css = Cana::app()->cache()->read($cacheid);
+		if (c::app()->cache()->cached($cacheid) && !$_REQUEST['nocache']) {
+			$css = c::app()->cache()->read($cacheid);
+			$mtime = c::cache()->mtime($cache);
 		} else {
 	
 			$file = c::config()->dirs->www.'assets/css/style.css';
@@ -21,28 +22,20 @@ class Controller_assets_css_bundle_css extends Crunchbutton_Controller_AssetBund
 				return 'url('.$img->output().')';
 			};
 			
-			$cache = 'dynamic-css-'.md5($_REQUEST['v']).'.css';
-			if (c::cache()->cached($cache) && !$_REQUEST['nocache']) {
-				$css = c::cache()->read($cache);
-				$mtime = c::cache()->mtime($cache);
-			} else {
-				//$css = preg_replace_callback('/url\(([a-z0-9\/\-_\.]+)\)/i',$callback, $css);
-				$css = preg_replace('/\t|\n/','',$css);
-	
-				$mtime = filemtime($file);
-	
-				c::cache()->write($cache, $css);
-			}
-			
-			$headers = apache_request_headers();
-	
-			if (isset($headers['If-Modified-Since'])) {
-				//header('Last-Modified: '.gmdate('D, d M Y H:i:s',$mtime).' GMT', true, 304);
-				//exit;
-			}
-			
-			Cana::app()->cache()->write($cacheid, $css);
+			//$css = preg_replace_callback('/url\(([a-z0-9\/\-_\.]+)\)/i',$callback, $css);
+			$css = preg_replace('/\t|\n/','',$css);	
+			$mtime = filemtime($file);
+			c::cache()->write($cache, $css);
+			c::app()->cache()->write($cacheid, $css);
 		}
+		
+		/*
+		$headers = apache_request_headers();
+		if (isset($headers['If-Modified-Since'])) {
+			//header('Last-Modified: '.gmdate('D, d M Y H:i:s',$mtime).' GMT', true, 304);
+			//exit;
+		}
+		*/
 
 		header('HTTP/1.1 200 OK');
 		header('Date: '.date('r'));
