de611727481a2722709e62e8762aac07159c4a58
arzynik <github@arzynik.com>
2013-02-20 09:42:56 -0800

    fixed null issue on texthacker api

--- include/controllers/default/crunchbutton/api/texthacker/user.php
+++ include/controllers/default/crunchbutton/api/texthacker/user.php
@@ -3,6 +3,10 @@
 class Controller_api_texthacker_user extends Crunchbutton_Controller_Rest {
 	public function init() {
 		$user = User::byPhone($this->request()['phone']);
+		if (!$user->count()) {
+			echo json_encode([]);
+			exit;
+		}
 		$userExport = $user->get(0)->exports();
 		unset($userExport['presets']);
 
