aa90486a14356d11c325da45544504dc2516dc20
Daniel Camargo <pererinha@gmail.com>
2013-03-15 15:11:33 -0300

    partial #529 (tip field changes)
    I'll test it on beta

--- www/assets/js/pages.js
+++ www/assets/js/pages.js
@@ -168,6 +168,11 @@ App.page.restaurant = function(id) {
 			App.trigger.delivery();
 		}
 		
+		// Force delivery case the restaurant doesn't takeout anymore
+		if( App.restaurant.delivery == '1' && App.restaurant.takeout == '0' ){
+			App.trigger.delivery();	
+		}
+		
 		$( '.restaurant-gift' ).hide();
 
 		App.credit.getCredit( function(){
