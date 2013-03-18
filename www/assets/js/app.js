1e5dde5394d69f429273ffb5ee589bdc1b1523f8
Daniel Camargo <pererinha@gmail.com>
2013-03-18 13:07:21 -0300

    partial #529

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -534,7 +534,7 @@ App.cart = {
 				var creditLeft = '<span class="gift-left"> - You\'ll still have $' + App.ceil( ( credit - this.total() ) ).toFixed( 2 ) + ' gift card left </span>';
 				credit = this.total();
 			} 
-			$('.cart-gift').html( '(- $' + credit + ' credit ' + creditLeft + ')' );
+			$('.cart-gift').html( '&nbsp;(- $' + credit + ' credit ' + creditLeft + ')' );
 		} else {
 			$('.cart-gift').html();
 		}
