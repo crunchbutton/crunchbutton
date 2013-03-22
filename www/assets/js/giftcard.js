9f7759d78facd63bea226f53d9c916868c448476
Daniel Camargo <pererinha@gmail.com>
2013-03-22 14:56:55 -0300

    partial #874

---
+++ www/assets/js/giftcard.js
@@ -0,0 +1,14 @@
+App.giftcard = {
+	api : {
+		sms : 'giftcard/...'
+	}
+}
+
+App.giftcard.init = function(){
+	// $('.wrapper').append(App.render('giftcard'));	
+	console.log( 'App.giftcard.init' );
+}
+
+App.giftcard.show = function(){
+	console.log( 'App.giftcard.show' );
+}
