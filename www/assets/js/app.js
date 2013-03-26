f6463d18eefa928eb7c8920def40dd6c6dd88ea6
Daniel Camargo <pererinha@gmail.com>
2013-03-26 18:05:23 -0300

    fixed the variable name

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1506,7 +1506,7 @@ $(function() {
 	});
 
 	$(document).on('click', '.link-home', function() {
-		if( App.restaurants.list && App.restaurants.lis.length > 0 ){
+		if( App.restaurants.list && App.restaurants.list.length > 0 ){
 			App.page.foodDelivery();
 		} else {
 			App.loadHome(true);
