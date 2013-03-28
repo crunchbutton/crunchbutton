6af4487f1f8c4c8e2085451c87dc14d2bebfd6be
Daniel Camargo <pererinha@gmail.com>
2013-03-28 16:26:05 -0300

    partial #874

---
+++ include/controllers/default/crunchbutton/admin/giftcards/content.php
@@ -0,0 +1,35 @@
+<?php
+
+class Controller_Admin_Giftcards_Content extends Crunchbutton_Controller_Account {
+
+	public function init() {
+
+		$search = [];
+	
+		if ($_REQUEST['limit']) {
+			$search['limit'] = intval($_REQUEST['limit']);
+		}
+		
+		if ($_REQUEST['id_user']) {
+			$search['id_user'] = $_REQUEST['id_user'];
+		}
+
+		if ($_REQUEST['type']) {
+			$search['type'] = $_REQUEST['type'];
+		}
+		
+		if ($_REQUEST['dates']) {
+			$dates = explode(',',$_REQUEST['dates']);
+			$search['start'] = $dates[0];
+			$search['end'] = $dates[1];
+		}
+		
+		if ($_REQUEST['restaurant']) {
+			$search['restaurant'] = $_REQUEST['restaurant'];
+		}
+
+		c::view()->giftcards = Crunchbutton_Promo::find($search);
+		c::view()->layout('layout/ajax');
+		c::view()->display('admin/giftcards/content');
+	}
+}
---
+++ include/controllers/default/crunchbutton/admin/giftcards/index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Controller_Admin_Giftcards extends Crunchbutton_Controller_Account {
+	public function init() {
+		c::view()->page = 'admin/giftcards';
+		c::view()->layout('layout/admin');
+		if( c::getPagePiece(2) == 'new' ){ 
+			c::view()->users = Crunchbutton_User::q('SELECT u.id_user, u.name, u.phone, u.email FROM user u INNER JOIN user_auth ua ON ua.id_user = u.id_user WHERE u.active = 1 ORDER BY u.name ASC');;
+			c::view()->display('admin/giftcards/new');	
+		} else {
+			$credit = Crunchbutton_Credit::o(c::getPagePiece(2));
+			if( $credit->id_credit ){
+				c::view()->credit = $credit;
+				c::view()->display('admin/giftcards/credit');	
+			} else {
+				// Show the credit's list
+				c::view()->display('admin/giftcards/index');
+			}
+		}
+	}
+}
