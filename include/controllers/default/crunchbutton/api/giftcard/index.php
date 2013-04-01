a47d032663313fdd3e917079fd8c71a509c55f22
Daniel Camargo <pererinha@gmail.com>
2013-04-01 15:39:33 -0300

    partial #965

--- include/controllers/default/crunchbutton/api/giftcard/index.php
+++ include/controllers/default/crunchbutton/api/giftcard/index.php
@@ -23,14 +23,11 @@ class Controller_api_Giftcard extends Crunchbutton_Controller_Rest {
 									$giftcard->id_user = $id_user;
 									$user = Crunchbutton_User::o( $id_user );
 									$giftcard->phone =  $user->phone;
-									$giftcard->email =  $user->email;
 								}
 								$giftcard->type = Crunchbutton_Promo::TYPE_GIFTCARD;
 								$giftcard->date = date('Y-m-d H:i:s');
 								$giftcard->save();
-								if( $giftcard->email ){
-									$giftcard->queNotifyEMAIL();
-								} else if( $giftcard->phone ){
+								if( $giftcard->phone ){
 									$giftcard->queNotifySMS();
 								}
 							}
