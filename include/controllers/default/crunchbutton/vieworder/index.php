690be601052982a8dcd94335bb121d52aa1f558f
arzynik <github@arzynik.com>
2013-04-01 21:55:37 -0400

    added security

--- include/controllers/default/crunchbutton/vieworder/index.php
+++ include/controllers/default/crunchbutton/vieworder/index.php
@@ -4,7 +4,7 @@ class Controller_vieworder extends Cana_Controller {
 	public function init() {
 
 		$order = Order::uuid(c::getPagePiece(1));
-		if (!$order->id_order) {
+		if (!$order->id_order || (!$_SESSION['admin'] && c::user()->id_user != $order->id_user)) {
 			die('invalid order');
 		}
 		$order = $order->get(0);
