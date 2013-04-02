e4eff8dabc8ee0e9534b335a1bacd34a0056a01d
Daniel Camargo <pererinha@gmail.com>
2013-04-02 11:25:10 -0300

    partial #951

--- include/library/Crunchbutton/Notification.php
+++ include/library/Crunchbutton/Notification.php
@@ -12,10 +12,8 @@ class Crunchbutton_Notification extends Cana_Table
 
 		$env = c::env() == 'live' ? 'live' : 'dev';
 		$num = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
-		// $mail = ($env == 'live' ? $this->value : '_EMAIL');
-		$fax = ($env == 'live' ? $this->value : '_PHONE_');
 		$mail = ($env == 'live' ? $this->value : '_EMAIL');
-		
+		$fax = ($env == 'live' ? $this->value : '_PHONE_');
 
 		switch ($this->type) {
 			case 'fax':
