1ee58b4f93eb1b884c63e083c44e4b70c03735d3
arzynik <github@arzynik.com>
2013-04-02 10:53:41 -0700

    reactivated auto tests

--- include/controllers/default/crunchbutton/admin/test/index.php
+++ include/controllers/default/crunchbutton/admin/test/index.php
@@ -2,18 +2,6 @@
 
 class Controller_admin_test extends Crunchbutton_Controller_Account {
 	public function init() {
-	
-		$geo = new Crunchbutton_Geo([
-			'adapter' => 'Geoip_Binary',
-			'file' => c::config()->dirs->root.'db/GeoLiteCity.dat'
-		]);
-		$geo->setIp('76.90.138.20')->populateByIp();
-
-		print_r($geo);
-
-		exit;
-//		Cana::view()->layout('layout/ajax');
-
 		c::config()->domain->theme = 'test';
 		c::buildView(['layout' =>  c::config()->defaults->layout]);
 		c::view()->useFilter(false);
