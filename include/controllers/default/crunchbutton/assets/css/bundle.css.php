0636e47291e7345fe2624215b0e784ba84efeb9a
arzynik <github@arzynik.com>
2013-04-01 21:59:17 -0400

    fixes #982. added print order button.

--- include/controllers/default/crunchbutton/assets/css/bundle.css.php
+++ include/controllers/default/crunchbutton/assets/css/bundle.css.php
@@ -11,6 +11,9 @@ class Controller_assets_css_bundle_css extends Crunchbutton_Controller_AssetBund
 	
 			$file = c::config()->dirs->www.'assets/css/style.css';
 			$css = file_get_contents($file);
+
+			$css .= file_get_contents(c::config()->dirs->www.'assets/css/font-awesome.css');
+
 			
 			if (strpos($_SERVER['HTTP_USER_AGENT'],'Windows') !== false) {
 				$file = c::config()->dirs->www.'assets/css/windows.css';
