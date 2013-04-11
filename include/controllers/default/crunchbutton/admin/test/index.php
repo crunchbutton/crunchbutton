87c33467b211a6128493b4496fabaca4139e57f6
Daniel Camargo <pererinha@gmail.com>
2013-04-11 17:55:42 -0300

    #1049 testing

--- include/controllers/default/crunchbutton/admin/test/index.php
+++ include/controllers/default/crunchbutton/admin/test/index.php
@@ -2,10 +2,22 @@
 
 class Controller_admin_test extends Crunchbutton_Controller_Account {
 	public function init() {
+
+		$order = Order::o(c::getPagePiece(2));
+
+		echo '<pre>';
+		foreach ( $order->restaurant()->notifications() as $n ) {
+			echo $n->type;
+			$n->send( $order );
+			echo '<br/>';
+		};
+
+		/*
 		c::config()->domain->theme = 'test';
 		c::buildView(['layout' =>  c::config()->defaults->layout]);
 		c::view()->useFilter(false);
 
 		c::view()->display('admin/test/index');
+		*/
 	}
 }
