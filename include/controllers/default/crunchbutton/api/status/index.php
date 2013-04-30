3108c5f3a4c442c287527d52d6f864f5251a29c0
arzynik <github@arzynik.com>
2013-04-29 18:15:05 -0700

    added some rest services for #1111

--- include/controllers/default/crunchbutton/api/status/index.php
+++ include/controllers/default/crunchbutton/api/status/index.php
@@ -2,7 +2,6 @@
 
 class Controller_api_status extends Crunchbutton_Controller_Rest {
 	public function init() {
-
 		// check to see that the database is working ok
 		$restaurant = Restaurant::q('select * from restaurant limit 1');
 		echo json_encode(['status' => $restaurant->id_restaurant ? 'online' : 'offline']);
