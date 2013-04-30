e1d48863bb812f7f344ee3cea492993f884bccbc
Daniel Camargo <pererinha@gmail.com>
2013-04-30 13:21:08 -0300

    partial #1100

--- include/controllers/default/crunchbutton/api/support/index.php
+++ include/controllers/default/crunchbutton/api/support/index.php
@@ -7,35 +7,81 @@ class Controller_api_Support extends Crunchbutton_Controller_Rest {
 			// Saves a suggestion
 			case 'post':
 
-				if (c::getPagePiece(2) == 'sms') {
-					
+				switch ( c::getPagePiece(2) ) {
+					case 'sms':
+						// Create a twilio session
+						$tsess = new Session_Twilio;
+						$tsess->phone = $this->request()['phone'];
+						$tsess->data = json_encode( $_REQUEST );
+						$tsess->save();
 
-					// Create a twilio session
-					$tsess = new Session_Twilio;
-					$tsess->phone = $this->request()['phone'];
-					$tsess->data = json_encode( $_REQUEST );
-					$tsess->save();
+						$support = new Crunchbutton_Support;
+						$support->type = Crunchbutton_Support::TYPE_BOX_NEED_HELP;
+						$support->name = $this->request()['name'];
+						$support->phone = $this->request()['phone'];
+						$support->message = $this->request()['message'];
+						$support->ip = $_SERVER['REMOTE_ADDR'];
+						$support->id_session_twilio = $tsess->id_session_twilio;
+						$support->date = date('Y-m-d H:i:s');
+						if( c::user()->id_user ){
+							$support->id_user = c::user()->id_user;	
+						}
+						$support->save();
+						echo $support->json();
+						$support->queNotify();
+					break;
+					case 'say':
 
-					$support = new Crunchbutton_Support;
-					$support->type = Crunchbutton_Support::TYPE_BOX_NEED_HELP;
-					$support->name = $this->request()['name'];
-					$support->phone = $this->request()['phone'];
-					$support->message = $this->request()['message'];
-					$support->ip = $_SERVER['REMOTE_ADDR'];
-					$support->id_session_twilio = $tsess->id_session_twilio;
-					$support->date = date('Y-m-d H:i:s');
-					if( c::user()->id_user ){
-						$support->id_user = c::user()->id_user;	
-					}
-					$support->save();
-					echo $support->json();
-					$support->queNotify();
+						$id_support = c::getPagePiece( 3 );
+
+						Log::debug( [ 'action' => 'Calling', 'id_support' => $id_support, 'type' => 'support' ] );
+
+						$support = Crunchbutton_Support::o( $id_support );
+
+						$message = ' . You received a new support sms . ';
+
+						if( $support->id_support ){
+
+							if( is_numeric( $support->name ) ){
+								$name = implode( ' . ', str_split( $support->name ) );
+							} else {
+								$name = $support->name;
+							}
+
+							$phone = implode( ' . ', str_split( $support->phone ) );
+
+							$message .= ' . Name . ' . $name . ' . ';
+							$message .= ' . Phone . ' . $phone . ' . ';
+							$message .= ' . Message . ' . $support->message . ' . ';
+						} 
+
+						$supportName = c::getPagePiece( 4 );
+
+						$message .= ' . ';
+
+						Log::debug( [ 'action' => 'Calling', 'said' => $message, 'supportName' => $supportName, 'type' => 'support' ] );
+
+						header('Content-type: text/xml');
+						echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n"
+									.'<Response>' . "\n"
+										.'<Say voice="' . c::config()->twilio->voice . '">' . "\n"
+											. 'Hello ' . $supportName . ' . , ' . "\n"
+											. $message . "\n"
+										.'</Say>' . "\n"
+									.'</Response>';
+						
+						break;
 				}
 
 			break;
 
 			case 'get':
-				echo json_encode(['error' => 'invalid object']);
+
+				switch ( c::getPagePiece( 2 ) ) {					
+					default:
+						echo json_encode(['error' => 'invalid object']);
+						break;
+				}
 		}
 	}
 }
