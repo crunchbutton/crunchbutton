199c35720c99be424e5ff6b63bd15cc9ec614b50
BDC <adambildersee@gmail.com>
2013-05-06 12:39:24 -0400

    weight adj restaurant admin

--- www/assets/js/admin_restaurant.js
+++ www/assets/js/admin_restaurant.js
@@ -1281,6 +1281,7 @@ var DOM_MAP = {
 				'input#restaurant-fee-restaurant' : ['fee_restaurant'],
 				'input#restaurant-fee-customer' : ['fee_customer'],
 				'input#restaurant-tax' : ['tax'],
+				'input#restaurant-weight-adj' : ['weight_adj'],
 			},
 			widget : [], // a list of widgets supporting 'apply' and 'flush' funcs etc
 			func : {
