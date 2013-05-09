cc61d3c4e656b3de07365cf9fccedf55221761ec
BDC <adambildersee@gmail.com>
2013-05-09 17:08:14 -0400

    dupe hipchat notification

--- include/controllers/default/cockpit/support/index.php
+++ include/controllers/default/cockpit/support/index.php
@@ -2,16 +2,17 @@
 
 class Controller_support extends Crunchbutton_Controller_Account {
 	public function init() {
+		c::view()->layout('layout/feather');
 		$support = Support::o(c::getPagePiece(2));
 		c::view()->page = 'support';
 
 		if( $support->id_support ){
 			// Show the support's form
 			c::view()->support = $support;
-			c::view()->display('support/support');	
+			c::view()->display('m/support/support');	
 		} else {
 			// Show the support's list
-			c::view()->display('support/index');
+			c::view()->display('m/support/index');
 		}
 	}
 }
