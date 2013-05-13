5fda9462f161a8ae330817aa8031b381041184d0
Daniel Camargo <pererinha@gmail.com>
2013-05-13 16:47:42 -0300

    partial #1250 - removed the tests

--- include/controllers/default/cockpit/test/index.php
+++ include/controllers/default/cockpit/test/index.php
@@ -2,12 +2,6 @@
 
 class Controller_test extends Crunchbutton_Controller_Account {
 	public function init() {
-
-		$notification = Notification_Log::o(c::getPagePiece(1));
-		$notification->maxCBCallToSupport();
-
-		exit;
-
 		c::config()->domain->theme = 'test';
 		c::buildView(['layout' =>  c::config()->defaults->layout]);
 		c::view()->useFilter(false);
