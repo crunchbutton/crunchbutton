342e1a2d26a237ee44d1a7843b854c33df2189a0
Daniel Camargo <pererinha@gmail.com>
2013-05-15 00:41:00 -0300

    #1281 testing the new email

--- include/library/Crunchbutton/Email/Promo.php
+++ include/library/Crunchbutton/Email/Promo.php
@@ -8,8 +8,8 @@ class Crunchbutton_Email_Promo extends Crunchbutton_Email {
 		
 		$params['to'] 				= $params['email'].', Crunchbutton Copy <_USERNAME_>';
 		$params['subject'] 		= $params['subject'];		
-		$params['from'] 			= ($params['order']->id_restaurant == 1 ? 'One Button Wenzel' : 'Crunchbutton').' <tech@_DOMAIN_>';
-		$params['reply']			= 'Tech Support <tech@_DOMAIN_>';
+		$params['from'] 			= ($params['order']->id_restaurant == 1 ? 'One Button Wenzel' : 'Crunchbutton').' <giftcard@_DOMAIN_>';
+		$params['reply']			= 'Crunchbutton<giftcard@_DOMAIN_>';
 
 		$this->buildView($params);
 		$this->view()->subject	= $params['subject'];
