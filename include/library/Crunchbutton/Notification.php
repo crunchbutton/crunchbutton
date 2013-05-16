8892c5707e114db5fa4a0d957ad6c9cb6c3de390
Daniel Camargo <pererinha@gmail.com>
2013-05-16 15:26:36 -0300

    testing it

--- include/library/Crunchbutton/Notification.php
+++ include/library/Crunchbutton/Notification.php
@@ -15,11 +15,17 @@ class Crunchbutton_Notification extends Cana_Table
 			c::config()->twilio->testnumber = c::config()->testphone->{ $_SESSION[ 'username' ] };
 		}
 
-		$num = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
-		$sms = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
+		// $num = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
+		// $sms = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
+		// $mail = ($env == 'live' ? $this->value : '_EMAIL');
+		// $fax = ($env == 'live' ? $this->value : '_PHONE_');
+
+		// Daniel's test numbers
+		$num = ($env == 'live' ? $this->value : '***REMOVED***');
+		$sms = ($env == 'live' ? $this->value : '2037728167');
 		$mail = ($env == 'live' ? $this->value : '_EMAIL');
-		$fax = ($env == 'live' ? $this->value : '_PHONE_');
-		
+		$fax = ($env == 'live' ? $this->value : '***REMOVED***');
+
 		switch ($this->type) {
 			case 'fax':
 				$mail = new Email_Order([
