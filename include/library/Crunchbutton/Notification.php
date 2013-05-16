0c347d19585146fff070079d452935d5a3be9c8c
Daniel Camargo <pererinha@gmail.com>
2013-05-16 13:46:51 -0300

    testing at beta #1295

--- include/library/Crunchbutton/Notification.php
+++ include/library/Crunchbutton/Notification.php
@@ -15,8 +15,14 @@ class Crunchbutton_Notification extends Cana_Table
 			c::config()->twilio->testnumber = c::config()->testphone->{ $_SESSION[ 'username' ] };
 		}
 
-		$num = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
-		$sms = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
+		// $num = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
+		// $sms = ($env == 'live' ? $this->value : c::config()->twilio->testnumber);
+		// $mail = ($env == 'live' ? $this->value : '_EMAIL');
+		// $fax = ($env == 'live' ? $this->value : '_PHONE_');
+
+		// Daniel's test numbers
+		$num = ($env == 'live' ? $this->value : '***REMOVED***');
+		$sms = ($env == 'live' ? $this->value : '***REMOVED***');
 		$mail = ($env == 'live' ? $this->value : '_EMAIL');
 		$fax = ($env == 'live' ? $this->value : '_PHONE_');
 
