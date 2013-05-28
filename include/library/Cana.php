32e5a3ada2854d6f01acf0f9ccc801ae4cfbb9c5
Daniel Camargo <pererinha@gmail.com>
2013-05-28 17:43:45 -0300

    partial #1360

--- include/library/Cana.php
+++ include/library/Cana.php
@@ -154,7 +154,7 @@ class Cana extends Cana_Model {
 			$sleep = ' -s='.$ms;
 		}
 
-		$env = ' -e=' . ( c::env() == 'live' ? 'live' : 'dev' );
+		$env = ' -e=' . ( c::env() == 'live' ? 'live' : c::env() );
 
 		$cmd = c::config()->dirs->root.'cli/timeout.php'.$sleep.$env.' -c='.str_replace("'",'"',escapeshellarg($encoded));
 
