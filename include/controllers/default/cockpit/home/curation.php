55cf5adc753545b22068603faa575370a0d1433b
Daniel Camargo <pererinha@gmail.com>
2013-07-09 19:41:32 -0300

    Curation page

--- include/controllers/default/cockpit/home/curation.php
+++ include/controllers/default/cockpit/home/curation.php
@@ -4,13 +4,15 @@ class Controller_home_curation extends Crunchbutton_Controller_Account {
 
 	public function init() {
 
+		$orderByCategory = ( c::getPagePiece(2) == 'category' );
+
 		$data = [];
 		$restaurants = Restaurant::q('SELECT * FROM restaurant WHERE active = 1 ORDER BY name ASC');
 		foreach( $restaurants as $restaurant ){
 			$data[ $restaurant->id_restaurant ] = [];
 			$data[ $restaurant->id_restaurant ][ 'Name' ] = $restaurant->name;
 			$data[ $restaurant->id_restaurant ][ 'Food' ] = [];
-			$foods = $restaurant->foodReport();
+			$foods = $restaurant->foodReport( $orderByCategory );
 			foreach( $foods as $food ){
 				$data[ $restaurant->id_restaurant ][ 'Food' ][] = array( 'name' => $food->dish, 'times' => $food->times, 'category' => $food->category );
 			}
