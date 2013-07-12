df5db4875a9ef0529e8a8371d7f47990439ed476
arzynik <github@arzynik.com>
2013-07-12 18:58:48 -0400

    fixes #1493

--- include/controllers/default/cockpit/test/index.php
+++ include/controllers/default/cockpit/test/index.php
@@ -2,7 +2,9 @@
 
 class Controller_test extends Crunchbutton_Controller_Account {
 	public function init() {
-
+		echo ini_get('upload_max_filesize');
+		echo ini_get('post_max_size');
+		exit;
 
 		c::config()->domain->theme = 'test';
 		c::buildView(['layout' =>  c::config()->defaults->layout]);
