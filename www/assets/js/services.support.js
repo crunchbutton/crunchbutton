e01a00f8c9595e6b8bc869286eef1a840a89d964
Daniel Camargo <pererinha@gmail.com>
2013-07-25 15:47:08 -0300

    partial #1503: Work on AngularJS Stuff - Support form fixes

--- www/assets/js/services.support.js
+++ www/assets/js/services.support.js
@@ -4,14 +4,15 @@ NGApp.factory( 'SupportService', function( $http, AccountService ){
 	var service = {
 		minimized : true,
 		maximized : false,
-		thanks : true
+		thanks : false,
+		form : {}
 	};
 
 	// Create a copy of the user values
 	service.account = AccountService;
-	service.name = angular.copy( service.account.user.name );
-	service.phone = angular.copy( service.account.user.phone );
-	service.message = '';
+	service.form.name = angular.copy( service.account.user.name );
+	service.form.phone = angular.copy( service.account.user.phone );
+	service.form.message = '';
 
 	service.toggle = function(){
 		service.minimized = !service.minimized;
@@ -22,7 +23,7 @@ NGApp.factory( 'SupportService', function( $http, AccountService ){
 	}
 
 	service.reset = function(){
-		service.message = '';
+		service.form.message = '';
 		service.thanks = false;
 	}
 
@@ -30,25 +31,25 @@ NGApp.factory( 'SupportService', function( $http, AccountService ){
 
 		service.purify();
 
-		if ( service.name == '' ){
+		if ( service.form.name == '' ){
 			alert( 'Please enter your name.' );
 			$('input[name=support-name]').focus();
 			return;
 		}
 
-		if ( service.phone == '' ){
+		if ( service.form.phone == '' ){
 			alert( 'Please enter your phone.' );
 			$('input[name=support-phone]').focus();
 			return;
 		}
 
-		if ( !App.phone.validate( service.phone ) ) {
+		if ( !App.phone.validate( service.form.phone ) ) {
 			alert( 'Please enter a valid phone.' );
 			$('input[name=support-phone]').focus();
 			return;
 		}
 
-		if ( service.message == '' ){
+		if ( service.form.message == '' ){
 			alert( 'Please enter the message.' );
 			$('textarea[name=support-message]').focus();
 			return;
@@ -63,16 +64,16 @@ NGApp.factory( 'SupportService', function( $http, AccountService ){
 			$http( {
 				method: 'POST',
 				url: url,
-				data: $.param( { name: service.name, phone: service.phone, message: service.message } ),
+				data: $.param( { name: service.form.name, phone: service.form.phone, message: service.form.message } ),
 				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 				} ).success( function( data ) { service.thanks = true; service.isSending = false; }	);
 		}
 	}
 
 	service.purify = function(){
-		service.name = $.trim( service.name );
-		service.phone = $.trim( service.phone );
-		service.message = $.trim( service.message );
+		service.form.name = $.trim( service.form.name );
+		service.form.phone = $.trim( service.form.phone );
+		service.form.message = $.trim( service.form.message );
 	}
 
 	return service;
