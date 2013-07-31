a7d88be875e79212feb2a25ece671625599b8de9
Daniel Camargo <pererinha@gmail.com>
2013-07-31 12:03:55 -0300

    partial #1524

--- include/library/Crunchbutton/Chart/Churn.php
+++ include/library/Crunchbutton/Chart/Churn.php
@@ -8,19 +8,21 @@ class Crunchbutton_Chart_Churn extends Crunchbutton_Chart {
 	public $groups = array( 
 												'group-churn-rate' => array(
 														'title' => 'Churn Rate',
+														'activeDays' => 60,
 														'charts' => array(  
 																'churn-rate-per-day' => array( 'title' => 'Day', 'interval' => 'day', 'type' => 'column', 'method' => 'byDay' ),
-																'churn-rate-per-week' => array( 'title' => 'Week', 'interval' => 'week', 'type' => 'column', 'method' => 'byWeek' ),
-																'churn-rate-per-month' => array( 'title' => 'Month', 'interval' => 'month', 'type' => 'column', 'method' => 'byMonth' ),
+																'churn-rate-per-week' => array( 'title' => 'Week', 'interval' => 'week', 'type' => 'column', 'method' => 'byWeek'),
+																'churn-rate-per-month' => array( 'title' => 'Month', 'interval' => 'month', 'type' => 'column', 'method' => 'byMonth'),
 															)
 												),
 												'group-churn-rate-per-active-user' => array(
 														'title' => 'Churn Rate per Active User',
+														'activeDays' => 60,
 														'tags' => array( 'main' ),
 														'charts' => array(  
-																'churn-rate-per-active-user-per-day' => array( 'title' => 'Day', 'interval' => 'day', 'type' => 'column', 'method' => 'activeByDay' ),
-																'churn-rate-per-active-user-per-week' => array( 'title' => 'Week', 'interval' => 'week', 'type' => 'column', 'method' => 'activeByWeek' ),
-																'churn-rate-per-active-user-per-month' => array( 'title' => 'Month', 'interval' => 'month', 'type' => 'column', 'method' => 'activeByMonth' ),
+																'churn-rate-per-active-user-per-day' => array( 'title' => 'Day', 'interval' => 'day', 'type' => 'column', 'method' => 'activeByDay'),
+																'churn-rate-per-active-user-per-week' => array( 'title' => 'Week', 'interval' => 'week', 'type' => 'column', 'method' => 'activeByWeek'),
+																'churn-rate-per-active-user-per-month' => array( 'title' => 'Month', 'interval' => 'month', 'type' => 'column', 'method' => 'activeByMonth'),
 															)
 												),
 										);
