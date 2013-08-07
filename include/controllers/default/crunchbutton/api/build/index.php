070573d0e2da65014a024e7e60f61e9633f06cf5
arzynik <github@arzynik.com>
2013-08-06 21:14:36 -0700

    updated some stuff for phonegap

---
+++ include/controllers/default/crunchbutton/api/build/index.php
@@ -0,0 +1,51 @@
+<?php
+
+class Controller_api_build extends Crunchbutton_Controller_Rest {
+	public function init() {
+	
+		// generate a list of assets to be used for the app
+		$files = [];
+
+		// views
+		foreach (new DirectoryIterator(c::config()->dirs->view.'default/crunchbutton/frontend') as $fileInfo) {
+			if (!$fileInfo->isDot()) {
+				$files[] = 'view/'.$fileInfo->getBasename('.phtml').'.html';
+			}
+		}
+
+		// images
+		$exclude = [
+			'/admin',
+			'/social',
+			'/mprinter',
+			'/micro',
+			'/giftcard',
+			'/food',
+			'/datepicker',
+			'/compat',
+			'/admin'
+		];
+		$path = c::config()->dirs->www.'assets/images';
+		$dir  = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
+		$fs = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
+		
+		foreach ($fs as $fileInfo) {
+			$p = str_replace($path,'',$fileInfo->getPath());
+			if ($fileInfo->isFile() && ((!$p && substr($fileInfo->getBasename(),0,1) != '.') || ($p && !in_array($p, $exclude)))) {
+				$files[] = str_replace('//','/','images/'.$p.'/'.$fileInfo->getBasename());
+			}
+		}
+		
+		// audio
+		$files[] = 'audio/crunch.mp3';
+		$files[] = 'audio/crunch.ogg';
+		
+		// javascript
+		$files[] = 'js/bundle.js';
+		
+		// css
+		$files[] = 'css/bundle.css';
+		
+		echo json_encode($files);
+	}
+}
