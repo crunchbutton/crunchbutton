34d97c4faa09fb46ba589147e72bf96b15dd0b7c
Daniel Camargo <pererinha@gmail.com>
2013-08-09 16:28:56 -0300

    ux change at restaurant's page

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -75,43 +75,6 @@ App.toggleMenu = function() {
 };
 
 
-
-/**
- * Loads up "community" keyword pages
- */
- /*
-	TODO: replace this method by the CommunityAliasService
- */
-App.routeAlias = function( id, success, error ) {
-	id = id.replace('/','').toLowerCase();
-	alias = App.aliases[id] || false;
-	success = success || function(){};
-	error = error || function(){};
-
-	if (alias) {
-		// Get the location of the alias
-		var loc = App.locations[alias.id_community];
-
-		if (loc.loc_lat && loc.loc_lon) {
-
-			var res = new Location({
-				lat: loc.loc_lat,
-				lon: loc.loc_lon,
-				type: 'alias',
-				verified: true,
-				prep: alias.prep,
-				city: alias.name_alt,
-				address: alias.name_alt
-			});
-			success({alias: res});
-			return;
-		}
-	}
-
-	error();
-};
-
-
 App.NGinit = function() {
 	$('body').attr('ng-controller', 'AppController');
 	angular.bootstrap(document,['NGApp']);
