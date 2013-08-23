257d10f3fb99792f7b0bd945969f8e78be422221
Daniel Camargo <pererinha@gmail.com>
2013-08-23 15:01:02 -0300

    CSS Optimization

--- include/controllers/default/crunchbutton/assets/css/bundle.css.php
+++ include/controllers/default/crunchbutton/assets/css/bundle.css.php
@@ -9,9 +9,6 @@ class Controller_assets_css_bundle_css extends Crunchbutton_Controller_AssetBund
 			$mtime = c::cache()->mtime($cache);
 		} else {
 	
-			/* this style file is temporarily  */
-			$file = c::config()->dirs->www.'assets/css/styles.revised.css';
-			$css = file_get_contents($file);
 			$file = c::config()->dirs->www.'assets/css/style.css';
 			$css .= file_get_contents($file);
 
