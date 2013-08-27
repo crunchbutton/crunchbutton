862797a3bbe88e583f8c8350f0b8c01c431318a2
Daniel Camargo <pererinha@gmail.com>
2013-08-27 10:10:31 -0300

    partial #1437

--- include/library/Crunchbutton/Preset.php
+++ include/library/Crunchbutton/Preset.php
@@ -14,7 +14,25 @@ class Crunchbutton_Preset extends Cana_Table {
 		$out['delivery_type'] = $order->delivery_type;
 		$out['pay_type'] = $order->pay_type;
 		foreach ($this->dishes() as $dish) {
-			$out['_dishes'][$dish->id_order_dish] = $dish->exports();
+			$_options = $dish->exports();
+
+			if( $order->id_order && $dish->id_dish ){
+				// Get the options that was set as default after the preset date - see #1437
+				$newOptions = Dish_Option::q( "SELECT do.*, op.id_option_parent FROM dish_option do INNER JOIN `order` o ON o.id_order = {$order->id_order} AND do.date > o.date INNER JOIN `option` op ON op.id_option = do.id_option WHERE do.id_dish = {$dish->id_dish} AND do.`default` = 1" );
+				foreach ( $newOptions as $newOption ) {
+					// This rule of #1437 isn't applied to select type - just checkbox
+					if( $newOption->id_option_parent ){
+						continue;
+					}
+					$opt = array();
+					$opt[ 'id_order_dish_option' ] = $newOption->id_dish_option;
+					$opt[ 'id_option' ] = $newOption->id_option;
+					$opt[ 'id_order_dish' ] = $dish->id_order_dish;
+					$opt[ 'id' ] = $newOption->id_dish_option;
+					$_options[ '_options' ][] = $opt;
+				}
+			}
+			$out['_dishes'][$dish->id_order_dish] = $_options;
 		}
 		return $out;
 	}
