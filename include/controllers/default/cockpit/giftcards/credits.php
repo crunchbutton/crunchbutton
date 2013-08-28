eccace26bee71bb6dd916c10f7bb3d96bc5983b3
Daniel Camargo <pererinha@gmail.com>
2013-08-28 14:07:48 -0300

    gift card - anti cheat

--- include/controllers/default/cockpit/giftcards/credits.php
+++ include/controllers/default/cockpit/giftcards/credits.php
@@ -12,14 +12,27 @@ class Controller_giftcards_credits extends Crunchbutton_Controller_Account {
 				$this->byphone();
 				break;
 
+			case 'byuser':
+				$this->byuser();
+				break;
+
 			default:
 
-				$credits = Crunchbutton_Credit::antifraud( 5 );
+				$credits_by_phone = Crunchbutton_Credit::antifraudByPhone( 5 );
+				$credits_by_user = Crunchbutton_Credit::antifraudByUser( 5 );
 				c::view()->page = 'giftcards';
-				c::view()->credits = $credits;
+				c::view()->credits_by_phone = $credits_by_phone;
+				c::view()->credits_by_user = $credits_by_user;
 				c::view()->display('giftcards/credits/index');
 				break;
 		}
+	}
+
+	private function byuser(){
+		$id_user = c::getPagePiece(3);
+		c::view()->user = Crunchbutton_User::o( $id_user );
+		c::view()->giftcards = Crunchbutton_Promo::byIdUser( $id_user );
+		c::view()->display( 'giftcards/credits/byphone' );
 	}
 
 	private function byphone(){
