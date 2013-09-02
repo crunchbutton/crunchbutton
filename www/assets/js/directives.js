7c665c68f7e0d1aec8c846f3e36cbdce3fa7fb03
Daniel Camargo <pererinha@gmail.com>
2013-09-02 14:11:33 -0300

    partial #1655

--- www/assets/js/directives.js
+++ www/assets/js/directives.js
@@ -12,6 +12,20 @@ NGApp.directive( 'facebookSigninButton', function ( AccountFacebookService ) {
 	};;
 });
 
+// Suggestion tool tip
+NGApp.directive( 'suggestionToolTip', function () {
+	return {
+		restrict: 'A',
+		templateUrl: 'assets/view/restaurant.suggestion.tooltip.html',
+		scope: {
+			type: '@'
+		},
+		controller: function ( $scope ) {
+			$scope.show = false;
+		}
+	};;
+});
+
 // Press enter directive
 NGApp.directive( 'ngEnter', function() {
 		return function( scope, element, attrs ) {
