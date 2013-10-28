89759ec457ccb0d9385e3baa0affe3067517797f
arzynik <github@arzynik.com>
2013-10-28 14:52:03 -0700

    added date month name methods

--- www/assets/js/util.js
+++ www/assets/js/util.js
@@ -103,3 +103,22 @@ if (window.jQuery) {
 		};
 	})(jQuery);
 }
+
+
+/* month methods */
+Date.prototype.getMonthName = function(lang) {
+	lang = lang && (lang in Date.locale) ? lang : 'en';
+	return Date.locale[lang].month_names[this.getMonth()];
+};
+
+Date.prototype.getMonthNameShort = function(lang) {
+	lang = lang && (lang in Date.locale) ? lang : 'en';
+	return Date.locale[lang].month_names_short[this.getMonth()];
+};
+
+Date.locale = {
+	en: {
+		month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		month_names_short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	}
+};
