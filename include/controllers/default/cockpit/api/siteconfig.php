55e8fb97d9ce0b017c209ed9f5ef497b0e0b5ec5
Daniel Camargo <pererinha@gmail.com>
2013-10-31 08:55:16 -0200

    partial #1964 - option to create admin groups / changed the user and group's api to RestAccount subclass.

--- include/controllers/default/cockpit/api/siteconfig.php
+++ include/controllers/default/cockpit/api/siteconfig.php
@@ -22,12 +22,12 @@ class Controller_api_siteconfig extends Crunchbutton_Controller_RestAccount {
 
 	private function save( $key, $value ){
 
-		$hasPermisstion = c::admin()->permission()->check( [ 'global' ] );
+		$hasPermission = c::admin()->permission()->check( [ 'global' ] );
 
-		if( !$hasPermisstion ){
+		if( !$hasPermission ){
 			switch ( $key ) {
 				case 'support-phone-afterhours':
-					$hasPermisstion = c::admin()->permission()->check( [ 'global', 'support-all', 'support-settings' ] );
+					$hasPermission = c::admin()->permission()->check( [ 'global', 'support-all', 'support-settings' ] );
 					break;
 				case 'referral-inviter-credit-value':
 				case 'referral-invited-credit-value':
@@ -35,12 +35,12 @@ class Controller_api_siteconfig extends Crunchbutton_Controller_RestAccount {
 				case 'referral-limit-per-code':
 				case 'referral-is-enable':
 				case 'referral-invites-limit-per-code':
-					$hasPermisstion = c::admin()->permission()->check( [ 'global', 'invite-promo' ] );
+					$hasPermission = c::admin()->permission()->check( [ 'global', 'invite-promo' ] );
 				break;
 			}
 		}
 
-		if( $hasPermisstion ){
+		if( $hasPermission ){
 			c::config()->site->config($key)->set($value);
 		}
 	}
--- include/library/Crunchbutton/Group.php
+++ include/library/Crunchbutton/Group.php
@@ -1,12 +1,30 @@
 <?php
 
 class Crunchbutton_Group extends Cana_Table {
+
 	public function __construct($id = null) {
 		parent::__construct();
 		$this
 			->table('group')
 			->idVar('id_group')
 			->load($id);
+	}
+
+	public function permissions(){
+		if( !$this->_permissions ){
+			$this->_permissions = c::db()->get( "SELECT * FROM admin_permission WHERE id_group = {$this->id_group}" );	
+		}
+		return $this->_permissions;
+	}
+
+	public function hasPermission( $permission ){
+		$permissions = $this->permissions();
+		foreach( $permissions as $_permission ){
+			if( $_permission->permission == $permission && $_permission->allow == 1 ){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static function find($search = []) {
@@ -28,6 +46,29 @@ class Crunchbutton_Group extends Cana_Table {
 			return Crunchbutton_Admin_Group::q( "SELECT a.* FROM admin a INNER JOIN admin_group ag ON ag.id_admin = a.id_admin AND ag.id_group = {$this->id_group}" );	
 		} 
 		return false;
+	}
+
+	public function removePermissions(){
+		c::db()->query( "DELETE FROM admin_permission WHERE id_group = {$this->id_group}" );
+	}
+
+	public function addPermissions( $permissions ){
+		foreach( $permissions as $key => $val ){
+			if( !$this->hasPermission( $key ) ){
+				$_permission = new Crunchbutton_Admin_Permission();
+				$_permission->id_group = $this->id_group;
+				$_permission->permission = $key;
+				$_permission->allow = 1;
+				$_permission->save();
+				$dependencies = $_permission->getDependency( $key );
+				if( $dependencies ){
+					foreach( $dependencies as $dependency ){
+						$this->addPermissions( array( $dependency => 1 ) );
+					}
+				}
+
+			}
+		}
 	}
 
 	public function usersTotal(){
