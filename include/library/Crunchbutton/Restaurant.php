e06462f2f879eb503a418ed8b6882082e7f7aa86
Daniel Camargo <pererinha@gmail.com>
2013-11-04 17:53:35 -0200

    permission to reps edit support tickets

--- include/library/Crunchbutton/Restaurant.php
+++ include/library/Crunchbutton/Restaurant.php
@@ -1302,6 +1302,34 @@ class Crunchbutton_Restaurant extends Cana_Table_Trackchange {
 	}
 
 
+	public function restaurantsUserHasPermission(){
+		$restaurants_ids = [];
+		$_permissions = new Crunchbutton_Admin_Permission();
+		$all = $_permissions->all();
+		// Get all restaurants permissions
+		$restaurant_permissions = $all[ 'restaurant' ][ 'permissions' ];
+		$permissions = c::admin()->getAllPermissionsName();
+		$restaurants_id = array();
+		foreach ( $permissions as $permission ) {
+			$permission = $permission->permission;
+			$info = $_permissions->getPermissionInfo( $permission );
+			$name = $info[ 'permission' ];
+			foreach( $restaurant_permissions as $restaurant_permission_name => $meta ){
+				if( $restaurant_permission_name == $name ){
+					if( strstr( $name, 'ID' ) ){
+						$regex = str_replace( 'ID' , '((.)*)', $name );
+						$regex = '/' . $regex . '/';
+						preg_match( $regex, $permission, $matches );
+						if( count( $matches ) > 0 ){
+							$restaurants_ids[] = $matches[ 1 ];
+						}
+					}
+				}
+			}
+		}
+		return array_unique( $restaurants_ids );
+	}
+
 	public function save() {
 		if (!$this->timezone) {
 			$this->timezone = 'America/New_York';
