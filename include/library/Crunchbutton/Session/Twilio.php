ec734b003fce3feb9fa9f9a4b8b65013cb675db3
Daniel Camargo <pererinha@gmail.com>
2013-11-05 17:22:02 -0200

    partial #1489 - incoming support texts seem to make 2 tickets instead of one

--- include/library/Crunchbutton/Session/Twilio.php
+++ include/library/Crunchbutton/Session/Twilio.php
@@ -1,10 +1,25 @@
 <?php
 
 class Crunchbutton_Session_Twilio extends Cana_Table {
-	public static function get() {
+	
+	public static function get( $data = false ) {
+
 		$sess = self::q('select * from session_twilio where id_session="'.c::auth()->session()->id_session.'"');
 
-		if (!$sess->id_session_twilio) {
+		// If the session id was lost, let's try to recover it by selecting the last support of the given phone number
+		// the support souldnt be more than 10 days old
+		if (!$sess->id_session_twilio && $data && $data[ 'From' ] && $data[ 'From' ] != '' ) {
+			$phone = str_replace( '+1','', $data['From'] );
+			$sess = self::q( "SELECT st.* FROM session_twilio st
+												INNER JOIN support s ON st.id_session_twilio = s.id_session_twilio 
+												WHERE st.phone = '{$phone}' AND DATEDIFF( NOW(), s.datetime ) <= 10
+												ORDER BY st.id_session_twilio DESC LIMIT 1" );
+			$sess->id_session = c::auth()->session()->id_session;
+			$sess->save();
+		}
+
+		// If we couldnt recovery the session, let's create a new one
+		if ( !$sess->id_session_twilio ) {
 			$sess = new Session_Twilio;
 			$sess->id_session = c::auth()->session()->id_session;
 			$sess->save();
