8c276a8c7b5cf24597de3c9e345cdc2a5fe55d36
pererinha <pererinha@gmail.com>
2013-11-18 16:20:12 -0200

    partial script of #2114 orphaned accounts

--- include/controllers/default/cockpit/test/recoveryUserAuth.php
+++ include/controllers/default/cockpit/test/recoveryUserAuth.php
@@ -9,19 +9,22 @@ class Controller_test_recoveryUserAuth extends Crunchbutton_Controller_Account {
 	public function init() {
 
 		// die( 'remove this die in order to get it working!!!' );
-
 		$auths = User_Auth::q( 'SELECT * FROM user_auth WHERE active = 0 AND auth IS NULL AND type = "local"' );
 		foreach ( $auths as $auth ) {
 			$phone = $auth->email;
 			if( $phone ){
 				$user_auth_facebook = User_Auth::q( 'SELECT * FROM user_auth INNER JOIN user ON user.id_user = user_auth.id_user AND user.phone = "' . $phone . '" WHERE user_auth.type = "facebook"' );
 				if( $user_auth_facebook->id_user ){
-					// $auth->id_user = $user_auth_facebook->id_user;
-					// $auth->save();
+					$auth->id_user = $user_auth_facebook->id_user;
+					$auth->active = 1;
+					$auth->save();
 					$this->users_ok[] = array( 'name' => $user_auth_facebook->name, 'phone' => $phone );
 				} else {
 					$user = User::q( 'SELECT * FROM user WHERE phone = "' . $phone . '" ORDER BY id_user DESC LIMIT 1' );
 					if( $user->id_user ){
+						$auth->id_user = $user->id_user;
+						$auth->active = 1;
+						$auth->save();
 						$this->users_ok[] = array( 'name' => $user->name, 'phone' => $phone );
 					} else {
 						$this->users_no_ok[] = array( 'phone' => $phone );
