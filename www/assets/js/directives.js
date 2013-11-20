a7fe147111606612098d4029795cb9f04f4f484f
Daniel Camargo <pererinha@gmail.com>
2013-11-20 19:37:03 -0200

    partial #1560 - add the directive ng-bind-html-unsafe (ngBindHtmlUnsafe) removed from angular 1.2.x

--- www/assets/js/directives.js
+++ www/assets/js/directives.js
@@ -14,6 +14,15 @@ NGApp.directive('geoComplete', function() {
 	};
 });
 
+NGApp.directive( 'ngBindHtmlUnsafe', [ function() {
+	return function( scope, element, attr ) {
+		element.addClass( 'ng-binding' ).data( '$binding', attr.ngBindHtmlUnsafe );
+		scope.$watch( attr.ngBindHtmlUnsafe, function ngBindHtmlUnsafeWatchAction( value ) {
+			element.html( value || '' );
+		} );
+	}
+} ] );
+
 NGApp.directive('addToCart', function() {
 	return {
 		restrict: 'A',
