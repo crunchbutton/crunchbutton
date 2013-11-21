6eea1934df555e84505c8e7ceeed3ff9ea4d646c
arzynik <github@arzynik.com>
2013-11-20 21:04:50 -0800

    fixes crunchbutton/issues#3

---
+++ include/controllers/default/quick/api/order/index.php
@@ -0,0 +1,57 @@
+<?php
+
+class Controller_api_order extends Crunchbutton_Controller_RestAccount {
+	public function init() {
+
+		$order = Order::uuid(c::getPagePiece(2));
+		if (!$order->id_order) {
+			$order = Order::o(c::getPagePiece(2));
+		}
+		
+		if (!$order->id_order) {
+			echo json_encode(['error' => 'invalid object']);
+			exit;
+
+		} else {
+			if (get_class($order) != 'Crunchbutton_Order') {
+				$order = $order->get(0);
+			}
+		}
+
+		if (1==1 || $this->method() == 'post') {
+			$res = [];
+
+			switch (c::getPagePiece(3)) {
+				case 'delivery-pickedup':
+					$res['status'] = $order->deliveryPickedup(c::admin());
+					break;
+
+				case 'delivery-delivered':
+					$res['status'] = $order->deliveryDelivered(c::admin());
+					break;
+
+				case 'delivery-accept':
+					$res['status'] = $order->deliveryAccept(c::admin());
+					break;
+
+				case 'delivery-reject':
+					$order->deliveryReject(c::admin());
+					$res['status'] = true;
+					break;
+
+				default:
+					echo json_encode(['error' => 'invalid action']);
+					exit;
+					break;
+			}
+		}
+		
+		if ($order->deliveryStatus())
+		$ret = $order->deliveryExports();
+		$ret['status'] = $res['status'];
+		
+		echo json_encode($ret);
+		exit;
+
+	}
+}
