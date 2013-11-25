347d14d006d80c8afd76b2deb0fa2d2bb6308efb
Daniel Camargo <pererinha@gmail.com>
2013-11-25 00:08:21 -0200

    partial #2144 - live hours bug

--- include/controllers/default/cockpit/tests/hours/index.php
+++ include/controllers/default/cockpit/tests/hours/index.php
@@ -3,7 +3,9 @@
 class Controller_tests_hours extends Crunchbutton_Controller_Account {
 	public function init() {
 		$restaurant = new Restaurant($_REQUEST['r'] ? intval($_REQUEST['r']) : 1);
-		$now = (new DateTime('now'))->format('Y-m-d H:i:s');
+		// echo '<pre>';var_dump( $restaurant->timezone );exit();
+		$now = new DateTime( 'now ', new DateTimeZone($restaurant->timezone));
+		$now = $now->format('Y-m-d H:i:s');
 		if (!$_REQUEST['t'] || $_REQUEST['t'] == 'NOW') {
 			$time = $now;
 			c::view()->now = true;
@@ -12,7 +14,8 @@ class Controller_tests_hours extends Crunchbutton_Controller_Account {
 		}
 		c::view()->restaurant = $restaurant;
 		c::view()->open = $restaurant->open($time);
-		c::view()->time = new DateTime($time, new DateTimeZone($restaurant->timezone));
+		c::view()->time = new DateTime( $time, new DateTimeZone( $restaurant->timezone ) );
+		c::view()->time->setTimezone( new DateTimeZone( $restaurant->timezone ) );
 		c::view()->display('tests/hours/index');
 	}
 }
