683a8767445293479b4d9a0774d8f62b70e9707a
Daniel Camargo <pererinha@gmail.com>
2013-11-27 13:05:55 -0200

    partial # 2183 - No Call for 1am Max CB - removed the tests

--- db/migrate/000152_group.sql
+++ db/migrate/000152_group.sql
@@ -4,6 +4,6 @@ ALTER TABLE `group` MODIFY `name` VARCHAR(20);
 
 INSERT INTO `config` (`key`, `value`) VALUES ( 'notification-max-call-support-group-name', 'max-call-support' );
 INSERT INTO `config` (`key`, `value`) VALUES ( 'notification-max-call-recall-after-min', '3' );
-INSERT INTO `config` (`key`, `value`) VALUES ( 'notification-max-call-support-say', "press 1 to confirm you've received this call. otherwise, we will call you back, fucker" );
+INSERT INTO `config` (`key`, `value`) VALUES ( 'notification-max-call-support-say', "press 1 to confirm you've received this call. otherwise, we will call you back fucker ." );
 
 INSERT INTO `group` (`name`, `description`) VALUES ( 'max-call-support', 'Users will receive the max call' );
