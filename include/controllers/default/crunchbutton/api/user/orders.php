18c2282b83ebd1ac967a6dce14609396b20458b9
Daniel Camargo <pererinha@gmail.com>
2013-12-10 15:44:03 -0200

    partial #2153 - click previous orders or legal from help page doesnt work right - debugging

--- include/controllers/default/crunchbutton/api/user/orders.php
+++ include/controllers/default/crunchbutton/api/user/orders.php
@@ -4,11 +4,15 @@ class Controller_api_user_orders extends Crunchbutton_Controller_Rest {
 	public function init() {
 		switch ($this->method()) {
 			case 'get':
+
+
 				$orders = c::user()->orders('compact');
 				if ( method_exists( $orders, 'count' ) && $orders->count() > 0) {
 					echo $orders->json();
+					Log::debug( [ 'id_user' => c::user()->id_user, 'orders' => $orders->count(), 'type' => 'api orders' ]);
 				} else {
 					echo json_encode( [] );
+					Log::debug( [ 'id_user' => c::user()->id_user, 'orders' => 0, 'type' => 'api orders' ]);
 				}
 				break;
 
