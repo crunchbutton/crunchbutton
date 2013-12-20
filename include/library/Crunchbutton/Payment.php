831985388516398990aa75feda4a9be385049684
pererinha <pererinha@gmail.com>
2013-12-19 22:46:33 -0200

    partial #2244 - reviewing the balanced payment

--- include/library/Crunchbutton/Payment.php
+++ include/library/Crunchbutton/Payment.php
@@ -4,7 +4,8 @@ class Crunchbutton_Payment extends Cana_Table {
 	public static function credit($params = null) {
 		$payment = new Payment((object)$params);
 		$payment->date = date('Y-m-d H:i:s');
-		$credit = Crunchbutton_Balanced_Credit::credit($payment->restaurant(), $payment->amount, $payment->note);
+		$payment_type = Crunchbutton_Restaurant_Payment_Type::byRestaurant( $payment->id_restaurant );
+		$credit = Crunchbutton_Balanced_Credit::credit( $payment_type, $payment->amount, $payment->note);
 		$payment->balanced_id = $credit->id;
 		$payment->env = c::getEnv(false);
 		$payment->save();
