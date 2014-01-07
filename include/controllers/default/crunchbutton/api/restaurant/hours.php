8d7cc80f710007c71b56717e2ea5652d04415f6f
Daniel Camargo <pererinha@gmail.com>
2014-01-07 18:39:54 -0200

    partial #2278 - exporting the hours at the restaurant's api

--- include/controllers/default/crunchbutton/api/restaurant/hours.php
+++ include/controllers/default/crunchbutton/api/restaurant/hours.php
@@ -2,6 +2,7 @@
 
 class Controller_api_restaurant_hours extends Crunchbutton_Controller_Rest {
 	public function init() {
+
 		$r = Restaurant::o( c::getPagePiece( 3 ) );
 		if( !$r->id_restaurant ){
 			echo json_encode( [ 'error' => 'invalid object' ] );
