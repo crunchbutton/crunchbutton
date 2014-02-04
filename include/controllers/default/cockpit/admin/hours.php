b4ee8a172123a26a2b407bd4ef20b1c6d7a0f5de
Daniel Camargo <pererinha@gmail.com>
2014-02-04 19:03:21 -0200

    partial #2346 - interface for admin working hours

---
+++ include/controllers/default/cockpit/admin/hours.php
@@ -0,0 +1,103 @@
+<?php
+
+class Controller_admin_hours extends Crunchbutton_Controller_Account {
+	
+	public function init() {
+
+		if (!c::admin()->permission()->check(['global'])) {
+			return ;
+		}
+
+		$reps = Admin::q( 'SELECT * FROM admin ORDER BY name ASC' );
+
+		switch ( c::getPagePiece(2) ) {
+			
+			case 'edit':
+
+				$year = c::getPagePiece(3) ? c::getPagePiece(3) : date( 'Y' );
+				$month = c::getPagePiece(4) ? c::getPagePiece(4) : date( 'm' );
+				$day = c::getPagePiece(5) ? c::getPagePiece(5) : date( 'd' );
+				$id_admin = c::getPagePiece(6);
+
+				if( !$id_admin ){
+					return '';
+				}
+
+				$date = $year . '-' . $month . '-' . $day;
+
+				$hours = Admin_Hour::segmentsByDate( $date, ', ', $id_admin );
+
+				c::view()->year = $year;
+				c::view()->month = $month;
+				c::view()->day = $day;
+				c::view()->segment = $hours[ $id_admin ][ 'hours' ];
+				c::view()->admin = Admin::o( $id_admin );
+				c::view()->layout( 'layout/ajax' );
+				c::view()->display( 'admin/hours/edit' );
+
+				break;
+
+			case 'add':
+
+				$year = c::getPagePiece(3) ? c::getPagePiece(3) : date( 'Y' );
+				$month = c::getPagePiece(4) ? c::getPagePiece(4) : date( 'm' );
+				$day = c::getPagePiece(5) ? c::getPagePiece(5) : date( 'd' );
+				$week = c::getPagePiece(6) ? c::getPagePiece(6) : date( 'W' );
+
+				$days = [];
+				for( $i = 0; $i <= 6; $i++ ){
+					$days[] = new DateTime( date( 'Y-m-d', strtotime( $year . 'W' . $week . $i ) ), new DateTimeZone( 'GMT'  ) );
+				}
+
+				c::view()->year = $year;
+				c::view()->month = $month;
+				c::view()->day = $day;
+				c::view()->days = $days;
+				c::view()->week = $week;
+				c::view()->reps = $reps;
+				c::view()->layout( 'layout/ajax' );
+				c::view()->display( 'admin/hours/add' );
+
+				break;
+			
+			default:
+
+				$year = c::getPagePiece(2) ? c::getPagePiece(2) : date( 'Y' ) ;
+				$week = c::getPagePiece(3) ? c::getPagePiece(3) : date( 'W' ) ;
+
+				if( intval( $week ) < 10 ){
+					$week = '0' . intval( $week );
+				}
+
+				$days = [];
+				for( $i = 0; $i <= 6; $i++ ){
+					$days[] = new DateTime( date( 'Y-m-d', strtotime( $year . 'W' . $week . $i ) ), new DateTimeZone( 'GMT'  ) );
+				}
+
+				if( $week <= 1 ){
+					$weekPrev = ( $year - 1 ) . '/52';
+				} else {
+					$weekPrev = ( $year ) . '/' . ( $week - 1 );
+				}
+				if( $week >= 52 ){
+					$weekNext = ( $year + 1 ) . '/01';
+				} else {
+					$weekNext = ( $year ) . '/' . ( $week + 1 );
+				}
+
+				c::view()->weekPrev = $weekPrev;
+				c::view()->weekNext = $weekNext;
+
+				c::view()->page = 'permissions';
+				c::view()->week = $week;
+				c::view()->year = $year;
+				c::view()->days = $days;
+				c::view()->reps = $reps;
+				c::view()->startDate = $startDate;
+				c::view()->display( 'admin/hours/index' );
+
+				break;
+		}
+
+	}
+}
---
+++ include/controllers/default/cockpit/api/admin.php
@@ -0,0 +1,173 @@
+<?php
+
+class Controller_api_admin extends Crunchbutton_Controller_RestAccount {
+	
+	public function init() {
+		
+		if (!c::admin()->permission()->check(['global'])) {
+			return ;
+		}
+		
+		switch ( c::getPagePiece( 2 ) ) {
+			case 'hours':
+				switch ( c::getPagePiece( 3 ) ) {
+					case 'add':
+						$this->addHours();
+						break;
+					case 'edit':
+						$this->editHours();
+						break;
+					case 'copy':
+						$this->copyHours();
+						echo json_encode( array( 'success' => true ) );
+						break;
+					break;
+					case 'copy-all':
+						$this->copyAllHours();
+						echo json_encode( array( 'success' => true ) );
+						break;
+					case 'remove':
+						$this->removeHours();
+						break;
+					default:
+						echo json_encode( [ 'error' => 'invalid object' ] );
+						break;
+				}
+			break;
+			default:
+				echo json_encode( [ 'error' => 'invalid object' ] );
+			break;
+		}
+	}
+
+	public function copyAllHours(){
+		$week = $this->request()[ 'week' ];
+		$year = $this->request()[ 'year' ];
+		$admins = Admin::q( 'SELECT * FROM admin' );
+		foreach( $admins as $admin ){
+			$this->copyHours( $admin->id_admin, $week, $year );
+		}
+	}
+
+	public function copyHours( $id_admin = false, $week = false, $year = false ){
+
+		$id_admin = ( $id_admin ) ? $id_admin : $this->request()[ 'id_admin' ];
+		$week = ( $week ) ? $week : $this->request()[ 'week' ];
+		$year = ( $year ) ? $year : $this->request()[ 'year' ];
+
+		if( $week < 10 ){
+			$week = '0' . intval( $week );
+		}
+
+		$admin = Admin::o( $id_admin );
+
+		$hoursToBeCopied = [];
+
+		$_days = [];
+		for( $i = 0; $i <= 6; $i++ ){
+			$days = [];
+			$date = new DateTime( date( 'Y-m-d', strtotime( $year . 'W' . $week . $i ) ), new DateTimeZone( 'GMT'  ) );;
+			$days[ 'actual' ] = $date->format( 'Y-m-d' );
+			$date->modify( '-7 days' );
+			$days[ 'prev' ] = $date->format( 'Y-m-d' );
+			$_days[] = $days;
+		}
+
+		foreach ( $_days as $days ) {
+			$actual = $days[ 'actual' ];
+			$prev = $days[ 'prev' ];
+			$segments = Crunchbutton_Admin_Hour::segmentsByDate( $prev, $_join = ', ', $id_admin );
+			Crunchbutton_Admin_Hour::removeByDateIdAdmin( $actual, $id_admin );
+			if( count( $segments ) > 0 ){
+				$segments = $segments[ $id_admin ][ 'hours' ];
+				$segments = explode( ',',  $segments );
+				$date_base = DateTime::createFromFormat( 'Y-m-d H:i:s', $actual . ' 00:00:00', new DateTimeZone( $admin->timezone ) );
+				foreach ( $segments as $segment ) {
+					$_hours = Crunchbutton_Admin_Hour::segmentToDate( $date_base, $segment );
+					if( $_hours ){
+						$hoursToBeCopied[] = [ 'start' => $_hours[ 'start' ], 'end' => $_hours[ 'end' ] ];	
+					}
+				}
+			}
+		}
+
+		foreach( $hoursToBeCopied as $hour ){
+			$admin_hour = new Crunchbutton_Admin_Hour();
+			$admin_hour->id_admin = $id_admin;
+			$admin_hour->id_admin_created = c::admin()->id_admin;
+			$admin_hour->date_start = $hour[ 'start' ];
+			$admin_hour->date_end = $hour[ 'end' ];
+			if( $admin_hour->date_start && $admin_hour->date_end ){
+				$admin_hour->save();	
+			}
+		}
+	}
+
+	public function removeHours(){
+		$id_admin = $this->request()[ 'id_admin' ];
+		$day = $this->request()[ 'day' ];
+		$month = $this->request()[ 'month' ];
+		$year = $this->request()[ 'year' ];
+		$date = $year . '-' . $month . '-' . $day;
+		Crunchbutton_Admin_Hour::removeByDateIdAdmin( $date, $id_admin );
+		echo json_encode( array( 'success' => true ) );
+	}
+
+	public function editHours(){
+		$id_admin = $this->request()[ 'id_admin' ];
+		$day = $this->request()[ 'day' ];
+		$month = $this->request()[ 'month' ];
+		$year = $this->request()[ 'year' ];
+		$date = $year . '-' . $month . '-' . $day;
+		Crunchbutton_Admin_Hour::removeByDateIdAdmin( $date, $id_admin );
+		$this->addHours();
+	}
+
+	public function addHours(){
+		$id_admin = $this->request()[ 'id_admin' ];
+		$day = $this->request()[ 'day' ];
+		$month = $this->request()[ 'month' ];
+		$year = $this->request()[ 'year' ];
+		$week = $this->request()[ 'week' ];
+		$segments = $this->request()[ 'hours' ];
+		$weekdays = $this->request()[ 'weekdays' ] ;
+
+		$admin = Admin::o( $id_admin );
+	
+		$hours = [];
+		$segments = explode( ',',  $segments );
+
+		if( count( $weekdays ) > 0 ){
+			foreach( $weekdays as $weekday ){
+				$date_base = DateTime::createFromFormat( 'Y-m-d H:i:s', $weekday . ' 00:00:00', new DateTimeZone( $admin->timezone ) );
+				foreach ( $segments as $segment ) {
+					$_hours = Crunchbutton_Admin_Hour::segmentToDate( $date_base, $segment );
+					if( $_hours ){
+						$hours[] = [ 'start' => $_hours[ 'start' ], 'end' => $_hours[ 'end' ] ];	
+					}
+				}
+			}
+		} else {
+			// add just the hour for the day
+			$date_base = DateTime::createFromFormat( 'Y-m-d H:i:s', $year . '-' . $month . '-' . $day . ' 00:00:00', new DateTimeZone( $admin->timezone ) );
+			foreach ( $segments as $segment ) {
+				$_hours = Crunchbutton_Admin_Hour::segmentToDate( $date_base, $segment );
+				if( $_hours ){
+					$hours[] = [ 'start' => $_hours[ 'start' ], 'end' => $_hours[ 'end' ] ];	
+				}
+			}
+		}
+
+		foreach( $hours as $hour ){
+			$admin_hour = new Crunchbutton_Admin_Hour();
+			$admin_hour->id_admin = $id_admin;
+			$admin_hour->id_admin_created = c::admin()->id_admin;
+			$admin_hour->date_start = $hour[ 'start' ];
+			$admin_hour->date_end = $hour[ 'end' ];
+			if( $admin_hour->date_start && $admin_hour->date_end ){
+				$admin_hour->save();	
+			}
+		}
+		echo json_encode( array( 'success' => true ) );
+	}
+}
