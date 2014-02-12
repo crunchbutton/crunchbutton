1a5293077790c53a53e1d767e6490c601e9609e2
Daniel Camargo <pererinha@gmail.com>
2014-02-12 21:07:16 -0200

    Issue 2352 - when delivery drivers don't accept an order, here's what should happen

--- include/library/Crunchbutton/Admin/Notification.php
+++ include/library/Crunchbutton/Admin/Notification.php
@@ -122,7 +122,7 @@ class Crunchbutton_Admin_Notification extends Cana_Table {
 		$type_delivery = Crunchbutton_Order::SHIPPING_DELIVERY;
 		$orderFromLast = ' 3 HOUR';
 
-		$query = "SELECT * FROM `order` o WHERE o.delivery_type = '{$type_delivery}' AND o.delivery_service = 1 AND o.date > DATE_SUB(NOW(), INTERVAL {$orderFromLast} ) AND o.date < DATE_SUB(NOW(), INTERVAL 3 MINUTE) ORDER BY o.id_order ASC";
+		$query = "SELECT * FROM `order` o WHERE o.delivery_type = '{$type_delivery}' AND o.delivery_service = 1 AND o.date > DATE_SUB(NOW(), INTERVAL {$orderFromLast} ) AND o.date < DATE_SUB(NOW(), INTERVAL 5 MINUTE) ORDER BY o.id_order ASC";
 
 		$orders = Crunchbutton_Order::q($query);
 
