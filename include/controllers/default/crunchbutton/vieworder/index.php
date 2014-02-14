0f78c9c4ede9dbdddcee3d135218cae51b80f836
Daniel Camargo <pererinha@gmail.com>
2014-02-14 17:22:22 -0200

    partial #2369

--- include/controllers/default/crunchbutton/vieworder/index.php
+++ include/controllers/default/crunchbutton/vieworder/index.php
@@ -3,16 +3,31 @@
 class Controller_vieworder extends Cana_Controller {
 	public function init() {
 
-		$order = Order::uuid(c::getPagePiece(1));
+		$order = Order::uuid( c::getPagePiece( 1 ) );
 		if (!$order->id_order || (!$_SESSION['admin'] && c::user()->id_user != $order->id_user)) {
 			die('invalid order');
 		}
+
+		switch ( c::getPagePiece( 2 ) ) {
+			case 'restaurant':
+				$version = 'restaurant';
+				break;
+			case 'customer':
+				$version = 'customer';
+				break;
+			default:
+				$version = 'cockpit';
+				break;
+		}
+		
+
 		$order = $order->get(0);
 
 		$mail = new Email_Order([
 			'order' => $order,
 			'user' => true,
-			'cockpit' => true
+			'cockpit' => true,
+			'version' => $version
 		]);
 		echo $mail->message();
 		exit;
