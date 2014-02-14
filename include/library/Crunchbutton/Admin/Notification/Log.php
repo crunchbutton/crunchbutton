adaf28cd97249e782e4c81bc7d4b5b18f7f83195
Daniel Camargo <pererinha@gmail.com>
2014-02-14 12:00:12 -0200

    partial #2388

--- include/library/Crunchbutton/Admin/Notification/Log.php
+++ include/library/Crunchbutton/Admin/Notification/Log.php
@@ -8,6 +8,23 @@ class Crunchbutton_Admin_Notification_Log extends Cana_Table {
 		return intval( $result->_items[0]->Total ); 
 	}
 
+	public function byOrder( $id_order ){
+		$query = "SELECT * FROM admin_notification_log a WHERE a.id_order = {$id_order} ORDER BY id_admin_notification_log ASC";
+		return Crunchbutton_Admin_Notification_Log::q( $query );
+	}
+
+	public function restaurant(){
+		return Crunchbutton_Restaurant::q( "SELECT r.* FROM restaurant r INNER JOIN `order` o ON o.id_restaurant = r.id_restaurant  WHERE id_order = {$this->id_order}" );
+	}
+
+	public function date() {
+		if ( ! isset($this->_date ) ) {
+			$this->_date = new DateTime( $this->date, new DateTimeZone( c::config()->timezone ) );
+			$this->_date->setTimezone( new DateTimeZone( $this->restaurant()->timezone ) );
+		}
+		return $this->_date;
+	}
+
 	public function register( $id_order ){
 		$attempts = Crunchbutton_Admin_Notification_Log::attempts( $id_order );
 		$log = new Crunchbutton_Admin_Notification_Log();
