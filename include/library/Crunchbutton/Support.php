797665a0426216abded365ce12d1e36af214918a
Daniel Camargo <pererinha@gmail.com>
2014-02-27 22:46:45 -0300

    cs - fixed the relative date

--- include/library/Crunchbutton/Support.php
+++ include/library/Crunchbutton/Support.php
@@ -373,6 +373,7 @@ class Crunchbutton_Support extends Cana_Table {
 	public function date() {
 		if (!isset($this->_date)) {
 			$this->_date = new DateTime($this->datetime, new DateTimeZone( 'UTC' ));
+			$this->_date->setTimezone( new DateTimeZone( c::config()->timezone ) );
 		}
 		return $this->_date;
 	}
