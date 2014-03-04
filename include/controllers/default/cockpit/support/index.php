8739942059c459b0db83b40c029005e85a0692af
Daniel Camargo <pererinha@gmail.com>
2014-03-04 14:17:59 -0300

    support button on dashboard should acuratly reflect open tickets #2453

--- include/controllers/default/cockpit/support/index.php
+++ include/controllers/default/cockpit/support/index.php
@@ -2,7 +2,10 @@
 
 class Controller_support extends Crunchbutton_Controller_Account {
 	public function init() {
-		header( 'Location: /support/plus/' );
+
+		$id_support = c::getPagePiece(1);
+		header( 'Location: /support/plus/' . $id_support );
+		
 		exit();
 	}
 }
