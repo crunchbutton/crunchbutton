4c59932701073f622c33eb8e32072662dc5f4c01
Daniel Camargo <pererinha@gmail.com>
2014-03-06 13:21:22 -0300

    delivery driver order list #2431

--- include/controllers/default/quick/home/index.php
+++ include/controllers/default/quick/home/index.php
@@ -23,8 +23,33 @@ class Controller_home extends Crunchbutton_Controller_Account {
 
 	public function showList(){
 
-		c::view()->orders = Order::deliveryOrders();
+		$orders = Order::deliveryOrders();
 
+		c::view()->justMineOrders = c::db()->escape( c::getPagePiece( 0 ) ) == 'mine';
+
+		$list = [];
+		foreach ( $orders as $order ) {
+			$order =  (object) array(
+										'id_order' => $order->id_order,
+										'lastStatus' => $order->deliveryLastStatus(),
+										'name' => $order->name,
+										'phone' => $order->phone,
+										'date' => $order->date(),
+										'restaurant' => $order->restaurant()->name,
+										);
+			if( !c::view()->justMineOrders || ( c::view()->justMineOrders && $order->lastStatus[ 'id_admin' ] == c::admin()->id_admin ) ){
+				$list[] = $order;
+			}
+		}
+
+		usort( $list, function( $a, $b ){
+			if( $a->lastStatus[ 'status' ] == $b->lastStatus[ 'status' ] ){
+				return $a->id_order < $b->id_order;
+			}
+			return ( $a->lastStatus[ 'order' ] > $b->lastStatus[ 'order' ] );
+		} );
+
+		c::view()->orders = $list;
 		c::view()->display( 'home/index' );
 	}
 
