dc8e2a2eb1a86d52d3e15f3ac3f1015f61d4633a
Daniel Camargo <pererinha@gmail.com>
2014-03-14 15:34:50 -0300

    Add orders per day, per community to community metrics #2647

--- include/controllers/default/cockpit/charts/community.php
+++ include/controllers/default/cockpit/charts/community.php
@@ -7,28 +7,47 @@ class Controller_charts_community extends Crunchbutton_Controller_Account {
 			return ;
 		}
 
-		$hasPermissionFullPermission = c::admin()->permission()->check( [ 'global', 'metrics-all', 'metrics-communities-all' ] );
-
-		$communities = Restaurant::getCommunitiesWithRestaurantsNumber();
-
-		if( !$hasPermissionFullPermission ){
-			$_communities = [];
-			foreach ( $communities as $community ) {
-				$permission_name = strtolower( $community->community );
-				$permission_name = str_replace( ' ' , '-', $permission_name );
-				$permission_name = "metrics-communities-{$permission_name}";
-				if( c::admin()->permission()->check( [ $permission_name ] ) ){
-					$_communities[] = $community;
+		switch ( c::getPagePiece( 2 ) ) {
+			case 'delivery-tips':
+				$this->data();
+				c::view()->display( 'charts/community/delivery_tips' );
+				break;
+			
+			case 'orders-per-day':
+				$hasPermissionFullPermission = c::admin()->permission()->check( [ 'global', 'metrics-all', 'metrics-communities-all' ] );
+				$communities = Restaurant::q( "SELECT DISTINCT( community ) FROM restaurant r INNER JOIN `order` o ON r.id_restaurant = o.id_restaurant WHERE o.date BETWEEN CURDATE() - INTERVAL 14 DAY AND CURDATE()" );
+				$_communities = [];
+				foreach ( $communities as $community ) {
+					$permission_name = strtolower( $community->community );
+					$permission_name = str_replace( ' ' , '-', $permission_name );
+					$permission_name = "metrics-communities-{$permission_name}";
+					if( $hasPermissionFullPermission || c::admin()->permission()->check( [ $permission_name ] ) ){
+						$_communities[] = $community;
+					}
 				}
+				c::view()->communities = $communities;
+				c::view()->display( 'charts/community/orders_per_day' );
+				break;
+
+			default:
+				$this->data();
+				c::view()->display( 'charts/community/index' );
+				break;
+		}
+	}
+
+	public function data(){
+		$hasPermissionFullPermission = c::admin()->permission()->check( [ 'global', 'metrics-all', 'metrics-communities-all' ] );
+		$communities = Restaurant::getCommunitiesWithRestaurantsNumber();
+		$_communities = [];
+		foreach ( $communities as $community ) {
+			$permission_name = strtolower( $community->community );
+			$permission_name = str_replace( ' ' , '-', $permission_name );
+			$permission_name = "metrics-communities-{$permission_name}";
+			if( $hasPermissionFullPermission || c::admin()->permission()->check( [ $permission_name ] ) ){
+				$_communities[] = $community;
 			}
-			$communities = $_communities;
 		}
 		c::view()->communities = $communities;
-
-		if( c::getPagePiece( 2 ) == 'delivery-tips' ){
-			c::view()->display( 'charts/community/delivery_tips' );
-		} else {
-			c::view()->display( 'charts/community/index' );
-		}
 	}
 }
