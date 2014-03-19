c8488cddd13337cc66d929a49004c07aaea49ee4
Daniel Camargo <pererinha@gmail.com>
2014-03-19 13:43:07 -0300

    partial #2494 - shift x driver - assignment

--- db/migrate/000176_shift_stuff.sql
+++ db/migrate/000176_shift_stuff.sql
@@ -20,6 +20,18 @@ CREATE TABLE `admin_shift_preference` (
   CONSTRAINT `admin_shift_preference_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+CREATE TABLE `admin_shift_assign` (
+  `id_admin_shift_assign` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_community_shift` int(11) unsigned DEFAULT NULL,
+  `id_admin` int(11) unsigned DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_admin_shift_assign`),
+  KEY `admin_shift_assign_ibfk_1` (`id_community_shift`),
+  KEY `admin_shift_assign_ibfk_2` (`id_admin`),
+  CONSTRAINT `admin_shift_assign_ibfk_1` FOREIGN KEY (`id_community_shift`) REFERENCES `community_shift` (`id_community_shift`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `admin_shift_assign_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
 CREATE TABLE `admin_shift_status` (
   `id_admin_shift_status` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `id_admin` int(11) unsigned DEFAULT NULL,
