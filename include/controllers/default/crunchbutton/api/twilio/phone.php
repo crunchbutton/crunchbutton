07b0d0a0e88ab4c13933f657fe202b4e974c3c7f
arzynik <github@arzynik.com>
2014-03-26 16:48:19 -0700

    moved around outgoing calls api

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -5,7 +5,7 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 	    header('Content-type: text/xml');
 		echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
 			.'<Response>'
-			.'<Dial ',($_REQUEST['phone'] ? '' : 'timeout="10"').' record="false">'.($_REQUEST['phone'] ? $_REQUEST['phone'] : '800-351-4161').'</Dial>'
+			.'<Dial timeout="10" record="false">800-351-4161</Dial>'
 			.'</Response>';
 			
 		exit;
