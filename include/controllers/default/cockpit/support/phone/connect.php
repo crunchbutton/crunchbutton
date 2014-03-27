c702093d9785e6c35b60c8e44b67052c733de4e4
arzynik <github@arzynik.com>
2014-03-26 18:00:20 -0700

    added connect url

---
+++ include/controllers/default/cockpit/support/phone/connect.php
@@ -0,0 +1,46 @@
+<?php
+
+class Controller_Support_Phone_Connect extends Crunchbutton_Controller_Account {
+
+	public function init() {
+	
+
+		switch (c::getPagePiece(3)) {
+			case 'restaurant':
+				$callerId = c::config()->twilio->live->outgoingRestaurant;
+				break;
+			case 'driver':
+				$callerId = c::config()->twilio->live->outgoingDriver;
+				break;
+			case 'customer':
+			default:
+				$callerId = c::config()->twilio->live->outgoingcustomer;
+				break;
+		}
+		
+		
+		$num = '_PHONE_';
+		$_REQUEST['phone'] = '_PHONE_';
+		$host = 'dev.crunchr.co';
+		//c::config()->host_callback
+
+		$twilio = new Services_Twilio(c::config()->twilio->live->sid, c::config()->twilio->live->token);
+		$call = $twilio->account->calls->create(
+			$callerId,
+			'+1'.$num,
+			'http://'.$host.'/api/twilio/outgoing/'.c::getPagePiece(3).'?PhoneNumber='.$_REQUEST['phone']
+		);
+
+		die('pick up your phone...');
+
+
+		exit;
+		
+		
+		
+		
+		
+
+	}
+}
+
