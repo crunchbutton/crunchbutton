de192eed671daae65b59cadacccec770a46882f0
Daniel Camargo <pererinha@gmail.com>
2014-03-28 19:01:21 -0300

    admin hours timezone problem

--- include/library/Crunchbutton/Admin/Hour.php
+++ include/library/Crunchbutton/Admin/Hour.php
@@ -86,7 +86,11 @@ class Crunchbutton_Admin_Hour extends Cana_Table {
 
 	public function date_start(){
 		if ( !isset( $this->_date_start ) ) {
-			$this->_date_start = DateTime::createFromFormat( 'Y-m-d H:i:s', $this->date_start, new DateTimeZone( $this->admin()->timezone ) );
+			if( Crunchbutton_Admin_Hour::validateTimezone( $this->admin()->timezone ) ){
+				$this->_date_end = DateTime::createFromFormat( 'Y-m-d H:i:s', $this->date_end, new DateTimeZone( $this->admin()->timezone ) );
+			} else {
+				echo '<pre>';var_dump( $this->admin() );exit();
+			}
 		}
 		return $this->_date_start;
 	}
