d5cf496049017b93f13caaa89b7cf0945ebf5682
arzynik <github@arzynik.com>
2014-03-31 17:40:26 -0700

    fixes #2786

--- include/controllers/default/crunchbutton/printorder/index.php
+++ include/controllers/default/crunchbutton/printorder/index.php
@@ -4,8 +4,9 @@ class Controller_printorder extends Cana_Controller {
 	public function init() {
 
 		$order = Order::uuid(c::getPagePiece(1));
-		if (!$order->id_order || (!$_SESSION['admin'] && c::user()->id_user != $order->id_user)) {
-			die('invalid order');
+		if (!$order->id_order) {
+			header('HTTP/1.0 404 Not Found');
+			exit;
 		}
 		$order = $order->get(0);
 		$order->_print = true;
