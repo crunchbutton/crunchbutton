9c0549768a91bcd748ed2a64e4b43dc70d9f613f
arzynik <github@arzynik.com>
2014-04-05 13:31:06 -0700

    basic tests

--- travis/Tests/bootstrap.php
+++ travis/Tests/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
 
+error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
+ini_set('display_errors',true);
+
+if (trim(`whoami`) == 'arzynik') {
+	ini_set('mysqli.default_socket','/Applications/MAMP/tmp/mysql/mysql.sock');
+}
+
 require_once('include/crunchbutton.php');
