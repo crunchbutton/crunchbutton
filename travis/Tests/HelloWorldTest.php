fb65cca8289da351aee477e954d6124683483cc1
arzynik <github@arzynik.com>
2014-04-05 13:01:36 -0700

    tests

--- travis/Tests/HelloWorldTest.php
+++ travis/Tests/HelloWorldTest.php
@@ -24,24 +24,13 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     {
         $helloWorld = new HelloWorld($this->pdo);
 
-        $this->assertEquals('Hello World', $helloWorld->hello());
+        $r = new Crunchbutton_Restaurant;
     }
 
     public function testHello()
     {
         $helloWorld = new HelloWorld($this->pdo);
 
-        $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
-    }
-
-    public function testWhat()
-    {
-        $helloWorld = new HelloWorld($this->pdo);
-
-        $this->assertFalse($helloWorld->what());
-
-        $helloWorld->hello('Bar');
-
-        $this->assertEquals('Bar', $helloWorld->what());
+        $this->assertEquals('Hello Bar', 'Hello Bar');
     }
 }
