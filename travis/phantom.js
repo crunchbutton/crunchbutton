51c1206f173e54223fdd126da0f96002c7a9976e
arzynik <github@arzynik.com>
2014-04-07 16:34:54 -0700

    phantom tests

--- travis/phantom.js
+++ travis/phantom.js
@@ -1,8 +1,12 @@
 var page = require('webpage').create();
-page.open('http://localhost', function(status) {
+page.open('http://localhost/robots.txt', function(status) {
+	/*
 	var title = page.evaluate(function() {
 		return document.body.innerText;
 	});
-	console.log('>> BODY ' + title);
+	console.log('>> BODY >> ' + title);
+	*/
+
+	console.log('>> CONTENT >> ' + page.content);
 	phantom.exit();
 });
