155a1b56e12ae151eccc7d987d431469183e14a8
arzynik <github@arzynik.com>
2014-04-16 13:22:38 -0700

    added serve and removed cache for non bundle requests

--- include/library/Crunchbutton/Scss.php
+++ include/library/Crunchbutton/Scss.php
@@ -17,4 +17,22 @@ class Crunchbutton_Scss extends Cana_Model {
 		
 		return $data;
 	}
+	
+	public static function serve($file) {
+
+		$data = self::compile($file);
+		$mtime = filemtime($file);
+
+		header('HTTP/1.1 200 OK');
+		header('Date: '.date('r'));
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s',$mtime).' GMT');
+		header('Accept-Ranges: bytes');
+		header('Content-Length: '.strlen($data));
+		header('Content-type: text/css');
+		header('Vary: Accept-Encoding');
+		header('Cache-Control: max-age=290304000, public');
+
+		echo $data;
+		exit;
+	}
 }
