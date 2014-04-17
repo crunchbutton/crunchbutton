269d78f0117891c6620420c687cce7bcf25dc46e
arzynik <github@arzynik.com>
2014-04-16 18:34:29 -0700

    added dynamic forwarding

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -2,10 +2,11 @@
 
 class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 	public function init() {
+		$_REQUEST['forward'] = preg_replace('/[^0-9]/','',$_REQUEST['forward']);
 	    header('Content-type: text/xml');
 		echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
 			.'<Response>'
-			.'<Dial timeout="10" record="false">310-935-1519</Dial>'
+			.'<Dial timeout="10" record="false">'.($_REQUEST['forward'] ? $_REQUEST['forward'] : '800-351-4161').'</Dial>'
 			.'</Response>';
 			
 		exit;
