3c512a191dd9e535b4ac2c0b1b033357fd712038
arzynik <github@arzynik.com>
2014-04-16 18:50:37 -0700

    added part of cockpit 2 copied from frontend app

---
+++ www/assets/cockpit/js/service.navigation.js
@@ -0,0 +1,57 @@
+// MainHeaderService service
+NGApp.factory( 'MainNavigationService', function( $http, $location, $rootScope, $route){ 
+	
+	// This variable will store the animation type.
+	$rootScope.animationClass = '';
+
+	var service = {
+		page : '',
+		buttons : {
+			location: false,
+			menu: true
+		}
+	};
+	
+	service.menu = App.toggleMenu;
+
+	service.getFood = function( cartItems ){
+		if( service.page == 'restaurant' &&
+				cartItems &&
+				service.restaurant && service.restaurant && service.restaurant._open &&
+				!App.rootScope.notificationBarStatus ){
+			angular.element( '.button-bottom-wrapper' ).addClass( 'button-bottom-show' );	
+		} else {
+			angular.element( '.button-bottom-wrapper' ).removeClass( 'button-bottom-show' );
+		}
+	}
+
+	service.home = function() {
+		if (App.isPhoneGap && cordova) {
+			$('html, body, .snap-content-inner').animate({scrollTop: 0}, 200, $.easing.easeInOutQuart ? 'easeInOutQuart' : null);
+		} else if (App.isMobile()) {
+			service.menu();
+		} else {
+			service.link();
+		}
+	}
+
+	service.goToRestaurants = function(){
+		return service.link( RestaurantsService.permalink );
+	}
+
+	/* the transitions type could be push, fade, pop or none */
+	service.link = function( path, transition ){
+		if( App.isNarrowScreen() || App.transitionForDesktop ){
+			App.rootScope.animationClass = transition ? 'animation-' + transition : '';
+		}
+		$location.path( path || '/' );
+		App.snap.close();
+	}
+
+	service.signin = function(){
+		service.modal.signinOpen();
+	}
+
+	return service;
+
+});
