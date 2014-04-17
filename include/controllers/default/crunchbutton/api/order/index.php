ebdfe3abb779ee4729a98bf218ee38ddb63490e1
Daniel Camargo <pererinha@gmail.com>
2014-04-17 16:21:26 -0300

    stealth call - text changes

--- include/controllers/default/crunchbutton/api/order/index.php
+++ include/controllers/default/crunchbutton/api/order/index.php
@@ -483,7 +483,7 @@ class Controller_api_order extends Crunchbutton_Controller_Rest {
 					default:
 						echo '<Gather action="/api/order/'.$order->id_order.'/doconfirmstealth" numDigits="1" timeout="10" finishOnKey="#" method="get">'
 							. '<Pause length="1" />'
-							.'<Say voice="'.c::config()->twilio->voice.'" loop="3">You have received a takeout order from a student through the fax machine. Please check the fax machine and press 1 to confirm that you received the order. Or press 2 and we will give you a call.. . . .</Say>'
+							.'<Say voice="'.c::config()->twilio->voice.'" loop="3">You have received a takeout order from a student through the fax machine. Please check the fax machine and press 1 to confirm that you received the order. Or press 2 and they will give you a call.. . . .</Say>'
 							.'</Gather>';
 						break;
 				}
