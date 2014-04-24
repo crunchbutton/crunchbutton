b0af697f52e5cab915b23518627345d26d1750f1
arzynik <github@arzynik.com>
2014-04-23 17:51:44 -0700

    partial unorganized cockpit 3.0 stuff

---
+++ include/controllers/default/cockpit2/api/orders/index.php
@@ -0,0 +1,7 @@
+<?php
+
+class Controller_api_orders extends Crunchbutton_Controller_RestAccount {
+	public function init() {
+		// return a list of orders based on params. show none with no params
+	}
+}
--- include/library/Crunchbutton/Controller/Account.php
+++ include/library/Crunchbutton/Controller/Account.php
@@ -2,12 +2,21 @@
 
 class Crunchbutton_Controller_Account extends Cana_Controller {
     public function __construct() {
-		if (!c::admin()->id_admin) {
-		    header('WWW-Authenticate: Basic realm="Crunchbutton - '.$_SERVER['PHP_AUTH_USER'].'"');
-		    header('HTTP/1.0 401 Unauthorized');
-		    die('unauth.');
-		    exit;
+    	if (c::config()->site->name == 'Cockpit2') {
+    		if (!c::admin()->id_admin) {
+    			// just prevents any other pages from being displayed since this is a blank page basicly
+    			c::view()->display('home/login');
+    			exit;
+    		}
+	    	
+    	} else {
+			if (!c::admin()->id_admin) {
+			    header('WWW-Authenticate: Basic realm="Crunchbutton - '.$_SERVER['PHP_AUTH_USER'].'"');
+			    header('HTTP/1.0 401 Unauthorized');
+			    die('unauth.');
+			    exit;
+			}
+			c::view()->layout('layout/core');
 		}
-		c::view()->layout('layout/core');
     }
 }
