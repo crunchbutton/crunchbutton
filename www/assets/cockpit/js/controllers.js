688b099f53e2c24aedceb34f67f77d024fd40e02
arzynik <github@arzynik.com>
2014-04-25 12:08:40 -0700

    partial #2648

--- www/assets/cockpit/js/controllers.js
+++ www/assets/cockpit/js/controllers.js
@@ -9,3 +9,7 @@ NGApp.controller( 'MainHeaderCtrl', function ( $scope, $rootScope) {
 NGApp.controller( 'SideMenuCtrl', function () {
 
 });
+
+NGApp.controller('LoginCtrl', function($scope, AccountService) {
+	$scope.account = AccountService;
+});
--- www/assets/cockpit/js/service.account.js
+++ www/assets/cockpit/js/service.account.js
@@ -1,3 +1,15 @@
 NGApp.factory('AccountService', function($http, $rootScope) {
+	var service = {};
+	
+	service.checkUser = function() {
+		if (App.config.user.id_admin) {
+			service.loggedin = true;
+		}
+	};
+	
+
+
+	
+	return service;
 
 });
