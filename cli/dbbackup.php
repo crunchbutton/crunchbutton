3c01ee81da82c4b3d1a838ea3f15da7f27ac6e17
arzynik <github@arzynik.com>
2014-04-25 23:43:07 -0700

    added backup script

---
+++ cli/dbbackup.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+//local
+
+$file = '../db/backup.sql';
+
+error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
+ini_set('display_errors',true);
+
+if (trim(`whoami`) == 'arzynik') {
+	ini_set('mysqli.default_socket','/Applications/MAMP/tmp/mysql/mysql.sock');
+	$dump = '/Applications/MAMP/Library/bin/mysqldump';
+} else {
+	$dump = 'mysqldump';
+}
+
+require_once('../include/crunchbutton.php');
+
+$connect = c::config()->db->{c::app()->env()};
+
+$cmd[] = 'rm '.$file;
+$cmd[] = $dump.' -q -u '.$connect->user.' -p'.$connect->pass.' '.$connect->db.' > '.$file;
+$cmd[] = 'sed  "s/\`devin\`@\`%\`/\`root\`@\`localhost\`/g" '.$file.' > '.$file.'tmp';
+$cmd[] = 'mv '.$file.'tmp '.$file;
+
+foreach ($cmd as $c) {
+	echo $c;
+	exec($c);
+}
+
