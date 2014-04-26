ee95dde859e0aad9a525e4a5e6369a471c41878d
arzynik <github@arzynik.com>
2014-04-26 16:02:17 -0700

    fixed excape

--- include/library/Crunchbutton/Admin/Auth.php
+++ include/library/Crunchbutton/Admin/Auth.php
@@ -8,17 +8,14 @@ class Crunchbutton_Admin_Auth extends Cana_Model {
 
 	public static function localLogin($email, $password) {
 		$password = self::passwordEncrypt($password);
-		$query = sprintf('
-			SELECT * 
-			FROM admin
-			WHERE
-				login="%s"
-				AND pass="%s"
-				AND active=1
-				LIMIT 1',
-			@mysql_real_escape_string($email),
-			@mysql_real_escape_string($password)
-		);
+		$query = '
+                        SELECT *
+                        FROM admin
+                        WHERE
+                             	login="'.c::db()->escape($email).'"
+                                AND pass="'.c::db()->escape($password).'"
+                                AND active=1
+                                LIMIT 1';
 
 		return Admin::q($query)->get(0);
 
