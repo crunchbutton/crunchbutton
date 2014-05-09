317e5504dc8b366396cdb9f4f046604e7a8188f6
arzynik <github@arzynik.com>
2014-05-09 11:18:57 -0700

    updated some balanced tests

--- travis/Tests/BalancedTest.php
+++ travis/Tests/BalancedTest.php
@@ -33,13 +33,45 @@ class BalancedTest extends PHPUnit_Framework_TestCase {
 
 		$card = [
 			'id' => $card->id,
-			'lastfour' => $card->last_four,
+			'lastfour' => '1111',
 			'uri' => $card->uri,
 			'card_type' => $card->card_type,
 			'month' => $card->expiration_month,
 			'year' => $card->expiration_year
 		];
 
+		$r = $charge->charge([
+			'amount' => '1.25',
+			'card' => $card,
+			'name' => $this->user->name,
+			'address' => '123 UNIT TEST',
+			'phone' => '234-567-8901',
+			'user' => $this->user,
+			'restaurant' => $this->restaurant
+		]);
+
+		$this->assertTrue($r['status'] ? true : $r['errors'][0]);
+	}
+	
+	public function testRefund() {
+		
+	}
+	
+	public function testDeposit() {
+		
+	}
+	
+	public function testChargeNewCard() {
+
+	}
+	
+	public function testChargeOldCard() {
+		$paymentType = $this->user->payment_type();
+		$charge = new Charge_Balanced([
+			'customer_id' => $this->user->balanced_id
+			'card_id' => $paymentType->balanced_id
+		]);
+		
 		$r = $charge->charge([
 			'amount' => '1.25',
 			'card' => $card,
