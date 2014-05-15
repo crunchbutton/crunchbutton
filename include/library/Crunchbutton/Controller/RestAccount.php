886ac18c4cedb25c9e7ec14a84130306c112cd60
arzynik <github@arzynik.com>
2014-05-15 10:03:27 -0700

    fixed restaccount controller

--- include/library/Crunchbutton/Controller/RestAccount.php
+++ include/library/Crunchbutton/Controller/RestAccount.php
@@ -2,12 +2,21 @@
 
 class Crunchbutton_Controller_RestAccount extends Crunchbutton_Controller_Rest {
     public function __construct() {
-		if (!c::admin()->id_admin) {
-		    header('WWW-Authenticate: Basic realm="Crunchbutton - '.$_SERVER['PHP_AUTH_USER'].'"');
-		    header('HTTP/1.0 401 Unauthorized');
-		    die('unauth.');
-		    exit;
+    	if (c::config()->site->name == 'Cockpit2') {
+    		if (!c::admin()->id_admin) {
+    			// just prevents any other pages from being displayed since this is a blank page basicly
+    			header('HTTP/1.1 401 Unauthorized');
+    			exit;
+    		}
+	    	
+    	} else {
+			if (!c::admin()->id_admin) {
+			    header('WWW-Authenticate: Basic realm="Crunchbutton - '.$_SERVER['PHP_AUTH_USER'].'"');
+			    header('HTTP/1.0 401 Unauthorized');
+			    die('unauth.');
+			}
 		}
+		
 		parent::__construct();
     }
 }
