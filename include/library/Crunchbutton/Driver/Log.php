3cfea1b793c430de76212a7691cfe45e22415825
Daniel Camargo <pererinha@gmail.com>
2014-05-19 08:20:08 -0300

    partial #2969

--- include/library/Crunchbutton/Driver/Log.php
+++ include/library/Crunchbutton/Driver/Log.php
@@ -10,6 +10,16 @@ class Crunchbutton_Driver_Log extends Cana_Table {
 			->load($id);
 	}
 
+	public function byDriver( $id_admin ){
+		if( $id_admin ){
+			$logs = Crunchbutton_Driver_Log::q( 'SELECT * FROM driver_log WHERE id_admin = "' . $id_admin . '"' );
+			
+		} else {
+			return [];
+		}
+		
+	}
+
 	public function exports(){
 		return $this->properties();
 	}
