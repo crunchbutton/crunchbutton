bd24b33d590fcc14b64b3cc7a5eab81235cf312c
Daniel Camargo <pererinha@gmail.com>
2014-05-20 21:20:55 -0300

    partial #2969 - page list

--- include/controllers/default/cockpit2/api/driver/list/index.php
+++ include/controllers/default/cockpit2/api/driver/list/index.php
@@ -24,16 +24,23 @@ class Controller_api_driver_list extends Crunchbutton_Controller_RestAccount {
 		$end = $start + $resultsPerPage;
 		$count = 1;
 
+		$docs = Cockpit_Driver_Document::all();
+
 		$list = [];
 		foreach( $drivers as $driver ){
 			if( $count >= $start && $count < $end ){
 				$data = $driver->exports( [ 'permissions', 'groups' ] );
-				$actions = Cockpit_Driver_Log::lastAction( $driver->id_admin );
-				if( $actions ){
-					$data[ 'actions' ] = [ 'last' => $actions, 'next' => Cockpit_Driver_Log::nextAction( $driver->id_admin ) ];
-				} else {
-					$data[ 'actions' ] = null;
+				$sentAllDocs = true;
+				foreach( $docs as $doc ){
+					if( $doc->required ){
+						$docStatus = Cockpit_Driver_Document_Status::document( $driver->id_admin, $doc->id_driver_document );	
+						if( !$docStatus->id_driver_document_status ){
+							$sentAllDocs = false;
+						}	
+					}
 				}
+				$data[ 'sent_all_docs' ] = $sentAllDocs;
+				
 				$list[] = $data;
 			}
 			$count++;
