a43f94d79410b2e6df8261628350d46dc1755e89
Daniel Camargo <pererinha@gmail.com>
2014-05-25 19:18:25 -0300

    partial #2816 - cron jobs

--- db/migrate/000192_cron_log.sql
+++ db/migrate/000192_cron_log.sql
@@ -3,10 +3,12 @@ CREATE TABLE `cron_log` (
   `description` varchar(40) DEFAULT NULL,
   `command` varchar(200) DEFAULT NULL,
   `start_date` datetime DEFAULT NULL,
-	`interval` enum('minute', 'hour', 'day', 'week', 'month', 'year') DEFAULT 'day',
-  `interval_unity` TINYINT( 2 ) NOT NULL DEFAULT '1',
-  `current_status` enum('idle', 'running') DEFAULT 'idle',
+  `interval` enum('minute','hour','day','week') DEFAULT 'day',
+  `interval_unity` tinyint(2) NOT NULL DEFAULT '1',
+  `current_status` enum('idle','running') DEFAULT 'idle',
+  `next_time` datetime DEFAULT NULL,
   `last_time` datetime DEFAULT NULL,
-  `last_time_status` enum('error', 'success') DEFAULT NULL,
+  `last_time_status` enum('error','success') DEFAULT NULL,
+  `interactions` int(11) DEFAULT '0',
   PRIMARY KEY (`id_cron_log`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
