29ef1ada2bb120204d24ddc5b9f1f2d0529d39e9
Daniel Camargo <pererinha@gmail.com>
2014-05-26 11:53:05 -0300

    partial #2816 - cron

---
+++ include/library/Crunchbutton/Cron/Job/RenotifyDrivers.php
@@ -0,0 +1,13 @@
+<?php
+
+class Crunchbutton_Cron_Job_RenotifyDrivers extends Crunchbutton_Cron_Log {
+
+	public function run(){
+
+		$notification = new Crunchbutton_Admin_Notification();
+		$notification->resendNotification();
+
+		// it always must call finished method at the end
+		$this->finished();
+	}
+}
