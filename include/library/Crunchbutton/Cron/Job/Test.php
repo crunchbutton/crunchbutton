5072d4d8435b479f230fcd4ce861e1f95ef118dd
Daniel Camargo <pererinha@gmail.com>
2014-05-27 20:08:51 -0300

    partial #2816 - cron

--- include/library/Crunchbutton/Cron/Job/Test.php
+++ include/library/Crunchbutton/Cron/Job/Test.php
@@ -6,6 +6,8 @@ class Crunchbutton_Cron_Job_Test extends Crunchbutton_Cron_Log {
 
 		Log::debug( [ 'desc' => 'testing the cron log', 'type' => 'cron-jobs' ] );	
 		
+		echo 'run!';
+
 		// it always must call finished method at the end
 		$this->finished();
 	}
