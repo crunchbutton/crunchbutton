2ac32b96cf5fa07b0ae59b7363f2c69e2d41cbd4
Daniel Camargo <pererinha@gmail.com>
2014-05-28 16:27:55 -0300

    testing timeout at cockpit.la

--- include/controllers/default/cockpit/test/cron.php
+++ include/controllers/default/cockpit/test/cron.php
@@ -2,6 +2,18 @@
 class Controller_test_cron extends Crunchbutton_Controller_Account {
 	public function init() {
 		echo '<pre>';
-		Crunchbutton_Cron_Log::start();
+		echo 'outside';
+
+		$log = new Crunchbutton_Test();
+
+		var_dump(exec('ls'));
+
+		$test = $log;
+		
+		// Timeout to run it asyncs
+			Cana::timeout( function() use( $test ) {
+				$test->register( date( 'Y-m-d H:i:s' ) );	
+			} );
+		// Crunchbutton_Cron_Log::start();
 	}
 }
