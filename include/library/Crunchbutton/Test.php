29ed8cfeb8d2a4a5cf8c83659e2f84455949ea50
Daniel Camargo <pererinha@gmail.com>
2014-05-28 17:16:45 -0300

    changed cron to run async

--- include/library/Crunchbutton/Test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Crunchbutton_Test extends Cana_Table {
-	public function register( $info ){
-		Log::debug( [ 'action' => 'Crunchbutton_Test:: ' . $info, 'type' => 'timeout' ] );
-	}
-}
