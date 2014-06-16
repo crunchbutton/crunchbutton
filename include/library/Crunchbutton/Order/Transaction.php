c212c8c62b33311179a7a2fccbf137040f0cd52a
Daniel Camargo <pererinha@gmail.com>
2014-06-16 10:24:53 -0300

    partial settlement #2250
    - payment schedule
    - option to reimburse cash orders

--- include/library/Crunchbutton/Order/Transaction.php
+++ include/library/Crunchbutton/Order/Transaction.php
@@ -2,6 +2,15 @@
 
 class Crunchbutton_Order_Transaction extends Cana_Table {
 
+	const TYPE_DEBIT = 'debit';
+	const TYPE_CREDIT = 'credit';
+	const TYPE_PAID_TO_RESTAURANT = 'paid-to-restaurant';
+	const TYPE_PAID_TO_DRIVER = 'paid-to-driver';
+	const PAYMENT_TYPE_GIFT = 'gift';
+	const PAYMENT_TYPE_CARD = 'card';
+	const SOURCE_CRUNCHBUTTON = 'crunchbutton';
+	const SOURCE_RESTAURANT = 'restaurant';
+
 	public function __construct($id = null) {
 		parent::__construct();
 		$this
