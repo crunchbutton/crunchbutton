202186f1022c126cf76bb55fd3f5be016f20e125
Daniel Camargo <pererinha@gmail.com>
2014-07-29 20:42:53 -0300

    rewards: retroactivly add points to users #3527
    test at beta

--- include/controllers/default/cockpit/test/reward.php
+++ include/controllers/default/cockpit/test/reward.php
@@ -2,7 +2,7 @@
 class Controller_Test_Reward extends Crunchbutton_Controller_Account {
 	public function init() {
 
-		die('remove this line!');
+		// die('remove this line!');
 
 		$reward = new Crunchbutton_Reward_Retroactively;
 		$reward->start();
