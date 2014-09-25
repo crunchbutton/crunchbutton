95bbf718f9d6be1d04a846d6963c83577cefbfc2
arzynik <github@arzynik.com>
2014-09-25 12:03:25 -0700

    Added twilio sms test

--- travis/Tests/TwilioTest.php
+++ travis/Tests/TwilioTest.php
@@ -1,7 +1,16 @@
 <?php
 
 class TwilioTest extends PHPUnit_Framework_TestCase {
-	public function testLogin() {
-		$this->assertTrue(true);
+	public function testSms() {
+		$env = c::getEnv();
+		$twilio = c::twilio();
+
+		$res = $twilio->account->sms_messages->create(
+			c::config()->twilio->{$env}->outgoingTextDriver,
+			'+1_PHONE_',
+			'TWILIO-TRAVIS-TEST'
+		);
+
+		$this->assertTrue($res->sid ? true : false);
 	}
 }
