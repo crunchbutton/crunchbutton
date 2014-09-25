1ae63856cd9903d8c9670c7421555c7b7545e2fd
arzynik <github@arzynik.com>
2014-09-25 14:36:04 -0700

    partial #3788
    requires new twilio library

---
+++ include/controllers/default/cockpit/test/longmessage.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+Crunchbutton_Message_Sms::send([
+	'to' => '_PHONE_',
+	'message' => 'The Mozambican War of Independence was an armed conflict between Portugal and the guerrilla forces of the Mozambique Liberation Front. It began on September 25, 1964, and ended with a cease fire on September 8, 1974. The war erupted from unrest and frustration amongst many indigenous Mozambican populations, who perceived foreign rule to be a form of exploitation and resented Portugals policies towards indigenous people. As successful self-determination movements spread throughout Africa after World War II, many Mozambicans became progressively nationalistic in outlook. For the other side, many enculturated indigenous Africans who were fully integrated into the social organization of Portuguese Mozambique reacted to the independentist claims with discomfort and suspicion. The ethnic Portuguese of the territory, including most of the ruling authorities, responded with increased military presence and fast-paced development projects. The Portuguese regular army held the upper hand during the conflict (propaganda pictured) but Mozambique achieved independence in 1975 after the Carnation Revolution in Portugal, ending 470 years of Portuguese colonial rule in the East African region. '
+]);
+
+exit;
+
+Crunchbutton_Message_Call::send([
+	'to' => '_PHONE_'
+	
+]);
+
+
+exit;
---
+++ include/library/Crunchbutton/Message/Call.php
@@ -0,0 +1,63 @@
+<?
+
+class Crunchbutton_Message_Call extends Crunchbutton_Message {
+	public static function send($from, $to = null, $url = null, $callback = null) {
+
+		if (is_array($from)) {
+			$to = $from['to'];
+			$url = $from['url'];
+			$callback = $from['callback'];
+			$from = $from['from'];
+		}
+
+		if (!$to || !$url) {
+			return false;
+		}
+
+		$env = c::getEnv();
+		if (!$from || $from == 'customer') {
+			$from = c::config()->twilio->{$env}->outgoingCustomer;
+		} elseif ($from == 'driver') {
+			$from = c::config()->twilio->{$env}->outgoingDriver;
+		} elseif ($from == 'restaurant') {
+			$from = c::config()->twilio->{$env}->outgoingRestaurant;
+		}
+
+		$from = self::formatNumber($from);
+		$to = self::formatNumber($to);
+		
+		if ($callback) {
+			$callback = ['StatusCallback' => 'http://'.c::config()->host_callback.$callback];
+		}
+		
+		if (!$from || !$to) {
+			return false;
+		}
+
+		try {
+			Log::debug([
+				'action' => 'calling',
+				'to' => $to,
+				'from' => $from,
+				'msg' => $url,
+				'callback' => $callback,
+				'type' => 'call'
+			]);
+
+			$call = c::twilio()->account->calls->create($fro, $to, $url, $callback);
+											
+		} catch (Exception $e) {
+			Log::error([
+				'action' => 'call failed',
+				'to' => $to,
+				'from' => $from,
+				'msg' => $url,
+				'callback' => $callback,
+				'type' => 'call'
+			]);
+		}
+
+		return $call;
+	}
+}
+
