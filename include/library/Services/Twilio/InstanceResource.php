ba2dcb7487b2c5e8632d4a0446a8be7fdadd2bfb
arzynik <github@arzynik.com>
2014-09-25 16:00:00 -0700

    Added new twilio library

--- include/library/Services/Twilio/InstanceResource.php
+++ include/library/Services/Twilio/InstanceResource.php
@@ -1,55 +1,84 @@
 <?php
 
 /**
- * Abstraction of an instance resource from the Twilio API.
- *
  * @category Services
  * @package  Services_Twilio
  * @author   Neuman Vong <neuman@twilio.com>
  * @license  http://creativecommons.org/licenses/MIT/ MIT
  * @link     http://pear.php.net/package/Services_Twilio
- */ 
-abstract class Services_Twilio_InstanceResource
-    extends Services_Twilio_Resource
-{
+ */
+
+/**
+ * Abstraction of an instance resource from the Twilio API.
+ */
+abstract class Services_Twilio_InstanceResource extends Services_Twilio_Resource {
+
     /**
-     * @param mixed $params An array of updates, or a property name
-     * @param mixed $value  A value with which to update the resource
+     * Make a request to the API to update an instance resource
      *
-     * @return null
+     * :param mixed $params: An array of updates, or a property name
+     * :param mixed $value:  A value with which to update the resource
+     *
+     * :rtype: null
+     * :throws: a :php:class:`RestException <Services_Twilio_RestException>` if
+     *      the update fails.
      */
     public function update($params, $value = null)
     {
         if (!is_array($params)) {
             $params = array($params => $value);
         }
-        $this->proxy->updateData($params);
+        $decamelizedParams = $this->client->createData($this->uri, $params);
+        $this->updateAttributes($decamelizedParams);
     }
 
-    /**
-     * Set this resource's proxy.
+    /*
+     * Add all properties from an associative array (the JSON response body) as
+     * properties on this instance resource, except the URI
      *
-     * @param Services_Twilio_DataProxy $proxy An instance of DataProxy
-     *
-     * @return null
+     * :param stdClass $params: An object containing all of the parameters of
+     *      this instance
+     * :return: Nothing, this is purely side effecting
+     * :rtype: null
      */
-    public function setProxy($proxy)
-    {
-        $this->proxy = $proxy;
+    public function updateAttributes($params) {
+        unset($params->uri);
+        foreach ($params as $name => $value) {
+            $this->$name = $value;
+        }
     }
 
     /**
      * Get the value of a property on this resource.
      *
-     * @param string $key The property name
+     * Instead of defining all of the properties of an object directly, we rely
+     * on the API to tell us which properties an object has. This method will
+     * query the API to retrieve a property for an object, if it is not already
+     * set on the object.
      *
-     * @return mixed Could be anything.
+     * If the call is to a subresource, eg ``$client->account->messages``, no
+     * request is made.
+     *
+     * To help with lazy HTTP requests, we don't actually retrieve an object
+     * from the API unless you really need it. Hence, this function may make API
+     * requests even if the property you're requesting isn't available on the
+     * resource.
+     *
+     * :param string $key: The property name
+     *
+     * :return mixed: Could be anything.
+     * :throws: a :php:class:`RestException <Services_Twilio_RestException>` if
+     *      the update fails.
      */
     public function __get($key)
     {
         if ($subresource = $this->getSubresources($key)) {
             return $subresource;
         }
-        return $this->proxy->$key;
+        if (!isset($this->$key)) {
+            $params = $this->client->retrieveData($this->uri);
+            $this->updateAttributes($params);
+        }
+        return $this->$key;
     }
 }
--- include/library/Services/Twilio/Resource.php
+++ include/library/Services/Twilio/Resource.php
@@ -8,44 +8,29 @@
  * @author   Neuman Vong <neuman@twilio.com>
  * @license  http://creativecommons.org/licenses/MIT/ MIT
  * @link     http://pear.php.net/package/Services_Twilio
- */ 
-abstract class Services_Twilio_Resource
-    implements Services_Twilio_DataProxy
-{
-    protected $name;
-    protected $proxy;
+ */
+abstract class Services_Twilio_Resource {
     protected $subresources;
 
-    public function __construct(Services_Twilio_DataProxy $proxy)
+    public function __construct($client, $uri, $params = array())
     {
         $this->subresources = array();
-        $this->proxy = $proxy;
-        $this->name = get_class($this);
-        $this->init();
+        $this->client = $client;
+
+        foreach ($params as $name => $param) {
+            $this->$name = $param;
+        }
+
+        $this->uri = $uri;
+        $this->init($client, $uri);
     }
 
-    protected function init()
+    protected function init($client, $uri)
     {
         // Left empty for derived classes to implement
     }
 
-    public function retrieveData($path, array $params = array())
-    {
-        return $this->proxy->retrieveData($path, $params);
-    }
-
-    public function deleteData($path, array $params = array())
-    {
-        return $this->proxy->deleteData($path, $params);
-    }
-
-    public function createData($path, array $params = array())
-    {
-        return $this->proxy->createData($path, $params);
-    }
-
-    public function getSubresources($name = null)
-    {
+    public function getSubresources($name = null) {
         if (isset($name)) {
             return isset($this->subresources[$name])
                 ? $this->subresources[$name]
@@ -54,32 +39,96 @@ abstract class Services_Twilio_Resource
         return $this->subresources;
     }
 
-    public function addSubresource($name, Services_Twilio_Resource $res)
-    {
-        $this->subresources[$name] = $res;
-    }
-
     protected function setupSubresources()
     {
         foreach (func_get_args() as $name) {
-            $constantized = ucfirst(Services_Twilio_Resource::camelize($name));
+            $constantized = ucfirst(self::camelize($name));
             $type = "Services_Twilio_Rest_" . $constantized;
-            $this->addSubresource($name, new $type($this));
+            $this->subresources[$name] = new $type(
+                $this->client, $this->uri . "/$constantized"
+            );
+        }
+    }
+
+    /*
+     * Get the resource name from the classname
+     *
+     * Ex: Services_Twilio_Rest_Accounts -> Accounts
+     *
+     * @param boolean $camelized Whether to return camel case or not
+     */
+    public function getResourceName($camelized = false)
+    {
+        $name = get_class($this);
+        $parts = explode('_', $name);
+        $basename = end($parts);
+        if ($camelized) {
+            return $basename;
+        } else {
+            return self::decamelize($basename);
         }
     }
 
     public static function decamelize($word)
     {
-        return preg_replace(
-            '/(^|[a-z])([A-Z])/e',
-            'strtolower(strlen("\\1") ? "\\1_\\2" : "\\2")',
+        $callback = create_function('$matches',
+            'return strtolower(strlen("$matches[1]") ? "$matches[1]_$matches[2]" : "$matches[2]");');
+
+        return preg_replace_callback(
+            '/(^|[a-z])([A-Z])/',
+            $callback,
             $word
         );
     }
 
-    public static function camelize($word)
-    {
-        return preg_replace('/(^|_)([a-z])/e', 'strtoupper("\\2")', $word);
+    /**
+     * Return camelized version of a word
+     * Examples: sms_messages => SMSMessages, calls => Calls,
+     * incoming_phone_numbers => IncomingPhoneNumbers
+     *
+     * @param string $word The word to camelize
+     * @return string
+     */
+    public static function camelize($word) {
+        $callback = create_function('$matches', 'return strtoupper("$matches[2]");');
+
+        return preg_replace_callback('/(^|_)([a-z])/',
+            $callback,
+            $word);
     }
+
+    /**
+     * Get the value of a property on this resource.
+     *
+     * @param string $key The property name
+     * @return mixed Could be anything.
+     */
+    public function __get($key) {
+        if ($subresource = $this->getSubresources($key)) {
+            return $subresource;
+        }
+        return $this->$key;
+    }
+
+    /**
+     * Print a JSON representation of this object. Strips the HTTP client
+     * before returning.
+     *
+     * Note, this should mainly be used for debugging, and is not guaranteed
+     * to correspond 1:1 with the JSON API output.
+     *
+     * Note that echoing an object before an HTTP request has been made to
+     * "fill in" its properties may return an empty object
+     */
+    public function __toString() {
+        $out = array();
+        foreach ($this as $key => $value) {
+            if ($key !== 'client' && $key !== 'subresources') {
+                $out[$key] = $value;
+            }
+        }
+        return json_encode($out, true);
+    }
+
 }
 
