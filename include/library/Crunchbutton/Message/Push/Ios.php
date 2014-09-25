e3bc9494fe1b56117d7cd19dcc67912ac79b32fe
arzynik <github@arzynik.com>
2014-09-25 16:34:07 -0700

    added separate push notification class

---
+++ include/library/Crunchbutton/Message/Push/Ios.php
@@ -0,0 +1,79 @@
+<?
+
+class Crunchbutton_Message_Push_Ios extends Crunchbutton_Message {
+	public static function send($to, $message = null, $id = null, $count = null) {
+
+		$sound = 'www/edm.wav';
+		$count = 1;
+		$id = 'push';
+
+		if (is_array($to)) {
+
+			$message = $to['message'];
+
+			if (isset($to['count'])) {
+				$count = $to['count'];
+			}
+			
+			if (isset($to['sound'])) {
+				$sound = $to['sound'];
+			}
+			
+			if (isset($to['id'])) {
+				$id = $to['id'];
+			}
+			
+			$to = $to['to'];
+		}
+
+		if (!$to || !$message) {
+			return false;
+		}
+		
+		if (!is_array($to)) {
+			$to = [$to];
+		}
+		
+		$message = trim($message);
+
+		$env = c::getEnv();
+
+		$certs = c::config()->dirs->root.'ssl/';
+
+		if ($env == 'live') {
+			
+		} else {
+			$push = new ApnsPHP_Push(
+				ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
+				$certs.'aps_development_com.crunchbutton.cockpit.pem'
+			);
+		}
+
+		$push->setRootCertificationAuthority($certs.'entrust_root_certification_authority.pem');
+		$push->connect();
+
+		foreach ($to as $t) {
+		
+			if (!$t) {
+				continue;
+			}
+
+			$msg = new ApnsPHP_Message($t);
+			$msg->setCustomIdentifier($id);
+			$msg->setText($message);
+			$msg->setSound($sound);
+			$msg->setExpiry(30);
+	
+			$msg->setBadge($count);
+	
+			$push->add($msg);
+		}
+
+		$push->send();
+		$push->disconnect();
+
+		$aErrorQueue = $push->getErrors();
+
+		return $aErrorQueue ? $aErrorQueue : true;
+	}
+}
