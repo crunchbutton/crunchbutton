9bd93b5b74ca508cd04168d8b537b378028af0a9
arzynik <github@arzynik.com>
2014-09-25 16:35:12 -0700

    added some notification tests

---
+++ travis/Tests/NotificationTest.php
@@ -0,0 +1,107 @@
+<?php
+
+class NotificationTest extends PHPUnit_Framework_TestCase {
+
+	public static function setUpBeforeClass() {
+		$name = get_called_class();
+
+		$r = (new Restaurant([
+			'name' => $name,
+			'active' => 1
+		]))->save();
+
+		$a = (new Admin([
+			'name' => $name,
+			'login' => null,
+			'active' => 1
+		]))->save();
+		
+		$u = (new User([
+			'name' => $name,
+			'phone' => '_PHONE_',
+			'address' => '123 main',
+			'active' => 1
+		]))->save();
+		
+		$o = (new Order([
+			'name' => $u->name,
+			'address' => $u->address,
+			'phone' => $u->phone,
+			'price' => '10',
+			'price_plus_delivery_markup' => '10',
+			'final_price' => '12.8',
+			'final_price_plus_delivery_markup' => '12.8',
+			'pay_type' => 'cash',
+			'delivery_type' => 'delivery',
+			'id_user' => $u->id_user,
+			'date' => date('Y-m-d H:i:s'),
+			'id_community' => '',
+			'active' => 1
+		]))->save();
+
+	}
+
+	public static function tearDownAfterClass() {
+		$name = get_called_class();
+
+		Restaurant::q('select * from restaurant where name="'.$name.'"')->delete();
+		User::q('select * from user where name="'.$name.'"')->delete();
+		Order::q('select * from `order` where name="'.$name.'"')->delete();
+		Admin::q('select * from admin where name="'.$name.'"')->delete();
+	}
+	
+	public function setUp() {
+		$name = get_called_class();
+
+		$this->restaurant = Restaurant::q('select * from restaurant where name="'.$name.'" order by id_restaurant desc limit 1')->get(0);
+		$this->driver = Admin::q('select * from admin where name="'.$name.'" order by id_admin desc limit 1')->get(0);
+		$this->user = User::q('select * from `user` where name="'.$name.'" order by id_user desc limit 1')->get(0);
+		$this->order = Order::q('select * from `order` where name="'.$name.'" order by id_order desc limit 1')->get(0);
+	}
+	
+	public function testCustomerReceipt() {
+		$this->order->receipt();
+	}
+
+	public function testNotifyDriverSms() {
+
+		$n = (new Crunchbutton_Admin_Notification([
+			'id_admin' => $this->driver->id_admin,
+			'type' => 'sms',
+			'value' => '_PHONE_',
+			'active' => 1
+		]))->save();
+		
+		$sids = $n->sendSms($this->order);
+		$status = true;
+		foreach ($sids as $res) {
+			if (!$res->sid) {
+				$status = false;
+				break;
+			}
+		}
+
+		$this->assertTrue($status);
+	}
+
+	public function testNotifyDriverPushIos() {
+
+		$n = (new Crunchbutton_Admin_Notification([
+			'id_admin' => $this->driver->id_admin,
+			'type' => 'push-ios',
+			'value' => 'bda4c763f2e2f2ec8b123a960fd2e9ecba591cf4a310253708156eed658a4bb2',
+			'active' => 1
+		]))->save();
+		
+		$status = $n->sendPushIos($this->order);
+		if (!$status) {
+			var_dump($status);
+		}
+
+		$this->assertTrue($status);
+	}
+
+
+
+
+}
