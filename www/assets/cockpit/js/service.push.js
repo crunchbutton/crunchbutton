d27073a02bf557ed9f7d5e88d025ee819552bdbc
arzynik <github@arzynik.com>
2014-10-01 11:15:51 -0700

    partial #3825

---
+++ www/assets/cockpit/js/service.push.js
@@ -0,0 +1,65 @@
+
+
+
+NGApp.factory('PushService', function($http) {
+
+	var service = {
+		id: null
+	};
+
+	service.init = function() {
+		if (!App.isPhoneGap) {
+			return;
+		}
+
+		document.addEventListener('pushnotification', function(e) {
+			service.receive(e.msg);
+		}, false);
+
+		parent.plugins.pushNotification.register(
+			function(id) {
+				service.id = id;
+				console.debug('Push id: ' + id);
+
+				$http({
+					method: 'POST',
+					url: App.service + 'config',
+					data: {key: 'push-ios', value: service.id},
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				});
+			},
+			function() {
+				console.error('Failed registering push notifications', arguments);
+			},
+			{
+				'badge': 'true',
+				'sound': 'true',
+				'alert': 'true',
+				'ecb': 'pushnotification'
+			});
+	};
+	
+	service.receive = function(msg) {
+		console.debug('Notification: ', msg);
+
+		var complete = function() {
+
+		};
+
+		// iOS
+		if (msg.alert) {
+			App.alert(msg.alert);
+		}
+
+		if (msg.badge) {
+			parent.plugins.pushNotification.setApplicationIconBadgeNumber(complete, complete, msg.badge);
+		}
+
+		if (msg.sound) {
+			var snd = new parent.Media(msg.sound.replace('www/',''));
+			snd.play();
+		}
+	}
+	
+	return service;
+});
