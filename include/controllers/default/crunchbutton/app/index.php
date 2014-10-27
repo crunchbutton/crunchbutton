c02637ba742f6ca7b397f6d543737e14c29eaa31
arzynik <github@arzynik.com>
2014-10-27 09:56:56 -0700

    fixes #3914

--- include/controllers/default/crunchbutton/app/index.php
+++ include/controllers/default/crunchbutton/app/index.php
@@ -2,7 +2,11 @@
 
 class Controller_app extends Cana_Controller {
 	public function init() {
-		header('Location: https://itunes.apple.com/app/id721780390');
+		if (c::app()->detect()->match('android')) {
+			header('Location: https://play.google.com/store/apps/details?id=com.crunchbutton');
+		} else {
+			header('Location: https://itunes.apple.com/app/id721780390');
+		}
 		exit;
 	}
 }
