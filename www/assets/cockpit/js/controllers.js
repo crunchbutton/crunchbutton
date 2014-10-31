9259db534baba7bf72d5e027c06d199f89f3c2b6
arzynik <github@arzynik.com>
2014-10-30 22:03:03 -0700

    added text or sms feature

--- www/assets/cockpit/js/controllers.js
+++ www/assets/cockpit/js/controllers.js
@@ -89,19 +89,20 @@ NGApp.controller( 'ProfileCtrl', function ( $scope, CustomerRewardService ) {
 NGApp.controller( 'NotificationAlertCtrl', function ($scope, $rootScope ) {
 	$rootScope.$on('notificationAlert', function(e, title, message, fn) {
 		$(':focus').blur();
-		console.log(fn);
+
 		var complete = function() {
 			$rootScope.closePopup();
-			console.log(fn, typeof fn);
 			if (typeof fn === 'function') {
 				fn();
 			}
 		};
+
 		if ($scope.$$phase) {
 			$scope.title = title;
 			$scope.message = message;
 			$scope.complete = complete;
 			App.dialog.show('.notification-alert-container');
+
 		} else {
 			$rootScope.$apply(function(scope) {
 				scope.title = title;
@@ -110,5 +111,15 @@ NGApp.controller( 'NotificationAlertCtrl', function ($scope, $rootScope ) {
 				App.dialog.show('.notification-alert-container');
 			});
 		}
+	});
+});
+
+NGApp.controller( 'CallText', function ($scope, $rootScope) {
+	$rootScope.$on('callText', function(e, num) {
+		$(':focus').blur();
+		$scope.number = num;
+		$scope.complete = $rootScope.closePopup;
+		App.dialog.show('.notification-call-text-container');
+
 	});
 });
