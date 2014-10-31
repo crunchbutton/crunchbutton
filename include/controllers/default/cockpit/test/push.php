84720c999580c665e5c706e13e7706c00fd2e573
arzynik <github@arzynik.com>
2014-10-31 12:58:35 -0700

    updated push test form my id

--- include/controllers/default/cockpit/test/push.php
+++ include/controllers/default/cockpit/test/push.php
@@ -21,12 +21,18 @@ class Controller_test_push extends Crunchbutton_Controller_Account {
 		$push->connect();
 		
 		// Instantiate a new Message with a single recipient
-		$message = new ApnsPHP_Message('bda4c763f2e2f2ec8b123a960fd2e9ecba591cf4a310253708156eed658a4bb2');
+		$message = new ApnsPHP_Message('b85dc7710abcd6a18aa6ff91ca165aa97fa02df23323d49c689a7d50fd47e800');
 		
 		// Set a custom identifier. To get back this identifier use the getCustomIdentifier() method
 		// over a ApnsPHP_Message object retrieved with the getErrors() message.
 		$message->setCustomIdentifier('order-recieved');
 		$message->setBadge(1);
+		
+//		$message->setActionLocKey('Show me!');
+//		$message->setLocKey('Hello %1$@, you have %2$@ new messages!'); // This will overwrite the text specified with setText() method.
+//		$message->setLocArgs(array('Steve', 5));
+//		$message->setCustomProperty('acme2', array('bang', 'whiz'));
+		$message->setCategory('support-message-test');
 		
 		// Set a simple welcome text
 		$message->setText('#5634: Devin has placed an order to Chipotle');
