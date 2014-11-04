aea0de4a3aed3123faa9e520aa176cedb5a72809
arzynik <github@arzynik.com>
2014-11-04 10:20:35 -0800

    got php event emiting working
    partial #3496

--- include/controllers/default/cockpit/test/chat.php
+++ include/controllers/default/cockpit/test/chat.php
@@ -1,9 +1,11 @@
 <?php
 class Controller_test_chat extends Crunchbutton_Controller_Account {
 	public function init() {
-		Chat::emit([
+		$res = Chat::emit([
 			'cmd' => 'ticket.message',
 			'message' => 'TEST'
 		]);
+		
+		echo 'res...<br>'.$res;
 	}
 }
