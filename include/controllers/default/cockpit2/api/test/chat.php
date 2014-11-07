a96a30ef36e4f7335371124b06abed9f064b2c27
arzynik <github@arzynik.com>
2014-11-06 16:46:48 -0800

    moved test

--- include/controllers/default/cockpit2/api/test/chat.php
+++ include/controllers/default/cockpit2/api/test/chat.php
@@ -1,5 +1,5 @@
 <?php
-class Controller_test_chat extends Crunchbutton_Controller_Account {
+class Controller_api_test_chat extends Crunchbutton_Controller_Account {
 	public function init() {
 
 		$message = Support_Message::o(2913);
