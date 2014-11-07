c5673b67b7e0f54e22f08c7a796b50fa08641d06
arzynik <github@arzynik.com>
2014-11-06 17:02:21 -0800

    fixed ssl error

--- include/library/Crunchbutton/Chat.php
+++ include/library/Crunchbutton/Chat.php
@@ -26,12 +26,15 @@ class Crunchbutton_Chat {
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, [
 			'Content-Type: application/json',
 			'Content-Length: ' . strlen($data)
 		]);
 
 		$res = curl_exec($ch);
+		curl_close($ch);
+
 		return $res;	
 	}
 }
