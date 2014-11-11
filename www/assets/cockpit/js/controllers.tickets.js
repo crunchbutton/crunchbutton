9b6419938810b89daeb56846df22a66263280423
arzynik <github@arzynik.com>
2014-11-10 20:07:41 -0800

    added judd to test notifications

--- www/assets/cockpit/js/controllers.tickets.js
+++ www/assets/cockpit/js/controllers.tickets.js
@@ -90,7 +90,7 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 			host: location.host
 		});
 
-		if (AccountService.user.id_admin == 1) {
+		if (AccountService.user.id_admin == 1 || AccountService.user.id_admin == 2) {
 			service.socket.emit('event.subscribe', 'ticket.all');
 		}
 	});
