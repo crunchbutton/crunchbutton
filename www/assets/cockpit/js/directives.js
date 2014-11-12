7fe15b18dc8fffdebf524513e2d559ebef4efb9d
arzynik <github@arzynik.com>
2014-11-12 11:21:18 -0800

    added dynamic preference toggling. added preference for global notifications.

--- www/assets/cockpit/js/directives.js
+++ www/assets/cockpit/js/directives.js
@@ -15,3 +15,37 @@ NGApp.directive('chatSend', function(TicketViewService) {
 		}
 	};
 });
+
+
+NGApp.directive('profilePreference', function (AccountService, $http) {
+	return {
+		restrict: 'A',
+		templateUrl: 'assets/view/general-profile-preference.html',
+		scope: {
+			content: '@',
+			key: '@',
+			type: '@'
+		},
+		controller: function ($scope) {
+			$scope.value = AccountService.user.prefs[$scope.key];
+			if ($scope.type == 'bool') {
+				$scope.value = $scope.value == '1' ? true : false;
+			}
+
+			$scope.change = function(value) {
+				console.log(value);
+
+				if ($scope.type == 'bool') {
+					value = value ? '1' : '0';
+				}
+				
+				$http({
+					method: 'POST',
+					url: App.service + 'config',
+					data: {key: $scope.key, value: value},
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				});
+			}
+		}
+	};;
+});
