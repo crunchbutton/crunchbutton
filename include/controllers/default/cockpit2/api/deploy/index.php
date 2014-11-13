1a60c2e152c35cdb99dfe7a1c00bf484a33e9b61
arzynik <github@arzynik.com>
2014-11-13 15:46:14 -0800

    partial #3787

--- include/controllers/default/cockpit2/api/deploy/index.php
+++ include/controllers/default/cockpit2/api/deploy/index.php
@@ -7,11 +7,37 @@ class Controller_api_deploy extends Crunchbutton_Controller_RestAccount {
 		if (!c::admin()->permission()->check(['server-deploy'])) {
 			exit;
 		}
-		
+
 		switch (c::getPagePiece(2)) {
+			case 'gitlog':
+				$logs = [];
+				exec('git log -n 20', $o);
+				foreach ($o as $k => $line) {
+					if (substr($line, 0, 6) == 'commit') {
+						$log = [];
+						$notes = '';
+						$log['commit'] = preg_replace('/commit /', '', $line);
+						$kk = $k;
+						if (preg_match('/Merge:/', $o[$kk+1])) {
+							$notes = $o[$kk+1];
+							$kk++;
+						}
+						
+						$log['author'] = preg_replace('/^Author: (.*) <.*$/i', '\\1', $o[$kk+1]);
+						$log['date'] = strtotime(trim(preg_replace('/Date:/', '', $o[$kk+2])));
+						$log['note'] =  ($notes ? $notes.'. ' : '').trim($o[$kk+4]);
+
+						$logs[] = $log;
+					}
+				}
+				echo json_encode($logs);
+				exit;
+				break;
+
 			case 'servers':
 				$r = Deploy_Server::q('select * from deploy_server');
 				break;
+
 			case 'server':
 				switch (c::getPagePiece(4)) {
 					case 'versions':
@@ -22,12 +48,25 @@ class Controller_api_deploy extends Crunchbutton_Controller_RestAccount {
 						break;
 				}
 				break;
+
 			case 'versions':
 				$r = Deploy_Version::q('select * from deploy_version order by date desc limit 10 ');
 				break;
+
 			case 'version':
-				if ($this->method == 'post') {
-					$r = new Deploy_Version($this->request());
+				if ($this->method() == 'post') {
+					$date = $this->request()['date'];
+					$d = strtotime($date);
+					if ($d < time()) {
+						$date = date('Y-m-d H:i:s');
+					}
+					$r = new Deploy_Version([
+						'date' => $date,
+						'version' => $this->request()['version'],
+						'id_deploy_server' => $this->request()['id_deploy_server'],
+						'status' => 'new',
+						'id_admin' => c::admin()->id_admin
+					]);
 					$r->save();
 				} else {
 					$r = Deploy_Version::o(c::getPagePiece(3));
@@ -35,7 +74,9 @@ class Controller_api_deploy extends Crunchbutton_Controller_RestAccount {
 				break;
 		}
 
-		echo $r->json();
+		if ($r) {
+			echo $r->json();
+		}
 		exit;
 
 	}
