93936c2f682a16d3048d07f7d335e01a28c17039
arzynik <github@arzynik.com>
2014-11-14 13:30:11 -0800

    fixed invite test

--- travis/Tests/UserTest.php
+++ travis/Tests/UserTest.php
@@ -5,7 +5,6 @@ class UserTest extends PHPUnit_Framework_TestCase {
 	public function testInvite() {
 		$code = Crunchbutton_User::inviteCodeGenerator();		
 		$match = preg_match('/[qwertyuiopasdfghjklzxcvbnm]{3}[123456789]{3}[qwertyuiopasdfghjklzxcvbnm]{3}/i',$code);
-
-		$this->assertTrue($match);
+		$this->assertTrue($match ? true : false);
 	}
 }
