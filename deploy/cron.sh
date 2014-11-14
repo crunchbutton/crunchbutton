7fe245c0d5d0b2c3a550fc322d290af6ff1920b3
arzynik <github@arzynik.com>
2014-11-14 11:28:17 -0800

    update cron

--- deploy/cron.sh
+++ deploy/cron.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-cd /home/$1/cli && php /home/$1/deploy/cron.php -e=live
+# add an entry like this
+# * * * * * root /home/server/deploy/cron.sh server /usr/bin/php > /home/cron-deploy.log 2>&1
+
+cd /home/$1/cli && $2 /home/$1/deploy/cron.php -e=live
