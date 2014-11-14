9c46a4abfc321abe0d13330208ab78b8493645ab
arzynik <github@arzynik.com>
2014-11-14 13:03:11 -0800

    partial #2960

---
+++ travis/dbmigrate.php
@@ -0,0 +1,15 @@
+<?php
+	
+$files = [];
+
+foreach (new DirectoryIterator('../db/migrate') as $fileInfo) {
+	if ($fileInfo->isDot()) continue;
+	$num = preg_replace('/^([0-9]+)_.*$/','\\1', $fileInfo->getFilename());
+	if ($num > 180) {
+		$files[] = $fileInfo->getFilename();
+	}
+}
+
+foreach ($files as $file) {
+	exec('mysql -uroot crunchbutton_travis < ../db/migrate/'.$file);
+}
