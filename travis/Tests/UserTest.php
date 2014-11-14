f2a2f2156993974a91e18dc20530d43f331cf42e
arzynik <github@arzynik.com>
2014-11-14 13:08:51 -0800

    fixed order of tests

--- travis/Tests/UserTest.php
+++ travis/Tests/UserTest.php
@@ -2,7 +2,7 @@
 
 class UserTest extends PHPUnit_Framework_TestCase {
 
-	public function inviteTest() {
+	public function testInvite() {
 		$code = Crunchbutton_User::inviteCodeGenerator();		
 		$match = preg_match('/[qwertyuiopasdfghjklzxcvbnm]{3}[123456789]{3}[qwertyuiopasdfghjklzxcvbnm]{3}/i',$code);
 
