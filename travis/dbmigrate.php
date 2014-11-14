9bd8be6184233b1a6c8943e610389d946540b024
arzynik <github@arzynik.com>
2014-11-14 14:36:46 -0800

    more debug info

--- travis/dbmigrate.php
+++ travis/dbmigrate.php
@@ -10,6 +10,23 @@ foreach (new DirectoryIterator('db/migrate') as $fileInfo) {
 	}
 }
 
+echo "\nRunning db migrate scripts...\n";
+
 foreach ($files as $file) {
-	exec('mysql -uroot crunchbutton_travis < db/migrate/'.$file);
+	echo $file."...\n";
+	$o = null;
+
+	exec('mysql -uroot crunchbutton_travis < db/migrate/'.$file.' 2>&1 &', $o);
+	$ret = implode("\n", $o);
+
+	if (preg_match('/ERROR [0-9]+/', $ret)) {
+		$error = true;
+	}
+	echo "\n";
+}
+
+if ($error) {
+	exit(1);
+} else {
+	exit(0);
 }
