0e1052b69c14a72d853aef1e9589cd92e03d4d85
arzynik <github@arzynik.com>
2014-11-14 18:10:47 -0800

    added driver deliveries function

--- include/library/Cockpit/Admin.php
+++ include/library/Cockpit/Admin.php
@@ -1,6 +1,19 @@
 <?php
 
 class Cockpit_Admin extends Crunchbutton_Admin {
+	
+	public function publicExports() {
+		$out = parent::publicExports();
+		foreach ($this->deliveries() as $order) {
+			$out['deliveries'][] = [
+				'id_order' => $order->id_order,
+				'status' => $order->stati[count($order->stati)-1]['status'],
+				'update' => $order->stati[count($order->stati)-1]['timestamp']
+			];
+		}
+
+		return $out;
+	}
 
 	public function location() {
 		if (!isset($this->_location)) {
@@ -23,6 +36,42 @@ class Cockpit_Admin extends Crunchbutton_Admin {
 			}
 		}
 		return false;
+	}
+	
+	public function deliveries() {
+		if (!isset($this->_deliveries)) {
+			$o = Order::q('
+				select o.*, oa.type as status, oa.timestamp as status_time from `order` o
+				left join order_action oa using (id_order)
+				where
+					id_admin="'.$this->id_admin.'"
+					and (oa.type="delivery-pickedup" or oa.type="delivery-accepted" or oa.type="delivery-delivered" or oa.type="delivery-rejected" or oa.type="delivery-transfered")
+					and o.date >= (curdate() - interval 50 day)
+				order by oa.timestamp asc
+			');
+			$orders = [];
+			foreach ($o as $order) {
+				if (!$orders[$order->id_order]) {
+					$orders[$order->id_order] = $order;
+					$orders[$order->id_order]->stati = [];
+				}
+				$orders[$order->id_order]->stati[] = [
+					'status' => $order->status,
+					'timestamp' => $order->status_time
+				];
+			}
+			foreach ($orders as $k => $order) {
+				$last = count($order->stati) - 1;
+				$inactive = ['delivery-rejected', 'delivery-transfered', 'delivery-delivered'];
+
+				if (in_array($order->stati[$last]['status'], $inactive)) {
+					unset($orders[$k]);
+					continue;
+				}
+			}
+			$this->_deliveries = $orders;
+		}
+		return $this->_deliveries;
 	}
 
 }
