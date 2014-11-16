b49c1436271eaa1da875063c0f492a1b522da35e
arzynik <github@arzynik.com>
2014-11-16 11:44:37 -0800

    removed ww forward

--- www/index.php
+++ www/index.php
@@ -20,6 +20,7 @@ if (isset($_REQUEST['__url']) && $_REQUEST['__url'] == 'index.php') {
 	$_REQUEST['__url'] = '';
 }
 
+/*
 if (preg_match('/^www.cockpit.la$/',$_SERVER['HTTP_HOST'])) {
 	$replace = 'cockpit.la';
 }
@@ -32,13 +33,12 @@ if ($replace) {
 	header('Location: http://'.$replace.$_SERVER['REQUEST_URI']);
 	exit;
 }
+*/
 
-/*
 if (preg_match('/^www\..*$/',$_SERVER['HTTP_HOST'])) {
 	header('Location: http://'.str_replace('www.','',$_SERVER['HTTP_HOST']).$_SERVER['REQUEST_URI']);
 	exit;
 }
-*/
 
 require_once '../include/crunchbutton.php';
 Cana::app()->displayPage();
