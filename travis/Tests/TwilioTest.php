2fd107b81578f395f39639f6d7d61bef1876d23d
arzynik <github@arzynik.com>
2014-11-17 11:48:03 -0800

    remoed old test

--- travis/Tests/TwilioTest.php
+++ travis/Tests/TwilioTest.php
@@ -2,17 +2,7 @@
 
 class TwilioTest extends PHPUnit_Framework_TestCase {
 	public function testSms() {
-	/*
-		$env = c::getEnv();
-		$twilio = c::twilio();
 
-		$res = $twilio->account->sms_messages->create(
-			c::config()->twilio->{$env}->outgoingTextDriver,
-			'+1_PHONE_',
-			'TWILIO-TRAVIS-TEST'
-		);
-		*/
-		
 		$res = Crunchbutton_Message_Sms::send([
 			'to' => '_PHONE_',
 			'message' => 'TWILIO-TRAVIS-TEST'
