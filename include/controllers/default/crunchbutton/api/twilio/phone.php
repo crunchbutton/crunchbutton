bea5d3912227360e413cb01db942452c0e37d446
Devin Smith <github@arzynik.com>
2014-11-24 15:39:06 -0800

    more call stuff

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -3,19 +3,18 @@
 class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 	public function init() {
 		
-		if (!$_REQUEST['noforward']) {
-			$_REQUEST['forward'] = preg_replace('/[^0-9]/','',$_REQUEST['forward']);
-		    header('Content-type: text/xml');
-			echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
-				.'<Response>'
-				.'<Dial timeout="10" record="false">'.($_REQUEST['forward'] ? $_REQUEST['forward'] : '800-351-4161').'</Dial>'
-				.'</Response>';
-		} else {
-			Call::logFromTwilio($_REQUEST);
-		}
-			
+		$_REQUEST['forward'] = preg_replace('/[^0-9]/','',$_REQUEST['forward']);
+	    header('Content-type: text/xml');
+	    
+	    $callback = 'http'.($_SERVER['HTTPS'] != 'on' ? '' : 's').'://'.$this->host_callback().'/api/twilio/phone/recording';
 
-			
+		echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
+			.'<Response>'
+			.'<Dial timeout="10" record="record-from-answer" trim="trim-silence" action="'.$callback.'">'.($_REQUEST['forward'] ? $_REQUEST['forward'] : '800-351-4161').'</Dial>'
+			.'</Response>';
+
+		Call::logFromTwilio($_REQUEST);
+
 		exit;
 	}
 }
