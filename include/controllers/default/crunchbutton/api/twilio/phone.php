9dd3455998227728ec68e1c5392d85cf224c9614
Devin Smith <github@arzynik.com>
2014-11-24 15:54:21 -0800

    more tests

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -4,17 +4,27 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 	public function init() {
 		
 		$_REQUEST['forward'] = preg_replace('/[^0-9]/','',$_REQUEST['forward']);
-	    header('Content-type: text/xml');
+		header('Content-type: text/xml');
 	    
-	    $callback = 'http'.($_SERVER['HTTPS'] != 'on' ? '' : 's').'://'.c::config()->host_callback.'/api/twilio/phone/recording';
+		die(c::getPagePiece(3));
+	    
+		switch (c::getPagePiece(3)) {
+		    case 'recording':
+				echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
+					.'<Response></Response>';
 
-		echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
-			.'<Response>'
-			.'<Dial timeout="10" record="record-from-answer" trim="trim-silence" action="'.$callback.'">'.($_REQUEST['forward'] ? $_REQUEST['forward'] : '800-351-4161').'</Dial>'
-			.'</Response>';
+		    	break;
+		    case 'call':
+				$callback = 'http'.($_SERVER['HTTPS'] != 'on' ? '' : 's').'://'.c::config()->host_callback.'/api/twilio/phone/recording';
+		
+				echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
+					.'<Response>'
+					.'<Dial timeout="10" record="record-from-answer" trim="trim-silence" action="'.$callback.'">'.($_REQUEST['forward'] ? $_REQUEST['forward'] : '800-351-4161').'</Dial>'
+					.'</Response>';
+		    	break;
+		}
 
 		$request = $this->request();
-		$request['CallSid'] = null;
 		Call::logFromTwilio($request);
 
 		exit;
