4450119b2d5991d7198548a50a6abd2e8a6e5372
Devin Smith <github@arzynik.com>
2014-11-29 17:53:13 -0800

    use travis env

--- include/library/Crunchbutton/App.php
+++ include/library/Crunchbutton/App.php
@@ -44,8 +44,11 @@ class Crunchbutton_App extends Cana_App {
 		}
 
 		// db by hostname
+		// travis
+		if (getenv('TRAVIS')) {
+			$db = 'travis';
 		// anything local
-		if (preg_match('/localhost$|^(crunch|cockpit|cockpitla).dev$/',$_SERVER['SERVER_NAME'])) {
+		} elseif (preg_match('/localhost$|^(crunch|cockpit|cockpitla).dev$/',$_SERVER['SERVER_NAME'])) {
 			$db = 'local';
 		// any one of our cull live urls, or staging prefixes
 		} elseif (preg_match('/^cockpit.la|cbtn.io|_DOMAIN_|cockpit._DOMAIN_|spicywithdelivery.com|(staging[0-9]?.(cockpit.la|crunchr.co))$/',$_SERVER['SERVER_NAME'])) {
