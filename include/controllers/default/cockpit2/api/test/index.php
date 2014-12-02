1f0bbbeb238b7483babcc1114b1e5f36906e5f2f
Daniel Camargo <pererinha@gmail.com>
2014-12-02 20:48:41 -0200

    partial #3993

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -2,8 +2,19 @@
 
 class Controller_api_test extends Crunchbutton_Controller_Rest {
 	public function init() {
+$admin = Admin::o( 5 );
+		$pexcard = $admin->pexcard();
+		// echo '<pre>';var_dump( $pexcard );exit();
+echo '<pre>';var_dump( $pexcard->addFundsOrderAccepeted( 39126 ) );exit();
+		// echo '<pre>';var_dump( Cockpit_Driver_Notify::send( 5, Cockpit_Driver_Notify::TYPE_ACCESS_INFO ) );exit();;
+$order = Order::o( 1222 );
+echo '<pre>';var_dump( $order->user()->firstName() );exit();
+		$user_auth = User_Auth::checkEmailExists( '_EMAIL' );
+$message = ( $user_auth->user()->firstName() ) ? $user_auth->user()->firstName() . ', ' : Crunchbutton_Message_Sms::greeting();
+echo '<pre>';var_dump( $message );exit();
+		// Crunchbutton_Community_Shift::addRemoveShiftFunds();
+		// echo '<pre>';var_dump( Crunchbutton_Admin_Shift_Assign::isFirstWeek( 4 ) );exit();;
 
-		Crunchbutton_Community_Shift::addRemoveShiftFunds();
 
 		// $action = Crunchbutton_Pexcard_Action::o( 1 );
 		// echo $action->json();exit;
