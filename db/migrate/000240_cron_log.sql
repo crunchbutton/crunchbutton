3d643a5bad4c3a1025d621357b4a24aa8dc08354
Daniel Camargo <pererinha@gmail.com>
2014-12-03 14:27:12 -0200

    partial #3993

---
+++ db/migrate/000240_cron_log.sql
@@ -0,0 +1,3 @@
+INSERT INTO `cron_log` ( `description`, `class`, `start_date`, `interval`, `interval_unity`, `current_status`, `next_time`, `finished`, `interactions`)
+VALUES
+	('Remove pex card funds 2 hours after the shift ends', 'Crunchbutton_Cron_Job_PexCardFunds', '2014-12-03 05:00:00', 'minute', 3, 'idle', NULL, NULL, 0);
--- include/library/Crunchbutton/Cron/Job/PexCardFunds.php
+++ include/library/Crunchbutton/Cron/Job/PexCardFunds.php
@@ -4,7 +4,7 @@ class Crunchbutton_Cron_Job_PexCardFunds extends Crunchbutton_Cron_Log {
 
 	public function run(){
 
-		Crunchbutton_Community_Shift::pexCardAddOrRemoveShiftFunds();
+		Crunchbutton_Community_Shift::pexCardRemoveShiftFunds();
 
 		// it always must call finished method at the end
 		$this->finished();
