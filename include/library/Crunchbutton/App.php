340870202b6745e7a3948f89291f451b47d564ef
Devin Smith <github@arzynik.com>
2014-12-16 20:09:50 -0800

    jobs

--- include/library/Crunchbutton/App.php
+++ include/library/Crunchbutton/App.php
@@ -51,7 +51,7 @@ class Crunchbutton_App extends Cana_App {
 		} elseif (preg_match('/localhost$|^(crunch|cockpit|cockpitla).dev$/',$_SERVER['SERVER_NAME'])) {
 			$db = 'local';
 		// any one of our cull live urls, or staging prefixes
-		} elseif (preg_match('/^cockpit.la|cbtn.io|_DOMAIN_|jobs._DOMAIN_|cockpit._DOMAIN_|spicywithdelivery.com|(staging[0-9]?.(cockpit.la|crunchr.co))$/',$_SERVER['SERVER_NAME'])) {
+		} elseif (preg_match('/^cockpit.la|cbtn.io|_DOMAIN_|cockpit._DOMAIN_|spicywithdelivery.com|(staging[0-9]?.(cockpit.la|crunchr.co))$/',$_SERVER['SERVER_NAME'])) {
 			$db = 'live';
 		// anything ._DOMAIN_ fails
 		} elseif (preg_match('/_DOMAIN_$/',$_SERVER['SERVER_NAME'])) {
