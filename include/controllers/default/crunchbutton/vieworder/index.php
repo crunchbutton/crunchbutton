5549a8ab056b378828119c0755f801d4ddc2b811
Daniel Camargo <pererinha@gmail.com>
2014-12-17 20:07:37 -0200

    partial #4306

--- include/controllers/default/crunchbutton/vieworder/index.php
+++ include/controllers/default/crunchbutton/vieworder/index.php
@@ -4,7 +4,7 @@ class Controller_vieworder extends Cana_Controller {
 	public function init() {
 
 		$order = Order::uuid( c::getPagePiece( 1 ) );
-		if (!$order->id_order || (!c::user()->id_admin && c::user()->id_user != $order->id_user)) {
+		if (!$order->id_order || (!$_SESSION['admin'] && c::user()->id_user != $order->id_user)) {
 			die('invalid order');
 		}
 
