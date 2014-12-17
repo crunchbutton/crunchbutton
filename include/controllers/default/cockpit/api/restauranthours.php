556ff69ef141434dcb385fbbdaf2145ccb42a743
Daniel Camargo <pererinha@gmail.com>
2014-12-17 17:52:41 -0200

    partial #4303

--- include/controllers/default/cockpit/api/restauranthours.php
+++ include/controllers/default/cockpit/api/restauranthours.php
@@ -1,9 +1,9 @@
 <?php
 
 class Controller_api_restaurantHours extends Crunchbutton_Controller_RestAccount {
-	
+
 	public function init() {
-		
+
 		if ( !c::admin()->permission()->check( [ 'global','community-all', 'community-restaurants' ] ) ) {
 			echo json_encode( [ 'error' => 'invalid object' ] );
 			exit;
@@ -13,10 +13,16 @@ class Controller_api_restaurantHours extends Crunchbutton_Controller_RestAccount
 			$restaurant = Restaurant::o( c::getPagePiece( 2 ) );
 			if( $restaurant->id_restaurant ){
 				$restaurant->open_for_business = $open_for_business;
+				$force_close_tagline = $this->request()[ 'force_close_tagline' ];;
+				if( $force_close_tagline ){
+					$restaurant->force_close_tagline = $force_close_tagline;
+				} else {
+					$restaurant->force_close_tagline = '';
+				}
 				$restaurant->save();
 				echo json_encode( array( 'success' => true ) );
 			} else {
-				echo json_encode( [ 'error' => 'invalid object' ] );	
+				echo json_encode( [ 'error' => 'invalid object' ] );
 			}
 		} else {
 			echo json_encode( [ 'error' => 'invalid object' ] );
