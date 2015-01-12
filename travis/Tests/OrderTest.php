054b9aaf85b9c0d15d945deb675fd91796c16945
Devin Smith <github@arzynik.com>
2015-01-12 12:22:23 -0800

    partial #768

--- travis/Tests/OrderTest.php
+++ travis/Tests/OrderTest.php
@@ -1,7 +1,97 @@
 <?php
 
 class OrderTest extends PHPUnit_Framework_TestCase {
-	public function testLogin() {
+
+	public static function setUpBeforeClass() {
+		$name = get_called_class();
+
+		$r = new Restaurant([
+			'name' => $name,
+			'active' => 1,
+			'delivery' => 1,
+			'credit' => 1,
+			'delivery_fee' => '1.5',
+			'confirmation' => 0,
+			'community' => 'test',
+			'timezone' => 'America/Los_Angeles'
+		]);
+		$r->save();
+
+		$h = new Hour([
+			'id_restaurant' => $r->id_restaurant,
+			'day' => strtolower(date('D')),
+			'time_open' => '0:01',
+			'time_close' => '23:59',
+		]);
+		$h->save();
+
+		$a = new Admin([
+			'name' => $name,
+			'login' => null,
+			'active' => 1
+		]);
+		$a->save();
+		
+		$u = new User([
+			'name' => $name,
+			'phone' => '_PHONE_',
+			'address' => '123 main',
+			'active' => 1
+		]);
+		$u->save();
+		
+		$d = new Dish([
+			'name' => $name,
+			'price' => '10',
+			'id_restaurant' => $r->id_restaurant,
+			'active' => 1
+		]);
+		$d->save();
+
+	}
+
+	public static function tearDownAfterClass() {
+		$name = get_called_class();
+
+		Restaurant::q('select * from restaurant where name="'.$name.'"')->delete();
+		User::q('select * from user where name="'.$name.'"')->delete();
+		Order::q('select * from `order` where name="'.$name.'"')->delete();
+		Admin::q('select * from admin where name="'.$name.'"')->delete();
+		Dish::q('select * from dish where name="'.$name.'"')->delete();
+	}
+	
+	public function setUp() {
+		$name = get_called_class();
+
+		$this->restaurant = Restaurant::q('select * from restaurant where name="'.$name.'" order by id_restaurant desc limit 1')->get(0);
+		$this->driver = Admin::q('select * from admin where name="'.$name.'" order by id_admin desc limit 1')->get(0);
+		$this->user = User::q('select * from `user` where name="'.$name.'" order by id_user desc limit 1')->get(0);
+		$this->dish = Dish::q('select * from `dish` where name="'.$name.'" order by id_dish desc limit 1')->get(0);
+	}
+
+	public function testOrder() {
+
+		$_POST = [
+			'address' => $this->user->address,
+			'phone' => $this->user->phone,
+			'name' => $this->user->name,
+			'cart' => [['id' => $this->dish->id_dish]],
+			'pay_type' => 'cash',
+			'delivery_type' => 'delivery',
+			'restaurant' => $this->restaurant->id_restaurant,
+			'make_default' => true,
+			'notes' => 'TEST',
+			'lat' => '33.175101',
+			'lon' => '-96.677810',
+			'local_gid' => 'RAND'
+		];
+		
+		$order = new Order;
+		$charge = $order->process($_POST);
+		//var_dump($charge);
+		//exit;
+
+
 		$this->assertTrue(true);
 	}
 }
