fedf9e1f9fa0a36efb8ee6c36d25356947e276f5
mnmeyers <mmeyers218@gmail.com>
2015-01-12 14:41:05 -0800

    #768

--- travis/Tests/bootstrap.php
+++ travis/Tests/bootstrap.php
@@ -5,12 +5,13 @@ ini_set('display_errors',true);
 putenv('PHPUNIT=1');
 
 if (trim(`whoami`) == 'mmeyers') {
-	$_GLOBALS['host-crunchbutton'] = 'http://localhost:8888/';
+	$GLOBALS['host-crunchbutton'] = 'http://localhost:8888/';
 } elseif (trim(`whoami`) == 'arzynik') {
-	$_GLOBALS['host-crunchbutton'] = 'http://crunchbutton.localhost/';
+	$GLOBALS['host-crunchbutton'] = 'http://crunchbutton.localhost/';
 } else {
-	$_GLOBALS['host-crunchbutton'] = 'http://localhost/';
+	$GLOBALS['host-crunchbutton'] = 'http://localhost/';
 }
+//die($_GLOBALS['host-crunchbutton']);
 
 if (trim(`whoami`) == 'arzynik' || trim(`whoami`) == 'mmeyers') {
 	ini_set('mysqli.default_socket','/Applications/MAMP/tmp/mysql/mysql.sock');
