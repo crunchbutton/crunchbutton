9fe80d21c23b858bf47a802e7d77a19ee3b7cd4a
mnmeyers <mmeyers218@gmail.com>
2015-01-12 12:04:22 -0800

    added myself to tests

--- travis/Tests/bootstrap.php
+++ travis/Tests/bootstrap.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ini_set('display_errors',true);
 putenv('PHPUNIT=1');
 
-if (trim(`whoami`) == 'arzynik') {
+if (trim(`whoami`) == 'arzynik' || trim(`whoami`) == 'mmeyers') {
 	ini_set('mysqli.default_socket','/Applications/MAMP/tmp/mysql/mysql.sock');
 }
 
