cdb31bf5219ea2546dd6a7655bce9b492f9b49e6
mnmeyers <mmeyers218@gmail.com>
2015-01-12 14:41:05 -0800

    #768

--- travis/Tests/SayOrderTest.php
+++ travis/Tests/SayOrderTest.php
@@ -9,7 +9,7 @@ class SayTest extends PHPUnit_Framework_TestCase { //ExampleTest
 
 ]))->save();
 
-$data = file_get_contents('http://localhost:8888/api/order/1/say');
+$data = file_get_contents($GLOBALS['host-crunchbutton'].'api/order/1/say');
 
 $this->assertTrue($data == '<?xml version="1.0" encoding="UTF-8"?><Response>
 <Gather action="/api/order/1/sayorder?id_notification=" numDigits="1" timeout="10" finishOnKey="#" method="get"><Say voice="male">Hello. This is crunchbutton, with an order for pickup.</Say><Pause length="1" /><Say voice="male">Press 1 to hear the order. Otherwise we will call back in 2 minutes.</Say><Pause length="5" /><Say voice="male">Press 1 to hear the order. Otherwise we will call back in 2 minutes.</Say><Pause length="5" /><Say voice="male">Press 1 to hear the order. Otherwise we will call back in 2 minutes.</Say><Pause length="5" /><Say voice="male">Press 1 to hear the order. Otherwise we will call back in 2 minutes.</Say><Pause length="5" /></Gather></Response>' ? true : false);
@@ -23,7 +23,7 @@ $this->assertTrue($data == '<?xml version="1.0" encoding="UTF-8"?><Response>
 
 ]))->save();
 
-$data = file_get_contents('http://localhost:8888/api/order/1/sayorder');
+$data = file_get_contents($GLOBALS['host-crunchbutton'].'api/order/1/sayorder');
 
 $this->assertTrue($data == '<?xml version="1.0" encoding="UTF-8"?><Response>
 <Gather action="/api/order/1/sayorderonly?id_notification=" numDigits="1" timeout="10" finishOnKey="#" method="get"><Say voice="male">Thank you. At the end of the message, you must confirm the order.</Say><Pause length="2" /><Say voice="male">Customer Phone number. 2. 3. 4. 5. 6. 7. 8. 9. 0. 1. .</Say><Pause length="1" /><Say voice="male"><![CDATA[Customer Name. UNIT TEST.]]></Say><Pause length="1" /><Say></Say><Pause length="1" /><Say voice="male">This order is for pickup. </Say><Pause length="2" /><Say voice="male"><![CDATA[
@@ -39,7 +39,7 @@ public function testSayOrderOnly() { //lowercase test, uppercase wtvr
 
 ]))->save();
 
-$data = file_get_contents('http://localhost:8888/api/order/1/sayorderonly');
+$data = file_get_contents($GLOBALS['host-crunchbutton'].'api/order/1/sayorderonly');
 
 $this->assertTrue($data == '<?xml version="1.0" encoding="UTF-8"?><Response>
 <Gather action="/api/order/1/sayorderonly?id_notification=" numDigits="1" timeout="10" finishOnKey="#" method="get"><Say voice="male">Customer Phone number. 2. 3. 4. 5. 6. 7. 8. 9. 0. 1. .</Say><Pause length="1" /><Say voice="male"><![CDATA[Customer Name. UNIT TEST.]]></Say><Pause length="1" /><Say></Say><Pause length="1" /><Say voice="male">This order is for pickup. </Say><Pause length="2" /><Say voice="male"><![CDATA[
@@ -54,7 +54,7 @@ $this->assertTrue($data == '<?xml version="1.0" encoding="UTF-8"?><Response>
 
 ]))->save();
 
-$data = file_get_contents('http://localhost:8888/api/order/1/doconfirm');
+$data = file_get_contents($GLOBALS['host-crunchbutton'].'api/order/1/doconfirm');
 
 $this->assertTrue($data == '<?xml version="1.0" encoding="UTF-8"?>
 <Response><Gather action="/api/order/1/doconfirm" numDigits="1" timeout="10" finishOnKey="#" method="get"><Say voice="male">Hello. This is crunchbutton,.</Say><Say voice="male" loop="3">Please press 1 to confirm that you just received order number 1. Or press 2 and we will resend the order. . . .</Say></Gather></Response>' ? true : false);
