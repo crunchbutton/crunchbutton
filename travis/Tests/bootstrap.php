b094f872dcad4f9f70b473e72e2fc416de3b191c
Devin Smith <github@arzynik.com>
2015-01-12 14:28:24 -0800

    added dynamic hosts

--- travis/Tests/bootstrap.php
+++ travis/Tests/bootstrap.php
@@ -4,6 +4,14 @@ error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ini_set('display_errors',true);
 putenv('PHPUNIT=1');
 
+if (trim(`whoami`) == 'mmeyers')) {
+	$_GLOBALS['host-crunchbutton'] = 'http://localhost:8888/';
+} elseif (trim(`whoami`) == 'arzynik')) {
+	$_GLOBALS['host-crunchbutton'] = 'http://crunchbutton.localhost/';
+} else {
+	$_GLOBALS['host-crunchbutton'] = 'http://localhost/';
+}
+
 if (trim(`whoami`) == 'arzynik' || trim(`whoami`) == 'mmeyers') {
 	ini_set('mysqli.default_socket','/Applications/MAMP/tmp/mysql/mysql.sock');
 }
