54a3a492f711b1d1eab418cb1e32175b5d01ef88
Pererinha <pererinha@gmail.com>
2015-01-14 13:25:56 -0200

    partial #4359

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -7,8 +7,8 @@ class Controller_api_test extends Cana_Controller {
 
 		Crunchbutton_Message_Sms::send([
 			'to' => $num,
-			'message' => "YAY! Crunchbutton for iPhone!\nhttp://_DOMAIN_/app",
-			'reason' => Crunchbutton_Message_Sms::REASON_APP_DOWNLOAD
+			'message' => 'test',
+			'reason' => Crunchbutton_Message_Sms::REASON_CUSTOMER_ORDER
 		]);
 
 	}
