9258e30332435391509b67fa3a1d8d002d0af3ae
Pererinha <pererinha@gmail.com>
2015-01-14 13:46:29 -0200

    partial #4359

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -1,15 +1,5 @@
 <?php
 
 class Controller_api_test extends Cana_Controller {
-	public function init(){
-
-		$num = '2037728167';
-
-		Crunchbutton_Message_Sms::send([
-			'to' => $num,
-			'message' => 'test',
-			'reason' => Crunchbutton_Message_Sms::REASON_CUSTOMER_ORDER
-		]);
-
-	}
+	public function init(){}
 }
