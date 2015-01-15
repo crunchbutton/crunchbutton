f464853b3815ad9bcaff19490c44435a04dae223
Pererinha <pererinha@gmail.com>
2015-01-15 16:24:29 -0200

    partial #4431

--- include/controllers/default/cockpit/api/community.php
+++ include/controllers/default/cockpit/api/community.php
@@ -34,6 +34,7 @@ class Controller_api_community extends Crunchbutton_Controller_RestAccount {
 			$community->close_all_restaurants = $this->request()[ 'close_all_restaurants' ];
 			$community->close_all_restaurants_note = $this->request()[ 'close_all_restaurants_note' ];
 			$community->close_all_restaurants_id_admin = c::admin()->id_admin;
+			$community->close_all_restaurants_date = date( 'Y-m-d H:i:s' );
 			$community->save();
 			echo json_encode( [ 'success' => true ] );
 		} else {
@@ -49,6 +50,7 @@ class Controller_api_community extends Crunchbutton_Controller_RestAccount {
 		$community->close_3rd_party_delivery_restaurants = $this->request()[ 'close_3rd_party_delivery_restaurants' ];
 		$community->close_3rd_party_delivery_restaurants_note = $this->request()[ 'close_3rd_party_delivery_restaurants_note' ];
 		$community->close_3rd_party_delivery_restaurants_id_admin = c::admin()->id_admin;
+		$community->close_3rd_party_delivery_restaurants_date = date( 'Y-m-d H:i:s' );
 		$community->save();
 		echo json_encode( [ 'success' => true ] );
 		exit;
