1fb445fa21efa5bd00f336ee096ff3675c213506
Pererinha <pererinha@gmail.com>
2015-01-15 16:38:58 -0200

    partial #4429

---
+++ include/controllers/default/crunchbutton/apply/index.php
@@ -0,0 +1,7 @@
+<?php
+
+class Controller_Apply extends Cana_Controller {
+	public function init() {
+		header( 'Location: https://docs.google.com/forms/d/1zmBx0XOxXi8UpqKX3n7lzXHJUbJqsMrjomqprYBRh0E/edit' );
+	}
+}
