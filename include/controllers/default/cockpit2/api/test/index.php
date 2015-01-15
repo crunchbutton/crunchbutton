c6b47eb60e8095ee4e3f96c9214cf4fcbdc107a1
Pererinha <pererinha@gmail.com>
2015-01-15 15:50:45 -0200

    partial #4270

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -5,7 +5,7 @@ class Controller_api_test extends Cana_Controller {
 	public function init(){
 
 		// Cockpit_Bounce_Back::run();
-		Crunchbutton_Pexcard_Transaction::loadTransactions();
+		// Crunchbutton_Pexcard_Transaction::loadTransactions();
 
 	}
 
