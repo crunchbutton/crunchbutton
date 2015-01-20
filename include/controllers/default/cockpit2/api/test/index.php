8a0adb6b874c80d41a17ac139c55568258363d20
Pererinha <pererinha@gmail.com>
2015-01-20 17:21:32 -0200

    partial #4270

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -4,8 +4,9 @@ class Controller_api_test extends Cana_Controller {
 
 	public function init(){
 
+		// Crunchbutton_Pexcard_Transaction::getOrderExpenses( '01/18/2015', '01/19/2015' );
 		// Crunchbutton_Community_Shift::pexCardRemoveShiftFunds();
-		Crunchbutton_Community_Shift::pexCardRemoveShiftFunds();
+		// Crunchbutton_Community_Shift::pexCardRemoveShiftFunds();
 
 	}
 
