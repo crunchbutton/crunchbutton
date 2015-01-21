582c7c54a289f87202f97aa4d0e84143758f03f2
Pererinha <pererinha@gmail.com>
2015-01-21 18:30:30 -0200

    Track how long communities are force closed every day #4446

--- db/migrate/000263_community_change.sql
+++ db/migrate/000263_community_change.sql
@@ -1,14 +1,3 @@
-CREATE TABLE `community_change` (
-  `id_community_change` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_community_change_set` int(11) unsigned DEFAULT NULL,
-  `field` varchar(255) DEFAULT NULL,
-  `old_value` varchar(255) DEFAULT NULL,
-  `new_value` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_community_change`),
-  KEY `id_community_change_set` (`id_community_change_set`),
-  CONSTRAINT `community_change_ibfk_1` FOREIGN KEY (`id_community_change_set`) REFERENCES `community_change_set` (`id_community_change_set`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
 CREATE TABLE `community_change_set` (
   `id_community_change_set` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `id_community` int(11) unsigned DEFAULT NULL,
@@ -22,4 +11,15 @@ CREATE TABLE `community_change_set` (
   CONSTRAINT `community_change_set_ibfk_1` FOREIGN KEY (`id_community`) REFERENCES `community` (`id_community`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `community_change_set_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `community_change_set_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `community_change` (
+  `id_community_change` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_community_change_set` int(11) unsigned DEFAULT NULL,
+  `field` varchar(255) DEFAULT NULL,
+  `old_value` varchar(255) DEFAULT NULL,
+  `new_value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_community_change`),
+  KEY `id_community_change_set` (`id_community_change_set`),
+  CONSTRAINT `community_change_ibfk_1` FOREIGN KEY (`id_community_change_set`) REFERENCES `community_change_set` (`id_community_change_set`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
