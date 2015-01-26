9ceeebc46c5f9b97a75c463629d97773ef3e489d
mnmeyers <mmeyers218@gmail.com>
2015-01-26 15:59:52 -0800

    Fixed  #4560

--- include/library/Crunchbutton/Community/Changeset.php
+++ include/library/Crunchbutton/Community/Changeset.php
@@ -19,30 +19,12 @@ class Crunchbutton_Community_Changeset extends Cana_Table {
 		}
 		return $this->_changes;
 	}
-
+	
 	public function admin() {
 		if (!isset($this->_admin)) {
 			$this->_admin = Admin::o($this->id_admin);
 		}
 		return $this->_admin;
-	}
-
-	public function date() {
-		if (!isset($this->_date)) {
-			$this->_date = new DateTime($this->timestamp, new DateTimeZone(c::config()->timezone));
-		}
-		return $this->_date;
-	}
-
-	public function user() {
-		if (!isset($this->_user)) {
-			$this->_user = User::o($this->id_user);
-		}
-		return $this->_user;
-	}
-
-	public function author() {
-		return $this->admin()->id_admin ? $this->admin() : $this->user();
 	}
 
 	public function __construct($id = null) {
