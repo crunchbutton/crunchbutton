4c2d6815c6659d01f89ca160a9d86030c3bda1f5
Pererinha <pererinha@gmail.com>
2015-01-27 20:53:03 -0200

    community closed fixed

--- include/library/Crunchbutton/Community/Changeset.php
+++ include/library/Crunchbutton/Community/Changeset.php
@@ -19,12 +19,19 @@ class Crunchbutton_Community_Changeset extends Cana_Table {
 		}
 		return $this->_changes;
 	}
-	
+
 	public function admin() {
 		if (!isset($this->_admin)) {
 			$this->_admin = Admin::o($this->id_admin);
 		}
 		return $this->_admin;
+	}
+
+	public function date() {
+		if (!isset($this->_date)) {
+			$this->_date = new DateTime($this->timestamp, new DateTimeZone(c::config()->timezone));
+		}
+		return $this->_date;
 	}
 
 	public function __construct($id = null) {
