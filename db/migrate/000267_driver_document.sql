be0d4976a35792f9a8d6fe1d5df2ef7703e0f21f
Pererinha <pererinha@gmail.com>
2015-01-27 11:37:07 -0200

    ability to create marketing rep accounts in cockpit.la #4462

--- db/migrate/000267_driver_document.sql
+++ db/migrate/000267_driver_document.sql
@@ -1,3 +1,9 @@
 ALTER TABLE `driver_document` ADD `type` enum('driver','marketing-rep' ) DEFAULT NULL;
 
 UPDATE driver_document SET type = 'driver';
+
+INSERT INTO `driver_document` ( `name`, `order`, `url`, `required`, `type`)
+VALUES
+	('Independent Contractor Agreement', 1, 'https://s3.amazonaws.com/uploads.hipchat.com/41812/280262/4pWiCQFWY7k9CSr/Youbeo%20Rep%20Agreement%202015%20OSG.pdf', 1, 'marketing-rep'),
+	('W9', 2, 'https://s3.amazonaws.com/uploads.hipchat.com/41812/283752/R816dLDLvXfD5cM/W9.pdf', 1, 'marketing-rep'),
+	('Direct Deposit', 3, 'https://s3.amazonaws.com/uploads.hipchat.com/41812/283752/CYoVFxyk8OtNydv/Direct%20Deposit%207.10.pdf', 1, 'marketing-rep');
