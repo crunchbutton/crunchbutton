61069f2347af662c1c978d6898ff69e0614fd9b5
Pererinha <pererinha@gmail.com>
2015-02-03 19:42:30 -0200

    change driver restaurant name when auto shutting down community #4514

--- include/controllers/default/crunchbutton/api/restaurants/index.php
+++ include/controllers/default/crunchbutton/api/restaurants/index.php
@@ -2,8 +2,13 @@
 
 class Controller_api_restaurants extends Crunchbutton_Controller_Rest {
 	public function init() {
+
 		$config = [];
+
+		$communities = [];
+
 		if ($_REQUEST['lat'] && $_REQUEST['lon']) {
+
 			$restaurants = Restaurant::byRange([
 				'lat' => c::db()->escape($_REQUEST['lat']),
 				'lon' => c::db()->escape($_REQUEST['lon']),
@@ -13,8 +18,11 @@ class Controller_api_restaurants extends Crunchbutton_Controller_Rest {
 			// check if the community is closed
 			$community_closed_message = [];
 			$_all_closed = true;
+			$id_community = 0;
 			foreach ($restaurants as $restaurant) {
-				$data = $restaurant->exports(['categories' => true]);
+				$data = $restaurant->exports( [ 'categories' => true ] );
+				$communities[ $restaurant->community()->id_community ] = true;
+				$id_community = $restaurant->community()->id_community;
 				if( $restaurant->open() ){
 					$_all_closed = false;
 				} else {
@@ -36,6 +44,27 @@ class Controller_api_restaurants extends Crunchbutton_Controller_Rest {
 
 				$config['restaurants'][] = $data;
 			}
+
+			// change driver restaurant name when auto shutting down community #4514
+			if( count( $communities ) == 1 && $id_community ){
+				$community = Community::o( $id_community );
+				if( $community->allThirdPartyDeliveryRestaurantsClosed() || $community->allRestaurantsClosed() ){
+					// Check if the community was auto shutdown
+					$autoShutdownAdmin = Admin::login( Crunchbutton_Community::AUTO_SHUTDOWN_COMMUNITY_LOGIN );
+					$id_admin = $autoShutdownAdmin->id_admin;
+					if( $id_admin == $community->close_3rd_party_delivery_restaurants_id_admin || $id_admin == $community->close_all_restaurants_id_admin ){
+						$driverRestaurant = $community->driverRestaurant();
+						if( $driverRestaurant->id_restaurant ){
+							for( $i = 0; $i < count( $config['restaurants'] ); $i++ ){
+								if( $config['restaurants'][ $i ][ 'id_restaurant' ] == $driverRestaurant->id_restaurant ){
+									$config['restaurants'][ $i ][ 'name' ] = $community->driver_restaurant_name;
+								}
+							}
+						}
+					}
+				}
+			}
+
 			if( $_all_closed && count( $community_closed_message ) == 1 ){
 				$closed_message = false;
 				foreach( $community_closed_message as $key => $val ){
