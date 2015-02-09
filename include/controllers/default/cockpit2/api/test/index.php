4b3e8c8207f34e28c3fb595249e4cb4637c1b4ce
Pererinha <pererinha@gmail.com>
2015-02-09 19:53:55 -0200

    get community auto shut down working #4703

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -3,23 +3,8 @@
 class Controller_api_test extends Cana_Controller {
 	public function init(){
 
-		echo '<pre>';var_dump( Crunchbutton_Admin_Notification::TYPE_SMS );exit();
+		//
+		Community::shutDownCommunities();
 
-		Crunchbutton_Message_Sms::send([
-			'to' => Crunchbutton_Support::getUsers(),
-			'message' => 'testing',
-			'reason' => Crunchbutton_Message_Sms::REASON_SUPPORT_WARNING
-		]);
-
-		// $res = Restaurant::o( 110 );
-		// echo '<pre>';var_dump( $res->adminReceiveSupportSMS() );exit();
-
-		// $admins = Crunchbutton_Support::getUsers();
-		// echo '<pre>';var_dump( $admins );exit();
-
-		// $restaurant = Crunchbutton_Restaurant::o(107);
-		// echo '<pre>';var_dump(  $restaurant->smartETA()  );exit();;
-		// $order = Order::o( 58027 );
-		// Crunchbutton_Order_Eta::create( $order );
 	}
 }
