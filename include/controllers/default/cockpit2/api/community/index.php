f037cdfc0ee9d155c07ee7c8f03102abdc56c176
Pererinha <pererinha@gmail.com>
2015-02-16 16:39:14 -0200

    create cron to check for closed communities #4613

--- include/controllers/default/cockpit2/api/community/index.php
+++ include/controllers/default/cockpit2/api/community/index.php
@@ -157,7 +157,7 @@ class Controller_api_community extends Crunchbutton_Controller_RestAccount {
 								$community->close_all_restaurants_date =  null;
 							}
 						}
-						if( $community->close_all_restaurants && $community->close_all_restaurants_note != $this->request()[ 'close_all_restaurants_note' ] ){
+						if( $community->close_all_restaurants && $this->request()[ 'close_all_restaurants_note' ] ){
 							$community->close_all_restaurants_note = $this->request()[ 'close_all_restaurants_note' ];
 						} else {
 							$community->close_all_restaurants_note = '';
@@ -174,11 +174,25 @@ class Controller_api_community extends Crunchbutton_Controller_RestAccount {
 							}
 
 						}
-						if( $community->close_3rd_party_delivery_restaurants && $community->close_3rd_party_delivery_restaurants_note != $this->request()[ 'close_3rd_party_delivery_restaurants_note' ] ){
+						if( $community->close_3rd_party_delivery_restaurants && $this->request()[ 'close_3rd_party_delivery_restaurants_note' ] ){
 							$community->close_3rd_party_delivery_restaurants_note = $this->request()[ 'close_3rd_party_delivery_restaurants_note' ];
 						} else {
 							$community->close_3rd_party_delivery_restaurants_note = '';
 						}
+
+						$dont_warn_till = $this->request()[ 'dont_warn_till_fmt' ];
+						if( $dont_warn_till && $community->close_all_restaurants && $community->close_3rd_party_delivery_restaurants ){
+							$dont_warn_till = new DateTime( $dont_warn_till, new DateTimeZone( c::config()->timezone ) );
+							$community->dont_warn_till = $dont_warn_till->format( 'Y-m-d H:i:s' );
+						} else {
+							$community->dont_warn_till = null;
+						}
+
+						if( $this->request()[ 'dont_warn_till_enabled' ] == '0' ){
+							// $community->dont_warn_till = null;
+						}
+
+
 
 						$community->save();
 
