196dcbd0567df46cf3fc86835689b5f065239db1
Jeff Tratner <jeffrey.tratner@gmail.com>
2015-02-15 20:12:49 -0800

    Fix typos, move metrics helpers into Cockpit library

--- include/controllers/default/cockpit2/api/metrics/index.php
+++ include/controllers/default/cockpit2/api/metrics/index.php
@@ -1,9 +1,5 @@
 <?php
 
-class MetricsConstants {
-	SECONDS_IN_DAY = 86400;
-}
-
 class MetricsHttpException extends Exception {
 	public $status_code;
 	public function __construct($status_code = 500, $message = null) {
@@ -15,193 +11,6 @@ class MetricsHttpException extends Exception {
 	}
 }
 
-class MetricsDateException extends Exception {
-}
-
-class Metrics_Helpers {
-	// date format for mysql groupings
-	const periodSQLFormats = [
-		'Y' => '%Y',
-		'M' => '%Y-%m', // month
-		'w' => '%Y-%U', // starting Sunday
-		'd' => '%Y-%m-%d',
-		'h' => '%Y-%m-%d %H',
-		'm' => '%Y-%m-%d %H:%i', // minute
-		's' => '%Y-%m-%d %H:%i:%s'
-	];
-
-	/**
-	* groupByIndex groups the given array of arrays by the values in groupIndex
-	* @param array $arrays - the arrays to group
-	* @param mixed $groupIndex - integer index or key to group on (checks if set on each element)
-	* @param mixed $defaultGroup (default null) - value to use when groupIndex is not set
-	* @return array $groupedArrays - groups of arrays by the values found at $groupIndex
-	*
-	* example:
-	* 	$ret = Metrics_Helpers::groupByIndex([[2, 'a', 3], [4, 'c', 5], [1, 'a', 16], []]);
-	* 	$ret == ['a' => [[2, 'a', 3], [1, 'a', 16]], 'c' => [[4, 'c', 5], null => [[]]];
-	*/
-	public static function groupByIndex($arrays, $groupIndex, $defaultGroup = null) {
-		$output = [];
-		foreach($arrays as $row) {
-			$groupValue = isset($row[$groupIndex]) ? $row[$groupIndex] : $defaultGroup;
-			if(!isset($output[$groupValue])) {
-				$arr = [];
-				$output[$groupValue] = $arr;
-			}
-			$output[$groupValue][] = $row;
-		}
-		return $output;
-	}
-
-	// whether date should be set to start or end.
-	const START_PERIOD = TRUE;
-	const END_PERIOD = FALSE;
-
-	// converts $timeString to a naive datetime using rules described by Metrics API below.
-	// (because it's far easier to work with naive datetimes internally than timestamps,
-	//  since we mostly care about Pacific-time orders)
-	public static function convertTime($timeString, $startDate = null) {
-		if(is_null($startDate)) {
-			$startDate = date_create('now');
-		} else {
-			// allow us to mutate start
-			$startDate = clone $startDate;
-		}
-		if($timeString == 'now') {
-			return $startDate;
-		} else if (is_numeric($timeString))i {
-			return date_create('@' . $timeString);
-		} else {
-			$period = substr($timeString, -1);
-			$n = substr($timeString, 0, strlen($timeString) - 1);
-			if(!is_numeric($n)) {
-				throw new MetricsDateException('time delta must be expressed as numeric value');
-			}
-			return MetricsDateHelper::modifyDate($startDate, $n, $period);
-		}
-	}
-}
-
-// wraps together a set of functions (publicly accessible via modifyDate) for
-// converting a Metrics API adjustment period into a date that starts at the
-// start of that period.
-// Valid identifiers are those as specified by the Metrics API. specify $n == 0
-// to move to edge of the current period. Expects naive-ish datetimes.
-class MetricsDateHelper {
-	// adjusts the date by the specified period, setting the date to either the start or end of the period depending on the value of $atStart
-	// see travis/Tests/MetricsTest.php for detailed examples of how this function works.
-	// but in essence moveDateToPeriod(date_create('2014-05-10'), -3, 'Y', START_PERIOD) == date_create('2011-05-10');
-	// AND moveDateToPeriod(date_create('2014-05-10', -3, 'Y', END_PERIOD) == date_create('2011-05-10 23:59:59');
-	public static function moveDateToPeriod($date, $n, $period, $atStart) {
-		// allow internal functions not to care about mutating the date
-		$date = clone $date;
-		// use the constant for clarity, even though it's just a boolean.
-		$atStart = $atStart == Metrics_Helper::START_PERIOD;
-		switch($period) {
-		case 'Y':
-			return MetricsDateHelper::_modifyYears($date, $n, $atStart);
-			break;
-		case 'M':
-			return MetricsDateHelper::_modifyMonths($date, $n, $atStart);
-			break;
-		case 'w':
-			return MetricsDateHelper::_modifyWeeks($date, $n, $atStart);
-			break;
-		case 'd':
-			return MetricsDateHelper::_modifyDays($date, $n, $atStart);
-			break;
-		case 'h':
-			return MetricsDateHelper::_modifyHours($date, $n, $atStart);
-			break;
-		case 'm':
-			return MetricsDateHelper::_modifyMinutes($date, $n, $atStart);
-			break;
-		case 's':
-			return MetricsDateHelper::_modifySeconds($date, $n, $atStart);
-			break;
-		default:
-			// not sure if we can really include the modifier with the date b/c otherwise won't be sanitized;
-			throw new MetricsDateException('invalid modifier');
-		}
-	}
-	public static function _modifyByPeriod($date, $n, $period) {
-		if($n == 0) {
-			return $date;
-		} else if($n > 0) {
-			return $date->modify('+' . $n . ' ' . $period);
-		} else {
-			return $date->modify($n . ' ' . $period);
-		}
-	}
-	// each modify function adjusts the date by the given period of time and resets it to the start of the period
-	public static function _modifySeconds($date, $n, $atStart) {
-		$date = $date->setTime($date->format('H'), $date->format('m'), $date->format('s'));
-		return MetricsDateHelper::_modifyByPeriod($date, $n, 'second');
-	}
-	public static function _modifyMinutes($date, $n, $atStart) {
-		if($atStart) {
-			$seconds = 0;
-		else {
-			$seconds = 59;
-		}
-		$date = $date->setTime($date->format('H'), $date->format('m'), $seconds);
-		return MetricsDateHelper::_modifyByPeriod($date, $n, 'minute');
-	}
-	public static function _modifyHours($date, $n, $atStart) {
-		$hours = $date->format('H')
-		if($atStart) {
-			$date = $date->setTime($hours, 0, 0);
-		} else {
-			$date = $date->setTime($hours, 59, 59);
-		}
-		return MetricsDateHelper::_modifyByPeriod($date, $n, 'hour');
-	}
-	public static function _modifyDays($date, $n, $atStart) {
-		if($atStart) {
-			$date = $date->setTime(0, 0, 0);
-		} else {
-			$date = $date->setTime(23, 59, 59);
-		}
-		return MetricsDateHelper::_modifyByPeriod($date, $n, 'day');
-	}
-	public static function _modifyWeeks($date, $n, $atStart) {
-		$daysFromSunday = $date->format('w');
-		if($atStart) {
-			// force to start on Sunday
-			$date = MetricsDateHelper::_modifyByPeriod($date, -$daysFromSunday, 'week');
-			$date = $date->setTime(23, 59, 59);
-		} else {
-			// force to Saturday
-			$date = MetricsDateHelper::_modifyByPeriod($date, (6 - $daysFromSunday), 'week');
-			$date = $date->setTime(0, 0, 0);
-		}
-		return $date;
-	}
-	public static function _modifyMonths($date, $n, $atStart) {
-		$year = $date->format('Y');
-		$month = $date->format('m');
-		$yearDelta = floor($n / 12);
-		$sign = ($n >= 0) ? 1 : -1;
-		$monthDelta = abs($n) - (12 * $yearDelta, $atStart);
-		$year += $yearDelta;
-		$month += $sign * $monthDelta;
-		$date = $date->setDate($year, $month, 1)->setTime(0, 0, 0);
-		if(!$atStart) {
-			// go to start of next month and then go one day back so we don't have to deal with different length months
-			$date = $date->modify('+1 month')->modify('-1 day')->setTime(23, 59, 59);
-		}
-		return $date;
-	}
-	public static function _modifyYears($date, $n, $atStart) {
-		$year = date('Y', strtotime($date));
-		if($atStart) {
-			return $date->setDate($year + $n, 1, 1)->setTime(0, 0, 0);
-		} else {
-			return $date->setDate($year + $n, 12, 31)->setTime(23, 59, 59);
-		}
-	}
-}
 
 // Metrics API
 //
@@ -223,12 +32,12 @@ class Controller_api_metrics extends Crunchbutton_Controller_RestAccount {
 			echo "ERROR: " . $e->$status_code . " msg: " . $e->$message;
 		}
 		try {
-			$startDate = Metrics_Helper::convertTime($_REQUEST['start'], Metrics_Helper::START_PERIOD);
+			$startDate = Cockpit_Metrics::getStartDate($_REQUEST['start']);
 		} catch (MetricsDateException $e) {
 			echo "ERROR: with start date" . $e->$message;
 		}
 		try {
-			$endDate = Metrics_Helper::convertTime($_REQUEST['end'], Metrics_Helper::END_PERIOD);
+			$endDate = Cockpit_Metrics::getEndDate($_REQUEST['end']);
 		} catch (MetricsDateException $e) {
 			echo "ERROR: with end date" . $e->$message;
 		}
@@ -255,10 +64,10 @@ class Controller_api_metrics extends Crunchbutton_Controller_RestAccount {
 
 	// constructs a query grouping orders by $period, Assumes sanitized inputs and is NOT safe from SQL injection.
 	public static function _buildOrdersQuery($communities, $startDate, $endDate, $period) {
-		if(!isset(self::periodSQLFormats[$period])) {
+		if(!isset(self::periodSQLFormats()[$period])) {
 			throw new MetricsHttpException(400, 'invalid period');
 		}
-		$periodFormat = self::periodSQLFormats[$period];
+		$periodFormat = self::periodSQLFormats()[$period];
 		$commString = '(' . implode(',', $communities) . ')';
 		$q = '
 			SELECT C.name community_name, DATE_FORMAT(O.date, ' . $periodFormat . ') date_group, MIN(date) start_date, COUNT(*) count FROM `order` O
@@ -270,7 +79,7 @@ class Controller_api_metrics extends Crunchbutton_Controller_RestAccount {
 				AND (O.likely_test IS NULL OR O.likely_test IS FALSE);
 		';
 		$queryResult = c::app()->metricsDB()->query($q);
-		return Metrics_Helper::groupByIndex($queryResult, 0);
+		return Cockpit_Metrics::groupByIndex($queryResult, 0);
 	}
 	// standard filters to get at test orders
 	const orderFilter = '
@@ -278,7 +87,7 @@ class Controller_api_metrics extends Crunchbutton_Controller_RestAccount {
 		AND restaurant.name NOT LIKE "%test%"
 		AND `order`.name NOT LIKE "%test%"
 		AND `order`.address NOT LIKE "%test%"
-		'
+		';
 	public static function dedupedOrderTable($select, $additionalFilters) {
 		$q = '
 			SELECT ' . $select . ' FROM `order` O
@@ -287,6 +96,6 @@ class Controller_api_metrics extends Crunchbutton_Controller_RestAccount {
 			WHERE community.name NOT LIKE "%test%"
 				AND restaurant.name NOT LIKE "%test%"
 				AND order.name NOT LIKE "%test%"
-			'
+			';
 	}
 }
--- travis/Tests/MetricsTest.php
+++ travis/Tests/MetricsTest.php
@@ -1,29 +1,34 @@
 <?php
-	class MetricsHelper extends PHPUnit_Framework_TestCase {
+	/**
+	 * @group Metrics
+	 **/
+	class MetricsHelperTest extends PHPUnit_Framework_TestCase {
 		public function testRelativeTimeConversion () {
 			// force import of all the names in API metrics
 			new Controller_api_metrics();
 			$end = date_create_from_format('Y-m-d', '2015-02-01');
 			/* $this->assertEqual(Metrics_Helpers::convertTime('now', $end), $end); */
-			$this->assertEqual(Metrics_Helpers::convertTime('-1d', $end, Metrics_Helper::START_PERIOD), date_create_from_format('Y-m-d', '2015-01-31'));
-			$this->assertEqual(Metrics_Helpers::convertTime('-5h', $end, Metrics_Helper::START_PERIOD), date_create_from_format('Y-m-d H:i', '2015-01-31 19:00'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-1d', $end), date_create_from_format('Y-m-d', '2015-01-31'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-5h', $end), date_create_from_format('Y-m-d H:i', '2015-01-31 19:00'));
 			// 0 <period> resets to start of period
 			$complexFormat = 'Y-m-d H:i:s';
 			$complexEnd = date_create_from_format($complexFormat, '2015-08-05 10:22:36');
-			$this->assertEqual(Metrics_Helpers::convertTime('-0d', $complexEnd, Metrics_Helper::START_PERIOD), date_create_from_format($complexFormat, '2015-08-05 00:00:00'));
-			$this->assertEqual(Metrics_Helpers::convertTime('-0d', $complexEnd, Metrics_Helper::END_PERIOD), date_create_from_format($complexFormat, '2015-08-05 23:59:59'));
-			$this->assertEqual(Metrics_Helpers::convertTime('-0h', $complexEnd, Metrics_Helper::START_PERIOD), date_create_from_format($complexFormat, '2015-08-05 10:00:00'));
-			$this->assertEqual(Metrics_Helpers::convertTime('-0m', $complexEnd, Metrics_Helper::START_PERIOD), date_create_from_format($complexFormat, '2015-08-05 10:22:00'));
-			$this->assertEqual(Metrics_Helpers::convertTime('-0M', $complexEnd, Metrics_Helper::START_PERIOD), date_create_from_format($complexFormat, '2015-08-01 00:00:00'));
-			$this->assertEqual(Metrics_Helpers::convertTime('-8M', $complexEnd, Metrics_Helper::START_PERIOD), date_create_from_format($complexFormat, '2014-12-01 00:00:00'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-0d', $complexEnd), date_create_from_format($complexFormat, '2015-08-05 00:00:00'));
+			$this->assertEqual(Cockpit_Metrics::getEndDate('-0d', $complexEnd), date_create_from_format($complexFormat, '2015-08-05 23:59:59'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-0h', $complexEnd), date_create_from_format($complexFormat, '2015-08-05 10:00:00'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-0m', $complexEnd), date_create_from_format($complexFormat, '2015-08-05 10:22:00'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-0M', $complexEnd), date_create_from_format($complexFormat, '2015-08-01 00:00:00'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-8M', $complexEnd), date_create_from_format($complexFormat, '2014-12-01 00:00:00'));
 			// should go to Sunday
-			$this->assertEqual(Metrics_Helpers::convertTime('-0W', $complexEnd, Metrics_Helper::START_PERIOD), date_create_from_format($complexFormat, '2015-08-02 00:00:00'));
+			$this->assertEqual(Cockpit_Metrics::getStartDate('-0W', $complexEnd), date_create_from_format($complexFormat, '2015-08-02 00:00:00'));
+            $this->assertEqual(Cockpit_Metrics::getStartDate('-2W', $complexEnd), date_create_from_format($complexFormat, '2015-07-19 00:00:00'));
 			// should go to Saturday at end of week
-			$this->assertEqual(Metrics_Helpers::convertTime('-0W', $complexEnd, Metrics_Helper::END_PERIOD), date_create_from_format($complexFormat, '2015-08-08 23:59:59'));
+			$this->assertEqual(Cockpit_Metrics::getEndDate('-0W', $complexEnd), date_create_from_format($complexFormat, '2015-08-08 23:59:59'));
+            $this->assertEqual(Cockpit_Metrics::getEndDate('-3W', $complexEnd), date_create_from_format($complexFormat, '2015-07-17 23:59:59'));
 			// should handle days in month correctly on weird months
-			$this->assertEqual(Metrics_Helpers::convertTime('+1M', date_create('2015-01-05'), Metrics_Helper::END_PERIOD), date_create_from_format($complexFormat, '2015-02-28 23:59:59'));
+			$this->assertEqual(Cockpit_Metrics::getEndDate('+1M', date_create('2015-01-05')), date_create_from_format($complexFormat, '2015-02-28 23:59:59'));
 			// should deal with spanning start and end of years with months
-			$this->assertEqual(Metrics_Helpers::convertTime('-3M', date_create('2015-02-05'), Metrics_Helper::END_PERIOD), date_create_from_format($complexFormat, '2014-12-31 23:59:59'));
+			$this->assertEqual(Cockpit_Metrics::getEndDate('-3M', date_create('2015-02-05')), date_create_from_format($complexFormat, '2014-12-31 23:59:59'));
 		}
 	}
 ?>
