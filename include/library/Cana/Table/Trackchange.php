0dfcd67c2f357d705a16c2a07624ca9fc25c627f
Devin Smith <github@arzynik.com>
2015-02-18 10:22:30 -0800

    fixes #4561

--- include/library/Cana/Table/Trackchange.php
+++ include/library/Cana/Table/Trackchange.php
@@ -5,9 +5,16 @@ class Cana_Table_Trackchange extends Cana_Table {
 		if ($this->{$this->idVar()}) {
 			$objectName = $this->_changeSetName ? $this->_changeSetName : get_class($this);
 			$objectName .= '_Changeset';
-			$this->_changeSet = new $objectName(Cana_Changeset::save($this));
+			$this->_changeSet = new $objectName(Cana_Changeset::save($this, $this->changeOptions() ? $this->changeOptions() : null));
 		}
 		parent::save();
+	}
+	
+	public function changeOptions($o = null) {
+		if (!is_null($o)) {
+			$this->_changeOptions = $o;
+		}
+		return $this->_changeOptions;
 	}
 
 	public function changeSet() {
