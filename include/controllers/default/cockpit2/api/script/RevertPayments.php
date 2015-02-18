1caf92d982efb6aa97c976f7b4f938a883ba7d6e
Pererinha <pererinha@gmail.com>
2015-02-18 17:15:09 -0200

    Reimbursements Fail #4789

--- include/controllers/default/cockpit2/api/script/RevertPayments.php
+++ include/controllers/default/cockpit2/api/script/RevertPayments.php
@@ -3,7 +3,7 @@
 class Controller_Api_Script_RevertPayments extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
-
+		die('hard');
 		// payment paying drivers for all shifts ever worked? #4799
 		// $payments = Payment::q( 'SELECT * FROM payment_schedule WHERE range_date = "02/09/2009 => 02/15/2015" AND pay_type = "payment" AND arbritary != 1 ' );
 		$payments = Payment_Schedule::q( "SELECT ps.* FROM payment_schedule ps WHERE DATE( ps.date ) = '2015-02-17' AND ps.pay_type = 'reimbursement' AND arbritary != 1" );
@@ -20,7 +20,7 @@ class Controller_Api_Script_RevertPayments extends Crunchbutton_Controller_RestA
 				echo $schedule->id_payment_schedule;
 				echo "\n";
 				$count++;
-				Crunchbutton_Settlement::revertPaymentByScheduleId( $schedule->id_payment_schedule );
+				// Crunchbutton_Settlement::revertPaymentByScheduleId( $schedule->id_payment_schedule );
 			} else {
 				echo "KEPT::: ";
 				echo $count . " => ";
