de5322600b1286ae9597927b695272cc42558089
mnmeyers <mmeyers218@gmail.com>
2015-02-18 11:19:01 -0800

    fixed #4507

--- include/controllers/default/crunchbutton/robots.txt/index.php
+++ include/controllers/default/crunchbutton/robots.txt/index.php
@@ -5,10 +5,10 @@ header('Content-type: text/txt');
 
 switch ($_SERVER['SERVER_NAME']) {
 	case '_DOMAIN_':
-		echo "User-agent: *\nDisallow: ";
+		echo "User-agent: *\nDisallow: /test/ ";
 		break;
 	default:
-		echo "User-agent: *\nDisallow: /";
+		echo "User-agent: *\nDisallow: /test/";
 		break;
 }
 
