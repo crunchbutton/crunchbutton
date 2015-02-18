6972c066092a752a70e7c01dd5be3287b9bb9aae
Pererinha <pererinha@gmail.com>
2015-02-18 16:08:44 -0200

    payment paying drivers for all shifts ever worked? #4799

--- include/controllers/default/cockpit2/api/script/RevertPayments.php
+++ include/controllers/default/cockpit2/api/script/RevertPayments.php
@@ -5,9 +5,10 @@
 class Controller_Api_Script_RevertPayments extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
-		die('remove this line');
-		$payments = Payment::q( 'SELECT * FROM payment_schedule WHERE range_date = "02/09/2009 => 02/15/2015" AND pay_type = "payment"' );
+		$payments = Payment::q( 'SELECT * FROM payment_schedule WHERE range_date = "02/09/2009 => 02/15/2015" AND pay_type = "payment" AND arbritary != 1 ' );
 		foreach( $payments as $payment ){
+			echo $payment->id_payment_schedule;
+			echo "\n";
 			Crunchbutton_Settlement::revertPaymentByScheduleId( $payment->id_payment_schedule );
 		}
 	}
