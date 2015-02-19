265cde130ca1d2190c34167c462ae95cb59dc058
Pererinha <pererinha@gmail.com>
2015-02-19 16:44:32 -0200

    fixed hours bug

--- include/controllers/default/crunchbutton/api/restaurant/index.php
+++ include/controllers/default/crunchbutton/api/restaurant/index.php
@@ -134,22 +134,23 @@ class Controller_api_restaurant extends Crunchbutton_Controller_Rest {
 			$json = ['error' => 'invalid object'];
 		}
 
-		// change driver restaurant name when auto shutting down community #4514
-		$community = $restaurant->community();
-		if( $community->allThirdPartyDeliveryRestaurantsClosed() || $community->allRestaurantsClosed() ){
+		if ( !$_SESSION['admin'] && !c::admin()->id_admin) {
+			// change driver restaurant name when auto shutting down community #4514
+			$community = $restaurant->community();
 			if( $community->allThirdPartyDeliveryRestaurantsClosed() || $community->allRestaurantsClosed() ){
-				// Check if the community was auto shutdown
-				$autoShutdownAdmin = Admin::login( Crunchbutton_Community::AUTO_SHUTDOWN_COMMUNITY_LOGIN );
-				$id_admin = $autoShutdownAdmin->id_admin;
-				if( $id_admin == $community->close_3rd_party_delivery_restaurants_id_admin || $id_admin == $community->close_all_restaurants_id_admin ){
-					$driverRestaurant = $community->driverRestaurant();
-					if( $driverRestaurant->id_restaurant == $restaurant->id_restaurant ){
-						$json[ 'name' ] = $community->driver_restaurant_name;
+				if( $community->allThirdPartyDeliveryRestaurantsClosed() || $community->allRestaurantsClosed() ){
+					// Check if the community was auto shutdown
+					$autoShutdownAdmin = Admin::login( Crunchbutton_Community::AUTO_SHUTDOWN_COMMUNITY_LOGIN );
+					$id_admin = $autoShutdownAdmin->id_admin;
+					if( $id_admin == $community->close_3rd_party_delivery_restaurants_id_admin || $id_admin == $community->close_all_restaurants_id_admin ){
+						$driverRestaurant = $community->driverRestaurant();
+						if( $driverRestaurant->id_restaurant == $restaurant->id_restaurant ){
+							$json[ 'name' ] = ( $community->driver_restaurant_name ? $community->driver_restaurant_name : $community->close_3rd_party_delivery_restaurants_note);
+						}
 					}
 				}
 			}
 		}
-
 
 		echo json_encode( $json );exit;
 	}
