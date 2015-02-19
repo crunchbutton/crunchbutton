87e883c715d527cc215a749b075aa7a1bc355836
Jeff Tratner <jeffrey.tratner@gmail.com>
2015-02-18 16:42:22 -0800

    Add index for test orders + remove extraneous query result fetch

--- db/migrate/000282_add_likely_test_column_to_order.sql
+++ db/migrate/000282_add_likely_test_column_to_order.sql
@@ -9,4 +9,6 @@ UPDATE `order` O
 JOIN community ON community.id_community = O.id_community
 JOIN restaurant ON restaurant.id_restaurant = O.id_restaurant
 SET likely_test = (community.name LIKE '%test%' OR restaurant.name LIKE '%test%' OR O.name LIKE '%test%' OR (O.address LIKE '%test%' AND O.address IS NOT NULL))
-WHERE likely_test IS NULL
+WHERE likely_test IS NULL;
+
+CREATE INDEX order_likely_test ON `order` (likely_test);
