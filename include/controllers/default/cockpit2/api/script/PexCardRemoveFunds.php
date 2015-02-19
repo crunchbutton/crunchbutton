08ad9ca73e1ad8be0ad56c1147cb2c0baf7e1fc8
Pererinha <pererinha@gmail.com>
2015-02-19 11:18:12 -0200

    pex card issues #4706

--- include/controllers/default/cockpit2/api/script/PexCardRemoveFunds.php
+++ include/controllers/default/cockpit2/api/script/PexCardRemoveFunds.php
@@ -6,6 +6,7 @@ class Controller_Api_Script_PexCardRemoveFunds extends Crunchbutton_Controller_R
 		$cards = Cockpit_Admin_Pexcard::q( 'SELECT * FROM admin_pexcard' );
 		$total = 0;
 		foreach( $cards as $card ){
+			$pexcard = $card;
 			if( !$card->isBusinessCard() && $card->isTestCard() ){
 				$driver = $card->admin();
 				if( !$driver->isWorking() ){
@@ -16,7 +17,7 @@ class Controller_Api_Script_PexCardRemoveFunds extends Crunchbutton_Controller_R
 						echo "\n";
 						echo "\n";
 						$total = $total + $amount;
-						$card->addFunds( [ 'action' => Crunchbutton_Pexcard_Action::ACTION_SHIFT_FINISHED, 'id_admin_shift_assign' => null, 'amount' => $amount ] );
+						$pexcard->addFunds( [ 'action' => Crunchbutton_Pexcard_Action::ACTION_SHIFT_FINISHED, 'id_admin_shift_assign' => null, 'amount' => $amount ] );
 					}
 				}
 			}
