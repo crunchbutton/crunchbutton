d2d8b72e5136fe7ed2e8143d81768def6ea721a9
Jeff Tratner <jeffrey.tratner@gmail.com>
2015-02-18 16:42:22 -0800

    Add index for test orders + remove extraneous query result fetch

--- include/library/Crunchbutton/Cron/Job/MarkLikelyTestOrders.php
+++ include/library/Crunchbutton/Cron/Job/MarkLikelyTestOrders.php
@@ -5,14 +5,12 @@ class Crunchbutton_Cron_Job_MarkLikelyTestOrders extends Crunchbutton_Cron_Log {
     // interval: minute
     // interval_unit: 20
     public function run() {
+        // updates orders that meet our likely test criteria and have not been changed recently.
         $queryResult = c::db()->query("UPDATE `order` O
             JOIN community ON community.id_community = O.id_community
             JOIN restaurant ON restaurant.id_restaurant = O.id_restaurant
             SET likely_test = (community.name LIKE '%test%' OR restaurant.name LIKE '%test%' OR O.name LIKE '%test%' OR (O.address LIKE '%test%' AND O.address IS NOT NULL))
             WHERE likely_test IS NULL");
-        // force query to run out and apply everywhere
-        while($r = $queryResult->fetch()) {
-        }
         $this->finished();
     }
 }
