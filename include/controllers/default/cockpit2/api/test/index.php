f3d6751c13d00f203e92300868f7ae6d0ee3177f
Pererinha <pererinha@gmail.com>
2015-02-21 13:00:27 -0200

    partial #4838

--- include/controllers/default/cockpit2/api/test/index.php
+++ include/controllers/default/cockpit2/api/test/index.php
@@ -2,9 +2,21 @@
 
 class Controller_api_test extends Cana_Controller {
 	public function init(){
+		// echo '<pre>';var_dump( $_REQUEST[ 'cockpit' ], $_SERVER['HTTP_HOST'] );exit();
+		// test
+		// $agent = Crunchbutton_Agent::getAgent();
 
-		//
-		Community::shutDownCommunities();
+
+		// $dt = '2015-02-20 08:02:03';
+
+		// $restaurant = Restaurant::o( 828 );
+		// echo '<pre>';var_dump( $restaurant->open( $dt ) );exit();
+
+		// $community = Community::o( 70 );
+
+		// echo '<pre>';var_dump( $community->forceCloseLog() );exit();;
+		// $community->reopenAutoClosedCommunity( $dt );
+		// Community::shutDownCommunities();
 
 	}
 }
