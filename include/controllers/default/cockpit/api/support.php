7dfccbe3bfaa10c1028c0e2baa4203173005a50a
Pererinha <pererinha@gmail.com>
2015-02-23 14:38:42 -0300

    SMS to drivers should show up in history logs #4773

--- include/controllers/default/cockpit/api/support.php
+++ include/controllers/default/cockpit/api/support.php
@@ -29,6 +29,7 @@ class Controller_api_support extends Crunchbutton_Controller_RestAccount {
 			case 'send-sms':
 				$message = $this->request()[ 'message' ];
 				$phones = explode( ';', $this->request()[ 'phones' ] );
+				$phones = array_unique( $phones );
 
 				foreach( $phones as $phone ){
 					$admin = Crunchbutton_Admin::getByPhone( $phone );
@@ -46,6 +47,9 @@ class Controller_api_support extends Crunchbutton_Controller_RestAccount {
 						'message' => $_message,
 						'reason' => Crunchbutton_Message_Sms::REASON_SUPPORT
 					] );
+
+					Crunchbutton_Support::createNewWarning(  [ 'dont_open_ticket' => true, 'body' => $_message, 'phone' => $phone ] );
+
 				}
 				echo json_encode( [ 'success' => true ] );
 				exit();
