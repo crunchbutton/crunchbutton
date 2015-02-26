20eabe2895a21cd13c5950b0f4142705371bf559
Devin Smith <github@arzynik.com>
2015-02-26 13:32:16 -0800

    php rendertest

---
+++ travis/Tests/RenderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class RenderTest extends PHPUnit_Framework_TestCase {
+	public function testRender() {
+		$file = file_get_contents('http://localhost/');
+		echo $file;
+		$this->assertTrue(strpos($file, '</html>'));
+	}
+}
+
