4783490e87a6111f62bbc8ca70dc541cb7294c51
Devin Smith <github@arzynik.com>
2015-03-03 10:44:53 -0800

    more tests

--- www/cron-heroku.php
+++ www/cron-heroku.php
@@ -5,9 +5,13 @@ set_time_limit(100);
 
 require_once('../include/crunchbutton.php');
 
+echo 'Testing...';
+
 $res = c::mailgun()->sendMessage(c::config()->mailgun->domain, [
 	'from' 		=> 'test@_DOMAIN_',
 	'to'		=> '_EMAIL',
 	'subject'	=> 'this is a test',
 	'html'		=> 'this is some <b>content</b>'
 ]);
+
+throw new Exception('This is a test');
