b6441b15d24fb63b6ea51751d134a5c72467962e
Devin Smith <github@arzynik.com>
2015-03-03 15:52:45 -0800

    worker test

--- cli/cron-heroku.php
+++ cli/cron-heroku.php
@@ -1,23 +1,16 @@
 #!/usr/bin/env php
 <?php
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors',true);
-set_time_limit(100);
+//set_time_limit(100);
 
 
 
-echo 'Testing...';
+echo "Starging worker...\n";
 
 require_once('/app/include/crunchbutton.php');
 
-$res = c::mailgun()->sendMessage(c::config()->mailgun->domain, [
-	'from' 		=> 'test@_DOMAIN_',
-	'to'		=> 'arzynik@gmail.com',
-	'subject'	=> 'this is a test',
-	'html'		=> 'this is some <b>content</b>'
-]);
-
-
-
-throw new Exception('exception test');
-
+while (true) {
+	echo "Worker running...\n";
+	sleep(15);
+}
