f86531517a7c330aebed04c4d5c75f01d7aa9d20
Devin Smith <github@arzynik.com>
2015-03-04 13:07:21 -0800

    query fix

--- include/controllers/default/cockpit2/api/tickets/index.php
+++ include/controllers/default/cockpit2/api/tickets/index.php
@@ -23,7 +23,7 @@ class Controller_api_tickets extends Crunchbutton_Controller_RestAccount {
 				-WILD-
 			FROM support s
 			INNER JOIN support_message sm ON s.id_support = sm.id_support
-			LEFT JOIN user u ON u.id_user=s.id_user
+			LEFT JOIN `user` u ON u.id_user=s.id_user
 			LEFT JOIN `order` o ON o.id_order=s.id_order
 			LEFT JOIN admin a ON a.id_admin=sm.id_admin
 			WHERE 1=1
@@ -79,10 +79,12 @@ class Controller_api_tickets extends Crunchbutton_Controller_RestAccount {
 
 		$q .= '
 			ORDER BY s.datetime DESC
-			LIMIT ?, ?
+			LIMIT ?
+			OFFSET ?
 		';
-		$keys[] = $offset;
 		$keys[] = $limit;
+		$keys[] = $offset;
+		
 
 		// do the query
 		$d = [];
