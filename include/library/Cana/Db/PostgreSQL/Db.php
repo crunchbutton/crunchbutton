544f8d2cc8507964380307cc67c66f45f94218c0
Devin Smith <github@arzynik.com>
2015-03-04 09:51:32 -0800

    replace unix timestamps

--- include/library/Cana/Db/PostgreSQL/Db.php
+++ include/library/Cana/Db/PostgreSQL/Db.php
@@ -41,6 +41,9 @@ class Cana_Db_PostgreSQL_Db extends Cana_Db_Base {
 		// replace add single quotes to interval statements
 		$query = preg_replace('/(interval) ([0-9]+) ([a-z]+)/i','\\1 \'\\2 \\3\'', $query);
 		
+		// replace unix_timestamp
+		$query = preg_replace('/unix_timestamp( )?\((.*?)\)/i','extract(epoch FROM \\2)', $query);
+		
 		// replace date formats
 		$query = preg_replace_callback('/date_format\(( )?(.*?),( )?("(.*?)"|\'(.*?)\')( )?\)/i',function($m) {
 			$find = ['/\%Y/', '/\%m/', '/\%d/', '/\%H/', '/\%i/', '/\%s/', '/\%W/'];
