5ebbbf11e98e883540842a2537108ea96e04764e
mnmeyers <mmeyers218@gmail.com>
2015-03-09 12:08:36 -0700

    fixed #5025 partial #5006

--- include/controllers/default/crunchbutton/api/test/email.php
+++ include/controllers/default/crunchbutton/api/test/email.php
@@ -1,8 +1,8 @@
 <?php
 class Controller_api_test_email extends Crunchbutton_Support  {
 public function init(){
-    $some_var = new Crunchbutton_Cron_Job_CSTicketsDigest;
-    $some_var->run();
+    $other_var = new Crunchbutton_Cron_Job_CSTicketsDigest;
+    $other_var->run($some_var);
 
     exit();
     }
