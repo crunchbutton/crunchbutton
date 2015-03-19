89b25790134ee085f4a45211676b6f4e8879e70d
Pererinha <pererinha@gmail.com>
2015-03-19 17:16:40 -0300

    retroactive points fixer and script #4849

--- include/controllers/default/cockpit2/api/script/RetroactivePoints.php
+++ include/controllers/default/cockpit2/api/script/RetroactivePoints.php
@@ -4,6 +4,8 @@ class Controller_Api_Script_RetroactivePoints extends Crunchbutton_Controller_Re
 
 	public function init() {
 
+		die('waiting: retroactive points fixer and script #4849');
+
 		Crunchbutton_Reward_Retroactively::rewardReferralRetroactively();
 
 		$reward = new Crunchbutton_Reward_Retroactively;
