ae1a8c51d5e1a8fb538048c8af3b337996bcc36f
mnmeyers <mmeyers218@gmail.com>
2015-03-23 11:10:04 -0700

    Revert "partial #5006 fixed #5071"
    This reverts commit 54c57bc67f4d00b1ade7337fe8bc426ef54af609.

--- db/migrate/000293_order_action_add_enum.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `order_action` CHANGE `type` `type`
-ENUM('delivery-pickedup','delivery-accepted','delivery-rejected','delivery-delivered','delivery-transfered','restaurant-accepted','restaurant-rejected','restaurant-ready', 'delivery-text-5min')
-CHARACTER SET utf8
-COLLATE utf8_general_ci
-NULL  DEFAULT NULL;
