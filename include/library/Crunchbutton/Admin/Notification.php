c49c1eb458530c82c5c7348392f572f6d36c2692
mnmeyers <mmeyers218@gmail.com>
2015-03-25 16:14:41 -0700

    fixed #5135

--- include/library/Crunchbutton/Admin/Notification.php
+++ include/library/Crunchbutton/Admin/Notification.php
@@ -26,7 +26,9 @@ class Crunchbutton_Admin_Notification extends Cana_Table {
 		$type_delivery = Crunchbutton_Order::SHIPPING_DELIVERY;
 		$orderFromLast = ' 3 HOUR';
 
-		$query = "SELECT * FROM `order` o WHERE o.delivery_type = '{$type_delivery}' AND o.delivery_service = true AND o.date > DATE_SUB(NOW(), INTERVAL {$orderFromLast} ) AND o.date < DATE_SUB(NOW(), INTERVAL 5 MINUTE) ORDER BY o.id_order ASC";
+		$query = "SELECT * FROM `order` o WHERE o.delivery_type = '{$type_delivery}' AND o.delivery_service = true AND o.date > DATE_SUB(NOW(), INTERVAL {$orderFromLast} ) AND o.date < DATE_SUB(NOW(), INTERVAL 5 MINUTE)
+AND o.name not like '%test%'
+ORDER BY o.id_order ASC";
 
 		$orders = Crunchbutton_Order::q($query);
 
