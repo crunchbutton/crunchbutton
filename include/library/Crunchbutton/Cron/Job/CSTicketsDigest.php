8032a7952a67211155e7174291c3eef5a4d3d27e
Pererinha <pererinha@gmail.com>
2015-03-30 23:25:17 -0300

    send a digest of all CS tickets since last digest every morning at 10 am #5006

--- include/library/Crunchbutton/Cron/Job/CSTicketsDigest.php
+++ include/library/Crunchbutton/Cron/Job/CSTicketsDigest.php
@@ -3,22 +3,22 @@
 
 class Crunchbutton_Cron_Job_CSTicketsDigest extends Crunchbutton_Cron_Log {
 
-    public function run($params){
+	public function run($params){
 
-        //Crunchbutton_Newusers::sendEmailCLI();
+		$supports = Crunchbutton_Support::q('SELECT
+			support.*
+			FROM support
+			WHERE
+			support.type != "WARNING"
+			AND support.datetime > date_sub(now(), interval 1 day)
+			ORDER BY support.id_support ASC
+			limit 250' );
 
-        $supports = Crunchbutton_Support::q('SELECT
-            support.*
-            FROM support
-            WHERE
-            support.type != "WARNING"
-            AND support.datetime > date_sub(now(), interval 1 day)
-            ORDER BY support.id_support ASC
-            limit 250');
-        $params = array('users' => Crunchbutton_Support::getUsers(), 'messages'=> $supports);
-        $some_var = new Crunchbutton_Email_CSDigest($params);
-//var_dump($supports);
-        // it always must call finished method at the end
-        $this->finished();
-    }
+		$params = array( 'to' => 'digests@_DOMAIN_', 'messages'=> $supports );
+		$email = new Crunchbutton_Email_CSDigest($params);
+		$email->send();
+
+		// it always must call finished method at the end
+		$this->finished();
+	}
 }
