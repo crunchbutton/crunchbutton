c445b3dbd974c38d0e2b1e48d4a9a62abcec1146
Devin Smith <github@arzynik.com>
2015-03-31 14:23:26 -0700

    debug info

--- include/library/Crunchbutton/Message/Push/Android.php
+++ include/library/Crunchbutton/Message/Push/Android.php
@@ -66,6 +66,11 @@ class Crunchbutton_Message_Push_Android extends Crunchbutton_Message {
 		$result = json_decode(curl_exec($ch));
 		curl_close($ch);
 		
+		if (!$result->success) {
+			print_r($fields);
+			print_r($result);
+		}
+		
 		return $result->success ? true : false;
 
 	}
