cec776196420c05ea97abfb9efe1c5615f7c9fd2
Pererinha <pererinha@gmail.com>
2015-03-31 14:24:40 -0300

    partial #5191

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -3,7 +3,12 @@
 class Controller_Api_Test_Sandbox extends Cana_Controller {
 	public function init(){
 
-		// some code here
+		$order = Order::o( $_GET[ 'id_order' ] );
+		if( $order->id_order ){
+			echo '<pre>';var_dump( $order->refund() );exit();;
+		} else {
+			echo '<pre>';var_dump( 'error' );exit();
+		}
 
 	}
 }
