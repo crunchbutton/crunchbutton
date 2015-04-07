1a23f0e66ec39173aa3fe559827b4f0e40b67d44
Pererinha <pererinha@gmail.com>
2015-04-07 14:05:27 -0300

    partial #5006

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -3,5 +3,10 @@
 class Controller_Api_Test_Sandbox extends Cana_Controller {
 	public function init(){
 		// nothing here
+		$tickets = Crunchbutton_Support::dailyDigest( 1 );
+		$params = array( 'to' => 'digests@_DOMAIN_', 'tickets' => $tickets );
+		$email = new Crunchbutton_Email_CSDigest( $params );
+		// $email->send();
+		echo $email->message();
 	}
 }
