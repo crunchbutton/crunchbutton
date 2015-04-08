7f8db792a802f53ffcae9bfd8e5fe42e4f6d4063
Pererinha <pererinha@gmail.com>
2015-04-08 18:38:24 -0300

    Some restaurants are getting blank orders #5297

--- include/library/Crunchbutton/Email/Order.php
+++ include/library/Crunchbutton/Email/Order.php
@@ -1,6 +1,7 @@
 <?php
 
 class Crunchbutton_Email_Order extends Crunchbutton_Email {
+
 	private $_mailConfig;
 
 	public function __construct($params) {
@@ -9,7 +10,7 @@ class Crunchbutton_Email_Order extends Crunchbutton_Email {
 		$params['subject'] = 'Customer Order #'.$params['order']->id_order;
 		$params['from'] = ($params['order']->id_restaurant == 1 ? 'One Button Wenzel' : 'Crunchbutton').' <support@_DOMAIN_>';
 		$params['reply'] = 'Crunchbutton <support@_DOMAIN_>';
-		$params['reason']			= Crunchbutton_Email_Address::REASON_ORDER;
+		$params['reason'] = Crunchbutton_Email_Address::REASON_ORDER;
 
 		$this->buildView($params);
 		$this->view()->subject = $params['subject'];
