97230444a31314a4be7bb93486cbf7a7f4461565
Devin Smith <github@arzynik.com>
2015-04-14 14:49:16 -0700

    temp solution for #5358

--- include/library/Crunchbutton/Community.php
+++ include/library/Crunchbutton/Community.php
@@ -48,6 +48,43 @@ class Crunchbutton_Community extends Cana_Table_Trackchange {
 		}
 		return $this->_restaurantsByLoc;
 	}
+	
+	public function type() {
+		if (!isset($this->_type)) {
+			$rs = $this->restaurants();
+			$third = false;
+			$first = false;
+			$takeout = false;
+
+			foreach ($rs as $r) {
+				if ($r->delivery_service && $r->delivery) {
+					$third = true;
+					continue;
+				}
+				if (!$r->delivery_service) {
+					$first = true;
+					continue;
+				}
+				if ($r->takeout) {
+					$takeout = true;
+					continue;
+				}
+			}
+			
+			if (!$third && !$first && $takeout) {
+				$this->_type = 'takeout';
+			} elseif($third && $first) {
+				$this->_type = 'both';
+			} elseif ($third) {
+				$this->_type = '3rd';
+			} elseif ($first) {
+				$this->_type = '1st';
+			} else {
+				$this->_type = 'none';
+			}
+		}
+		return $this->_type;
+	}
 
 	/**
 	 * Returns all the restaurants that belong to this Community
