4b9097e4da36af58ce40347f7d0826153986ffb8
Pererinha <pererinha@gmail.com>
2015-04-16 13:16:56 -0300

    Add stripe payments to restaurant settlement #5248

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -1,8 +1,23 @@
 <?php
 
-class Controller_Api_Test_Sandbox extends Cana_Controller {
+class Controller_Api_Test_Sandbox extends Crunchbutton_Controller_Account {
 
 	public function init() {
-		//
+
+		$env = 'dev';
+		\Stripe\Stripe::setApiKey(c::config()->stripe->{$env}->secret);
+
+
+		// Create a transfer to the specified recipient
+		$transfer = \Stripe\Transfer::create(array(
+		  "amount" => 1000, // amount in cents
+		  "currency" => "usd",
+		  "recipient" => 'rp_15sFdPJMXBWnTQ4r8KsQgby1',
+		  "bank_account" => 'ba_15sFdCJMXBWnTQ4rDVBYSciO',
+		  "statement_descriptor" => "Testing")
+		);
+
+		echo '<pre>';var_dump( $transfer );exit();
+
 	}
 }
