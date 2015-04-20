ec2943664855b9c0062fb18f3b02a7a820153108
Devin Smith <github@arzynik.com>
2015-04-20 13:41:44 -0700

    update to Jasmin 2.2
    - updated jasmine library
    - created a new console reporter based on old one
    - updated page tests
    - moved page rendering into run.js
    - fixed ssl errors with external libraries

--- travis/phantom/tests/Page.js
+++ travis/phantom/tests/Page.js
@@ -1,158 +1,43 @@
-var page = require('webpage').create();
-page.settings.webSecurityEnabled = false;
+/*
+	Jasmin 2.0+
+*/
 
-console.log('Reading ' + phantom.url);
+var assert = require('chai').assert;
 
 describe('Page', function() {
-	
-	var content = false;
-
-	beforeEach(function() {
-		page.open(phantom.url, function(status) {
-			var onPageReady = function() {
-				setTimeout(function() {
-					content = page.content;
-				},5000);
-			};
-			
-			function checkReadyState() {
-		        setTimeout(function() {
-		            var readyState = page.evaluate(function() {
-		                return document.readyState;
-		            });
-		
-		            if (readyState === 'complete') {
-		                onPageReady();
-		            } else {
-		                checkReadyState();
-		            }
-		        });
-		    }
-		
-		    checkReadyState();
-		});
+	var content = phantom.content;
+	beforeEach(function (done) {
+		content = phantom.content;
+		done();
 	});
-	
-	// for some reason this is needed so that the next test gets content
-    it('should wait 5 seconds for content -- DEBUG', function() {
-		
-		waitsFor(function() {
-			return content;
-		});
-
-	    runs(function() {
-			expect(true).toBeTruthy();
-	    });
-    });
 
     it('should finish rendering', function() {
+		//assert.equals(page.content.match(/\<\/html\>/i) ? true : false, true);
+		expect(content.match(/facebook-jssdk/i)).toBeTruthy();
 		
-		waitsFor(function() {
-			return content;
-		});
-
-	    runs(function() {
-			console.log('rendered');
-			//console.log(content);
-			expect(content.match(/facebook-jssdk/i)).toBeTruthy();
-	    });
     });
-    
-    it('should be javascript bundled', function() {
-		
-		waitsFor(function() {
-			return content;
-		});
 
-	    runs(function() {
-			expect(content.match(/bundle.js/i)).toBeTruthy();
-	    });
+    it('should be javascript bundled', function() {
+		expect(content.match(/bundle.js/i)).toBeTruthy();
     });
     
     it('should be css bundled', function() {
-		
-		waitsFor(function() {
-			return content;
-		});
-
-	    runs(function() {
-			expect(content.match(/bundle.css/i)).toBeTruthy();
-	    });
+		expect(content.match(/bundle.css/i)).toBeTruthy();
     });
     
-    it('should contain App object', function() {
-	    
-	    var done = false;
-		
-		waitsFor(function() {
-			return content;
+    it('should contain App object', function(done) {
+		var service = page.evaluate(function() {
+			return App.service;
 		});
-
-	    runs(function() {
-			var service = page.evaluate(function() {
-				return App.service;
-			});
-			expect(service).toBeTruthy();
-			done = true;
-	    });
-	    
-		waitsFor(function() {
-			return done;
-		});
+		expect(service).toBeTruthy();
+		done();
     });
     
-    it('should contain an Angular rootscope object', function() {
-	    
-	    var done = false;
-		
-		waitsFor(function() {
-			return content;
+    it('should contain an Angular rootscope object', function(done) {
+		var service = page.evaluate(function() {
+			return App.rootScope.$id;
 		});
-
-	    runs(function() {
-			var service = page.evaluate(function() {
-				return App.rootScope.$id;
-			});
-			expect(service).toBeTruthy();
-			done = true;
-	    });
-	    
-		waitsFor(function() {
-			return done;
-		});
+		expect(service).toBeTruthy();
+		done();
     });
  });
-
-
-
-
-
-/*
-this is a partial example if we ever decide to use Jasmin 2.0+
-
-var assert = require('chai').assert;
-var page = require('webpage').create();
-var url = phantom.libraryPath.match(/arzynik/i) ? 'http://crunchbutton.localhost/' : 'http://localhost/';
-
-describe("Page output", function() {
-	
-	var content;
-	
-	beforeEach(function (done) {
-		page.open(url, function(status) {
-			console.log(page.content);
-			content = page.content;
-			done();
-		});
-	});
-
-    it("should have a closing html tag", function() {
-
-		console.log(page.content);
-		//assert.equals(page.content.match(/\<\/html\>/i) ? true : false, true);
-		expect('asd').toBe(false);
-		
-    });
- });
-
-*/
