8b2b3ad033a29101e470a5bbfe522446e1dba714
Devin Smith <github@arzynik.com>
2015-04-22 11:19:09 -0700

    partial #5414

--- include/library/Crunchbutton/Queue/Driver.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class Crunchbutton_Queue_Driver extends Crunchbutton_Queue {
-}
--- include/library/Crunchbutton/Queue/Order.php
+++ include/library/Crunchbutton/Queue/Order.php
@@ -17,13 +17,32 @@ class Crunchbutton_Queue_Order extends Crunchbutton_Queue {
 		// send customer a receipt in 30 seconds
 		$q = Queue::create([
 			'type' => 'order-receipt',
-			'id_order' => $this->id_order,
+			'id_order' => $this->order()->id_order,
 			'seconds' => 30
 		]);
 		
+		// send restaurants notifications
+		$this->order()->notifyRestaurants();
 		
-		// notify people
-		$this->order()->notify();
+		// get active community drivers
+		$drivers = $this->order()->getDriversToNotify();
+		
+		// perform delivery logistics only if there are multiple drivers and it is enabled
+		if ($this->order()->community()->delivery_logistics && $drivers->count() > 1) {
+			$l = new Order_Logistics($this->order());
+
+			// queue notifications for drivers at specific times
+			foreach ($l->drivers() as $driver) {
+				$q = Queue::create([
+					'type' => 'notification-driver',
+					'id_order' => $this->order()->id_order,
+					'id_admin' => $driver->id_admin,
+					'seconds' => $driver->seconds
+				]);
+			}
+		} else {
+			$this->order()->notifyDrivers();
+		}
 		
 		return self::STATUS_SUCCESS;
 	}
