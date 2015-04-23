8a60ede619af54d5b9d066cc75b62e8219fecb1b
Pererinha <pererinha@gmail.com>
2015-04-23 16:35:22 -0300

    pex reports: double-counting orders after midnight #5364

---
+++ db/migrate/000313_pexcard_transaction.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `pexcard_transaction` ADD `transactionTime_pst` DATETIME NULL;
--- include/library/Crunchbutton/Cron/Job/PexLoadTransactions.php
+++ include/library/Crunchbutton/Cron/Job/PexLoadTransactions.php
@@ -5,7 +5,7 @@ class Crunchbutton_Cron_Job_PexLoadTransactions extends Crunchbutton_Cron_Log {
 	public function run(){
 
 		Crunchbutton_Pexcard_Transaction::loadTransactions();
-
+		Crunchbutton_Pexcard_Transaction::convertTimeZone();
 		// it always must call finished method at the end
 		$this->finished();
 	}
