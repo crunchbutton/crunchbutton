53c0a90c5ceca204c493d625e361dba8b20d69f0
Pererinha <pererinha@gmail.com>
2015-04-30 12:13:13 -0300

    Allow search on Settlement Drivers list #5453

--- www/assets/cockpit/js/controllers.settlement.js
+++ www/assets/cockpit/js/controllers.settlement.js
@@ -823,7 +823,7 @@ NGApp.controller( 'SettlementDriversPaymentsCtrl', function ( $scope, $rootScope
 			$scope.ready = false;
 			SettlementService.drivers.payments({
 				'page': $scope.query.page,
-				'id_driver': $scope.query.driver,
+				'search': $scope.query.search,
 				'pay_type': $scope.query.type,
 				'payment_status': $scope.query.status
 			}, function( data ){
