cb14662873eba04b84b1fab1b5cd189ceaae3150
Pererinha <pererinha@gmail.com>
2015-05-07 08:25:05 -0300

    Add "Per-Order Pay" Column to Driver Settlement #5325

--- include/controllers/default/cockpit2/api/settlement/index.php
+++ include/controllers/default/cockpit2/api/settlement/index.php
@@ -4,7 +4,10 @@ class Controller_Api_Settlement extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
 
-		$this->resultsPerPage = 10;
+		$this->_driverBegin();
+		exit();
+
+		$this->resultsPerPage = 20;
 
 		if( !c::admin()->permission()->check( ['global', 'settlement' ] ) ){
 			$this->_error();
