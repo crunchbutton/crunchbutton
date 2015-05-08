936523b0e147b632b3d5ef06b6858e9989b89152
Devin Smith <github@arzynik.com>
2015-05-08 13:56:48 -0700

    added cache adapters

---
+++ include/library/Cana/Cache/File.php
@@ -0,0 +1,37 @@
+<?php
+
+class Cana_Cache_File extends Cana_Model {
+	public $suffix = '.cache';
+
+	public function __construct($params) {
+		if (isset($params->dir)) {
+			$this->dir = c::config()->dirs->cache.$params->dir.'/';
+		}
+		if (isset($params->expire)) {
+			$this->expire = $params->expire;
+		}
+		if (isset($params->suffix)) {
+			$this->suffix = $params->suffix;
+		}
+	}
+	
+	public function cached($fileName, $expire = null) {
+		if (file_exists($this->dir.sha1($fileName).$suffix) && filemtime($this->dir.sha1($fileName).$suffix) < time()+(!is_null($expire) ? $expire : $this->expire)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public function read($fileName) {
+		return unserialize(file_get_contents($this->dir.sha1($fileName).$suffix));
+	}
+
+	public function write($fileName, $file) {
+		return file_put_contents($this->dir.sha1($fileName).$suffix, serialize($file));
+	}
+	
+	public function mtime($fileName) {
+		return filemtime($this->dir.sha1($fileName).$suffix);
+	}
+}
---
+++ include/library/Cana/Cache/Redis.php
@@ -0,0 +1,35 @@
+<?php
+
+class Cana_Cache_Redis extends Cana_Model {
+	public function __construct($params) {
+		if (isset($params->server)) {
+			$this->server = $params->server;
+		}
+		if (isset($params->expire)) {
+			$this->expire = $params->expire;
+		}
+		
+		$this->redis = new Predis\Client(array(
+			'host' => parse_url($this->server, PHP_URL_HOST),
+			'port' => parse_url($this->server, PHP_URL_PORT),
+			'password' => parse_url($this->server, PHP_URL_PASS),
+		));
+	}
+	
+	public function cached($fileName, $expire = null) {
+		$v = $this->read($fileName);
+		return $v ? $v : false;
+	}
+	
+	public function read($fileName) {
+		return $this->redis->get($filename);
+	}
+
+	public function write($fileName, $file) {
+		return $this->redis->set($filename, $file);
+	}
+	
+	public function mtime($fileName) {
+		return filemtime($this->dir.sha1($fileName).$suffix);
+	}
+}
