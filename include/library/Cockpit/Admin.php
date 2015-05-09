c6a108cdbee978ad66a1fae1a54176426009d4c4
Devin Smith <github@arzynik.com>
2015-05-09 09:23:08 -0700

    fixed query

--- include/library/Cockpit/Admin.php
+++ include/library/Cockpit/Admin.php
@@ -44,8 +44,8 @@ class Cockpit_Admin extends Crunchbutton_Admin {
 	}
 
 	public function location() {
-		if (!isset($this->_location)) {
-			$this->_location = Admin_Location::q('SELECT * FROM admin_location WHERE id_admin="'.$this->id_admin.'" ORDER BY date DESC LIMIT 1')->get(0);
+		if (!isset($this->_location) && $this->id_admin) {
+			$this->_location = Admin_Location::q('SELECT * FROM admin_location WHERE id_admin=? ORDER BY date DESC LIMIT 1', [$this->id_admin])->get(0);
 		}
 		return $this->_location;
 	}
