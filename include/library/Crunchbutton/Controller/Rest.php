26037e97ef51b7ecc79a9b21b8943df4ef0abf6e
Devin Smith <github@arzynik.com>
2015-05-11 12:40:01 -0700

    only return header errors when requested

--- include/library/Crunchbutton/Controller/Rest.php
+++ include/library/Crunchbutton/Controller/Rest.php
@@ -3,47 +3,52 @@
 class Crunchbutton_Controller_Rest extends Cana_Controller_Rest {
 	
     public function __construct() {
-		ini_set('display_errors', 0);
-
-		register_shutdown_function(function() use ($q) {
-			$e = error_get_last();
-			
-
-			// i think E_ERROR is the only one we care about
-			switch($e['type']) {
-				case E_ERROR:
-				case E_PARSE:
-				case E_CORE_ERROR:
-				case E_CORE_WARNING:
-				case E_COMPILE_ERROR:
-				case E_USER_ERROR:
-				case E_USER_NOTICE:
-				case E_RECOVERABLE_ERROR:
-					header('PHP-Fatal-Error: '. json_encode($e));
-					header('HTTP/1.0 500 Server Error');
-					break;
-			}
-		});
+		$headers = getallheaders();
+		$this->headerErrors = $headers['X-Requested-With'] == 'XMLHttpRequest' || $headers['Http-Error'] ? true : false;
 		
-		c::app()->exceptionHandler(function($e) {
-			$backtracels = [];
-			foreach($e->getTrace() as $k=>$v){
-				if ($v['function'] == "include" || $v['function'] == "include_once" || $v['function'] == "require_once" || $v['function'] == "require"){
-					$backtracels[] = "#".$k." ".$v['function']."(".$v['args'][0].") called at [".$v['file'].":".$v['line']."]";
-				} else {
-					$backtracels[] = "#".$k." ".$v['function']."() called at [".$v['file'].":".$v['line']."]";
+		if ($this->headerErrors) {
+			ini_set('display_errors', 0);
+
+			register_shutdown_function(function() use ($q) {
+				$e = error_get_last();
+
+
+				// i think E_ERROR is the only one we care about
+				switch($e['type']) {
+					case E_ERROR:
+					case E_PARSE:
+					case E_CORE_ERROR:
+					case E_CORE_WARNING:
+					case E_COMPILE_ERROR:
+					case E_USER_ERROR:
+					case E_USER_NOTICE:
+					case E_RECOVERABLE_ERROR:
+						header('PHP-Fatal-Error: '. json_encode($e));
+						header('HTTP/1.0 500 Server Error');
+						break;
 				}
-			}
-			
-			$error = [
-				'message' => $e->getMessage(),
-				'type' => 'throw',
-				'trace' => $backtracels
-			];
-			
-			header('PHP-Fatal-Error: '. json_encode($error));
-			header('HTTP/1.0 500 Server Error');
-		});
+			});
+
+			c::app()->exceptionHandler(function($e) {
+				$backtracels = [];
+				foreach($e->getTrace() as $k=>$v){
+					if ($v['function'] == "include" || $v['function'] == "include_once" || $v['function'] == "require_once" || $v['function'] == "require"){
+						$backtracels[] = "#".$k." ".$v['function']."(".$v['args'][0].") called at [".$v['file'].":".$v['line']."]";
+					} else {
+						$backtracels[] = "#".$k." ".$v['function']."() called at [".$v['file'].":".$v['line']."]";
+					}
+				}
+
+				$error = [
+					'message' => $e->getMessage(),
+					'type' => 'throw',
+					'trace' => $backtracels
+				];
+
+				header('PHP-Fatal-Error: '. json_encode($error));
+				header('HTTP/1.0 500 Server Error');
+			});
+		}
 
     	$find = '/(api\.|log\.)/';
     	if (preg_match($find, $_SERVER['SERVER_NAME'])) {
