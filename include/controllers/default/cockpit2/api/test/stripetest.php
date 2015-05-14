1fe4f0b52652485387f4f64437c932d33f8c1af1
Devin Smith <github@arzynik.com>
2015-05-14 09:45:58 -0700

    added some bad users

--- include/controllers/default/cockpit2/api/test/stripetest.php
+++ include/controllers/default/cockpit2/api/test/stripetest.php
@@ -1,26 +1,35 @@
 <?php
 
+$badusers = [
+	7497,
+	7507,
+	2549,
+	7485,
+	7515,
+	7517,
+	7518,
+	7520,
+	7526
+];
+
+foreach ($badusers as $u) {
+	$q .= ' and `user`.id_user != '.$u.' ';
+}
+
 $p = Crunchbutton_User::q('
 	select count(p.id_user_payment_type) c, `user`.* from user_payment_type p
 	left join `user` using(id_user)
 	where p.balanced_id is not null
 	and p.stripe_id is null
+	'.$q.'
 	group by user.id_user
 	having c = 1
 	order by c desc
-	limit 50
+	limit 20
 ');
 
-$max = 10;
-
 foreach ($p as $user) {
-	if ($i >= $max) {
-		break;
-	}
-	$status = $user->tempConvertBalancedToStripe();
-	if ($status) {
-		$i++;
-	}
+	$user->tempConvertBalancedToStripe();
 }
 
 echo "\n\nALL DONE";
