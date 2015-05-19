f6908ce5a0152d5abfc4727ab083e9c2ff5aa44c
Pererinha <pererinha@gmail.com>
2015-05-18 21:42:29 -0300

    partial #5634

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -4,26 +4,13 @@ class Controller_Api_Test_Sandbox extends Crunchbutton_Controller_Account {
 
 	public function init() {
 
+		echo '<pre>';
 
-		echo '<pre>';var_dump(  Crunchbutton_Promo::userHasAlreadyUsedDiscountCode( 'daniel', '***REMOVED***' )  );exit();;
-
-		$giftcards = Crunchbutton_Promo::validateNotesField( 'daniel', 1, '***REMOVED***' );
-		foreach ( $giftcards[ 'giftcards' ] as $giftcard ) {
-			if( $giftcard->id_promo ){
-				echo '<pre>';var_dump( $giftcard->id_promo );exit();
-				// if( !$giftCardAdded ){
-
-					echo json_encode( $giftcard->properties() );exit;
-					$this->giftcardValue = $giftcard->value;
-					$giftCardAdded = true;
-					break;
-				// }
-			}
-		}
-// echo json_encode( $giftcards );exit;
-		// $restaurant = Restaurant::o( 789 );
-		// echo '<pre>';var_dump( $restaurant->smartETA( true ) );exit();
-
+		// $dt = '2015-05-10 18:56:00';
+		// $community = Crunchbutton_Community::o( 126 );
+		// $community->shutDownCommunity( $dt );
+		$notification = new Crunchbutton_Admin_Notification();
+		$notification->resendNotification();
 
 	}
 }
