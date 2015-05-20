2ef1004c946b7f9ba41e89ea385d59c0c10e616f
Pererinha <pererinha@gmail.com>
2015-05-20 19:54:34 -0300

    Log order errors #5544

--- include/library/Crunchbutton/Log/Monitor/Credit/Card/Fail.php
+++ include/library/Crunchbutton/Log/Monitor/Credit/Card/Fail.php
@@ -25,7 +25,7 @@ class Crunchbutton_Log_Monitor_Credit_Card_Fail extends Crunchbutton_Log_Monitor
 	// check if it has 4 or more registers in the last hour
 	public function fourOrMoreLogsLastHour(){
 		// Check if there were more than 4 logs at the last hour
-		$query = 'SELECT * FROM log_monitor WHERE type = ? AND date < DATE_SUB( NOW(), INTERVAL 1 HOUR )';
+		$query = 'SELECT * FROM log_monitor WHERE type = ? AND date > DATE_SUB( NOW(), INTERVAL 1 HOUR )';
 		$logs = Crunchbutton_Log_Monitor::q( $query, [ self::type() ] );
 		if( $logs->count() >= 4 ){
 			$body = 'Important: In last 1 hour we had ' . $logs->count() . ' credit card errors!';
