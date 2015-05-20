745b18433ad60ba80d2a12d039f906be6ad8b2fc
Pererinha <pererinha@gmail.com>
2015-05-20 09:28:33 -0300

    add rechecking of resources if endpoint fails #5598

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -4,14 +4,14 @@ class Controller_Api_Test_Sandbox extends Crunchbutton_Controller_Account {
 
 	public function init() {
 
-		$order = Order::o( 157201 );
+		// $order = Order::o( 157201 );
 		// $order->pexcardFunds();
 
 
-
-		$status = $order->status()->last();
+//
+		// $status = $order->status()->last();
 		// echo '<pre>';var_dump( $status[ 'driver' ] );exit();
-		echo '<pre>';var_dump( $status );exit();
+		// echo '<pre>';var_dump( $status );exit();
 
 		if( $_GET && $_GET[ 'id_queue' ] ){
 			echo $_GET[ 'id_queue' ];
