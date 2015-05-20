2b7dcde5c20d99b9eadadcc78059957ae3bdb377
Pererinha <pererinha@gmail.com>
2015-05-20 20:06:11 -0300

    Log order errors #5544

--- db/migrate/000330_cron_log.sql
+++ db/migrate/000330_cron_log.sql
@@ -1,3 +1,3 @@
 INSERT INTO `cron_log` (`description`, `class`, `start_date`, `interval`, `interval_unity`, `current_status`, `next_time`, `finished`, `interactions`)
 VALUES
-	( 'Monitor log table ', 'Crunchbutton_Cron_Job_RestaurantFixNotify', '2014-05-20 09:00:00', 'minute', 10, 'idle', null, null, 0);
+	( 'Monitor log table ', 'Crunchbutton_Cron_Job_LogMonitor', '2014-05-20 09:00:00', 'minute', 10, 'idle', null, null, 0);
