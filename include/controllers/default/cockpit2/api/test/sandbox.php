bb3de8085bcac4d769336fb0224008b39d754d66
Pererinha <pererinha@gmail.com>
2015-05-21 16:24:26 -0300

    change the cron to use 3rd database #5659

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -3,31 +3,13 @@
 class Controller_Api_Test_Sandbox extends Crunchbutton_Controller_Account {
 	public function init() {
 
-		// $cron = new Crunchbutton_Cron_Log;
-		// $cron->start();
-		// cliEnv
-		// travis
-
-
-		$connect = c::config()->db->{ 'crondb' };
-
-		if ($connect->encrypted) {
-			$connect->user = c::crypt()->decrypt($connect->user);
-			$connect->pass = c::crypt()->decrypt($connect->pass);
+		$properties = ( array ) c::db()->dbo();
+		foreach( $properties as $p ){
+			if( $p->host ){
+				$host = $p->host;
+			}
 		}
 
-		$db = new Cana_Db($connect);
-
-		$users = $db->get( 'SELECT * FROM admin ORDER BY id_admin ASC LIMIT 1 ' );
-		echo '<pre>';var_dump( $users );exit();
-
-		Cana::timeout( function() use( $job ) {
-			Log::debug([
-					'action' => 'login at crondb',
-					'type' => 'changed-db'
-				]);
-		}, 0, true, 'crondb' );
-		// $monitor = new Crunchbutton_Cron_Job_LogMonitor;
-		// $monitor->run();
+		echo '<pre>';var_dump( [ 'desc' => 'testing the cron log', 'host' => $host, 'type' => 'cron-jobs' ] );exit();
 	}
 }
