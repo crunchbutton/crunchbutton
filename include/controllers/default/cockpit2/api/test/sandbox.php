3908d5a9210c393c43a15e8e111235d3059eb6e5
Pererinha <pererinha@gmail.com>
2015-05-21 09:36:10 -0300

    test confgdb connection

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -9,10 +9,21 @@ class Controller_Api_Test_Sandbox extends Crunchbutton_Controller_Account {
 		// travis
 
 
+		$connect = c::config()->db->{ 'crondb' };
+
+		if ($connect->encrypted) {
+			$connect->user = c::crypt()->decrypt($connect->user);
+			$connect->pass = c::crypt()->decrypt($connect->pass);
+		}
+
+		$db = new Cana_Db($connect);
+
+		$users = $db->get( 'SELECT * FROM admin ORDER BY id_admin ASC LIMIT 1 ' );
+		echo '<pre>';var_dump( $users );exit();
 
 		Cana::timeout( function() use( $job ) {
 			Log::debug([
-					'action' => 'log at travis',
+					'action' => 'login at crondb',
 					'type' => 'changed-db'
 				]);
 		}, 0, true, 'crondb' );
