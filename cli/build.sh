d14e08481e6fd0a8d45a92d7c164606bc2ad965e
Devin Smith <github@arzynik.com>
2015-05-24 11:54:53 -0700

    build tests

--- cli/build.sh
+++ cli/build.sh
@@ -3,6 +3,11 @@
 ./_build.php crunchbutton.com | tr -d "\t\n\r" > ../www/build/crunchbutton.html
 ./_build.php cockpit.la | tr -d "\t\n\r" > ../www/build/cockpit.html
 
+echo $1
+ls $1
+php -r "echo 'test';"
+/usr/bin/env php -r "echo 'test2';"
+
 cat ../www/.htaccess ../www/.htaccess.heroku > ../www/.htaccess.move
 mv ../www/.htaccess.move ../www/.htaccess
 rm -f ../www/.htaccess.heroku
