f25c1566977c2b4dd25dc589766fb9034b0539be
Devin Smith <github@arzynik.com>
2015-05-24 11:38:32 -0700

    htaccess and build changes

--- cli/build.sh
+++ cli/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./_build.php crunchbutton.com | tr -d "\t\n\r" > ../www/build/crunchbutton.html
 ./_build.php cockpit.la | tr -d "\t\n\r" > ../www/build/cockpit.html
+cat ../www/.htaccess ../www/.htaccess.heroku > ../www/.htaccess
