647da1cca2f44225574e5772bfdca4d1d5ab7162
Devin Smith <github@arzynik.com>
2015-05-24 12:03:32 -0700

    build tests

--- cli/build.sh
+++ cli/build.sh
@@ -2,11 +2,8 @@
 
 PHPPATH=$1/.heroku/php/bin/php
 
-./_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > ../www/build/crunchbutton.html
-./_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > ../www/build/cockpit.html
-
-echo $PHPPATH
-$PHPPATH -r "echo 'test';"
+$PHPPATH -f ./_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > ../www/build/crunchbutton.html
+$PHPPATH -f ./_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > ../www/build/cockpit.html
 
 
 cat ../www/.htaccess ../www/.htaccess.heroku > ../www/.htaccess.move
