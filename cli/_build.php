9f319f1f92121b708d6add76962ab2b3fc3a3e8f
Devin Smith <github@arzynik.com>
2015-05-25 13:19:05 -0700

    build tests

--- cli/_build.php
+++ cli/_build.php
@@ -4,9 +4,11 @@
 // create static assets
 
 $host = $argv[1];
+$uri = $argv[2];
 
 error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ini_set('display_errors',true);
 $_REQUEST['__host'] = $_SERVER['HTTP_HOST'] = $_SERVER['SERVER_NAME'] = $host;
+$_REQUEST['__url'] = $uri;
 require_once('../include/crunchbutton.php');
 Cana::app()->displayPage();
