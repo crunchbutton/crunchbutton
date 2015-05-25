7f1946bdd3bea20e1b6c9f2dae1b65c55e97275c
Devin Smith <github@arzynik.com>
2015-05-25 15:44:29 -0700

    more build stuff

--- cli/build.sh
+++ cli/build.sh
@@ -5,10 +5,10 @@ PHPPATH=$1/.heroku/php/bin/php
 $PHPPATH -f ./_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > ../www/build/crunchbutton.html
 $PHPPATH -f ./_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > ../www/build/cockpit.html
 
-$PHPPATH -f ./_build.php crunchbutton.com /assets/css/bundle.css?s=style $PHPPATH | tr -d "\t\n\r" > ../www/assets/cockpit/css/bundle.css
+$PHPPATH -f ./_build.php crunchbutton.com /assets/css/bundle.css?s=style $PHPPATH | tr -d "\t\n\r" > ../www/assets/css/bundle.css
 $PHPPATH -f ./_build.php cockpit.la /assets/cockpit/css/bundle.css?s=cockpit $PHPPATH | tr -d "\t\n\r" > ../www/assets/cockpit/css/bundle.css
 
-$PHPPATH -f ./_build.php crunchbutton.com /assets/js/bundle.js?s=app $PHPPATH | tr -d "\t\n\r" > ../www/assets/cockpit/js/bundle.js
-$PHPPATH -f ./_build.php cockpit.la /assets/cockpit/js/bundle.js?s=cockpit $PHPPATH | tr -d "\t\n\r" > ../www/assets/cockpit/css/bundle.js
+$PHPPATH -f ./_build.php crunchbutton.com /assets/js/bundle.js?s=app $PHPPATH | tr -d "\t\n\r" > ../www/assets/js/bundle.js
+$PHPPATH -f ./_build.php cockpit.la /assets/cockpit/js/bundle.js?s=cockpit $PHPPATH | tr -d "\t\n\r" > ../www/assets/cockpit/js/bundle.js
 
 mv ../www/.htaccess.heroku ../www/.htaccess
