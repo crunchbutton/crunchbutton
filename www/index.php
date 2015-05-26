60bfbc9970aef5f4c7cfa2fd33b99e774d39ecd5
Devin Smith <github@arzynik.com>
2015-05-26 10:56:51 -0700

    more server routing changes

--- www/index.php
+++ www/index.php
@@ -19,6 +19,15 @@ ini_set('zlib.output_compression_level',9);
 if (isset($_REQUEST['__url']) && $_REQUEST['__url'] == 'index.php') {
 	$_REQUEST['__url'] = '';
 }
+// no reason to pass __url
+if (!$_REQUEST['__url']) {
+	$request = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+	$dir = dirname($_SERVER['SCRIPT_NAME']);
+	$base = substr($dir, -1) == '/' ? $dir : $dir.'/';
+	$url = preg_replace('/^'.str_replace('/','\\/',''.$dir).'/','',$request);
+	$url = substr($url, 0, 1) == '/' ? $url : '/'.$url;
+	$_REQUEST['__url'] = substr($url, 1);
+}
 
 if (isset($_GET['__host'])) {
 	setcookie('__host', $_GET['__host'], 0, '/');
