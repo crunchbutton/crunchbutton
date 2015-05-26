8ea5adb6dc0688fbb57060e90aed1ea27d1162da
Devin Smith <github@arzynik.com>
2015-05-26 10:56:38 -0700

    server routing changes

--- include/controllers/default/crunchbutton/assets/scss/index.php
+++ include/controllers/default/crunchbutton/assets/scss/index.php
@@ -3,8 +3,7 @@
 class Controller_assets_scss extends Crunchbutton_Controller_AssetBundle {
 	public function init() {
 		$path = c::config()->dirs->www.'assets/scss/';
-		$file = c::getPagePiece(2).'.scss';
-
+		$file = c::getPagePiece(2);
 		Scss::serve($path.$file);
 	}
 }
