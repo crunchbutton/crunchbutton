f26ef5474adeba85c7f9d2773cfe688291400c95
Pererinha <pererinha@gmail.com>
2015-05-28 21:01:42 -0300

    create customer suggestions page in cockpit.la #5474

--- include/controllers/default/cockpit/suggestions/index.php
+++ include/controllers/default/cockpit/suggestions/index.php
@@ -3,17 +3,19 @@
 class Controller_suggestions extends Crunchbutton_Controller_Account {
 	public function init() {
 
+		header( 'location:http://cockpit.la/suggestions' );
+
 		if (!c::admin()->permission()->check(['global', 'suggestions-all', 'suggestions-list-page'])) {
 			return ;
 		}
-		
+
 		$suggestion = Suggestion::o(c::getPagePiece(1));
 		c::view()->page = 'suggestions';
 
 		if( $suggestion->id_suggestion ){
 			// Show the suggestion's form
 			c::view()->suggestion = $suggestion;
-			c::view()->display('suggestions/suggestion');	
+			c::view()->display('suggestions/suggestion');
 		} else {
 			// Show the suggestions's list
 			c::view()->display('suggestions/index');
