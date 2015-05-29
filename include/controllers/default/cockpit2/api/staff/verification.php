df0a5c9974e78d408bd27fbbdc74457739018be0
Devin Smith <github@arzynik.com>
2015-05-29 11:52:11 -0700

    added methods to reprocess driver verification

---
+++ include/controllers/default/cockpit2/api/staff/verification.php
@@ -0,0 +1,23 @@
+<?php
+
+class Controller_api_staff_verification extends Crunchbutton_Controller_RestAccount {
+	public function init() {
+		if (!c::admin()->permission()->check(['global', 'permission-all', 'permission-users'])) {
+			$this->error(401);
+		}
+		
+		$admins = Admin::q('
+			select admin.*
+			from admin
+			left join admin_payment_type using(id_admin)
+			where
+				admin.active=true
+				and admin_payment_type.stripe_id is not null
+				and admin_payment_type.verified=false
+		');
+		
+		foreach ($admins as $admin) {
+			$admin->autoStripeVerify();
+		}
+	}
+}
