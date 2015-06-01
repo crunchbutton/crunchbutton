cc53a8230a0c090ed8046bd61e1b647f0b4efbc0
Pererinha <pererinha@gmail.com>
2015-06-01 17:08:58 -0300

    partial #5359

--- include/controllers/default/cockpit2/api/test/sandbox.php
+++ include/controllers/default/cockpit2/api/test/sandbox.php
@@ -3,9 +3,7 @@
 class Controller_Api_Test_Sandbox extends Crunchbutton_Controller_Account {
 	public function init() {
 
-		$admin = Admin::o( 5 );
-		echo '<pre>';var_dump( $admin->communitiesHeDeliveriesFor() );exit();
-
+		die('hard');
 
 	}
 }
