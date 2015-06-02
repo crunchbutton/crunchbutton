e8fb887cdcb8c8d6bbc38969f2fc3c9419d8fccb
Devin Smith <github@arzynik.com>
2015-06-02 12:38:14 -0700

    removed old suggestion code

--- include/library/Crunchbutton/Suggestion.php
+++ include/library/Crunchbutton/Suggestion.php
@@ -35,63 +35,6 @@ class Crunchbutton_Suggestion extends Cana_Table {
 		]);
 	}
 
-	public static function find($search = []) {
-		$query = 'SELECT `suggestion`.* FROM `suggestion` LEFT JOIN restaurant USING(id_restaurant) WHERE id_suggestion IS NOT NULL ';
-
-		if ($search['type']) {
-			$query .= " and type='".$search['type']."' ";
-		}
-
-		if ($search['status']) {
-			$query .= " and status='".$search['status']."' ";
-		}
-
-		if ($search['start']) {
-			$s = new DateTime($search['start']);
-			$query .= " and DATE(`date`)>='".$s->format('Y-m-d')."' ";
-		}
-
-		if ($search['end']) {
-			$s = new DateTime($search['end']);
-			$query .= " and DATE(`date`)<='".$s->format('Y-m-d')."' ";
-		}
-
-		if ($search['restaurant']) {
-			$query .= ' and `suggestion`.id_restaurant='.$search['restaurant'].' ';
-		}
-
-		if ($search['community']) {
-			$query .= ' and `suggestion`.id_community='.$search['community'].' ';
-		}
-
-		if ($search['search']) {
-			$qn =  '';
-			$q = '';
-			$searches = explode(' ',$search['search']);
-			foreach ($searches as $word) {
-				if ($word{0} == '-') {
-					$qn .= ' and `suggestion`.name not like "%'.substr($word,1).'%" ';
-					$qn .= ' and `suggestion`.content not like "%'.substr($word,1).'%" ';
-				} else {
-					$q .= '
-						and (`suggestion`.name like "%'.$word.'%"
-						or `suggestion`.content like "%'.$word.'%")
-					';
-				}
-			}
-			$query .= $q.$qn;
-		}
-
-		$query .= 'ORDER BY `date` DESC';
-
-		if ($search['limit']) {
-			$query .= ' limit '.$search['limit'].' ';
-		}
-
-		$suggestions = self::q($query);
-		return $suggestions;
-	}
-
 	public function restaurant() {
 		return Restaurant::o($this->id_restaurant);
 	}
