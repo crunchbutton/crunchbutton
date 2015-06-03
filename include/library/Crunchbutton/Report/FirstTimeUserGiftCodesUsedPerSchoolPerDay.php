c47a44d134629e58de67d5fe4f055d7764d50c42
Pererinha <pererinha@gmail.com>
2015-06-03 07:47:53 -0300

    partial #5584 - Gift Card Report

--- include/library/Crunchbutton/Report/FirstTimeUserGiftCodesUsedPerSchoolPerDay.php
+++ include/library/Crunchbutton/Report/FirstTimeUserGiftCodesUsedPerSchoolPerDay.php
@@ -12,9 +12,9 @@ class Crunchbutton_Report_FirstTimeUserGiftCodesUsedPerSchoolPerDay extends Cana
 									(SELECT SUM(1) orders,
 													phone
 									 FROM `order` o
-									 WHERE DATE_FORMAT(o.date, '%%Y%%m%%d') <= '%s'
+									 WHERE o.date <= '%s 23:59:59'
 									 GROUP BY o.phone
-									 HAVING orders = 1) orders ON orders.phone = o.phone AND DATE_FORMAT( o.date, '%%Y%%m%%d' ) = '%s'
+									 HAVING orders = 1) orders ON orders.phone = o.phone AND o.date BETWEEN '%s 00:00:00' AND '%s 23:59:59'
 								INNER JOIN
 									(SELECT o.id_order
 									 FROM `order` o
@@ -23,7 +23,7 @@ class Crunchbutton_Report_FirstTimeUserGiftCodesUsedPerSchoolPerDay extends Cana
 									 INNER JOIN credit giftcard ON giftcard.id_credit = c.id_credit_debited_from
 									 AND giftcard.id_promo IS NOT NULL
 									 AND giftcard.credit_type = 'cash'
-									 AND c.credit_type = 'cash' WHERE DATE_FORMAT( o.date, '%%Y%%m%%d' ) = '%s'
+									 AND c.credit_type = 'cash' WHERE o.date BETWEEN '%s 00:00:00' AND '%s 23:59:59'
 								) giftcard ON giftcard.id_order = o.id_order
 								INNER JOIN restaurant_community rc ON rc.id_restaurant = o.id_restaurant
 								INNER JOIN community c ON rc.id_community = c.id_community
@@ -35,7 +35,7 @@ class Crunchbutton_Report_FirstTimeUserGiftCodesUsedPerSchoolPerDay extends Cana
 
 		for( $i = $start; $i <= $end; $i++ ){
 			$date = strval( $i );
-			$query = sprintf( $pattern, $date, $date, $date, $date );
+			$query = sprintf( $pattern, $date, $date, $date, $date, $date, $date );
 			$results = c::db()->get( $query );
 			if( count( $results ) ){
 				$formatted_date = new DateTime( $date, new DateTimeZone( c::config()->timezone ) );
