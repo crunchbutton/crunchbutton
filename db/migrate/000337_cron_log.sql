bd019b2afb34f3d2163c7f0a3825392345810316
Pererinha <pererinha@gmail.com>
2015-06-08 20:07:57 -0300

    add community hours to community exports #4901

--- db/migrate/000337_cron_log.sql
+++ db/migrate/000337_cron_log.sql
@@ -1,3 +1,3 @@
 INSERT INTO `cron_log` (`description`, `class`, `start_date`, `interval`, `interval_unity`, `current_status`, `next_time`, `finished`, `interactions`)
 VALUES
-	( 'Monitor log table ', 'Crunchbutton_Cron_Job_CommunityClosedMessage', '2014-06-08 09:00:00', 'minute', 10, 'idle', null, null, 0);
+	( 'Community closed messages ', 'Crunchbutton_Cron_Job_CommunityClosedMessage', '2014-06-08 09:00:00', 'minute', 10, 'idle', null, null, 0);
