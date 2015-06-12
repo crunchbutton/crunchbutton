b455ba378ee1f1bbafd29ff22362f3232125d387
Pererinha <pererinha@gmail.com>
2015-06-11 21:28:32 -0300

    partial #5754

--- travis/Tests/SettlementTest.php
+++ travis/Tests/SettlementTest.php
@@ -46,7 +46,7 @@ class SettlementTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( $totals[ 'gift_card' ], 0 );
 		$this->assertEquals( $totals[ 'delivery_fee_collected' ] , -3 );
 		$this->assertEquals( $totals[ 'standard_reimburse' ] , 39.75 );
-		$this->assertEquals( $totals[ 'total_reimburse' ], 0 );
+		$this->assertEquals( $totals[ 'total_reimburse' ], 39.75 );
 		$this->assertEquals( $totals[ 'total_payment' ], 14.6 );
 
 	}
