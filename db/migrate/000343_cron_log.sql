4b0963391f7aeb8eb1beffb3c1343ed69122d789
Pererinha <pererinha@gmail.com>
2015-06-15 17:09:29 -0300

    create phone lookup table #4169

---
+++ db/migrate/000343_cron_log.sql
@@ -0,0 +1,3 @@
+INSERT INTO `cron_log` (`description`, `class`, `start_date`, `interval`, `interval_unity`, `current_status`, `next_time`, `finished`, `interactions`)
+VALUES
+	( 'Keeps the phone table updated', 'Crunchbutton_Cron_Job_UpdatePhoneTable', '2014-06-15 13:00:00', 'minute', 5, 'idle', null, null, 0);
