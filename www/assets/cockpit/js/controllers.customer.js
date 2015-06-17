5d56d0ba4bc6d67be11751e73044045977c7ea54
Pererinha <pererinha@gmail.com>
2015-06-16 22:26:47 -0300

    cockpit.la: create credits listview page #5730

--- www/assets/cockpit/js/controllers.customer.js
+++ www/assets/cockpit/js/controllers.customer.js
@@ -6,7 +6,13 @@ NGApp.config(['$routeProvider', function($routeProvider) {
 			templateUrl: 'assets/view/customers.html',
 			reloadOnSearch: false
 
-		}).when('/customer/:id', {
+		})
+		.when('/customer/credit/:id', {
+			action: 'customer',
+			controller: 'CustomerCreditCtrl',
+			templateUrl: 'assets/view/customers-credit.html'
+		})
+		.when('/customer/:id', {
 			action: 'customer',
 			controller: 'CustomerCtrl',
 			templateUrl: 'assets/view/customers-customer.html'
@@ -34,6 +40,27 @@ NGApp.controller('CustomersCtrl', function ($rootScope, $scope, CustomerService,
 	});
 });
 
+NGApp.controller('CustomerCreditCtrl', function ($rootScope, $routeParams, $scope, CreditService, CustomerService, ViewListService) {
+
+	CustomerService.get($routeParams.id, function(d) {
+		$rootScope.title = d.name + ' | Credits';
+		$scope.customer = d;
+	});
+
+	angular.extend($scope, ViewListService);
+
+	$scope.view({
+		scope: $scope,
+		watch: {},
+		update: function() {
+			CreditService.history( $routeParams.id, function(d) {
+				$scope.credits = d.results;
+				$scope.complete(d);
+			});
+		}
+	});
+});
+
 NGApp.controller('CustomerCtrl', function ($scope, $routeParams, $interval, CustomerService, OrderService, CreditService, $rootScope) {
 
 	$scope.loading = true;
@@ -42,7 +69,7 @@ NGApp.controller('CustomerCtrl', function ($scope, $routeParams, $interval, Cust
 		$rootScope.title = d.name + ' | Customers';
 		$scope.customer = d;
 
-		if ($scope.customer.address.indexOf(',') > -1) {
+		if ($scope.customer && $scope.customer.address && $scope.customer.address.indexOf(',') > -1) {
 			var address = $scope.customer.address.split(',');
 			$scope.customer.address = address.shift() + "\n" + address.join(',');
 		}
@@ -51,17 +78,20 @@ NGApp.controller('CustomerCtrl', function ($scope, $routeParams, $interval, Cust
 	});
 
 	var credits = function(){
-		CreditService.history( $routeParams.id, function( d ){
+		CreditService.status( $routeParams.id, function( d ){
 			$scope.credits = d;
+			console.log('$scope.credits',$scope.credits);
 		} );
 	}
 
-	OrderService.list({user: $routeParams.id}, function(d) {
-		$scope.orders = d.results;
-		$scope.count = d.count;
-		$scope.pages = d.pages;
-		$scope.loading = false;
-	});
+	$scope.orders = function(){
+		OrderService.list({user: $routeParams.id}, function(d) {
+			$scope.orders = d.results;
+			$scope.count = d.count;
+			$scope.pages = d.pages;
+			$scope.loading = false;
+		});
+	}
 
 	credits();
 
--- www/assets/cockpit/js/service.credit.js
+++ www/assets/cockpit/js/service.credit.js
@@ -3,6 +3,7 @@ NGApp.factory( 'CreditService', function( $rootScope, $resource, ResourceFactory
 	var credit = $resource( App.service + 'credit/:action/:id_user/:method', { action: '@action', id_user: '@id_user', method: '@method' }, {
 				'add' : { 'method': 'POST', params : { action: 'add' } },
 				'history' : { 'method': 'GET', params : { action: 'log', method: 'history' } },
+				'status' : { 'method': 'GET', params : { action: 'log', method: 'status' } },
 			}
 		);
 
@@ -11,6 +12,12 @@ NGApp.factory( 'CreditService', function( $rootScope, $resource, ResourceFactory
 	service.add = function( params, callback ){
 		credit.add( params, function( json ){
 			callback( json );
+		} );
+	}
+
+	service.status = function( id_user, callback ){
+		credit.status( { 'id_user': id_user }, function( data ){
+			callback( data );
 		} );
 	}
 
