bfdf7a917e0decdb1ae587ece096df907232d143
Pererinha <pererinha@gmail.com>
2015-06-22 19:06:41 -0300

    testing restaccount

--- include/library/Crunchbutton/Controller/RestAccount.php
+++ include/library/Crunchbutton/Controller/RestAccount.php
@@ -3,18 +3,18 @@
 class Crunchbutton_Controller_RestAccount extends Crunchbutton_Controller_Rest {
     public function __construct() {
     	if (c::config()->site->name == 'Cockpit2') {
-    		if (!c::admin()->id_admin) {
+    		if (!c::admin()->id_admin || !c::user()->id_admin) {
     			// just prevents any other pages from being displayed since this is a blank page basicly
     			$this->error(401);
     		}
-	    	
+
     	} else {
 			if (!c::admin()->id_admin) {
 			    header('WWW-Authenticate: Basic realm="Crunchbutton - '.$_SERVER['PHP_AUTH_USER'].'"');
 			    $this->error(401);
 			}
 		}
-		
+
 		parent::__construct();
     }
 }
