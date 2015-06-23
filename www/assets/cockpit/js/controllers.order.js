83fec3dd582b00e6afbb4c29b58339e024977d2d
Pererinha <pererinha@gmail.com>
2015-06-23 16:36:10 -0300

    cockpit.la: Ability to re-assign an order and change status #5801

--- www/assets/cockpit/js/controllers.order.js
+++ www/assets/cockpit/js/controllers.order.js
@@ -287,7 +287,21 @@ NGApp.controller('OrderDeliveryStatusCtrl', function ( $scope, $rootScope, Order
 	} );
 
 	$scope.driverChanged = function(){
+		processOptions();
+	}
+
+	var processOptions = function(){
 		$scope.notify_customer = ( id_driver != $scope.status.driver.id_admin );
+		$scope.statuses = [];
+		angular.forEach( OrderService.statuses, function( value, key ) {
+			var add = true;
+			if( ( value.value == 'rejected' && id_driver != $scope.status.driver.id_admin ) ){
+				add = false;
+			}
+			if( add ){
+				$scope.statuses.push( value );
+			}
+		} );
 	}
 
 	var load = function(){
@@ -299,13 +313,12 @@ NGApp.controller('OrderDeliveryStatusCtrl', function ( $scope, $rootScope, Order
 			if( data && data.driver && data.driver.id_admin ){
 				id_driver = data.driver.id_admin;
 			}
+			processOptions();
 		} );
 		DriverService.byCommunity( id_community, function( data ){
 			$scope.drivers = data;
 		} );
 	}
-
-	$scope.statuses = OrderService.statuses;
 
 	$scope.text5MinAway = function(){
 
