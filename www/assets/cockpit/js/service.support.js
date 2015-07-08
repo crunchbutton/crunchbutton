329a7ceb7f68a36435111d6589d7ec621cc6b56d
Pererinha <pererinha@gmail.com>
2015-07-07 22:23:29 -0300

    when clicking on a ticket on the sidepane cockpit, load side first #5914

--- www/assets/cockpit/js/service.support.js
+++ www/assets/cockpit/js/service.support.js
@@ -1,4 +1,4 @@
-NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams, NotificationService, AccountService, SocketService, TicketService) {
+NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams, $timeout, NotificationService, AccountService, SocketService, MainNavigationService, TicketService) {
 
 	var service = {
 		isTyping: false,
@@ -7,9 +7,126 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 		socket: SocketService.socket
 	};
 
+	service.sideInfo = { id_support: null };
+
+	service.sideInfo.setTicket = function( id_support ){
+		service.sideInfo.id_support = id_support;
+		service.sideInfo.reset();
+	}
+
+	service.sideInfo.reset = function(){
+		service.sideInfo.data = { id_support: service.sideInfo.id_support, messages: [], page: 0, loaded: 0, total: null, has_more: false };
+		service._private = { first_load: true, current_scroll: 0, could_load: true };
+		service.sideInfo.update_controller()
+	};
+
+	service.sideInfo.scroll = function( action ){
+
+		var container = $('.support-chat-contents-scroll');
+		switch( action ){
+			case 'begin':
+				$timeout( function(){
+					container.stop( true, false ).animate( { scrollTop: container[0].scrollHeight }, 0 );
+					service.sideInfo.release();
+				}, 300 );
+				break;
+			case 'current':
+				service._private.current_scroll = container[0].scrollHeight;
+				break;
+			default:
+				var position = action;
+				setTimeout(function() {
+					var finalScroll = container[0].scrollHeight;
+					var goToScroll = finalScroll - service._private.current_scroll;
+					container[0].scrollTop = goToScroll;
+					service.sideInfo.release();
+				}, 100 );
+				break;
+		}
+	}
+
+	service.sideInfo.release = function(){
+		setTimeout(function() { service._private.could_load = true }, 500 );
+	}
+
+	service.sideInfo.add_message = function( message ){
+		service.sideInfo.data.messages.push( message );
+		service.sideInfo.data.total++;
+		service.sideInfo.data.loaded++;
+		service.sideInfo.update_controller();
+		service.sideInfo.scroll( 'begin' );
+	}
+
+	service.sideInfo.load_ticket_page = function(){
+		if( service.sideInfo.id_support ){
+			MainNavigationService.link( '/ticket/' + service.sideInfo.id_support );
+		}
+	}
+
+	service.sideInfo.update_controller = function(){
+		$rootScope.$broadcast( 'triggerTicketInfoUpdated', service.sideInfo.data );
+	}
+
+	service.sideInfo.load = function(){
+
+		if( service.sideInfo.id_support ){
+
+			if( !service._private.could_load ){
+				return false;
+			}
+
+			service._private.could_load = false;
+
+			service.sideInfo.data.page++;
+
+			var params = { id_support: service.sideInfo.id_support, page: service.sideInfo.data.page };
+
+			TicketService.side_info( params, function( data ){
+
+				service.sideInfo.scroll( 'current' );
+
+				service.sideInfo.data.pexcard = data.pexcard;
+				service.sideInfo.data.restaurant = data.restaurant;
+				service.sideInfo.data.order = data.order;
+				service.sideInfo.data.total = data.messages.total;
+
+				var messages = [];
+				for( x in data.messages.list ){
+					if( data.messages.list[ x ].id_support_message ){
+						messages.push( data.messages.list[ x ] );
+					}
+				}
+
+				for( x in service.sideInfo.data.messages ){
+					if( service.sideInfo.data.messages[ x ].id_support_message ){
+						messages.push( service.sideInfo.data.messages[ x ] );
+					}
+				}
+
+				service.sideInfo.data.messages = messages;
+
+				service.sideInfo.data.loaded = service.sideInfo.data.messages.length;
+
+				service.sideInfo.data.has_more = ( service.sideInfo.data.loaded >= service.sideInfo.data.total ) ? false : true;
+
+				service.sideInfo.update_controller();
+
+				if( service._private.first_load ){
+					service.sideInfo.scroll( 'begin' );
+					service._private.first_load = false;
+					service.sideInfo.load_ticket_page();
+				} else {
+					service.sideInfo.scroll();
+				}
+
+			} );
+			return true;
+		}
+	};
+
 	service.setViewTicket = function(id) {
 		service.scope.viewTicket = id;
-		service.messages = [];
+		service.sideInfo.setTicket( id );
 	};
 
 	$rootScope.$on('triggerViewTicket', function(e, ticket) {
@@ -19,6 +136,7 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 		if( !$rootScope.supportToggled ){
 
 			service.scope.viewTicket = ticket;
+			service.scope.ticket = ticket;
 
 			$rootScope.supportToggled = true;
 
@@ -92,22 +210,20 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 	service.send = function(message, add_as_note, callback) {
 		var add_as_note = ( add_as_note ? true : false );
 		var guid = App.guid();
-		if( !service.id_support ){
+		if( !service.sideInfo.id_support ){
 			return;
 		}
-		TicketService.message({
-			id_support: service.id_support,
+		TicketService.message( {
+			id_support: service.sideInfo.id_support,
 			body: message,
 			guid: guid,
 			note: add_as_note
 		}, function(d) {
-			console.log('d',d);
-			for (var x in service.messages) {
-				console.log('service.messages[x].guid',service.messages[x].guid);
-				if (service.messages[x].guid == guid) {
+			for ( var x in service.sideInfo.data.messages ) {
+				if ( service.sideInfo.data.messages[x].guid == guid ) {
 					d.guid = guid;
-					service.messages[x] = d;
-					notified.push(d.id_support_message);
+					service.sideInfo.data.messages[x] = d;
+					notified.push( d.id_support_message );
 					break;
 				}
 			}
@@ -117,7 +233,7 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 			callback()
 		} else {
 			service.scope.$apply(function() {
-				$rootScope.$broadcast( 'newSupportMessage', { body: message, name: AccountService.user.firstName, timestamp: new Date().getTime(), sending: true, guid: guid } );
+				service.sideInfo.add_message( { body: message, name: AccountService.user.firstName, timestamp: new Date().getTime(), sending: true, guid: guid } );
 			});
 		}
 	};
