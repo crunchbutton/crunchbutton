b73d86983436b9351d224f73d3fdd004a2b3acf2
Pererinha <pererinha@gmail.com>
2015-08-03 15:47:40 -0300

    create a cron or a way to identify cron errors #6147

--- include/library/Crunchbutton/Cron/Job/Test.php
+++ include/library/Crunchbutton/Cron/Job/Test.php
@@ -39,6 +39,6 @@ class Crunchbutton_Cron_Job_Test extends Crunchbutton_Cron_Log {
 		Log::debug( [ 'desc' => 'testing the cron log', 'host' => $host, 'type' => 'cron-jobs' ] );
 
 		// it always must call finished method at the end
-		// $this->finished();
+		$this->finished();
 	}
 }
