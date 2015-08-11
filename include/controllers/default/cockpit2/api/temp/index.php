0ab825cdb899765e8cae8e253c74487fa7cc5205
Pererinha <pererinha@gmail.com>
2015-08-11 15:38:29 -0300

    partial #5321

--- include/controllers/default/cockpit2/api/temp/index.php
+++ include/controllers/default/cockpit2/api/temp/index.php
@@ -3,8 +3,7 @@
 class Controller_api_temp extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
-
-		Crunchbutton_Community::shutDownCommunities();
+		Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
 	}
 
 }
