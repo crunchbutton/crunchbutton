46257ac4fc2939de38a6e4b744e65590a81e9042
Pererinha <pererinha@gmail.com>
2015-08-12 19:43:53 -0300

    apple notification test

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 	public function init() {
 			$r = Crunchbutton_Message_Push_Ios::send([
-			'to' => '7f3dac43d5654495a95933ed5394e08eaa7333286d8145aefa962a54306d4dbe',
+			'to' => 'e95396adea2d968bfdcad91ace0acb2ff2e6e06daf1b5e41405440012067ada3',
 			'message' => 'test',
 			'count' => 1,
 			'id' => 'order-1',
