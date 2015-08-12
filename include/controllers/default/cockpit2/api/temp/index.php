63cee173e48af20571e990a54e0fbaf77d4a94d7
Pererinha <pererinha@gmail.com>
2015-08-12 07:26:29 -0300

    when you create a new community driver_checkin is YES

--- include/controllers/default/cockpit2/api/temp/index.php
+++ include/controllers/default/cockpit2/api/temp/index.php
@@ -1,10 +1,8 @@
 <?php
 
 class Controller_api_temp extends Crunchbutton_Controller_RestAccount {
-
 	public function init() {
-
-		Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
+		// Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
+		Crunchbutton_Admin_Shift_Assign::assignAdminToShift( 5, 58311, false );
 	}
-
 }
