ec05cda758164185652a563c4ac52fdac97ecb57
Edward Chen <etchen@gmail.com>
2015-08-17 14:07:29 -0700

    Add minimum of 1 minute before cleanup cron job notifies driver of order.

--- include/library/Crunchbutton/Cron/Job/DriverFixNotify.php
+++ include/library/Crunchbutton/Cron/Job/DriverFixNotify.php
@@ -9,7 +9,7 @@ class Crunchbutton_Cron_Job_DriverFixNotify extends Crunchbutton_Cron_Log {
 		left join restaurant using (id_restaurant)
 		left join admin_notification_log using (id_order)
 		where restaurant.delivery_service=1 and restaurant.active=true and admin_notification_log.id_admin_notification_log is null
-		and `order`.date > date_sub(now(), interval 10 minute)
+		and `order`.date > date_sub(now(), interval 10 minute) and `order`.date < date_sub(now(), interval 1 minute)
 		order by `order`.id_order desc
 		';
 		$orders = Order::q($q);
@@ -25,3 +25,4 @@ class Crunchbutton_Cron_Job_DriverFixNotify extends Crunchbutton_Cron_Log {
 		$this->finished();
 	}
 }
+
