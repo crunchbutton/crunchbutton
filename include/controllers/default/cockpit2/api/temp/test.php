ecb94e1a1c6715820251f194795a9d858e85bbdc
Pererinha <pererinha@gmail.com>
2015-08-17 23:23:13 -0300

    test shutdown community error

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,16 +3,17 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 	public function init() {
 
-		Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
-		echo '<pre>';var_dump( 1 );exit();
+		// Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
+		// echo '<pre>';var_dump( 1 );exit();
 
 		// $community = Community::o( 67 );
 		// echo '<pre>';var_dump( $community->assignedShiftHours() );exit();
-		// echo '<pre>';var_dump( $community->deliveryHours() );exit();;
+		// echo '<pre>';var_dump( $community->deliveryHours() );exit();
 
 			// $r = Restaurant::o( 3756 );
 			// echo '<pre>';var_dump( $r->delivery_service );exit();
 			// echo $r->closed_message();
 			// exit;
+			Crunchbutton_Community::shutDownCommunities();
 	}
 }
