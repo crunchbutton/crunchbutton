998a7471836f645acfdf420283d86283274e284a
Pererinha <pererinha@gmail.com>
2015-08-18 16:17:18 -0300

    partial #6216

--- www/assets/cockpit/js/service.support.js
+++ www/assets/cockpit/js/service.support.js
@@ -180,9 +180,9 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 						// update the chat room
 						console.log('service.sideInfo.id_support',service.sideInfo.id_support);
 						console.log('d.id_support',d.id_support);
-						if( service.sideInfo.id_support && d.id_support && service.sideInfo.id_support != d.id_support ){
+						if( service.sideInfo.id_support && d.id_support && parseInt( service.sideInfo.id_support ) != parseInt( d.id_support ) ){
 							console.log('force_first_page??');
-							// service.sideInfo.force_first_page();
+							service.sideInfo.force_first_page();
 						}
 
 						if (d.id_admin == AccountService.user.id_admin) {
