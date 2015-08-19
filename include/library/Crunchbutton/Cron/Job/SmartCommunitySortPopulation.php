cd6d3dcfe75409f73c78a1569cf6b2512b85f283
Pererinha <pererinha@gmail.com>
2015-08-18 22:20:52 -0300

    Smart population of "our most popular locations" on UI2 #6056

--- include/library/Crunchbutton/Cron/Job/SmartCommunitySortPopulation.php
+++ include/library/Crunchbutton/Cron/Job/SmartCommunitySortPopulation.php
@@ -4,34 +4,9 @@ class Crunchbutton_Cron_SmartCommunitySortPopulation extends Crunchbutton_Cron_L
 
 	public function run(){
 
-		// Select driver with unverified accounts
-		$drivers = Admin::q( 'SELECT a.* FROM admin_payment_type apt INNER JOIN admin a ON apt.id_admin = a.id_admin WHERE apt.verified = 0 AND apt.stripe_account_id IS NOT NULL' );
-		foreach( $drivers as $driver ){
-			$this->verify( $driver );
-		}
+		Crunchbutton_Community::smartSortPopulation();
 
 		// it always must call finished method at the end
 		$this->finished();
-	}
-
-	public function verify( $driver ){
-		if( !$driver->isStripeVerified() ){
-
-			$community = '';
-			$communities = $driver->communitiesHeDeliveriesFor();
-			foreach( $communities as $c ){
-				$community .= $commas . $c->name;
-				$commas = ', ';
-			}
-
-			$params = [];
-			$params[ 'login' ] = $driver->login;
-			$params[ 'driver' ] = $driver->name;
-			$params[ 'community' ] = $community;
-			$params[ 'payments' ] = $driver->unPaidPayments();
-
-			$mail = new Crunchbutton_Email_Payment_DriverUnverifiedAccount( $params );
-			$mail->send();
-		}
 	}
 }
