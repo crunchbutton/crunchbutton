479074fddfec93b6a26c1a1dc49362c438a67f88
Devin Smith <github@arzynik.com>
2015-08-24 13:47:24 -0700

    push notification changes

--- include/controllers/default/cockpit2/api/temp/pushdevin.php
+++ include/controllers/default/cockpit2/api/temp/pushdevin.php
@@ -27,99 +27,32 @@ class Controller_api_temp_pushdevin extends Crunchbutton_Controller_RestAccount 
 		]);
 		*/
 
+		$cockpit = ['bda4c763f2e2f2ec8b123a960fd2e9ecba591cf4a310253708156eed658a4bb2','b85dc7710abcd6a18aa6ff91ca165aa97fa02df23323d49c689a7d50fd47e800'];
+
 		$order = Order::o(1);
 		$message = '#'.$order->id.': '.$order->user()->name.' has placed an order to '.$order->restaurant()->name.'.';
-
+// cockpit
+// live - 8d9b2a99aa4754686eb76ff3a20c007c808470a7327107e786f6cf0e1696f7ac
+// beta - b85dc7710abcd6a18aa6ff91ca165aa97fa02df23323d49c689a7d50fd47e800
+		
+// crunchbutton
+// beta - addc8d82f9faf739a5c47d10e21041176bd5ba8695bce9e36c6eae47e37c4aac
 		$r = Crunchbutton_Message_Push_Ios::send([
-			'to' => '8d9b2a99aa4754686eb76ff3a20c007c808470a7327107e786f6cf0e1696f7ac',
+			'to' => 'addc8d82f9faf739a5c47d10e21041176bd5ba8695bce9e36c6eae47e37c4aac',
 			'message' => $message,
 			'count' => 1,
 			'id' => 'order-'.$order->id,
 			'sound' => Crunchbutton_Message_Push_Ios::SOUND_NEW_ORDER,
 			'showInForeground' => true,
-			'link' => '/drivers/order/'.$order->id,
-			'app' => 'cockpit',
-			'env' => 'live'
+			'link' => '/work',
+			'app' => 'crunchbutton',
+			'env' => 'dev'
 		]);
 		
 		var_dump($r);
 		
 		exit;
 
-		
-		$cockpit = ['bda4c763f2e2f2ec8b123a960fd2e9ecba591cf4a310253708156eed658a4bb2','b85dc7710abcd6a18aa6ff91ca165aa97fa02df23323d49c689a7d50fd47e800'];
-		$cockpit = ['b85dc7710abcd6a18aa6ff91ca165aa97fa02df23323d49c689a7d50fd47e800'];
-		//$crunchbutton = ['addc8d82f9faf739a5c47d10e21041176bd5ba8695bce9e36c6eae47e37c4aac','a9245362e4a008eba8f701c4d6fb698f7a0c6232c89e689a67ca033f82e40166','b9229ae5121244b9af4309699e878ee615e9f230eb7fd789e22958f89b7ea2ca'];
-		$crunchbutton = ['addc8d82f9faf739a5c47d10e21041176bd5ba8695bce9e36c6eae47e37c4aac'];
-
-		$certs = c::config()->dirs->root.'ssl/';
-		//$env = 'live';
-		$env = 'beta';
-
-		if ($env == 'live') {
-			$push = new ApnsPHP_Push(
-				ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION,
-				$certs.'2015.aps_production_com.crunchbutton.cockpit.pem'
-			);
-		} else {
-			$push = new ApnsPHP_Push(
-				ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
-				$certs.'2015.aps_development_com.crunchbutton.cockpit.pem'
-			);
-		}
-		
-		$push->setRootCertificationAuthority($certs.'entrust_root_certification_authority.pem');
-		$push->connect();
-		
-		foreach ($cockpit as $t) {
-			$msg = new ApnsPHP_Message($t);
-			$msg->setText('please slack me if you recieve this');
-			//$msg->setCustomIdentifier();
-			//$msg->setSound($sound);
-			//$msg->setExpiry(30);
-			$msg->setBadge(rand(1,999));
-			
-			if ($category) {
-				//$msg->setCategory('123new');
-			}
-
-			$push->add($msg);
-		}
-		
-		$push->send();
-		$push->disconnect();
-
-		
-		if ($env == 'live') {
-			$push = new ApnsPHP_Push(
-				ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION,
-				$certs.'2015.aps_production_com.crunchbutton.pem'
-			);
-		} else {
-			$push = new ApnsPHP_Push(
-				ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
-				$certs.'2015.aps_development_com.crunchbutton.pem'
-			);
-		}
-		
-		$push->setRootCertificationAuthority($certs.'entrust_root_certification_authority.pem');
-		$push->connect();
-		
-		foreach ($crunchbutton as $t) {
-			$msg = new ApnsPHP_Message($t);
-			$msg->setCustomIdentifier('order-1');
-			$msg->setText('please slack me if you recieve this');
-			$msg->setCustomProperty('link', '/work');
-			$msg->setCustomProperty('showInForeground', true);
-			$msg->setSound('default');
-			$msg->setExpiry(30);
-			$msg->setBadge(rand(1,999));
-			
-			$push->add($msg);
-		}
-		
-		$push->send();
-		$push->disconnect();
 
 	}
 }
