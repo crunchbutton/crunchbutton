f5f38f5f6a0af67ba249e0b3a24543b0d24edb42
Edward Chen <etchen@gmail.com>
2015-08-25 16:38:16 -0700

    Get rid of hacks for notifications.

--- include/library/Crunchbutton/Cron/Job/DriverFixNotify.php
+++ include/library/Crunchbutton/Cron/Job/DriverFixNotify.php
@@ -4,23 +4,12 @@ class Crunchbutton_Cron_Job_DriverFixNotify extends Crunchbutton_Cron_Log {
 
 	public function run(){
 
-//		$q = '
-//		select `order`.* from `order`
-//		left join restaurant using (id_restaurant)
-//		left join admin_notification_log using (id_order)
-//		where restaurant.delivery_service=1 and restaurant.active=true and admin_notification_log.id_admin_notification_log is null
-//		and `order`.date > date_sub(now(), interval 10 minute) and `order`.date < date_sub(now(), interval 1 minute)
-//		order by `order`.id_order desc
-//		';
-
 		$q = '
 		select `order`.* from `order`
 		left join restaurant using (id_restaurant)
 		left join admin_notification_log using (id_order)
-		left join community as c on `order`.id_community = c.id_community
 		where restaurant.delivery_service=1 and restaurant.active=true and admin_notification_log.id_admin_notification_log is null
-		and `order`.date > date_sub(now(), interval 10 minute) and `order`.date < date_sub(now(), interval 1 minute) and
-		(c.id_community is null or (c.id_community is not null and c.delivery_logistics is null))
+		and `order`.date > date_sub(now(), interval 10 minute) and `order`.date < date_sub(now(), interval 1 minute)
 		order by `order`.id_order desc
 		';
 		$orders = Order::q($q);
