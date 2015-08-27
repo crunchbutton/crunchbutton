5d20f84ba7af2aa180ce2bc4fe5b956531673d9c
Devin Smith <github@arzynik.com>
2015-08-27 16:35:27 -0700

    db test stuff

--- include/library/Crunchbutton/App.php
+++ include/library/Crunchbutton/App.php
@@ -179,15 +179,13 @@ class Crunchbutton_App extends Cana_App {
 		
 		
 
-		
+		$this->buildAuth($this->db());
 		
 
 				
 		if (getenv('HEROKU')) {
 			error_log('>> EXITING >>>>>>>>>');
 			//die((string)rand(1,999999));
-		} else {
-			$this->buildAuth($this->db());
 		}
 
 
