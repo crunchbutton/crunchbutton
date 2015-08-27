e7230f69e342bf6209fc8557216f1d6831f0fd09
Devin Smith <github@arzynik.com>
2015-08-27 14:25:29 -0700

    db test

---
+++ www/dbtest.php
@@ -0,0 +1,52 @@
+<?php
+error_reporting(E_ALL ^ ( E_NOTICE | E_STRICT | E_DEPRECATED ) );
+ini_set('display_errors',true);
+
+$args = new stdClass;
+$args->url = getenv('DATABASE_URL');
+
+if ($args->url) {
+	preg_match('/^(mysql:\/\/)(.*):(.*)@(.*):([0-9]+)\/([a-z0-9\._]+)(\?sslca=(.*))?$/u', $args->url, $matches);
+	$args->user = $matches[2];
+	$args->pass = $matches[3];
+	$args->host = $matches[4];
+	$args->port = $matches[5];
+	$args->db = $matches[6];
+	$args->sslca = $matches[8];
+}
+
+if (!$args->dsn) {
+	$args->dsn = 'mysql:host='.$args->host.';dbname='.$args->db.';charset=utf8';
+}
+
+if ($args->persistent) {
+	$options[PDO::ATTR_PERSISTENT] = true;
+}
+
+if ($args->sslca) {
+	$options[PDO::MYSQL_ATTR_SSL_CA] = $args->sslca;
+	$options[PDO::ATTR_TIMEOUT] = 4;
+	$options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+}
+		
+if (getenv('HEROKU')) {
+	error_log('>> CONNECTING TO DATABASE...');
+}
+
+$db = new \PDO($args->dsn, $args->user, $args->pass, $options);
+
+$this->database($args->db);
+
+$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+$db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+		
+if (getenv('HEROKU')) {
+	error_log('>> CONNECTED');
+}
+
+
+$stmt = $db->prepare($query);
+$stmt->execute($args);
+
+die('done');
