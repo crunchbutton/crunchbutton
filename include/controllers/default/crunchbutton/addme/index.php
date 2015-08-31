0f4ba97b002d6e5c4f8bba1c084f15c5047f33e2
Devin Smith <github@arzynik.com>
2015-08-31 12:00:25 -0700

    fixes #2056

--- include/controllers/default/crunchbutton/addme/index.php
+++ include/controllers/default/crunchbutton/addme/index.php
@@ -2,6 +2,8 @@
 
 class Controller_addme extends Cana_Controller {
 	public function init() {
+		header('Location: /owners');
+		exit;
 		c::view()->layout('layout/landing');
 		c::view()->display('addme/index');
 	}
