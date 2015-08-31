9c239b5bd7e972edaaaf2b6addb8cd1747bd1c7a
Pererinha <pererinha@gmail.com>
2015-08-31 17:23:03 -0300

    partial #5321

---
+++ db/migrate/000379_cron.sql
@@ -0,0 +1,3 @@
+INSERT INTO `cron_log` (`description`, `class`, `start_date`, `interval`, `interval_unity`, `current_status`, `next_time`, `finished`, `interactions`, `env`)
+VALUES
+	( 'Temp cron to change the referral codes #5321', 'Crunchbutton_Cron_Job_TempReferralCode', '2014-05-26 06:00:00', 'minute', 10, 'idle', '2015-08-31 13:20:00', '2015-08-31 13:19:03', 1, 'crondb');
