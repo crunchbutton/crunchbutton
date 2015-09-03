3b97ec89c7df6aa4654c66e63fb30396fe3a4282
Pererinha <pererinha@gmail.com>
2015-09-02 21:52:41 -0300

    partial #6549

--- www/assets/js/admin_restaurant.js
+++ www/assets/js/admin_restaurant.js
@@ -1459,16 +1459,11 @@ var DOM_MAP = {
 							dayhours = days.indexOf(day)*2400;
 							for(i in hours[day]) {
 								segment = hours[day][i];
-								// if( segment[0] ){
-									m0 = /(\d+):(\d+)/.exec(segment[0]);
-									b = (dayhours + parseInt(m0[1], 10) * 100 + parseInt(m0[2], 10));
-								// }
-								// if( segment[1] ){
-									m1 = /(\d+):(\d+)/.exec(segment[1]);
-									e = (dayhours + parseInt(m1[1], 10) * 100 + parseInt(m1[2], 10));
-								// }
+								m0 = /(\d+):(\d+)/.exec(segment[0]);
+								b = (dayhours + parseInt(m0[1], 10) * 100 + parseInt(m0[2], 10));
+								m1 = /(\d+):(\d+)/.exec(segment[1]);
+								e = (dayhours + parseInt(m1[1], 10) * 100 + parseInt(m1[2], 10));
 								hfmmm.push({b : b, e : e});
-
 							}
 						}
 						// 2. sort
