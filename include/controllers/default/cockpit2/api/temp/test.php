6b21978b7986dd19dcda07ef4c3bcee5fe3cfa4b
Pererinha <pererinha@gmail.com>
2015-09-08 19:52:05 -0300

    community shudtown error

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -7,6 +7,7 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 		// Crunchbutton_Admin_Shift_Assign_Confirmation::warnCS( $assignment );
 		// $community = Community::o( 70 );
 		// $community->shutDownCommunity();
-		Community::shutDownCommunities();
+		Crunchbutton_Community::shutDownCommunities();
+		echo 'done';
 	}
 }
