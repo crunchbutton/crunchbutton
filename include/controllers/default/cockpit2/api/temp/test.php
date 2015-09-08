0445b19e6b0626c7692cde91d96d88b1be4045d6
Pererinha <pererinha@gmail.com>
2015-09-08 19:50:47 -0300

    community shutdown error

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -5,6 +5,8 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 		// $assignment = Crunchbutton_Admin_Shift_Assign::o( 62233 );
 		// Crunchbutton_Admin_Shift_Assign_Confirmation::confirm( $assignment );
 		// Crunchbutton_Admin_Shift_Assign_Confirmation::warnCS( $assignment );
-
+		// $community = Community::o( 70 );
+		// $community->shutDownCommunity();
+		Community::shutDownCommunities();
 	}
 }
