d28b4b7ec129e95cfd09a5a5ed7dd1bf13cbd1b9
Devin Smith <github@arzynik.com>
2015-09-09 12:52:04 -0700

    removed a test

--- include/controllers/default/crunchbutton/api/config/index.php
+++ include/controllers/default/crunchbutton/api/config/index.php
@@ -2,12 +2,7 @@
 
 class Controller_api_config extends Crunchbutton_Controller_Rest {
 	public function init() {
-		
-		if (getenv('HEROKU')) {
-			exit;
-			error_log('>> DISPLAYING CONFIG...');
-		}
-		
+
 		switch ($this->method()) {
 			case 'post':
 				if ($this->request()['ab']) {
