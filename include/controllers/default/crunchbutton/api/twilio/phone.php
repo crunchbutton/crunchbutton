5e32a5cfcc0b88643553beb1f9a687e9f1ed1b27
Pererinha <pererinha@gmail.com>
2015-09-10 20:43:39 -0300

    partial twilio problem

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -2,7 +2,7 @@
 
 class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 	public function init() {
-		
+
 		$_REQUEST['forward'] = preg_replace('/[^0-9]/','',$_REQUEST['forward']);
 		header('Content-type: text/xml');
 
@@ -15,7 +15,7 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 		    	break;
 		    case 'call':
 				$callback = 'http'.($_SERVER['HTTPS'] != 'on' ? '' : 's').'://'.c::config()->host_callback.'/api/twilio/phone/recording';
-		
+
 				echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
 					.'<Response>'
 					.'<Dial timeout="10" record="record-from-answer" trim="trim-silence" action="'.$callback.'">'.($_REQUEST['forward'] ? $_REQUEST['forward'] : '800-351-4161').'</Dial>'
@@ -24,7 +24,7 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 		}
 
 		$request = $this->request();
-		Call::logFromTwilio($request);
+		// Call::logFromTwilio($request);
 
 		exit;
 	}
