a5392cd68eaaa67d9502a653b574ee27f83757bc
Edward Chen <etchen@gmail.com>
2015-09-10 14:45:44 -0700

    Add more debugging information for driver notifications.

--- include/library/Crunchbutton/Queue/Notification/Driver/Priority.php
+++ include/library/Crunchbutton/Queue/Notification/Driver/Priority.php
@@ -7,9 +7,18 @@ class Crunchbutton_Queue_Notification_Driver_Priority extends Crunchbutton_Queue
 		$driver = $this->driver();
 		$order = $this->order();
 		if( $order->id_order && $driver->id_admin ){
+			$hostname = gethostname();
+			$pid = getmypid();
+			$ppid =  posix_getppid();
+
 			$notifications = $driver->activeNotifications();
 			foreach( $notifications as $notification ){
 				$notification->sendPriority( $order );
+				Log::debug([
+					'order' => $order->id_order,
+					'action' =>  '#'.$order->id_order.' sending ** QUEUE ** priority notification to ' . $driver->name . ' # ' . $notification->value,
+					'type' => 'delivery-driver', 'hostname' => $hostname, 'pid' => $pid, 'ppid' => $ppid
+				]);
 			}
 		}
 
