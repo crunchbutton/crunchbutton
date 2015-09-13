23d97f9027779927f058e5ef31476e75fe63fdab
Pererinha <pererinha@gmail.com>
2015-09-13 18:09:42 -0300

    partial #6580

--- include/controllers/default/cockpit2/api/settlement/index.php
+++ include/controllers/default/cockpit2/api/settlement/index.php
@@ -4,9 +4,6 @@ class Controller_Api_Settlement extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
 
-		// $this->_driverBegin();
-		// exit;
-
 		$this->resultsPerPage = 20;
 
 		if( !c::admin()->permission()->check( ['global', 'settlement' ] ) ){
@@ -608,9 +605,6 @@ class Controller_Api_Settlement extends Crunchbutton_Controller_RestAccount {
 		$end = $this->request()['end'];
 		$id_driver = $this->request()['id_driver'];
 
-		// $start = '08/21/2015';
-		// $end = '08/28/2015';
-
 		if( !$start || !$end ){
 			$this->_error();
 		}
@@ -685,20 +679,7 @@ class Controller_Api_Settlement extends Crunchbutton_Controller_RestAccount {
 				$driver[ 'pay_type' ] = [ 'payment_type' => '-' ];
 			}
 
-			if( $id_driver ){
-			// if( true ){
-				// if( $id_driver == $driver[ 'id_admin' ] ){
-				// if(
-						// $driver[ 'id_admin' ] ==  12196 ||
-						// $driver[ 'id_admin' ] ==  12202 ||
-						// $driver[ 'id_admin' ] ==  12199 ||
-						// $driver[ 'id_admin' ] ==  12205
-					// ){
-					$out[ 'drivers' ][] = $driver;
-				// }
-			} else {
-				$out[ 'drivers' ][] = $driver;
-			}
+			$out[ 'drivers' ][] = $driver;
 		}
 		echo json_encode( $out );
 	}
