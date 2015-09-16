7f13bff53d937305613bfdb7963acd137251344b
Devin Smith <github@arzynik.com>
2015-09-16 14:29:45 -0700

    built in server tests

--- www/index.php
+++ www/index.php
@@ -16,6 +16,28 @@ set_time_limit(100);
 ini_set('zlib.output_compression','On');
 ini_set('zlib.output_compression_level',9);
 
+
+
+// routing config for built in web server. we only use this for travis temporarily
+if (function_exists('php_sapi_name') && php_sapi_name() == 'cli-server') {
+	$path = pathinfo($_SERVER['SCRIPT_FILENAME']);
+
+	$file = substr($_SERVER['SCRIPT_NAME'],1);
+	$allowed = ['scss','php'];
+	if (file_exists($file) && !in_array($path['extension'], $allowed)) {
+		
+		$info = @trim(explode(':',exec('file --mime-type '.$file))[1]);
+
+		header('Content-Type: '.$info);
+		readfile($_SERVER['SCRIPT_FILENAME']);
+		exit;
+
+	} else {
+		$_SERVER['SCRIPT_NAME'] = '/index.php';
+	}
+}
+
+
 if (isset($_REQUEST['__url']) && $_REQUEST['__url'] == 'index.php') {
 	$_REQUEST['__url'] = '';
 }
@@ -23,7 +45,9 @@ if (isset($_REQUEST['__url']) && $_REQUEST['__url'] == 'index.php') {
 if (!$_REQUEST['__url']) {
 	$request = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
 	$dir = dirname($_SERVER['SCRIPT_NAME']);
+
 	$base = substr($dir, -1) == '/' ? $dir : $dir.'/';
+
 	$url = preg_replace('/^'.str_replace('/','\\/',''.$dir).'/','',$request);
 	$url = substr($url, 0, 1) == '/' ? $url : '/'.$url;
 	$_REQUEST['__url'] = substr($url, 1);
