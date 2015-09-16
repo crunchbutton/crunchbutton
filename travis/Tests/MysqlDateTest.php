54dd941f65664a51baa578c857769efe5dfb999e
Devin Smith <github@arzynik.com>
2015-09-16 15:33:33 -0700

    datatype mismatch

--- travis/Tests/MysqlDateTest.php
+++ travis/Tests/MysqlDateTest.php
@@ -7,6 +7,6 @@ class MysqlDateTest extends PHPUnit_Framework_TestCase {
 		$dbd = c::db()->get('select now() as d')->get(0);
 		$phpd = date('Y-m-d H:i:s');
 		
-		$this->assertEquals($dbd['d'], $phpd);
+		$this->assertEquals($dbd->d, $phpd);
 	}
 }
