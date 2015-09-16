b03348b98664eea842d37a451b8f7b97aa4ae45f
Devin Smith <github@arzynik.com>
2015-09-15 21:17:39 -0700

    added always restart

--- cli/docker-build.sh
+++ cli/docker-build.sh
@@ -2,4 +2,4 @@
 
 docker rm -f crunchbutton
 docker build -f deploy/docker/Dockerfile.nginx.fpm.php7 -t crunchbutton .
-docker run -p 80:80 --name crunchbutton crunchbutton
+docker run --restart always -p 80:80 --name crunchbutton crunchbutton
