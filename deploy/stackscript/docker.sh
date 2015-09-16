c153cdaa66c1e71b540882eddb76a9d832d0c5fb
Devin Smith <github@arzynik.com>
2015-09-16 09:33:41 -0700

    update docker script

--- deploy/stackscript/docker.sh
+++ deploy/stackscript/docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# included setup_github for private repos
 source <ssinclude StackScriptID="13192">
 
-# <UDF name="dockerfile" Label="Select Docker file" oneOf="Dockerfile.apache.php5.6,Dockerfile.nginx.fpm.php5.6,Dockerfile.apache.php7,Dockerfile.nginx.fpm.php7" default="Dockerfile.nginx.fpm.php5.6" />
+# <UDF name="dockerfile" Label="Select Docker file" oneOf="Dockerfile.apache.php5.6,Dockerfile.nginx.fpm.php5.6,Dockerfile.apache.php7,Dockerfile.nginx.fpm.php7" default="Dockerfile.nginx.fpm.php7" />
+# <udf name="sshport" label="Port for SSH" example="3333" default="3333" />
+# <udf name="rootusr" label="Admin username" example="chickenbutt" default="chickenbutt" />
+# <udf name="rootpass" label="Admin password" example="eggscomefromchickenbutts" default="eggscomefromchickenbutts" />
 
 
 if [ -f /etc/apt/sources.list ]; then
@@ -18,12 +23,33 @@ fi
 
 curl -sSL https://get.docker.com/ | sh
 
-service docker start
+if [ -f /etc/apt/sources.list ]; then
+	update-rc.d docker.io defaults
+elif [ -f /etc/yum.conf ]; then
+	systemctl enable docker
+	service docker start
+fi
 
+
+# secure stuff
+sed -i "s/^#Port .*$/Port $SSHPORT/" /etc/ssh/sshd_config
+service sshd restart
+
+sed -i 's/^#PermitRootLogin .*$/PermitRootLogin no/' /etc/ssh/sshd_config
+#sed -i 's/^#PasswordAuthentication .*$/PasswordAuthentication no/' /etc/ssh/sshd_config
+  
+
+useradd $ROOTUSR
+echo "$ROOTUSR:$ROOTPASS"|chpasswd
+sudo adduser $ROOTUSR sudo
+echo "AllowUsers $ROOTUSR" >> /etc/ssh/sshd_config
+
+
+# install keys n stuff from included file
 setup_github
 
 git clone git@github.com:crunchbutton/crunchbutton.git /home/docked
 cd /home/docked
 docker build -f deploy/docker/$DOCKERFILE -t docked .
-docker run -p 80:80 --name docked docked
 rm -Rf /home/docked
+docker run --restart always -p 80:80 --name docked docked
