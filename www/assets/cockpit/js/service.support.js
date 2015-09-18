38943f76e42f6eb88589432074d3d2076e9e2b43
Pererinha <pererinha@gmail.com>
2015-09-18 20:25:59 -0300

    partial #6729

--- www/assets/cockpit/js/service.support.js
+++ www/assets/cockpit/js/service.support.js
@@ -138,8 +138,8 @@ NGApp.factory('TicketViewService', function($rootScope, $resource, $routeParams,
 	$rootScope.$on('triggerViewTicket', function(e, ticket) {
 
 		if( service.scope.viewTicket && service.scope.ticket && service.scope.viewTicket.id_support == ticket.id_support && service.scope.ticket.id_support == ticket.id_support ){
-			service.sideInfo.force_first_page();
-			return;
+			// service.sideInfo.force_first_page();
+			// return;
 		}
 
 		NotificationService.check();
