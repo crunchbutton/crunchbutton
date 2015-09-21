085d0b18c03d4fe488240ae08e3a16f4e02c2ffb
Pererinha <pererinha@gmail.com>
2015-09-21 17:28:16 -0300

    cron job problem

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,9 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 	public function init() {
 
-			Crunchbutton_Community_Shift::warningDriversBeforeTheirShift();
-
-		Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
+		Order::ticketsForNotGeomatchedOrders();
 
 	}
 }
