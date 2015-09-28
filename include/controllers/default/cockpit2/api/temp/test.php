476efdcc61258c19f7e1662de8be20092329d9d4
Daniel Camargo <pererinha@gmail.com>
2015-09-28 15:33:51 -0300

    Pex card not auto-emptying at end of shift #6274

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 	public function init() {
 
-		Order::ticketsForNotGeomatchedOrders();
+		Crunchbutton_Community_Shift::pexCardRemoveShiftFunds();
 
 	}
 }
