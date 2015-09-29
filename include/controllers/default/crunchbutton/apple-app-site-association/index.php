78e6941f8d52bcf4481181f2aac6a6ac64bdac2b
Devin Smith <github@arzynik.com>
2015-09-29 10:34:33 -0700

    added universal links file

---
+++ include/controllers/default/crunchbutton/apple-app-site-association/index.php
@@ -0,0 +1,15 @@
+<?
+
+header('Content-Type: application/pkcs7-mime');
+
+?>{
+    "applinks": {
+        "apps": [],
+        "details": [
+            {
+                "appID": ""T237M9V2RSID.com.crunchbutton",
+                "paths": [ "*" ]
+            }
+        ]
+    }
+}
