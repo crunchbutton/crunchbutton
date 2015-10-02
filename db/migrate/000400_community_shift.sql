6316969bd59907a3e1e49991b649b2afe3a7328f
Daniel Camargo <pererinha@gmail.com>
2015-10-02 18:05:38 -0300

    partial #6684

--- db/migrate/000400_community_shift.sql
+++ db/migrate/000400_community_shift.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `community_shift` ADD `id_admin` int(11) unsigned DEFAULT NULL;
-ALTER TABLE `community_shift` ADD KEY `community_shift_ibfk_3` (`id_admin`);
-ALTER TABLE `community_shift` ADD CONSTRAINT `community_shift_ibfk_3` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL ON UPDATE SET NULL;
+ALTER TABLE `community_shift` ADD `id_driver` int(11) unsigned DEFAULT NULL;
+ALTER TABLE `community_shift` ADD KEY `community_shift_ibfk_3` (`id_driver`);
+ALTER TABLE `community_shift` ADD CONSTRAINT `community_shift_ibfk_3` FOREIGN KEY (`id_driver`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 ALTER TABLE `community_shift` ADD `date` datetime DEFAULT NULL;
