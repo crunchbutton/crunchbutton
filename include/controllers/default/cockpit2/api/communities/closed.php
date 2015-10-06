3b773a26d87ae085dfde543829a39e758bd08359
Daniel Camargo <pererinha@gmail.com>
2015-10-06 10:05:53 -0300

    Sort Closed Communities by Most Recent #6905

--- include/controllers/default/cockpit2/api/communities/closed.php
+++ include/controllers/default/cockpit2/api/communities/closed.php
@@ -4,7 +4,7 @@ class Controller_api_communities_closed extends Crunchbutton_Controller_Rest {
 
 	public function init() {
 		$out = [];
-		$communities = Crunchbutton_Community::q( 'SELECT * FROM community WHERE ( close_all_restaurants = 1 OR close_3rd_party_delivery_restaurants = 1 OR is_auto_closed = 1 ) AND active = 1 ORDER BY name ASC' );
+		$communities = Crunchbutton_Community::q( 'SELECT * FROM community WHERE ( close_all_restaurants = 1 OR close_3rd_party_delivery_restaurants = 1 OR is_auto_closed = 1 ) AND active = 1 AND name NOT LIKE "%test%" AND name NOT LIKE "%template%" ORDER BY name ASC' );
 		foreach( $communities as $community ){
 			$data = [ 'name' => $community->name, 'id_community' => $community->id_community, 'permalink' => $community->permalink ];
 			$data[ 'close_all_restaurants' ] = $community->close_all_restaurants;
@@ -12,9 +12,16 @@ class Controller_api_communities_closed extends Crunchbutton_Controller_Rest {
 			$data[ 'is_auto_closed' ] = $community->is_auto_closed;
 
 			$data[ 'drivers_working' ] = $community->activeDrivers();;
-			$data[ 'log' ] = $community->closedSince()[0];
+			$data[ 'log' ] = $community->closedSince( true )[0];
 			$out[] = $data;
 		}
+
+
+		usort( $out,
+		function( $a, $b ) {
+			return ( $a[ 'log' ][ 'sort_date' ] < $b[ 'log' ][ 'sort_date' ] );
+		} );
+
 		echo json_encode( $out );exit;
 	}
 }
