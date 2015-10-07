05b0a7ca512c0d6ff7e7d3cf89064254d32f9d2f
Daniel Camargo <pererinha@gmail.com>
2015-10-07 18:04:53 -0300

    Campus Managers are receiving Payment Failure text blast meant for drivers #5973

--- include/library/Crunchbutton/Settlement.php
+++ include/library/Crunchbutton/Settlement.php
@@ -1639,7 +1639,16 @@ class Crunchbutton_Settlement extends Cana_Model {
 
 				$first_name = Crunchbutton_Message_Sms::greeting( $driver->firstName() );
 
-				$message = $first_name . 'Hey your payment from Crunchbutton.com failed. Make sure you entered your payment info right at http://cockpit.la/drivers/docs/payment. Just email us back at payment@_DOMAIN_ if you have questions!';
+				$link = '';
+				if( $driver->isDriver() ){
+					$link = 'http://cockpit.la/drivers/docs/payment';
+				}
+				if( $driver->isMarketingRep() || $driver->isCampusManager() ){
+					$link = 'http://cockpit.la/staff/marketing-rep/docs/payment';
+				}
+
+				$message = $first_name . 'Hey your payment from Crunchbutton.com failed. Make sure you entered your payment info right at ' . $link . '. Just email us back at payment@_DOMAIN_ if you have questions!';
+
 				if( $driver->phone ){
 					Crunchbutton_Message_Sms::send( [ 'from' => 'driver', 'to' => $driver->phone, 'message' => $message, 'reason' => Crunchbutton_Message_Sms::REASON_SETTLEMENT_FAIL ] );
 					// Crunchbutton_Support::createNewWarning( [ 'phone' => $driver->phone, 'body' => $message ] );
