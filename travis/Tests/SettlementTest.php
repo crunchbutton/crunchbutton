8f31c3eb5ec119fed6f48f7ff9085797426914ce
Daniel Camargo <pererinha@gmail.com>
2015-10-13 19:35:27 -0300

    Have some communities' drivers get paid a different amount per order instead of the delivery fee #6170

--- travis/Tests/SettlementTest.php
+++ travis/Tests/SettlementTest.php
@@ -213,7 +213,7 @@ class SettlementTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( $this->hourly_payment_info[ 'total_spent' ], 20 );
 		$this->assertEquals( $this->hourly_payment_info[ 'delivery_fee_collected' ], -3 );
 		$this->assertEquals( $this->hourly_payment_info[ 'standard_reimburse' ], 10 );
-		$this->assertEquals( $this->hourly_payment_info[ 'total_payment_per_order' ], 4 );
+		$this->assertEquals( $this->hourly_payment_info[ 'total_payment_per_order' ], 7 );
 		$this->assertEquals( $this->hourly_payment_info[ 'total_payment' ], 41 );
 	}
 
