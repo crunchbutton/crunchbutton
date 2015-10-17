80d4b6d23e450bc13f139ef6a3b00ecd03a8a802
Daniel Camargo <pererinha@gmail.com>
2015-10-17 10:33:06 -0300

    cron test

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -2,8 +2,6 @@
 
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 	public function init() {
-		Cockpit_Driver_Log::nativeAppLogin();
-		Cockpit_Driver_Log::enabledLocation();
-		Cockpit_Driver_Log::enabledPush();
+		Crunchbutton_Community::shutDownCommunities();
 	}
 }
