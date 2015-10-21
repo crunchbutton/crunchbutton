7f54602f8b61902355e00f3c86484adbd9fb13ca
Daniel Camargo <pererinha@gmail.com>
2015-10-21 00:24:23 -0200

    change beta.cockpit.la to use dev

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -2,7 +2,6 @@
 
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 	public function init() {
-		$driver = Admin::o( 5 );
-		echo '<pre>';var_dump( $driver->openedCommunity() );exit();
+		echo '<pre>';var_dump( c::env(), c::db() );exit();
 	}
 }
