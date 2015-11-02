8bb1228de3f59e33e543d8951e544113b2bb581a
Devin Smith <github@arzynik.com>
2015-11-02 11:46:52 -0800

    fixed a mysql date test

--- travis/Tests/MysqlDateTest.php
+++ travis/Tests/MysqlDateTest.php
@@ -1,12 +1,16 @@
 <?php
 
 class MysqlDateTest extends PHPUnit_Framework_TestCase {
-	
+
 	public function testDate() {
 		// SET GLOBAL time_zone = timezone;
 		$dbd = c::db()->get('select now() as d')->get(0);
 		$phpd = date('Y-m-d H:i:s');
-		
-		$this->assertEquals($dbd->d, $phpd);
+
+		$r = function($i) {
+			return substr($i, 0, -(strlen($i) - strrpos($i, ':')));
+		};
+
+		$this->assertEquals($r($dbd->d), $r($phpd);
 	}
 }
