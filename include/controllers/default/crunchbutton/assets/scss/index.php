56edd003d48f4ac2a11e61cc8b5381c76920c23c
Devin Smith <github@arzynik.com>
2015-11-03 17:39:02 -0800

    docker test

--- include/controllers/default/crunchbutton/assets/scss/index.php
+++ include/controllers/default/crunchbutton/assets/scss/index.php
@@ -4,6 +4,10 @@ class Controller_assets_scss extends Crunchbutton_Controller_AssetBundle {
 	public function init() {
 		$path = c::config()->dirs->www.'assets/scss/';
 		$file = c::getPagePiece(2);
+		// test
+		if (getenv('DOCKER')) {
+			die($file);
+		}
 		Scss::serve($path.$file);
 	}
 }
