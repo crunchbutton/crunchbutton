53f545e1231217aba29127e51a706a863bd4bae7
Devin Smith <github@arzynik.com>
2015-11-03 22:04:01 -0800

    clear test again

--- include/controllers/default/crunchbutton/assets/scss/index.php
+++ include/controllers/default/crunchbutton/assets/scss/index.php
@@ -4,10 +4,6 @@ class Controller_assets_scss extends Crunchbutton_Controller_AssetBundle {
 	public function init() {
 		$path = c::config()->dirs->www.'assets/scss/';
 		$file = c::getPagePiece(2);
-		// test
-		if (getenv('DOCKER')) {
-			die($file);
-		}
 		Scss::serve($path.$file);
 	}
 }
