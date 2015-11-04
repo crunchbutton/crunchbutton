f01e30f1550ad3a50848dd939c5110d6cf9fc60a
Pererinha <pererinha@gmail.com>
2015-11-04 06:43:45 -0200

    partial #7063

--- include/controllers/default/cockpit2/api/staff/activations.php
+++ include/controllers/default/cockpit2/api/staff/activations.php
@@ -3,13 +3,18 @@
 class Controller_api_staff_activations extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
-		$invites = Crunchbutton_Referral::q( 'SELECT r.*, o.name AS customer, o.date AS date, psr.id_payment_schedule_referral FROM referral r
+
+		$id_admin = c::user()->id_admin;
+		$invites = Crunchbutton_Referral::q( 'SELECT r.*, u.name AS customer FROM referral r INNER JOIN user u ON u.id_user = r.id_user_invited WHERE r.id_admin_inviter = ? AND r.new_user = 1 ORDER BY r.id_referral DESC', [ $id_admin ] );
+
+
+		$_invites = Crunchbutton_Referral::q( 'SELECT r.*, o.name AS customer, o.date AS date, psr.id_payment_schedule_referral FROM referral r
 																						INNER JOIN `order` o ON r.id_order = o.id_order
 																						LEFT JOIN payment_schedule_referral psr ON r.id_referral = psr.id_referral
 																						WHERE r.id_admin_inviter = ? ORDER BY r.id_referral DESC', [ c::user()->id_admin ] );
 		$out = [ 'paid' => 0, 'not_paid' => 0, 'total' => 0, 'not_paid_activations' => [] ];
 		foreach( $invites as $invite ){
-			if( $invite->id_payment_schedule_referral ){
+			if( $invite->wasPaid() ){
 				$out[ 'paid' ]++;
 			} else {
 				$out[ 'not_paid' ]++;
