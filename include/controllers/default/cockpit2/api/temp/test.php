4b97c238ea635b64b98cac262eb0734c9604240d
Daniel Camargo <pererinha@gmail.com>
2015-11-05 17:38:07 -0200

    notification test #6522

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,6 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		Order::ticketForCampusCashOrder();
+		$test = Order::o( 219755 );
+		$test->notifyDriverAboutCustomerChanges( [ 'name' => 'NAME', 'phone' =>  555, 'address' => 'Address' ] );
 	}
 }
