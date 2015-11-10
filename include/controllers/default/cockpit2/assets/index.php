fb241a4b935832dfc742f3953c5f953a6dcf39ea
Daniel Camargo <pererinha@gmail.com>
2015-11-09 23:23:24 -0200

    partial #7092

--- include/controllers/default/cockpit2/assets/index.php
+++ include/controllers/default/cockpit2/assets/index.php
@@ -7,23 +7,22 @@ class Controller_assets extends Crunchbutton_Controller_AssetBundle {
 
 		$file = array_pop($pages);
 		$path = implode('/',$pages);
-		
+
 		if ($file == 'bundle.css') {
 			array_shift(c::config()->controllerStack);
 			c::displayPage('assets/css/bundle.css');
 			exit;
 		}
-		
+
 		if ($file == 'bundle.js') {
 			array_shift(c::config()->controllerStack);
 			c::displayPage('assets/js/bundle.js');
 			exit;
 		}
 
-		if (preg_match('/\.scss$/i',$_SERVER['REDIRECT_URL'])) {
+		if( preg_match('/\.scss$/i',$_SERVER['REDIRECT_URL']) || (strpos($_SERVER['REQUEST_URI'],'.scss') !== false) ){
 			$path = c::config()->dirs->www.'assets/'.$path.'/';
-			//$file = $file.'.scss';
-			Scss::serve($path.$file);
+			Crunchbutton_Scss::serve($path.$file);
 		}
 	}
 }
