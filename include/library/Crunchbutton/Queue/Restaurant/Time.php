b3713b04e619c5f7b65d6494f07b33e86b7b5606
Daniel Camargo <pererinha@gmail.com>
2015-11-17 21:04:59 -0200

    change how we do open calculations #6902

---
+++ include/library/Crunchbutton/Queue/Restaurant/Time.php
@@ -0,0 +1,10 @@
+<?php
+
+class Crunchbutton_Queue_Restaurant_Time extends Crunchbutton_Queue {
+	public function run() {
+
+		Crunchbutton_Restaurant_Time::register( $this->id_restaurant );
+
+		return self::STATUS_SUCCESS;
+	}
+}
