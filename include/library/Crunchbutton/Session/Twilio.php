ed7b081026562302e74090c33c964a18a8201427
Devin Smith <github@arzynik.com>
2015-11-18 14:18:22 -0800

    a bunch of session shit

--- include/library/Crunchbutton/Session/Twilio.php
+++ include/library/Crunchbutton/Session/Twilio.php
@@ -4,7 +4,7 @@ class Crunchbutton_Session_Twilio extends Cana_Table {
 
 	public static function capture( $data = false ) {
 
-		$sess = self::q('select * from session_twilio where id_session="'.c::auth()->session()->id_session.'"');
+		$sess = self::q('select * from session_twilio where id_session="'.c::auth()->session()->adapter()->id_session.'"');
 
 		// If the session id was lost, let's try to recover it by selecting the last support of the given phone number
 		// the support souldnt be more than 10 days old
@@ -15,14 +15,14 @@ class Crunchbutton_Session_Twilio extends Cana_Table {
 												INNER JOIN support s ON st.id_session_twilio = s.id_session_twilio
 												WHERE st.phone = '{$phone}' AND DATEDIFF( NOW(), s.datetime ) <= 10
 												ORDER BY st.id_session_twilio DESC LIMIT 1" );
-			$sess->id_session = c::auth()->session()->id_session;
+			$sess->id_session = c::auth()->session()->adapter()->id_session;
 			$sess->save();
 		}
 
 		// If we couldnt recovery the session, let's create a new one
 		if ( !$sess->id_session_twilio ) {
 			$sess = new Session_Twilio;
-			$sess->id_session = c::auth()->session()->id_session;
+			$sess->id_session = c::auth()->session()->adapter()->id_session;
 			$sess->save();
 		}
 
