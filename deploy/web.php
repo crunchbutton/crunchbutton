b203919f218c329c42eda929e6a573034a4731c2
Devin Smith <github@arzynik.com>
2015-11-19 14:22:37 -0800

    removed dev from composer

--- deploy/web.php
+++ deploy/web.php
@@ -6,7 +6,7 @@ include(dirname(__FILE__).'/gitupdate.php');
 if (file_exists('/home/'.$params->path.'/vendor')) {
 	$cmd = 'su -l deploy -c "cd /home/'.$params->path.' && composer update --ignore-platform-reqs"';
 } else {
-	$cmd = 'su -l deploy -c "cd /home/'.$params->path.' && composer install --ignore-platform-reqs"';
+	$cmd = 'su -l deploy -c "cd /home/'.$params->path.' && composer install --no-dev --ignore-platform-reqs"';
 }
 
 exec($cmd.' 2>&1 &', $o);
