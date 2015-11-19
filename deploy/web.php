40ead94a5a81f43d13b2d9a4fd9702773897498b
Devin Smith <github@arzynik.com>
2015-11-19 14:18:09 -0800

    fixed some deploy stuff

--- deploy/web.php
+++ deploy/web.php
@@ -4,9 +4,9 @@ include(dirname(__FILE__).'/gitupdate.php');
 
 
 if (file_exists('/home/'.$params->path.'/vendor')) {
-	$cmd = 'su -l deploy -c "composer update --ignore-platform-reqs"';
+	$cmd = 'su -l deploy -c "cd /home/'.$params->path.' && composer update --ignore-platform-reqs"';
 } else {
-	$cmd = 'su -l deploy -c "composer install --ignore-platform-reqs"';
+	$cmd = 'su -l deploy -c "cd /home/'.$params->path.' && composer install --ignore-platform-reqs"';
 }
 
 exec($cmd.' 2>&1 &', $o);
