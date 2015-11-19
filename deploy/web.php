84522036156c536bba64ce97832b22c4b766eb6a
Devin Smith <github@arzynik.com>
2015-11-19 10:25:09 -0800

    adds composer to deploy script

--- deploy/web.php
+++ deploy/web.php
@@ -1,3 +1,13 @@
 <?php
 
 include(dirname(__FILE__).'/gitupdate.php');
+
+
+if (file_exists('/home/'.$params->path.'/vendor')) {
+	$cmd = 'su -l deploy -c "composer update --ignore-platform-reqs"';
+} else {
+	$cmd = 'su -l deploy -c "composer install --ignore-platform-reqs"';
+}
+
+exec($cmd.' 2>&1 &', $o);
+echo implode("\n", $o);
