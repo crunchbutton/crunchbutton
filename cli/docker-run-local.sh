40daf95403dc72f5352ae0e8b237355f4d525e93
Devin Smith <github@arzynik.com>
2015-11-20 11:48:51 -0800

    docker updates

--- cli/docker-run-local.sh
+++ cli/docker-run-local.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 docker rm -f crunchbutton
-#docker build -f deploy/docker/Dockerfile.local.devin -t crunchbutton .
-#docker run -p 80:80 -v ~/Sites/crunchbutton:/opt/www --name crunchbutton crunchbutton
-
 docker build -t crunchbutton .
-docker run --entrypoint="" -e DEBUG=1 -e DATABASE_URL=mysql://root:root@172.17.0.1:3306/crunchbutton -p 8000:80 -v ~/Sites/crunchbutton:/var/www/app/ --name crunchbutton crunchbutton
-
-
-#docker run -e DEBUG=1 -e DATABASE_URL=mysql://crunchapple:n0on3l\!k35appl35\!every0nel0v35b4c0n\!@db3.crunchbutton.com/crunchbutton -p 8000:80 -v ~/Sites/crunchbutton:/var/www/app/ --name crunchbutton crunchbutton
-
+# replace database url with whatever your vbbox inet is:
+#vboxnet2: flags=8943<UP,BROADCAST,RUNNING,PROMISC,SIMPLEX,MULTICAST> mtu 1500
+#	ether 0a:00:27:00:00:02
+#	inet 192.168.99.1 netmask 0xffffff00 broadcast 192.168.99.255
+docker run --entrypoint="" -e DEBUG=1 -e DATABASE_URL=mysql://root:root@192.168.99.1:3306/crunchbutton -p 8000:80 -v $(pwd):/var/www/app/ --name crunchbutton crunchbutton
