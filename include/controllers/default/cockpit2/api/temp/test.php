2a4f0868c43de78b4095d6046d1785a22dc5d101
Daniel Camargo <pererinha@gmail.com>
2015-11-25 08:06:28 -0200

    test cron session

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,20 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		// $restaurant = Restaurant::o( 107 );
-		// echo json_encode($restaurant->timeInfo( null, 'cockpit' ));exit;
-		// Crunchbutton_Restaurant_Time::store( 'America/New_York' );
-		// Crunchbutton_Queue::process();
-		// 491019
-
-		if( $_GET[ 'id_queue' ] ){
-			Crunchbutton_Queue::processQueue( $_GET[ 'id_queue' ] );
-			die('hard');
-		}
-
-		if( $_GET[ 'tz' ] ){
-			Crunchbutton_Restaurant_Time::store( $_GET[ 'tz' ] );
-			die('hard');
-		}
+		c::db()->query('delete from session where token is null and id_user is NULL and date_activity < date_sub(now(), interval 1 month) limit 5000');
+		die('hard');
 	}
 }
