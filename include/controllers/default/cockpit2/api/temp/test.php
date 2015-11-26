da3ec8f886320ffe582d115202866aed5903e871
Daniel Camargo <pererinha@gmail.com>
2015-11-26 15:03:46 -0200

    Android cockpit app - Orders page freeze issue #7154

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,6 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		$order = Order::o( 220603 );
-		echo $order->message( 'sms-admin' );
+		die('hard');
 	}
 }
