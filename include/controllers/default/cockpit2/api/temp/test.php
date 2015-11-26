d4acd23809cc33c960eb9e7bb6b805f93cd25110
Daniel Camargo <pererinha@gmail.com>
2015-11-26 09:33:51 -0200

    Cockpit.la telling driver to upload payment info when already uploaded #7170

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		c::db()->query('delete from session where token is null and id_user is NULL and date_activity < date_sub(now(), interval 1 month) limit 5000');
-		die('hard');
+		$order = Order::o( 220603 );
+		echo $order->message( 'sms-admin' );
 	}
 }
