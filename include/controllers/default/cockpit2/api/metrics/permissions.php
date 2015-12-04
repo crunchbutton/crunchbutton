a12ea8d4bc2eb021915ca9f77079909d74a4b6ae
Daniel Camargo <pererinha@gmail.com>
2015-12-04 18:13:38 -0200

    add Full Order Value gross revenue graph (including markups, customer fee, etc) in cockpit.la #6947

--- include/controllers/default/cockpit2/api/metrics/permissions.php
+++ include/controllers/default/cockpit2/api/metrics/permissions.php
@@ -1,15 +1,18 @@
 <?php
 
 /**
- * Mini-handler to allow front end to list just the communities to which the 
+ * Mini-handler to allow front end to list just the communities to which the
  * user has permission to access metrics..
  **/
 class Controller_api_Metrics_Permissions extends Crunchbutton_Controller_RestAccount {
 	public function init() {
 		$out = [];
 		$communities = Cockpit_Metrics::availableCommunities();
+		if (c::admin()->permission()->check(['global' ])) {
+			$out[] = [ 'id_community' => -1, 'name' => 'All Communities', 'active' => true ];
+		}
 		foreach($communities as $c) {
-			$out[] = $c->properties();
+			$out[] = [ 'id_community' => $c->id_community, 'name' => $c->name, 'active' => $c->active ];
 		}
 		echo json_encode($out);
 	}
