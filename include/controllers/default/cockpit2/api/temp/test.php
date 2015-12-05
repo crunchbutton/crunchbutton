0f3a00f674b0846a35ef51cf60ceef498e1fd035
Daniel Camargo <pererinha@gmail.com>
2015-12-05 13:04:53 -0200

    partial #5189

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,10 +3,5 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		// $a = Crunchbutton_Address::byAddress( 'Condomínio do Edifício Primula - R. Max Colin, 941 - Centro, Joinville - SC, 89204-040, Brazil' );
-		// echo '<pre>';var_dump( $a );exit();
-		// Order::ticketsForNotGeomatchedOrders();
-		$order = Order::o( 233062 );
-		$order->approve_address();
 	}
 }
