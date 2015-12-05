42f200809361ff6a5246897787646f45752e7c7b
Daniel Camargo <pererinha@gmail.com>
2015-12-05 13:37:32 -0200

    partial #5189

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,5 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
+			$log = Crunchbutton_Phone_Log::o( $_GET[ 'id' ] );
+			$log->emit();
 	}
 }
