34282535b8d2a1b63712439ec3807f5df51bac8e
Daniel Camargo <pererinha@gmail.com>
2015-12-07 09:56:42 -0200

    Clicking the names in the support tab on support takes us to a random employee's page #7208

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,5 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-			$log = Crunchbutton_Phone_Log::o( $_GET[ 'id' ] );
-			$log->emit();
 	}
 }
