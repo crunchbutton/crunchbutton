d886a6d34af5fc5cba3b5b0318b085999e9320c4
Daniel Camargo <pererinha@gmail.com>
2015-12-08 17:33:32 -0200

    partial #7172

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,5 +3,7 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
+		$c = Crunchbutton_Cron_Job_PexPreProcessReport::o( 111 );
+		$c->run();
 	}
 }
--- include/library/Crunchbutton/Cron/Job/PexPreProcessReport.php
+++ include/library/Crunchbutton/Cron/Job/PexPreProcessReport.php
@@ -9,7 +9,7 @@ class Crunchbutton_Cron_Job_PexPreProcessReport extends Crunchbutton_Cron_Log {
 		$now->modify( '-2 days' );
 		$start = $now->format( 'm/d/Y' );
 
-		Log::debug( [ 'start'=> $start, 'end' => $end, 'type' => 'pex-report' 'started' => true ] );
+		Log::debug( [ 'start'=> $start, 'end' => $end, 'type' => 'pex-report', 'started' => true ] );
 
 		Crunchbutton_Pexcard_Transaction::processReport( $start, $end );
 
