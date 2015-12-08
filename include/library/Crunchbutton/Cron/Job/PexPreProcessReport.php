4189ca7e98d80f442e5cbcaa3d455267005b6d76
Daniel Camargo <pererinha@gmail.com>
2015-12-08 17:27:49 -0200

    partial #7172

--- include/library/Crunchbutton/Cron/Job/PexPreProcessReport.php
+++ include/library/Crunchbutton/Cron/Job/PexPreProcessReport.php
@@ -4,19 +4,16 @@ class Crunchbutton_Cron_Job_PexPreProcessReport extends Crunchbutton_Cron_Log {
 
 	public function run(){
 
-		$lastReportDate = c::db()->get( 'SELECT date FROM pexcard_report_order ORDER BY date ASC LIMIT 1' )->get( 0 );
-		if( $lastReportDate->date ){
-			$lastReportDate = new DateTime( $lastReportDate->date, new DateTimeZone( c::config()->timezone ) );
-		} else {
-			$lastReportDate = new DateTime( 'now', new DateTimeZone( c::config()->timezone ) );
-		}
-		$end = $lastReportDate->format( 'm/d/Y' );
-		$lastReportDate->modify( '-5 days' );
-		$start = $lastReportDate->format( 'm/d/Y' );
+		$now = new DateTime( 'now', new DateTimeZone( c::config()->timezone ) );
+		$end = $now->format( 'm/d/Y' );
+		$now->modify( '-2 days' );
+		$start = $now->format( 'm/d/Y' );
 
-		Log::debug( [ 'start'=> $start, 'end' => $end, 'type' => 'pex-report' ] );
+		Log::debug( [ 'start'=> $start, 'end' => $end, 'type' => 'pex-report' 'started' => true ] );
 
 		Crunchbutton_Pexcard_Transaction::processReport( $start, $end );
+
+		Log::debug( [ 'start'=> $start, 'end' => $end, 'type' => 'pex-report', 'finished' => true ] );
 
 		// it always must call finished method at the end
 		$this->finished();
