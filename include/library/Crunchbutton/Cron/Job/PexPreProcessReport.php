23c8a2a493738a100b03180080d70d61042128e3
Pererinha <pererinha@gmail.com>
2015-12-08 21:47:28 -0200

    pex card cron job error

--- include/library/Crunchbutton/Cron/Job/PexPreProcessReport.php
+++ include/library/Crunchbutton/Cron/Job/PexPreProcessReport.php
@@ -6,7 +6,7 @@ class Crunchbutton_Cron_Job_PexPreProcessReport extends Crunchbutton_Cron_Log {
 
 		$now = new DateTime( 'now', new DateTimeZone( c::config()->timezone ) );
 		$end = $now->format( 'm/d/Y' );
-		$now->modify( '-2 days' );
+		$now->modify( '-1 days' );
 		$start = $now->format( 'm/d/Y' );
 
 		Log::debug( [ 'start'=> $start, 'end' => $end, 'type' => 'pex-report', 'started' => true ] );
