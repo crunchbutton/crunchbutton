76110ca786ab9efebb61afec1ff5f8a37ea64cf5
Daniel Camargo <pererinha@gmail.com>
2015-12-14 20:00:39 -0200

    partial #7221

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,14 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		$c = Crunchbutton_Cron_Job_PexPreProcessReport::o( 111 );
-		$c->run();
+		$driver = Admin::o( 14604 );
+		echo '<pre>';var_dump( $driver->name, $driver->areSettlementDocsOk() );
+		echo "\n----------------\n";
+		$driver = Admin::o( 255 );
+		echo '<pre>';var_dump( $driver->name, $driver->areSettlementDocsOk() );
+		echo "\n----------------\n";
+		$driver = Admin::o( 13422 );
+		echo '<pre>';var_dump( $driver->name, $driver->areSettlementDocsOk() );
+
 	}
 }
