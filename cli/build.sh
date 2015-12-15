330668e9e9294d52f9f12afce0017c767656cb4d
Devin Smith <github@arzynik.com>
2015-12-15 14:50:47 -0800

    updated dockerfile for aws

--- cli/build.sh
+++ cli/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PHPPATH=$1/.heroku/php/bin/php
+PHPPATH=php
 
 $PHPPATH -f ./_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > ../www/build/crunchbutton.html
 $PHPPATH -f ./_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > ../www/build/cockpit.html
@@ -8,7 +8,7 @@ $PHPPATH -f ./_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > ../www/build/coc
 $PHPPATH -f ./_build.php crunchbutton.com assets/css/bundle.css s=style $PHPPATH > ../www/assets/css/bundle.css
 $PHPPATH -f ./_build.php cockpit.la assets/cockpit/css/bundle.css s=cockpit $PHPPATH > ../www/assets/cockpit/css/bundle.css
 
-$PHPPATH -f ./_build.php crunchbutton.com assets/js/bundle.js s=app $PHPPATH | ../www/assets/js/bundle.js
+$PHPPATH -f ./_build.php crunchbutton.com assets/js/bundle.js s=app $PHPPATH > ../www/assets/js/bundle.js
 $PHPPATH -f ./_build.php cockpit.la assets/cockpit/js/bundle.js s=cockpit $PHPPATH > ../www/assets/cockpit/js/bundle.js
 
 mv ../www/.htaccess.heroku ../www/.htaccess
