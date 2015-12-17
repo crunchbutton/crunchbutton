29f811b03cbc5e30a15750cdbb7e181e8d380c67
Daniel Camargo <pererinha@gmail.com>
2015-12-17 13:19:49 -0200

    partial #2367 4)

--- include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
+++ include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
@@ -7,7 +7,7 @@ class Crunchbutton_Cron_Job_ProcessPreOrder extends Crunchbutton_Cron_Log {
 		$now = new DateTime( 'now', new DateTimeZone( c::config()->timezone ) );
 		$now->modify( '+ 90 min' );
 
-		$orders = Order::q( 'SELECT * FROM `order` WHERE preordered = 1 AND preorder_processed = 0 AND refunded = 0 AND date_delivery <= ?', [ $now->format( 'Y-m-d H:i:s' ) ] );
+		$orders = Order::sq( 'SELECT * FROM `order` WHERE preordered = 1 AND preorder_processed = 0 AND refunded = 0 AND date_delivery <= ?', [ $now->format( 'Y-m-d H:i:s' ) ] );
 		foreach( $orders as $order ){
 			$this->process( $order );
 		}
