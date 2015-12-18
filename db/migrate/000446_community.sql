d0580b76cae634f9d5d3c7af4220b7778471f52f
Daniel Camargo <pererinha@gmail.com>
2015-12-18 15:55:37 -0200

    partial #7281

---
+++ db/migrate/000446_community.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `community` ADD COLUMN `notify_non_shift_drivers` tinyint(1) unsigned NOT NULL DEFAULT 0;
+ALTER TABLE `community` ADD COLUMN `notify_non_shift_drivers_min` int(11) unsigned NOT NULL DEFAULT 5;
