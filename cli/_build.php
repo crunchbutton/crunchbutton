5be17cce69c70bcba5f22400f5a136debc88f805
Devin Smith <github@arzynik.com>
2015-12-18 12:57:12 -0800

    some prebuild changes

--- cli/_build.php
+++ cli/_build.php
@@ -1,6 +1,8 @@
 #!/usr/bin/env php
 <?php
 
+error_reporting(E_ALL ^ ( E_NOTICE | E_STRICT | E_DEPRECATED ) );
+
 // create static assets
 
 $host = $argv[1];
@@ -14,5 +16,5 @@ error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ini_set('display_errors',true);
 $_REQUEST['__host'] = $_SERVER['HTTP_HOST'] = $_SERVER['SERVER_NAME'] = $host;
 $_REQUEST['__url'] = $uri;
-require_once('../include/crunchbutton.php');
+require_once('include/crunchbutton.php');
 Cana::app()->displayPage();
--- cli/build.sh
+++ cli/build.sh
@@ -2,13 +2,13 @@
 
 PHPPATH=php
 
-$PHPPATH -f ./_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > ../www/build/crunchbutton.html
-$PHPPATH -f ./_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > ../www/build/cockpit.html
+$PHPPATH -f cli/_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > www/build/crunchbutton.html
+$PHPPATH -f cli/_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > www/build/cockpit.html
 
-$PHPPATH -f ./_build.php crunchbutton.com assets/css/bundle.css s=style $PHPPATH > ../www/assets/css/bundle.css
-$PHPPATH -f ./_build.php cockpit.la assets/cockpit/css/bundle.css s=cockpit $PHPPATH > ../www/assets/cockpit/css/bundle.css
+$PHPPATH -f cli/_build.php crunchbutton.com assets/css/bundle.css s=style $PHPPATH > www/assets/css/bundle.css
+$PHPPATH -f cli/_build.php cockpit.la assets/cockpit/css/bundle.css s=cockpit $PHPPATH > www/assets/cockpit/css/bundle.css
 
-$PHPPATH -f ./_build.php crunchbutton.com assets/js/bundle.js s=app $PHPPATH > ../www/assets/js/bundle.js
-$PHPPATH -f ./_build.php cockpit.la assets/cockpit/js/bundle.js s=cockpit $PHPPATH > ../www/assets/cockpit/js/bundle.js
+$PHPPATH -f cli/_build.php crunchbutton.com assets/js/bundle.js s=app $PHPPATH > www/assets/js/bundle.js
+$PHPPATH -f cli/_build.php cockpit.la assets/cockpit/js/bundle.js s=cockpit $PHPPATH > www/assets/cockpit/js/bundle.js
 
-mv ../www/.htaccess.heroku ../www/.htaccess
+mv www/.htaccess.heroku www/.htaccess
