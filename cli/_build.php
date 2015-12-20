76c8bfd3eca03f57114075f36ad57a88a776e603
Devin Smith <github@arzynik.com>
2015-12-20 11:37:04 -0800

    fixed path

--- cli/_build.php
+++ cli/_build.php
@@ -16,5 +16,5 @@ error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ini_set('display_errors',true);
 $_REQUEST['__host'] = $_SERVER['HTTP_HOST'] = $_SERVER['SERVER_NAME'] = $host;
 $_REQUEST['__url'] = $uri;
-require_once('include/crunchbutton.php');
+require_once('../include/crunchbutton.php');
 Cana::app()->displayPage();
