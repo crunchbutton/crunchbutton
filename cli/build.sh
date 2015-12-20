e532849085d4aa3bc69b3df781f24b7679a2c1d9
Devin Smith <github@arzynik.com>
2015-12-20 11:19:38 -0800

    set to absolute paths

--- cli/build.sh
+++ cli/build.sh
@@ -2,13 +2,13 @@
 
 PHPPATH=php
 
-$PHPPATH -f cli/_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > www/build/crunchbutton.html
-$PHPPATH -f cli/_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > www/build/cockpit.html
+$PHPPATH -f /app/cli/_build.php crunchbutton.com $PHPPATH | tr -d "\t\n\r" > /app/www/build/crunchbutton.html
+$PHPPATH -f /app/cli/_build.php cockpit.la $PHPPATH | tr -d "\t\n\r" > /app/www/build/cockpit.html
 
-$PHPPATH -f cli/_build.php crunchbutton.com assets/css/bundle.css s=style $PHPPATH > www/assets/css/bundle.css
-$PHPPATH -f cli/_build.php cockpit.la assets/cockpit/css/bundle.css s=cockpit $PHPPATH > www/assets/cockpit/css/bundle.css
+$PHPPATH -f /app/cli/_build.php crunchbutton.com assets/css/bundle.css s=style $PHPPATH > /app/www/assets/css/bundle.css
+$PHPPATH -f /app/cli/_build.php cockpit.la assets/cockpit/css/bundle.css s=cockpit $PHPPATH > /app/www/assets/cockpit/css/bundle.css
 
-$PHPPATH -f cli/_build.php crunchbutton.com assets/js/bundle.js s=app $PHPPATH > www/assets/js/bundle.js
-$PHPPATH -f cli/_build.php cockpit.la assets/cockpit/js/bundle.js s=cockpit $PHPPATH > www/assets/cockpit/js/bundle.js
+$PHPPATH -f /app/cli/_build.php crunchbutton.com assets/js/bundle.js s=app $PHPPATH > /app/www/assets/js/bundle.js
+$PHPPATH -f /app/cli/_build.php cockpit.la assets/cockpit/js/bundle.js s=cockpit $PHPPATH > /app/www/assets/cockpit/js/bundle.js
 
-mv www/.htaccess.heroku www/.htaccess
+mv /app/www/.htaccess.heroku /app/www/.htaccess
