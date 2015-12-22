150973f891108cd88781184e298f4ce53ed9db51
Daniel Camargo <pererinha@gmail.com>
2015-12-22 16:44:44 -0200

    cron

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,6 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		$r = Restaurant::o(107);
-		echo '<pre>';var_dump( $r->preOrderHours() );exit();
+		Crunchbutton_Cron_Log::test();
 	}
 }
