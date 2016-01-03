cc2b8df0a8ee7af58108ecb23025377b59bd477e
Devin Smith <github@arzynik.com>
2016-01-03 14:16:47 -0800

    keep alive

--- cli/build_upload.sh
+++ cli/build_upload.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-cd /app/cli && /app/cli/build.sh
-echo "starting..." > /var/log/build.log
-tail -f /var/log/build.log && cd /app/cli && /app/cli/_build_upload.php -e=live
+
+echo "done." > /var/log/build.log
+
+cd /app/cli
+/app/cli/build.sh
+/app/cli/_build_upload.php -e=live
+tail -f /var/log/build.log
