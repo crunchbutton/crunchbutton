409e39fbfe130b447a3064aa70d4363cc938cfa5
Devin Smith <github@arzynik.com>
2016-01-04 09:29:16 -0800

    use s3 for all assets

--- include/library/Crunchbutton/Deploy/Server.php
+++ include/library/Crunchbutton/Deploy/Server.php
@@ -1,18 +1,21 @@
 <?php
 
 class Crunchbutton_Deploy_Server extends Cana_Table {
+	const AMAZON_S3_CACHE = 23;
+
 	public function exports() {
 		$ex = $this->properties();
 		$ex['version'] = $this->version() ? $this->version()->exports() : null;
 		return $ex;
 	}
-	
+
 	public static function byName($name) {
 		return Crunchbutton_Deploy_Server::q('select * from deploy_server where name=?', [$name])->get(0);
 	}
 
 	public static function currentVersion() {
-		$server = self::byName($_SERVER['SERVER_NAME']);
+		//$server = self::byName($_SERVER['SERVER_NAME']);
+		$server = new Crunchbutton_Deploy_Server(self::AMAZON_S3_CACHE);
 		if ($server) {
 			$version = $server->version();
 		}
@@ -22,7 +25,8 @@ class Crunchbutton_Deploy_Server extends Cana_Table {
 		if (!$v) {
 			$v = Cana_Util::gitVersion();
 		}
-		return md5($v);
+		//return md5($v);
+		return $v;
 	}
 
 	public function version() {
@@ -37,7 +41,7 @@ class Crunchbutton_Deploy_Server extends Cana_Table {
 		}
 		return $this->_version;
 	}
-	
+
 	public function versions() {
 		if (!isset($this->_versions)) {
 			$this->_versions = Crunchbutton_Deploy_Version::q('
@@ -71,7 +75,7 @@ class Crunchbutton_Deploy_Server extends Cana_Table {
 		}
 		return $this->_commits;
 	}
-	
+
 	public function createTag($version) {
 		$name = date('ymd');
 		$inc = '00';
@@ -92,17 +96,17 @@ class Crunchbutton_Deploy_Server extends Cana_Table {
 				return false;
 			}
 		}
-		
+
 		$tag = $name.'.'.$inc;
 
 		$repo = explode('/', $this->repo);
 		$log = Github::createTag($repo[0], $repo[1], $tag, $version, 'Created from Cockpit Deployment Tool');
-		
+
 		$this->_commits = null;
 
 		return $tag;
 	}
-	
+
 	public function tags() {
 		if (!isset($this->_tags) || $this->_tags === null) {
 			$repo = explode('/', $this->repo);
