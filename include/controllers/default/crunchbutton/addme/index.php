b0a8fee57f73bd19568925762a1f874218dba37e
Devin Smith <github@arzynik.com>
2016-01-04 09:58:57 -0800

    removed lots of ui1 assets

--- include/controllers/default/crunchbutton/addme/index.php
+++ include/controllers/default/crunchbutton/addme/index.php
@@ -4,7 +4,5 @@ class Controller_addme extends Cana_Controller {
 	public function init() {
 		header('Location: /owners');
 		exit;
-		c::view()->layout('layout/landing');
-		c::view()->display('addme/index');
 	}
 }
