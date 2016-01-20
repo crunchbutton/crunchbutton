6539131a9aad0adaebf958c9b2b522f3ee370d23
Daniel Camargo <pererinha@gmail.com>
2016-01-20 10:36:29 -0200

    partial #7532

--- include/library/Crunchbutton/Queue/Pexcard/Action.php
+++ include/library/Crunchbutton/Queue/Pexcard/Action.php
@@ -4,7 +4,12 @@ class Crunchbutton_Queue_Pexcard_Action extends Crunchbutton_Queue {
 
 	public function run() {
 
-		$this->pexcard_action()->run();
+		if( $this->pexcard_action() ){
+			echo "### running Crunchbutton_Queue_Pexcard_Action";
+			$this->pexcard_action()->run();
+		} else {
+			return self::STATUS_FAILED;
+		}
 
 		return self::STATUS_SUCCESS;
 	}
