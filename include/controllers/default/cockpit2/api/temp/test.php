d962092775e8c01d9d500b97be2f332bef514cb0
Daniel Camargo <pererinha@gmail.com>
2016-01-20 10:28:55 -0200

    partial #7532

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -4,18 +4,8 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
 
-		$q = Crunchbutton_Queue_Pexcard_Action::o( 33 );
+		$q = Crunchbutton_Queue_Pexcard_Action::o( 2138424 );
 		$q->run();
-
-die('hard');
-		$admin = Admin::o( 1 );
-		echo $admin->name . "\n";
-		echo "isDriver: " . $admin->isDriver() . "\n";
-		echo "isMarketingRep: " . $admin->isMarketingRep() . "\n";
-		echo "isCampusManager: " . $admin->isCampusManager() . "\n";
-		echo "isSupport: " . $admin->isSupport() . "\n";
-		die('hard');
-
 
 	}
 }
