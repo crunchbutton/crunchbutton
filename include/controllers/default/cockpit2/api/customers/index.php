f8fd765d8edbff5228ac46eb8449a5ba0ffd83c8
Daniel Camargo <pererinha@gmail.com>
2016-01-22 11:17:24 -0200

    display email in support ticket #7289

--- include/controllers/default/cockpit2/api/customers/index.php
+++ include/controllers/default/cockpit2/api/customers/index.php
@@ -53,14 +53,15 @@ class Controller_api_customers extends Crunchbutton_Controller_RestAccount {
 				'search' => stripslashes($search),
 				'fields' => [
 					'`user`.name' => 'like',
-					'`user`.phone' => 'likephone',
 					'`user`.address' => 'like',
+					'`user`.email' => 'like',
 					'`order`.name' => 'like',
-					'`order`.phone' => 'likephone',
 					'`order`.address' => 'like',
 					'restaurant.name' => 'like',
 					'community.name' => 'like',
-					'`user`.id_user' => 'inteq'
+					'`user`.id_user' => 'inteq',
+					'`user`.phone' => 'likephone',
+					'`order`.phone' => 'likephone',
 				]
 			]);
 			$q .= $s['query'];
