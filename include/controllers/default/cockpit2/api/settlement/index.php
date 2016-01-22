40c3962bf6269d1ffb9d4b18614ee1b696eaa4d8
Daniel Camargo <pererinha@gmail.com>
2016-01-22 11:49:26 -0200

    partial  #7570

--- include/controllers/default/cockpit2/api/settlement/index.php
+++ include/controllers/default/cockpit2/api/settlement/index.php
@@ -40,6 +40,9 @@ class Controller_Api_Settlement extends Crunchbutton_Controller_RestAccount {
 							case 'download-summary':
 								$this->_driverDownloadSummary();
 								break;
+							case 'info-payroll':
+								$this->_infoPayRoll();
+								break;
 							default:
 								$this->_error();
 								break;
@@ -939,6 +942,52 @@ class Controller_Api_Settlement extends Crunchbutton_Controller_RestAccount {
 		} else {
 			echo json_encode( [ 'error' => true ] );
 		}
+	}
+
+	public function _infoPayRoll(){
+
+		$year = c::getPagePiece( 4 ) ? c::getPagePiece( 4 ) : date( 'Y' ) ;
+		$query = "SELECT
+					  p.id_driver,
+					  a.name,
+					  a.phone,
+					  a.email,
+					  SUM( amount ) AS payment
+					  FROM payment p
+					  INNER JOIN admin a ON a.id_admin = p.id_driver
+					  WHERE YEAR( p.date ) = ?
+					  AND p.pay_type = 'payment'
+					  AND p.stripe_id IS NOT NULL
+					  AND p.env = 'live' AND p.payment_status = 'succeeded'
+					GROUP BY id_driver
+					ORDER BY a.name ASC
+		";
+
+		$payments = c::db()->get( $query, [ $year ] );
+		$out = [];
+		$out[] = join( ',', [	'ID', 'Name', 'Address', 'SSN','Amount' ] );
+		foreach( $payments as $payment ){
+			$driver = Admin::o( $payment->id_driver );
+			$address = $driver->payment_type()->address;
+			$address = str_replace( "\n", "", $address );
+			$address = str_replace( ",", ";", $address );
+			$name = $payment->name ? $payment->name : $driver->name;
+			$ssn = $driver->ssn();
+			$out[] = join( ',', [	$payment->id_driver,
+														$name,
+														$address,
+														$ssn,
+														$payment->payment
+								] );
+		}
+		header('Expires: 0');
+		header('Cache-control: private');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Content-Description: File Transfer');
+		header('Content-Type: text/csv');
+		header('Content-disposition: attachment; filename="info-for-1099-year-' . $year . '.csv"');
+
+		echo preg_replace(["/\r\n/","/\r/"],["\n","\n"], join( "\n", $out ) );
 	}
 
 	public function _driverDownloadSummary(){
