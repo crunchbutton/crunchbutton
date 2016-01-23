a2a0ae80efbc4a4da0de7bc9ce0aa5a592b9e193
Daniel Camargo <pererinha@gmail.com>
2016-01-23 13:23:41 -0200

    partial #7421 - fixed customer name

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -4,12 +4,9 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
 
-		$q = Crunchbutton_Queue_Notification_Minutes_Way::o( 2162915 );
-		$q->run();
-		die('hard');
-
-		$order = Order::o( 243192 );
-		$order->textCustomer( Cockpit_Order::I_AM_5_MINUTES_AWAY );
+		Event::emit([
+			'room' => [ 'tickets', 'ticket.update' ]
+		], 'change_ticket_status', [] );
 
 
 	}
