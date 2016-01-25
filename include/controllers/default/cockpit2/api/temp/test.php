64cdb82d63b8da6a90250b320c647502399f2f01
Daniel Camargo <pererinha@gmail.com>
2016-01-25 19:44:27 -0200

    payments scheduled for months? #7607

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -4,8 +4,5 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
 
-		$set = new Settlement;
-		echo '<pre>';var_dump( $set->payDriver( 52919 ) );exit();;
-
 	}
 }
