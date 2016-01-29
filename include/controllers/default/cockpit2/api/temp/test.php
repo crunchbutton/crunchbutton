c2671d4df9f7954f27b9111f63992411b1ac0f5c
Daniel Camargo <pererinha@gmail.com>
2016-01-28 23:15:47 -0200

    missing closing logs #7615

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,6 +3,19 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-		Cockpit_Admin_Pexcard::pexCardRemoveCardFundsDaily();
+		echo '<pre>';var_dump( strval( 0 ) );;
+		echo "\n";var_dump( strval( "0" ) );;
+		echo "\n";var_dump( strval( false ) );;
+		echo "\n";var_dump( strval( true ) );;
+
+		// if( $this->request()[ 'close_all_restaurants' ] ){
+			$c = Community::o( 6 );
+			$c->close_all_restaurants = $this->request()[ 'close_all_restaurants' ];
+			$c->save();
+		// }
+
+
+		echo "c->close_all_restaurants: " . $c->close_all_restaurants;
+
 	}
 }
