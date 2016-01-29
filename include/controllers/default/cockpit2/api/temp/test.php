089e3402aa5e2964815df14dc888df523a65fde3
Daniel Camargo <pererinha@gmail.com>
2016-01-29 19:17:27 -0200

    partial #7721

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -3,7 +3,11 @@
 class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init(){
-			Community::removeForceClose();
+			Crunchbutton_Message_Sms::send([
+			'to' => '203-772-8167',
+			'message' => 'testing',
+			'reason' => Crunchbutton_Message_Sms::REASON_CUSTOMER_ORDER
+		]);
 
 	}
 }
