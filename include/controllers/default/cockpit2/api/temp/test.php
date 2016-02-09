705d68264dfd9933bf6fb48203341296c817f19e
Daniel Camargo <pererinha@gmail.com>
2016-02-09 13:03:54 -0200

    PEX API Limits #7717

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -12,18 +12,20 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 		// 112966
 		// 612
 // Crunchbutton_Pexcard_Token::createToken();
-		Cockpit_Admin_Pexcard::pexCardRemoveCardFundsDaily();
+		// Cockpit_Admin_Pexcard::pexCardRemoveCardFundsDaily();
 
 		// echo json_encode(  Crunchbutton_Pexcard_Details::account()  );exit;
 
-		// $pex = Cockpit_Admin_Pexcard::o( 1390 );
+		// $pex = Cockpit_Admin_Pexcard::o( 167628 );
 		// $pex->addArbitraryFunds( 5, 'test' );
 		// $pex->runQueRemoveFunds();
 
 // echo '<pre>';var_dump( Crunchbutton_Pexcard_Token::getToken() );exit();
+		if( $_GET[ 'id_action' ] ){
+			$action = Crunchbutton_Pexcard_Action::o( $_GET[ 'id_action' ] );
+			$action->run();
+		}
 
-		// $action = Crunchbutton_Pexcard_Action::o( 167286 );
-		// $action->run();
 		// Crunchbutton_Cron_Job_CreatePexCardToken::run();
 
 		// echo '<pre>';var_dump( $pex->properties() );exit();
