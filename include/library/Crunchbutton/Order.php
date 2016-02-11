ef5ea548d7dfde8abc4e0a693cfd1c981ea39ab0
Daniel Camargo <pererinha@gmail.com>
2016-02-11 15:31:41 -0200

    Driver received message of priority order, no orders were available #7772

--- include/library/Crunchbutton/Order.php
+++ include/library/Crunchbutton/Order.php
@@ -1345,11 +1345,9 @@ class Crunchbutton_Order extends Crunchbutton_Order_Trackchange {
 			$interval3Min = $now->format( 'Y-m-d H:i:s' );
 			$now->modify( '+ 3 minutes' );
 
-			$now->modify( '- 2 hour' );
-			$preorder_date_start = $now->format( 'Y-m-d H:i:s' );
+			$now->modify( '+ 6 hours' );
+			$preorder_date = $now->format( 'Y-m-d H:i:s' );
 
-			$now->modify( '+ 8 hours' );
-			$preorder_date_end = $now->format( 'Y-m-d H:i:s' );
 
 			if (!$admin) {
 				$admin = c::admin();
@@ -1368,13 +1366,13 @@ class Crunchbutton_Order extends Crunchbutton_Order_Trackchange {
 				'(op.id_order is null and ((c.delivery_logistics is null) or (o.date < ? and ' .
 				'c.delivery_logistics is not null) or (o.preordered=1)))  or (op.id_order is not null and op.priority_expiration < ?) ' .
 				'or (op.id_order is not null and op.priority_expiration >= ? and op.id_admin = ? '.
-				'and op.priority_given != ?)) and o.delivery_service=true and o.delivery_type = "delivery" and ( o.date > ? OR ( o.preordered = 1 and o.date_delivery < ? and o.date_delivery > ? ) )'.
+				'and op.priority_given != ?)) and o.delivery_service=true and o.delivery_type = "delivery" and ( o.date > ? OR ( o.preordered = 1 and o.date_delivery < ? AND o.date_delivery > ? ) )'.
 				'and ' . $where . ' ORDER BY o.id_order';
 //			$op = Crunchbutton_Order_Priority::PRIORITY_LOW;
 //			print "The query params: $nowString, $nowString, $admin->id_admin, $op, $interval\n";
 
 			return Order::q($query, [$interval3Min, $nowString, $nowString, $admin->id_admin,
-				Crunchbutton_Order_Priority::PRIORITY_LOW, $interval, $preorder_date_end, $preorder_date_start ]);
+				Crunchbutton_Order_Priority::PRIORITY_LOW, $interval, $preorder_date, $interval]);
 
 			//
 		}
