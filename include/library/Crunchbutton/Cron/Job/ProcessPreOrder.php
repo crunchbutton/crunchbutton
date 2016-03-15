a6b190ad75ae60890ed57ff01a4fdef73eacc35a
Daniel Camargo <pererinha@gmail.com>
2016-03-15 09:01:49 -0300

    pre-order warning inaccurate because drivers are scheduled #7968

--- include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
+++ include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
@@ -8,17 +8,10 @@ class Crunchbutton_Cron_Job_ProcessPreOrder extends Crunchbutton_Cron_Log {
 		$now->modify( Crunchbutton_Order::PRE_ORDER_INTERVAL );
 		$orders = Order::q( 'SELECT * FROM `order` WHERE preordered = 1 AND preorder_processed = 0 AND refunded = 0 AND date_delivery <= ?', [ $now->format( 'Y-m-d H:i:s' ) ] );
 		foreach( $orders as $order ){
-			$community = $order->community();
-			if( $community->allThirdPartyDeliveryRestaurantsClosed() || $community->allRestaurantsClosed() ){
+			if( $order->hasDriverToDeliveryPreOrder() === false ){
 				Crunchbutton_Admin_Notification::warningAboutNoRepsWorking( $order );
 			} else {
-				// $hasDrivers = ( $community->activeDrivers() > 0 );
-				// $minutesToBeDelivered = $order->minutesToBeDelivered();
-				// if the community has no drivers and we have just 30 minutes to delivery the order
-				// it will process the order and warn cs - "reps failed to pickup order" message comes through for preorders #7906
-				// if( ( $minutesToBeDelivered && $minutesToBeDelivered < 30 ) || $hasDrivers ){
 				$this->process( $order );
-				// }
 			}
 		}
 		// it always must call finished method at the end
