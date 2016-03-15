7a578192b65c4860c76cae11c1879fc51059679f
Daniel Camargo <pererinha@gmail.com>
2016-03-15 09:08:50 -0300

    pre-order warning inaccurate because drivers are scheduled #7968

--- include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
+++ include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
@@ -8,7 +8,7 @@ class Crunchbutton_Cron_Job_ProcessPreOrder extends Crunchbutton_Cron_Log {
 		$now->modify( Crunchbutton_Order::PRE_ORDER_INTERVAL );
 		$orders = Order::q( 'SELECT * FROM `order` WHERE preordered = 1 AND preorder_processed = 0 AND refunded = 0 AND date_delivery <= ?', [ $now->format( 'Y-m-d H:i:s' ) ] );
 		foreach( $orders as $order ){
-			if( $order->hasDriverToDeliveryPreOrder() === false ){
+			if( !$order->hasDriverToDeliveryPreOrder() ){
 				Crunchbutton_Admin_Notification::warningAboutNoRepsWorking( $order );
 			} else {
 				$this->process( $order );
