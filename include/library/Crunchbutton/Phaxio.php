b9bea7217c07eeb1ad405924b4cf7b377e846054
Daniel Camargo <pererinha@gmail.com>
2016-03-31 23:05:23 -0300

    s'Wings and Zaroka (yale restaurants) not receiving orders #8030

--- include/library/Crunchbutton/Phaxio.php
+++ include/library/Crunchbutton/Phaxio.php
@@ -7,15 +7,15 @@ class Crunchbutton_Phaxio {
 		$cmd = 'curl https://api.phaxio.com/v1/send '
 			.'-F "to='.$params['to'].'" '
 			.'-F "filename=@'.$params['file'].'" ';
-		if ($params['id_notification_log']) {	
+		if ($params['id_notification_log']) {
 			// Staging / Devs do not work with https
 			if( c::config()->host_callback == 'staging.crunchr.co' || $env == 'dev' ){
 				$protocol = 'http';
 			} else {
 				$protocol = 'https';
 			}
-
-			$cmd .= '-F "callback_url=' . $protocol . '://'.c::config()->host_callback.'/api/notification/'.$params['id_notification_log'].'/callback" ';
+			$cmd .= '-F "callback_url=' . $protocol . '://cockpit.la/api/notification/'.$params['id_notification_log'].'/callback" ';
+			// $cmd .= '-F "callback_url=' . $protocol . '://'.c::config()->host_callback.'/api/notification/'.$params['id_notification_log'].'/callback" ';
 		}
 		$cmd .= '-F "api_key='.c::config()->phaxio->{$env}->key.'" '
 			.'-F "api_secret='.c::config()->phaxio->{$env}->secret.'"';
@@ -60,7 +60,7 @@ class Crunchbutton_Phaxio {
 		exec($cmd, $return);
 
 		$return = json_decode(trim(join('',$return)));
-	
+
 		if ($return) {
 			foreach ($return as $key => $value) {
 				$this->{$key} = $value;
