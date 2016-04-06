26f15a9458bb92e23386c612b25d6c4197c1c917
Daniel Camargo <pererinha@gmail.com>
2016-04-06 11:05:31 -0300

    "No Drivers to deliver pre-order" message comes through for first party restaurants #8033

--- include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
+++ include/library/Crunchbutton/Cron/Job/ProcessPreOrder.php
@@ -6,7 +6,7 @@ class Crunchbutton_Cron_Job_ProcessPreOrder extends Crunchbutton_Cron_Log {
 
 		$now = new DateTime( 'now', new DateTimeZone( c::config()->timezone ) );
 		$now->modify( Crunchbutton_Order::PRE_ORDER_INTERVAL );
-		$orders = Order::q( 'SELECT * FROM `order` WHERE preordered = 1 AND preorder_processed = 0 AND refunded = 0 AND date_delivery <= ?', [ $now->format( 'Y-m-d H:i:s' ) ] );
+		$orders = Order::q( 'SELECT * FROM `order` o INNER JOIN restaurant r ON r.id_restaurant = o.id_restaurant WHERE o.preordered = 1 AND o.preorder_processed = 0 AND o.refunded = 0 AND o.delivery_type = "delivery" AND r.delivery_service = 1 AND date_delivery <= ?', [ $now->format( 'Y-m-d H:i:s' ) ] );
 		foreach( $orders as $order ){
 			if( !$order->hasDriverToDeliveryPreOrder() ){
 				Crunchbutton_Admin_Notification::warningAboutNoRepsWorking( $order );
