5ec7db8309cf5a5975f81fad423580c9b2ff9f17
Daniel Camargo <pererinha@gmail.com>
2016-05-09 19:14:01 -0300

    texting with @ doesn't work #8168

--- include/library/Crunchbutton/Admin.php
+++ include/library/Crunchbutton/Admin.php
@@ -181,7 +181,7 @@ class Crunchbutton_Admin extends Cana_Table_Trackchange {
 	}
 
 	public function getByPhone( $phone, $activeOnly = false){
-		return Admin::q( "SELECT * FROM admin a WHERE phone = ? ".($activeOnly ? 'AND a.active=true ' : '')." ORDER BY id_admin DESC LIMIT 1", [ Phone::clean( $phone ) ] )->get( 0 );
+		return Admin::q( "SELECT a.* FROM admin a INNER JOIN phone p ON a.id_phone = p.id_phone WHERE p.phone = ? ".($activeOnly ? 'AND a.active=true ' : '')." ORDER BY id_admin DESC LIMIT 1", [ Phone::clean( $phone ) ] )->get( 0 );
 	}
 
 	public function getByPhoneSetup( $phone ){
