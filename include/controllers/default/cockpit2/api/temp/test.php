3b2e705d25f98a7e0e07e57c22e0fc99e1ba44af
Daniel Camargo <pererinha@gmail.com>
2016-05-18 14:04:05 -0300

    SSL certificate problem: self signed certificate in certificate chain #8293

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -7,5 +7,6 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 		$assignment = Crunchbutton_Admin_Shift_Assign::o(82712);
 		Crunchbutton_Admin_Shift_Assign_Confirmation::askWithTextMessage($assignment);
 		// Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
+		echo 'end';
 	}
 }
