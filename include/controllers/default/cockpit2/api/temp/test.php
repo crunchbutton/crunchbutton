dcb70782cda3724e0d15ec3dcbed0d599e2d263e
Daniel Camargo <pererinha@gmail.com>
2016-05-18 11:23:42 -0300

    UC Riverside drivers not receiving check in texts #8289

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -4,6 +4,8 @@ class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
 
 	public function init() {
 		echo 'starting...';
-		Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
+		$assignment = Crunchbutton_Admin_Shift_Assign::o(82712);
+		Crunchbutton_Admin_Shift_Assign_Confirmation::askWithTextMessage($assignment);
+		// Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
 	}
 }
