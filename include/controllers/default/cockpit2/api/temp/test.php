f0bfc42455b2c2e64c16e141b5f389d29cd6483b
Daniel Camargo <pererinha@gmail.com>
2016-05-31 09:43:19 -0300

    Cockpit: Display file size limits for document uploads #8307

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -1,12 +1,8 @@
 <?php
 
-class Controller_api_temp_test extends Crunchbutton_Controller_RestAccount {
+class Controller_api_temp_test extends Crunchbutton_Controller_Account {
 
 	public function init() {
-		echo 'starting...';
-		$assignment = Crunchbutton_Admin_Shift_Assign::o(82712);
-		Crunchbutton_Admin_Shift_Assign_Confirmation::askWithTextMessage($assignment);
-		// Crunchbutton_Admin_Shift_Assign_Confirmation::warningDriversBeforeTheirShift();
-		echo 'end';
+		phpinfo();
 	}
 }
