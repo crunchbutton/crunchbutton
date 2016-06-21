697feecdccbd6270a32b4ff0dbe4ff97aa403a93
Daniel Camargo <pererinha@gmail.com>
2016-06-21 12:11:20 -0300

    Forward calls to drivers when they're on CS #8402

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -15,7 +15,7 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 		    	break;
 		    case 'call':
 				$callback = 'http'.($_SERVER['HTTPS'] != 'on' ? '' : 's').'://'.c::config()->host_callback.'/api/twilio/phone/recording';
-				$forwardTo = Crunchbutton_Support::forwardCSCall($_POST);
+				$forwardTo = Crunchbutton_Support::forwardCSCall($this->request());
 				echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
 					.'<Response>'
 					.'<Dial timeout="10" record="record-from-answer" trim="trim-silence" action="'.$callback.'">'.($forwardTo).'</Dial>'
