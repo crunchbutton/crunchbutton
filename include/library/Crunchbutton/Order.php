9fdc007cf462549d635744854c07cbb1eda83aab
Pererinha <pererinha@gmail.com>
2016-08-01 17:42:05 -0300

    pex cards over-loading for some drivers #8451

--- include/library/Crunchbutton/Order.php
+++ include/library/Crunchbutton/Order.php
@@ -3298,6 +3298,10 @@ class Crunchbutton_Order extends Crunchbutton_Order_Trackchange {
 									$loadCard = false;
 								}
 
+								if($order->pay_type == 'cash'){
+									$loadCard = false;
+								}
+
 								if( $loadCard ){
 
 									$pexcard->addFundsOrderAccepeted( $order->id_order );
