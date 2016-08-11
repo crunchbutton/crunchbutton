e43c5e8be32f2cb526085d2ac8c6a706324aed1c
Pererinha <pererinha@gmail.com>
2016-08-11 17:06:24 -0300

    make a number which, when you dial it, dials 9# #8466

--- include/controllers/default/crunchbutton/api/twilio/phone.php
+++ include/controllers/default/crunchbutton/api/twilio/phone.php
@@ -21,11 +21,20 @@ class Controller_api_twilio_phone extends Crunchbutton_Controller_Rest {
 					.'<Dial timeout="10" record="record-from-answer" trim="trim-silence" action="'.$callback.'">'.($forwardTo).'</Dial>'
 					.'</Response>';
 		    	break;
+			case 'dial':
+				$callback = 'http'.($_SERVER['HTTPS'] != 'on' ? '' : 's').'://'.c::config()->host_callback.'/api/twilio/phone/recording';
+				$forwardTo = Crunchbutton_Support::forwardCSCall($this->request());
+				echo '<?xml version="1.0" encoding="UTF-8"?>'."\n"
+					.'<Response>'
+						.'<Say>Testing</Say>'
+						.'<Dial timeout="1">9</Dial>'
+						.'<Dial timeout="1">#</Dial>'
+					.'</Response>';
+		    	break;
 		}
 
 		$request = $this->request();
 		Call::logFromTwilio($request);
-
 		exit;
 	}
 }
