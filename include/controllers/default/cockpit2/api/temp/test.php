b251adaa5a090f82e4e92c64115cd289fb657efa
Pererinha <pererinha@gmail.com>
2016-09-01 18:01:42 -0300

    check all old transfers for failure #8494

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -1,8 +1,10 @@
 <?php
-
 class Controller_api_temp_test extends Crunchbutton_Controller_Rest {
-
 	public function init() {
-		Log::debug([$_POST, $_GET]);
+		$payments = Payment::q('SELECT * FROM payment WHERE stripe_id IS NOT NULL AND payment_date_checked IS NULL LIMIT 100');
+		foreach ($payments as $payment) {
+			$payment->checkStripeStatus();
+		}
+		echo 'end';
 	}
 }
