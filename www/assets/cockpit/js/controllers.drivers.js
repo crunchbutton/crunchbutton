914482dfb8f68832135d33005200ebe762528308
Pererinha <pererinha@gmail.com>
2016-09-28 17:17:19 -0300

    let drivers close and unclose restaurants just for today themselves #8558

--- www/assets/cockpit/js/controllers.drivers.js
+++ www/assets/cockpit/js/controllers.drivers.js
@@ -26,6 +26,12 @@ NGApp.config(['$routeProvider', function($routeProvider) {
 			templateUrl: '/assets/view/drivers-community.html'
 
 		})
+		.when('/drivers/restaurants', {
+			action: 'drivers-restaurants',
+			controller: 'DriversRestaurantsCtrl',
+			templateUrl: '/assets/view/drivers-restaurants.html'
+
+		})
 		.when('/drivers/notification/:id?', {
 			action: 'drivers-notification',
 			controller: 'DriversNotificationCtrl',
@@ -39,6 +45,29 @@ NGApp.controller( 'CommunityResourcesDriverCtrl', function ($rootScope, $scope, 
 		$scope.communities = data;
 	});
 } );
+
+
+NGApp.controller( 'DriversRestaurantsCtrl', function ($scope, DriverRestaurantsService ) {
+	$scope.loading = true;
+	var load = function(){
+		$scope.communities = null;
+		DriverRestaurantsService.status( function( json ){
+			$scope.communities = json;
+			$scope.loading = false;
+		} );
+	}
+
+	$scope.closeForToday = function(restaurant){
+		DriverRestaurantsService.closeRestaurantForToday(restaurant.id_restaurant, function(){load()});
+	}
+
+	$scope.forceReopenForToday = function(restaurant){
+		DriverRestaurantsService.forceReopenReopenForToday(restaurant.id_restaurant, function(){load()});
+	}
+
+	load();
+
+});
 
 NGApp.controller( 'DriversCommunityCtrl', function ($scope, $filter, DriverCommunityService ) {
 
