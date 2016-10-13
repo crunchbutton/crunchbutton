c9581ec3ff0f285d1fb152bc882100877610177a
Devin Smith <github@arzynik.com>
2016-10-13 20:00:46 +0900

    invite queue

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: vendor/bin/heroku-php-nginx -C conf/nginx.conf www/
 cron: cd /app/cli && php /app/cli/_master_cron.php -e=crondb
-queue: cd /app/cli && php /app/cli/_queue.php -e=crondb
+queue: cd /app/cli && php /app/cli/_queue.php 0 30 -e=crondb
--- cli/_queue.php
+++ cli/_queue.php
@@ -8,12 +8,14 @@ set_time_limit(100);
 require_once('../include/crunchbutton.php');
 
 $start = time();
-$end = $start + 59;
+$end = $start + ($argv[1] ? $argv[1] : 59);
+$wait = $argv[2] ? $argv[2] : 1;
+$infinite = $argv[1] == '0' ? true : false;
 
 echo "\x1B[44mRunning Queue on db:".c::env()."...\x1B[0m\n";
-while (time() < $end) {
+while (time() < $end || $infinite) {
 	$c = Crunchbutton_Queue::process();
 	echo $c ? ("\x1B[32mFinished running ".$c." queue items\x1B[0m\n") : "\x1B[31mQueue is empty.\x1B[0m\n";
-	sleep(1);
+	sleep($wait);
 }
 echo "Done.\n\n";
