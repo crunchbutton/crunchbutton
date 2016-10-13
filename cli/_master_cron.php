03a5b5c462bbba96c9c3b56d316049aa73f9b68e
Devin Smith <github@arzynik.com>
2016-10-13 20:10:50 +0900

    cron fixes

--- cli/_master_cron.php
+++ cli/_master_cron.php
@@ -7,12 +7,10 @@ set_time_limit(100);
 require_once('../include/crunchbutton.php');
 echo "\x1B[44mRunning Cron on db:".c::env()."...\x1B[0m\n";
 
-if ($argv[1] == 'loop') {
-	while (time() < $end || $infinite) {
-		echo "\x1B[32mStarting cron...\x1B[0m\n";
-		Crunchbutton_Cron_Log::start();
-		sleep(60);
-	}
-} else {
+while (true) {
+	echo "\x1B[32mStarting cron...\x1B[0m\n";
 	Crunchbutton_Cron_Log::start();
+	sleep(60);
 }
+
+echo "Done.\n\n";
