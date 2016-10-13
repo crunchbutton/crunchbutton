8121f4537660a1214a081eadd532e461310f3fca
Devin Smith <github@arzynik.com>
2016-10-13 20:06:18 +0900

    make infinite loop cron

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: vendor/bin/heroku-php-nginx -C conf/nginx.conf www/
-cron: cd /app/cli && php /app/cli/_master_cron.php -e=crondb
-queue: cd /app/cli && php /app/cli/_queue.php 0 30 -e=crondb
+cron: cd /app/cli && php /app/cli/_master_cron.php loop -e=crondb
+queue: cd /app/cli && php /app/cli/_queue.php 0 15 -e=crondb
--- cli/_master_cron.php
+++ cli/_master_cron.php
@@ -7,4 +7,12 @@ set_time_limit(100);
 require_once('../include/crunchbutton.php');
 echo "\x1B[44mRunning Cron on db:".c::env()."...\x1B[0m\n";
 
-Crunchbutton_Cron_Log::start();
+if ($argv[1] == 'loop') {
+	while (time() < $end || $infinite) {
+		echo "\x1B[32mStarting cron...\x1B[0m\n";
+		Crunchbutton_Cron_Log::start();
+		sleep(60);
+	}
+} else {
+	Crunchbutton_Cron_Log::start();
+}
