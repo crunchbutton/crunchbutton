22375268e96f7d7212f76251576ee392058456c1
Devin Smith <github@arzynik.com>
2016-10-13 20:22:03 +0900

    debug

--- cli/_master_cron.php
+++ cli/_master_cron.php
@@ -10,6 +10,7 @@ echo "\x1B[44mRunning Cron on db:".c::env()."...\x1B[0m\n";
 while (true) {
 	echo "\x1B[32mStarting cron...\x1B[0m\n";
 	Crunchbutton_Cron_Log::start();
+	echo "\x1B[32mCron complete.\x1B[0m\n";
 	sleep(60);
 }
 
