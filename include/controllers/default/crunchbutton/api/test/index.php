ced090743082225831900ce2911fff94eacfd214
Devin Smith <github@arzynik.com>
2016-10-15 10:28:02 +0900

    test

--- include/controllers/default/crunchbutton/api/test/index.php
+++ include/controllers/default/crunchbutton/api/test/index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo c::getEnv()."\n";
-echo c::env()."\n";
-echo $_SERVER['SERVER_NAME'];
+class Controller_api_test extends Crunchbutton_Controller_Rest {
+	public function init() {
+		echo c::getEnv()."\n";
+		echo c::env()."\n";
+		echo $_SERVER['SERVER_NAME'];
+	}
+}
