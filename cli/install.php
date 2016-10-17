ce8227575ffa07827e9e0e43f3e2e008e4583775
Devin Smith <github@arzynik.com>
2016-10-17 12:17:01 +0900

    heroku stuff

---
+++ cli/install.php
@@ -0,0 +1,56 @@
+<?php
+// this script will run after a successfull heroku deployment
+
+echo "\nCreating db schema...";
+
+$url = parse_url(getenv('JAWSDB_URL'));
+$type = $url['scheme'] == 'postgres' ? 'pgsql' : 'mysql';
+
+$db = new \PDO($type.':host='.$url['host'].($url['port'] ? ';port='.$url['port'] : '').';dbname='.substr($url['path'], 1), $url['user'], $url['pass'], $options);
+$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+$db->exec(file_get_contents('db/dump.sql'));
+
+echo "complete.\n";
+
+echo "Running db migrate scripts...\n";
+
+$dirs = ['db/migrate'];
+
+if (getenv('TRAVISPOSTGRES')) {
+	echo "Running db migrate scripts for Postgres...\n";
+	$dirs[] = 'db/migratepostgres';
+}
+
+$error = false;
+
+foreach ($dirs as $dir) {
+
+	$files = [];
+
+	foreach (new DirectoryIterator($dir) as $fileInfo) {
+		if ($fileInfo->isDot()) continue;
+		$num = preg_replace('/^([0-9]+)_.*$/','\\1', $fileInfo->getFilename());
+		if ($num > 488) {
+			$files[] = $fileInfo->getFilename();
+		}
+	}
+
+	natcasesort($files);
+
+	foreach ($files as $file) {
+		echo "	".$file."...";
+		$db->exec(file_get_contents($dir.'/'.$file));
+		echo "complete.\n";
+	}
+}
+
+echo "Inserting dummy data...";
+$db->exec(file_get_contents('db/dump.sql'));
+echo "complete.\n";
+
+if ($error) {
+	exit(1);
+} else {
+	exit(0);
+}
