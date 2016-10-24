bc62c9c1df97df65b99fa0ddc7b1120b81bd7b70
Pererinha <pererinha@gmail.com>
2016-10-24 20:02:15 -0200

    Duplicate order confirmation texts sent to customer 1 hr after order already confirmed and cancelled. #8597

--- include/library/Crunchbutton/Queue/Order/Receipt.php
+++ include/library/Crunchbutton/Queue/Order/Receipt.php
@@ -2,6 +2,16 @@
 
 class Crunchbutton_Queue_Order_Receipt extends Crunchbutton_Queue {
 	public function run() {
+
+		// Check if it was not sent yet
+		$queue = Crunchbutton_Queue::q('SELECT * FROM queue WHERE id_order = ? AND type = ? AND status = ? ORDER by id_queue DESC LIMIT 1',[$this->id_order, self::TYPE_ORDER_RECEIPT, self::STATUS_SUCCESS])->get(0);
+
+		if($queue->id_queue){
+			$this->info = 'Confirmation already sent: '. $queue->id_queue;
+			$this->save();
+			return self::STATUS_SUCCESS;
+		}
+
 		// send customer a receipt
 		$this->order()->receipt();
 
