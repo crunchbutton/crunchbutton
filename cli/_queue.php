f074c188788e8be5b12731c1795ac6e14e6968ed
Devin Smith <github@arzynik.com>
2016-10-28 16:44:54 +0900

    debug

--- cli/_queue.php
+++ cli/_queue.php
@@ -5,6 +5,8 @@ error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', true);
 set_time_limit(100);
 
+echo "\x1B[44mBoot cron...\x1B[0m\n";
+
 require_once('../include/crunchbutton.php');
 
 $start = time();
