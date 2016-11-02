7953a13fd0e73dfad7c6d28e1ac0bf1b5951b86a
Pererinha <pererinha@gmail.com>
2016-11-02 16:57:41 -0200

    travis test

--- travis/Tests/DatabaseTest.php
+++ travis/Tests/DatabaseTest.php
@@ -3,7 +3,6 @@
 class DatabaseTest extends PHPUnit_Framework_TestCase {
 	public function testDb() {
 		$c = c::db()->get('select * from config limit 1')->get(0);
-		echo '<pre>';var_dump( $c );exit(1);
 		$this->assertTrue($c->id_config ? true : false);
 	}
 }
