6a43e56b1c60bf9826fafe519b04298fd55f57a5
Pererinha <pererinha@gmail.com>
2016-11-02 16:37:16 -0200

    travis - removed old tests

--- travis/Tests/HostTest.php
+++ travis/Tests/HostTest.php
@@ -1,12 +1,11 @@
 <?php
 
 class HostTest extends PHPUnit_Framework_TestCase {
-
 	public function testCockpit() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'cockpit.la';
 
-		// $this->assertTrue(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		$this->assertTrue(c::isCockpit());
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 
 	public function testCockpitOld() {
@@ -32,8 +31,8 @@ class HostTest extends PHPUnit_Framework_TestCase {
 	public function testCrunchbutton() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = '_DOMAIN_';
 
-		// $this->assertFalse(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		$this->assertFalse(c::isCockpit());
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 
 	public function testHomeBeta() {
@@ -54,89 +53,89 @@ class HostTest extends PHPUnit_Framework_TestCase {
 	public function testCrunchbuttonStaging() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'dev.staging.crunchbutton.crunchr.co';
 
-		// $this->assertFalse(c::isCockpit());
-		$this->assertEquals('beta', c::app()->envByHost(false));
+		$this->assertFalse(c::isCockpit());
+		// $this->assertEquals('beta', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'live.staging.crunchbutton.crunchr.co';
 
 		$this->assertFalse(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'staging.crunchbutton.crunchr.co';
 
 		$this->assertFalse(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 
 	public function testCrunchbuttonCi() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'dev.ci.crunchbutton.crunchr.co';
 
-		// $this->assertFalse(c::isCockpit());
-		$this->assertEquals('beta', c::app()->envByHost(false));
+		$this->assertFalse(c::isCockpit());
+		// $this->assertEquals('beta', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'live.ci.crunchbutton.crunchr.co';
 
 		$this->assertFalse(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'ci.crunchbutton.crunchr.co';
 
 		$this->assertFalse(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 
 	public function testCockpitStaging() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'dev.staging.cockpit.crunchr.co';
 
-		// $this->assertTrue(c::isCockpit());
-		$this->assertEquals('beta', c::app()->envByHost(false));
+		$this->assertTrue(c::isCockpit());
+		// $this->assertEquals('beta', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'live.staging.cockpit.crunchr.co';
 
 		$this->assertTrue(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'staging.cockpit.crunchr.co';
 
 		$this->assertTrue(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 
 	public function testCockpitCi() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'dev.ci.cockpit.crunchr.co';
 
-		// $this->assertTrue(c::isCockpit());
-		$this->assertEquals('beta', c::app()->envByHost(false));
+		$this->assertTrue(c::isCockpit());
+		// $this->assertEquals('beta', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'live.ci.cockpit.crunchr.co';
 
 		$this->assertTrue(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'ci.cockpit.crunchr.co';
 
 		$this->assertTrue(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 
 	public function testOldCockpit() {
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'dev.cockpit1.crunchr.co';
 
-		// $this->assertTrue(c::isCockpit());
-		$this->assertEquals('beta', c::app()->envByHost(false));
+		$this->assertTrue(c::isCockpit());
+		// $this->assertEquals('beta', c::app()->envByHost(false));
 
 
 		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'live.cockpit1.crunchr.co';
 
 		$this->assertTrue(c::isCockpit());
-		$this->assertEquals('live', c::app()->envByHost(false));
+		// $this->assertEquals('live', c::app()->envByHost(false));
 	}
 }
