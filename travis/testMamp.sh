4354c9ca1ccc47fb8a72492415ff4f4d35de38d3
Pererinha <pererinha@gmail.com>
2016-11-15 17:32:11 -0200

    travis - settlement

--- travis/testMamp.sh
+++ travis/testMamp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $1 ]; then
-	/Applications/MAMP/bin/php/php5.6.10/bin/php ~/Desktop/phpunit-5.6.2.phar --configuration travis/phpunit.basic.xml --filter $1
+	/Applications/MAMP/bin/php/php5.6.10/bin/php ~/Dev/test/phpunit-5.6.3.phar --configuration travis/phpunit.basic.xml --filter $1
 else
-	/Applications/MAMP/bin/php/php5.6.10/bin/php ~/Desktop/phpunit-5.6.2.phar --configuration travis/phpunit.basic.xml
+	/Applications/MAMP/bin/php/php5.6.10/bin/php ~/Dev/test/phpunit-5.6.3.phar --configuration travis/phpunit.basic.xml
 fi
