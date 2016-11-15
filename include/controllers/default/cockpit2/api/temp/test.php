a2d2328d7d55e5dde243d468fbbb8c6fa9015629
Pererinha <pererinha@gmail.com>
2016-11-15 17:15:16 -0200

    Lot of cron error notifications #8648

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -1,11 +1,5 @@
 <?php
 class Controller_api_temp_test extends Crunchbutton_Controller_Rest {
 	public function init() {
-		$job = Crunchbutton_Cron_Job_CommunityNotification::o(119);
-		$job->run();
-		echo '<pre>';var_dump( 1 );
-		$job = Crunchbutton_Cron_Job_RestaurantFixNotify::o(28);
-		$job->run();
-		echo '<pre>';var_dump( 2 );
 	}
 }
