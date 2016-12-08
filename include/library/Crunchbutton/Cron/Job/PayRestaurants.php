79db6d178156fa0c995de214f11de5bf7b68a78e
Pererinha <pererinha@gmail.com>
2016-12-08 19:10:38 -0200

    Settlement queue is causing delay problems #8671

--- include/library/Crunchbutton/Cron/Job/PayRestaurants.php
+++ include/library/Crunchbutton/Cron/Job/PayRestaurants.php
@@ -3,11 +3,13 @@
 class Crunchbutton_Cron_Job_PayRestaurants extends Crunchbutton_Cron_Log {
 
 	public function run(){
-
-		$settlement = new Crunchbutton_Settlement;
-
-		$settlement->doRestaurantsPayments();
-
+		$now = new DateTime( 'now', new DateTimeZone( Crunchbutton_Community_Shift::CB_TIMEZONE ) );
+		if(intval($now->format('Hms')) > Crunchbutton_Settlement::TIME_WINDOW_START && intval($now->format('Hms')) < Crunchbutton_Settlement::TIME_WINDOW_END){
+			$settlement = new Crunchbutton_Settlement;
+			$settlement->doRestaurantsPayments();
+		} else {
+			echo 'Crunchbutton_Cron_Job_PayRestaurants is not at during the window time!';
+		}
 		// it always must call finished method at the end
 		$this->finished();
 	}
