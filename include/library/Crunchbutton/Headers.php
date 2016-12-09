aebd3a80cdd67064744bf8e03c0f4787a42cd2ce
Pererinha <pererinha@gmail.com>
2016-12-09 15:57:33 -0200

    login in

--- include/library/Crunchbutton/Headers.php
+++ include/library/Crunchbutton/Headers.php
@@ -57,7 +57,7 @@ if (!function_exists('getallheaders')) {
 		$headers = '';
 		foreach ($_SERVER as $name => $value) {
 			if (substr($name, 0, 5) == 'HTTP_') {
-				@$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+				$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
 			}
 		}
 		return $headers;
