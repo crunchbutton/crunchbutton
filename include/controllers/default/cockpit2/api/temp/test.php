fd86133cbfdef252d666d8653ce840ad85901560
Pererinha <pererinha@gmail.com>
2016-12-13 11:28:17 -0200

    Pexcard report not showing "Spent with Pex Card" after 12/2 #8676
    partial

--- include/controllers/default/cockpit2/api/temp/test.php
+++ include/controllers/default/cockpit2/api/temp/test.php
@@ -1,5 +1,15 @@
 <?php
 class Controller_api_temp_test extends Crunchbutton_Controller_Rest {
 	public function init() {
+		$now = new DateTime( $_GET['date'], new DateTimeZone( c::config()->timezone ) );
+		$end = $now->format( 'm/d/Y' );
+		$now->modify( '-1 days' );
+		$start = $now->format( 'm/d/Y' );
+		Crunchbutton_Pexcard_Transaction::saveTransactionsByPeriod( $start, $end );
+
+
+		// Crunchbutton_Pexcard_Transaction::convertTimeZone();
+
+		echo 'finished';
 	}
 }
