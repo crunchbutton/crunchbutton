d90f9f99d18222d702e2a8b20d2c3331bb368fc1
Devin Smith <github@arzynik.com>
2017-04-06 17:20:19 -0700

    remove deploy scripts

--- deploy/cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# add an entry like this
-# * * * * * root /home/server/deploy/cron.sh server /usr/bin/php > /home/cron-deploy.log 2>&1
-
-cd /home/$1/cli && $2 /home/$1/deploy/cron.php -e=live
--- deploy/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include(dirname(__FILE__).'/gitupdate.php');
-
-echo "\nRestarting services...\n";
-
-$o = null;
-exec('service httpd restart 2>&1 &', $o);
-echo implode("\n", $o)."\n";
