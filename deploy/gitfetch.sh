f5088c4cf1847809fcbdd1e157369085f8f64414
Devin Smith <github@arzynik.com>
2017-04-06 17:20:19 -0700

    remove deploy scripts

--- deploy/gitfetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# for this to work apache ALL=(deploy) NOPASSWD: /home/server/deploy/gitfetch.sh must be added to sudoers
-
-cd /home/server && git fetch
--- deploy/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include(dirname(__FILE__).'/gitupdate.php');
-
-$cmd = 'su -l deploy -c "cd /home/'.$params->path.' && composer install --no-dev --ignore-platform-reqs"';
-
-exec($cmd.' 2>&1 &', $o);
-echo implode("\n", $o);
