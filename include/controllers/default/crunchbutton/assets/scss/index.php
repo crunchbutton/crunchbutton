a591903a3fab71b843d8a51bfc76b79a55803217
Daniel Camargo <pererinha@gmail.com>
2020-06-27 14:48:52 -0300

    fixed the bundle scss url

--- include/controllers/default/crunchbutton/assets/scss/index.php
+++ include/controllers/default/crunchbutton/assets/scss/index.php
@@ -3,7 +3,7 @@
 class Controller_assets_scss extends Crunchbutton_Controller_AssetBundle {
 	public function init() {
 		$path = c::config()->dirs->www.'assets/scss/';
-		$file = c::getPagePiece(2);
+		$file = c::getPagePiece(0);
 		Scss::serve($path.$file);
 	}
 }
